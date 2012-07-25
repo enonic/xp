ba07a7f6d5ad22b15b492b9857a97f8bedb2888b
Sten Roger Sandvik <srs@enonic.com>
2012-07-25 10:31:10 +0200

    Added more rest placeholders

--- modules/wem-web/src/main/java/com/enonic/wem/web/rest2/resource/account/AccountResource.java
+++ modules/wem-web/src/main/java/com/enonic/wem/web/rest2/resource/account/AccountResource.java
@@ -1,11 +1,14 @@
 package com.enonic.wem.web.rest2.resource.account;
 
+import java.util.List;
+
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.springframework.stereotype.Component;
 
@@ -22,7 +25,31 @@ public final class AccountResource
                                   @QueryParam("userstores") @DefaultValue("") String userStores,
                                   @QueryParam("organizations") @DefaultValue("") String organizations )
     {
-        // TODO: Fill inn implementation code here. Start with hibernate code from AccountController.
+        // TODO: Fill inn implementation code here.
         return new AccountsResult();
+    }
+
+    @GET
+    @Path("export-query")
+    public Response exportQuery( @QueryParam("sort") @DefaultValue("") String sort, @QueryParam("dir") @DefaultValue("ASC") String sortDir,
+                                 @QueryParam("query") @DefaultValue("") String query,
+                                 @QueryParam("type") @DefaultValue("users,groups,roles") String types,
+                                 @QueryParam("userstores") @DefaultValue("") String userStores,
+                                 @QueryParam("organizations") @DefaultValue("") String organizations,
+                                 @QueryParam("encoding") @DefaultValue("ISO-8859-1") String characterEncoding,
+                                 @QueryParam("separator") @DefaultValue("t") String separator )
+    {
+        // TODO: Fill inn implementation code here.
+        return null;
+    }
+
+    @GET
+    @Path("export-keys")
+    public Response exportKeys( @QueryParam("key") List<String> keys,
+                                @QueryParam("encoding") @DefaultValue("ISO-8859-1") String characterEncoding,
+                                @QueryParam("separator") @DefaultValue("t") String separator )
+    {
+        // TODO: Fill inn implementation code here.
+        return null;
     }
 }
