3f372ae368e5b3ff6ed9036c0c218cfea996514f
Viktar Fiodarau <vfi@enonic.com>
2012-07-31 13:26:45 +0300

    CMS-6 Improve rest service for account export
    We want to improve the rest service for exporing accounts. The export should be implemented in two services:
    
        account/export-query (export to csv based on query)
        account/export-keys (export to csv based on selected keys - should probably be POST instead of GET)
    
    I have created placeholders in code in AccountResource.exportKeys(..) and AccountResource.exportQuery(..). Please also create tests for this. Plenty of examples. Use mocks as dependent services and do not wire it into complex spring stuff.
    
    NOTE! This task only covers the actual rest service. Do not change the JavaScript in Admin Console now. The new rest service lives under /admin/rest/account/export and do not break existing rest services.

--- modules/wem-web/src/main/java/com/enonic/wem/web/rest2/resource/account/AccountResource.java
+++ modules/wem-web/src/main/java/com/enonic/wem/web/rest2/resource/account/AccountResource.java
@@ -1,6 +1,8 @@
 package com.enonic.wem.web.rest2.resource.account;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.ws.rs.DefaultValue;
@@ -17,10 +19,13 @@ import org.springframework.stereotype.Component;
 import com.enonic.wem.core.search.Facets;
 import com.enonic.wem.core.search.SearchSortOrder;
 import com.enonic.wem.core.search.account.AccountIndexField;
+import com.enonic.wem.core.search.account.AccountKey;
 import com.enonic.wem.core.search.account.AccountSearchHit;
 import com.enonic.wem.core.search.account.AccountSearchQuery;
 import com.enonic.wem.core.search.account.AccountSearchResults;
 import com.enonic.wem.core.search.account.AccountSearchService;
+import com.enonic.wem.core.search.account.AccountType;
+import com.enonic.wem.web.rest2.service.account.AccountCsvExportService;
 
 import com.enonic.cms.core.security.group.GroupEntity;
 import com.enonic.cms.core.security.group.GroupKey;
@@ -41,6 +46,7 @@ public final class AccountResource
 
     private AccountSearchService accountSearchService;
 
+    private AccountCsvExportService accountCsvExportService;
 
     @GET
     public AccountsResult search( @QueryParam("start") @DefaultValue("0") int start, @QueryParam("limit") @DefaultValue("10") int limit,
@@ -66,22 +72,56 @@ public final class AccountResource
                                  @QueryParam("userstores") @DefaultValue("") String userStores,
                                  @QueryParam("organizations") @DefaultValue("") String organizations,
                                  @QueryParam("encoding") @DefaultValue("ISO-8859-1") String characterEncoding,
-                                 @QueryParam("separator") @DefaultValue("t") String separator )
+                                 @QueryParam("separator") @DefaultValue("\t") String separator )
+        throws UnsupportedEncodingException
     {
-        // TODO: Fill inn implementation code here.
-        return null;
+        final int accountsExportLimit = 5000;
+        final AccountSearchQuery searchQuery =
+            buildSearchQuery( query, types, userStores, organizations, 0, accountsExportLimit, sort, sortDir );
+        final AccountSearchResults searchResults = accountSearchService.search( searchQuery );
+
+        return createCsvExportResponse( searchResults, separator, characterEncoding );
     }
 
     @GET
     @Path("export-keys")
     public Response exportKeys( @QueryParam("key") List<String> keys,
                                 @QueryParam("encoding") @DefaultValue("ISO-8859-1") String characterEncoding,
-                                @QueryParam("separator") @DefaultValue("t") String separator )
+                                @QueryParam("separator") @DefaultValue("\t") String separator )
+        throws UnsupportedEncodingException
     {
-        // TODO: Fill inn implementation code here.
-        return null;
+        AccountSearchResults searchResults = getAccountListForKeys( keys );
+
+        return createCsvExportResponse( searchResults, separator, characterEncoding );
     }
 
+    private Response createCsvExportResponse( AccountSearchResults results, String separator, String encoding )
+        throws UnsupportedEncodingException
+    {
+        String content = accountCsvExportService.generateCsv( results, separator );
+        String attachmentHeader = "attachment; filename=" + accountCsvExportService.getExportFileName( new Date() );
+        return Response.ok( content.getBytes( encoding ) ).type( "text/csv" ).
+            header( "Content-Encoding", encoding ).
+            header( "Content-Disposition", attachmentHeader ).build();
+    }
+
+    AccountSearchResults getAccountListForKeys( final List<String> keys )
+    {
+        // TODO: refactor this when accounts API and model classes are in place
+        final AccountSearchResults accounts = new AccountSearchResults( 0, keys.size() );
+        for ( final String key : keys )
+        {
+            final AccountType type = findAccountType( key );
+            final AccountSearchHit account = new AccountSearchHit( new AccountKey( key ), type, 0 );
+            accounts.add( account );
+        }
+        return accounts;
+    }
+
+    private AccountType findAccountType( final String accountKey )
+    {
+        return userDao.findByKey( accountKey ) == null ? AccountType.GROUP : AccountType.USER;
+    }
 
     AccountSearchQuery buildSearchQuery( String query, String types, String userstores, String organizations, int start, int limit,
                                          String sort, String dir )
@@ -157,4 +197,17 @@ public final class AccountResource
     {
         this.accountSearchService = accountSearchService;
     }
+
+    @Autowired
+    public void setAccountCsvExportService( AccountCsvExportService accountCsvExportService )
+    {
+        this.accountCsvExportService = accountCsvExportService;
+    }
+
+    @Autowired
+    public AccountCsvExportService getAccountCsvExportService()
+    {
+        return this.accountCsvExportService;
+    }
+
 }
