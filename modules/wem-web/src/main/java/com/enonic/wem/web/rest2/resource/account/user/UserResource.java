18a631c6c27f75d99f4253224a17d183381183d7
Viktar Fiodarau <vfi@enonic.com>
2012-08-01 15:54:36 +0300

    CMS-11 Improve rest service for user photo
    Create the rest service for user photo in new class using new rules. Implement the following code part:
    
    com.enonic.wem.web.rest2.resource.account.user.UserResource.getPhoto(...)
    
    Please write tests for it using same principles as the other rest services.

--- modules/wem-web/src/main/java/com/enonic/wem/web/rest2/resource/account/user/UserResource.java
+++ modules/wem-web/src/main/java/com/enonic/wem/web/rest2/resource/account/user/UserResource.java
@@ -1,18 +1,35 @@
 package com.enonic.wem.web.rest2.resource.account.user;
 
+import java.awt.image.BufferedImage;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.enonic.wem.web.rest.account.UserPhotoService;
+import com.enonic.wem.web.rest.exception.EntityNotFoundException;
+
+import com.enonic.cms.core.security.user.UserEntity;
+import com.enonic.cms.store.dao.UserDao;
 
 @Path("account/user")
 @Produces(MediaType.APPLICATION_JSON)
 @Component
 public final class UserResource
 {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private UserPhotoService photoService;
+
     @GET
     @Path("{key}")
     public UserResult getInfo( @PathParam("key") final String key )
@@ -24,9 +41,39 @@ public final class UserResource
     @GET
     @Path("{key}/photo")
     @Produces("image/png")
-    public byte[] getPhoto( @PathParam("key") final String key )
+    public Response getPhoto( @PathParam("key") final String key )
+        throws Exception
     {
-        // TODO: Implementation here.
-        return null;
+        UserEntity entity = userDao.findByKey( key );
+        if ( entity == null )
+        {
+            throw new EntityNotFoundException();
+        }
+        if ( entity.getPhoto() == null )
+        {
+            entity = userDao.findBuiltInAnonymousUser();
+        }
+        BufferedImage image = photoService.renderPhoto( entity, 100 );
+        return Response.ok( image ).build();
+    }
+
+    public UserDao getUserDao()
+    {
+        return userDao;
+    }
+
+    public void setUserDao( final UserDao userDao )
+    {
+        this.userDao = userDao;
+    }
+
+    public UserPhotoService getPhotoService()
+    {
+        return photoService;
+    }
+
+    public void setPhotoService( final UserPhotoService photoService )
+    {
+        this.photoService = photoService;
     }
 }
