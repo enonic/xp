787b8ef0904b6c668577c3b104fb0e545850eef9
Sten Roger Sandvik <srs@enonic.com>
2012-08-02 09:25:24 +0200

    Fixed home resolving so that it uses temp dir instead for home dir.

--- modules/wem-core/src/test/java/com/enonic/wem/itest/home/HomeDirFactory.java
+++ modules/wem-core/src/test/java/com/enonic/wem/itest/home/HomeDirFactory.java
@@ -1,22 +1,65 @@
 package com.enonic.wem.itest.home;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
+import com.google.common.io.Files;
+import com.google.common.io.Resources;
 
 import com.enonic.cms.core.home.HomeDir;
 
 @Component
 @Profile("itest")
 public final class HomeDirFactory
-    implements FactoryBean<HomeDir>
+    implements FactoryBean<HomeDir>, InitializingBean, DisposableBean
 {
+    private HomeDir homeDir;
+
+    @Override
+    public void afterPropertiesSet()
+        throws Exception
+    {
+        this.homeDir = new HomeDir( Files.createTempDir() );
+        copyFile( "config/cms.properties" );
+        copyFile( "config/vhost.properties" );
+    }
+
+    @Override
+    public void destroy()
+    {
+        FileUtils.deleteQuietly( this.homeDir.toFile() );
+    }
+
+    private void copyFile( final String resource )
+        throws IOException
+    {
+        final String from = "/homeDir/" + resource;
+        final File to = new File( this.homeDir.toFile(), resource );
+
+        final URL url = getClass().getResource( from );
+        if ( url == null )
+        {
+            throw new IOException( "Resource [" + from + "] not found" );
+        }
+
+        Files.createParentDirs( to );
+        Files.copy( Resources.newInputStreamSupplier( url ), to );
+    }
 
     public HomeDir getObject()
     {
-        return new HomeDir( new File( "./src/test/homeDir" ) );
+        return this.homeDir;
     }
 
     public Class<?> getObjectType()
