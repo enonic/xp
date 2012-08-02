f3c076b25c34a264a89a86921754481d2f601f75
Sten Roger Sandvik <srs@enonic.com>
2012-08-02 09:26:44 +0200

    Cleaned up homeDirFactory.

--- modules/wem-core/src/test/java/com/enonic/wem/itest/home/HomeDirFactory.java
+++ modules/wem-core/src/test/java/com/enonic/wem/itest/home/HomeDirFactory.java
@@ -4,9 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.FactoryBean;
