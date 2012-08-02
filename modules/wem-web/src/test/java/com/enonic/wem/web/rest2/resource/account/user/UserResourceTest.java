457f7302482971b08cb4a73d185374c6fe59c43b
Sten Roger Sandvik <srs@enonic.com>
2012-08-02 11:01:39 +0200

    Added fest assertions that is used for comparing images

--- modules/wem-web/src/test/java/com/enonic/wem/web/rest2/resource/account/user/UserResourceTest.java
+++ modules/wem-web/src/test/java/com/enonic/wem/web/rest2/resource/account/user/UserResourceTest.java
@@ -5,13 +5,13 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import org.fest.assertions.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import com.google.common.io.ByteStreams;
 
-import com.enonic.wem.web.rest.account.UserPhotoService;
 import com.enonic.wem.web.rest2.resource.AbstractResourceTest;
 
 import com.enonic.cms.core.security.user.UserEntity;
@@ -25,8 +25,6 @@ public class UserResourceTest
 {
     private UserDao userDao;
 
-    private UserPhotoService photoService;
-
     private UserResource userResource;
 
     @Before
@@ -34,7 +32,6 @@ public class UserResourceTest
         throws Exception
     {
         userDao = Mockito.mock( UserDao.class );
-        photoService = Mockito.mock( UserPhotoService.class );
         userResource = new UserResource();
         userResource.setUserDao( userDao );
     }
@@ -45,13 +42,12 @@ public class UserResourceTest
     {
         UserEntity user = createUser( "ASDD8F7S9F9AFAF7A89F7A87F98A7F9A87FA89F79AS98G7A9" );
         Mockito.when( userDao.findByKey( "ASDD8F7S9F9AFAF7A89F7A87F98A7F9A87FA89F79AS98G7A9" ) ).thenReturn( user );
-        Mockito.when( photoService.renderPhoto( user, 100 ) ).thenReturn( getPhoto() );
 
         BufferedImage image = userResource.getPhoto( "ASDD8F7S9F9AFAF7A89F7A87F98A7F9A87FA89F79AS98G7A9", 100 );
-        assert ( compareImages( getPhoto(), image ) );
+        Assertions.assertThat( image ).isEqualTo( readPhoto() );
     }
 
-    private BufferedImage getPhoto()
+    private BufferedImage readPhoto()
         throws IOException
     {
         return ImageIO.read( getClass().getResource( "x-user.png" ) );
@@ -71,37 +67,10 @@ public class UserResourceTest
         return user;
     }
 
-    private UserStoreEntity createUserstore( String name )
+    private UserStoreEntity createUserstore( final String name )
     {
         UserStoreEntity userstore = new UserStoreEntity();
         userstore.setName( name );
         return userstore;
-    }
-
-    private boolean compareImages( BufferedImage image1, BufferedImage image2 )
-    {
-        int width;
-        int height;
-        boolean imagesEqual = true;
-
-        if ( image1.getWidth() == ( width = image2.getWidth() ) && image1.getHeight() == ( height = image2.getHeight() ) )
-        {
-
-            for ( int x = 0; imagesEqual == true && x < width; x++ )
-            {
-                for ( int y = 0; imagesEqual == true && y < height; y++ )
-                {
-                    if ( image1.getRGB( x, y ) != image2.getRGB( x, y ) )
-                    {
-                        imagesEqual = false;
-                    }
-                }
-            }
-        }
-        else
-        {
-            imagesEqual = false;
-        }
-        return imagesEqual;
     }
 }
