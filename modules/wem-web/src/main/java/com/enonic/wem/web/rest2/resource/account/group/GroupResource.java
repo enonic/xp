322496a6bc487067eed9e9a0f160905a4cf1cdae
Alex Rodriguez <aro@enonic.com>
2012-08-06 15:26:03 +0200

    CMS-54 Improve rest service for group details

--- modules/wem-web/src/main/java/com/enonic/wem/web/rest2/resource/account/group/GroupResource.java
+++ modules/wem-web/src/main/java/com/enonic/wem/web/rest2/resource/account/group/GroupResource.java
@@ -1,23 +1,68 @@
 package com.enonic.wem.web.rest2.resource.account.group;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.enonic.cms.core.security.group.GroupEntity;
+import com.enonic.cms.core.security.group.GroupType;
+import com.enonic.cms.store.dao.GroupDao;
 
 @Path("account/group")
 @Produces(MediaType.APPLICATION_JSON)
 @Component
 public final class GroupResource
 {
+    private GroupDao groupDao;
+
     @GET
     @Path("{key}")
     public GroupResult getInfo( @PathParam("key") final String key )
     {
-        // TODO: Implementation here. Do not implement "account graph" since this will be implemented elsewhere
-        return null;
+        final GroupEntity groupEntity = this.groupDao.find( key );
+        if ( groupEntity == null )
+        {
+            return null;
+        }
+        else
+        {
+            final Collection members = getGroupMembers( groupEntity );
+            return new GroupResult( groupEntity, members );
+        }
     }
+
+    private Collection getGroupMembers( final GroupEntity groupEntity )
+    {
+        final List members = new ArrayList();
+        final Set<GroupEntity> memberGroups = groupEntity.getMembers( false );
+        for ( GroupEntity memberGroup : memberGroups )
+        {
+            if ( memberGroup.getType() == GroupType.USER )
+            {
+                members.add( memberGroup.getUser() );
+            }
+            else
+            {
+                members.add( memberGroup );
+            }
+        }
+        return members;
+    }
+
+    @Autowired
+    public void setGroupDao( final GroupDao groupDao )
+    {
+        this.groupDao = groupDao;
+    }
+
 }
