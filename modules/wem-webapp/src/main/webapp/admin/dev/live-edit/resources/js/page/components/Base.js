4e0228b0ac3cc79f3c189a324f2e6097f59ed599
Thomas Andersen <tan@enonic.com>
2012-08-10 09:33:17 +0200

    Made the base classes self executing (for scoping)

--- modules/wem-webapp/src/main/webapp/admin/dev/live-edit/resources/js/page/components/Base.js
+++ modules/wem-webapp/src/main/webapp/admin/dev/live-edit/resources/js/page/components/Base.js
@@ -1,54 +1,59 @@
-// Namespace
-AdminLiveEdit.page = {};
-AdminLiveEdit.page.components = {};
-
-AdminLiveEdit.page.components.Base = function () {
-    this.selector = '';
-    this.highlightColor = '#141414';
-};
-
-AdminLiveEdit.page.components.Base.prototype = {
-    attachMouseOverEvent: function () {
-        var self = this;
-        $liveedit(document).on('mouseover', this.selector, function (event) {
-            var $component = $liveedit(this);
-            var componentIsDescendantOfSelected = $component.parents('.live-edit-selected-component').length === 1;
-            // TODO: remove reference to DragDrop, use PubSub.
-            var disableHover = componentIsDescendantOfSelected || AdminLiveEdit.ui2.DragDrop.isDragging();
-            if (disableHover) {
-                return;
-            }
-            event.stopPropagation();
-
-            $liveedit.publish('/page/component/highlight', [$component, self.highlightColor]);
-        });
-    },
+(function () {
+    // Namespaces
+    AdminLiveEdit.page = {};
+    AdminLiveEdit.page.components = {};
 
 
-    attachMouseOutEvent: function () {
-        $liveedit(document).on('mouseout', function (event) {
-            $liveedit.publish('/page/component/hide-highlighter');
-        });
-    },
+    AdminLiveEdit.page.components.Base = function () {
+        this.selector = '';
+        this.highlightColor = '#141414';
+    };
 
 
-    attachClickEvent: function () {
-        $liveedit(document).on('click touchstart', this.selector, function (event) {
-            event.stopPropagation();
-            event.preventDefault();
-            var $closestComponentFromTarget = $liveedit(event.target).closest('[data-live-edit-type]');
-            var componentIsSelected = $closestComponentFromTarget.hasClass('live-edit-selected-component');
-            if (componentIsSelected) {
-                $liveedit.publish('/page/component/deselect');
-            } else {
-                $liveedit.publish('/page/component/select', [$closestComponentFromTarget]);
-            }
-            return false;
-        });
-    },
+    AdminLiveEdit.page.components.Base.prototype = {
+        attachMouseOverEvent: function () {
+            var self = this;
+            $liveedit(document).on('mouseover', this.selector, function (event) {
+                var $component = $liveedit(this);
+                var componentIsDescendantOfSelected = $component.parents('.live-edit-selected-component').length === 1;
+                // TODO: remove reference to DragDrop, use PubSub.
+                var disableHover = componentIsDescendantOfSelected || AdminLiveEdit.ui2.DragDrop.isDragging();
+                if (disableHover) {
+                    return;
+                }
+                event.stopPropagation();
+
+                $liveedit.publish('/page/component/highlight', [$component, self.highlightColor]);
+            });
+        },
 
 
-    getAll: function () {
-        return $liveedit(this.selector);
-    }
-};
+        attachMouseOutEvent: function () {
+            $liveedit(document).on('mouseout', function (event) {
+                $liveedit.publish('/page/component/hide-highlighter');
+            });
+        },
+
+
+        attachClickEvent: function () {
+            $liveedit(document).on('click touchstart', this.selector, function (event) {
+                event.stopPropagation();
+                event.preventDefault();
+                var $closestComponentFromTarget = $liveedit(event.target).closest('[data-live-edit-type]');
+                var componentIsSelected = $closestComponentFromTarget.hasClass('live-edit-selected-component');
+                if (componentIsSelected) {
+                    $liveedit.publish('/page/component/deselect');
+                } else {
+                    $liveedit.publish('/page/component/select', [$closestComponentFromTarget]);
+                }
+                return false;
+            });
+        },
+
+
+        getAll: function () {
+            return $liveedit(this.selector);
+        }
+
+    };
+}());
--- modules/wem-webapp/src/main/webapp/admin/dev/live-edit/resources/js/ui2/Base.js
+++ modules/wem-webapp/src/main/webapp/admin/dev/live-edit/resources/js/ui2/Base.js
@@ -1,33 +1,35 @@
-// Namespace
-AdminLiveEdit.ui2 = {};
+(function () {
+    // Namespace
+    AdminLiveEdit.ui2 = {};
 
-// Class
-AdminLiveEdit.ui2.Base = function () {
-    this.$element = $liveedit([]);
-};
-
-
-// Methods
-AdminLiveEdit.ui2.Base.prototype = {
-    blankImage: 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
-
-    createElement: function (htmlString) {
-        var $element = $liveedit(htmlString);
-        $element.attr('id', 'live-edit-cmp-' + AdminLiveEdit.Util.createGUID());
-        this.$element = $element;
-
-        return this.$element;
-    },
+    // Class
+    AdminLiveEdit.ui2.Base = function () {
+        this.$element = $liveedit([]);
+    };
 
 
-    appendTo: function ($parent) {
-        if ($parent.length > 0 && this.$element.length > 0) {
-            $parent.append(this.$element);
+    // Methods
+    AdminLiveEdit.ui2.Base.prototype = {
+        blankImage: 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+
+        createElement: function (htmlString) {
+            var $element = $liveedit(htmlString);
+            $element.attr('id', 'live-edit-cmp-' + AdminLiveEdit.Util.createGUID());
+            this.$element = $element;
+
+            return this.$element;
+        },
+
+
+        appendTo: function ($parent) {
+            if ($parent.length > 0 && this.$element.length > 0) {
+                $parent.append(this.$element);
+            }
+        },
+
+
+        getEl: function () {
+            return this.$element;
         }
-    },
-
-
-    getEl: function () {
-        return this.$element;
-    }
-};
+    };
+}());
