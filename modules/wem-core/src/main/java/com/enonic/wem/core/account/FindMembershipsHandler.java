79ec106cbff675f4af6d1f2180e1c73ac7b8edd6
Alex Rodriguez <aro@enonic.com>
2012-08-15 09:16:32 +0200

    CMS-112 Implement FindMemberships in new API

--- modules/wem-core/src/main/java/com/enonic/wem/core/account/FindMembershipsHandler.java
+++ modules/wem-core/src/main/java/com/enonic/wem/core/account/FindMembershipsHandler.java
@@ -1,15 +1,41 @@
 package com.enonic.wem.core.account;
 
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.Sets;
+
+import com.enonic.wem.api.account.AccountKey;
+import com.enonic.wem.api.account.AccountKeySet;
+import com.enonic.wem.api.account.AccountType;
 import com.enonic.wem.api.command.account.FindMemberships;
+import com.enonic.wem.api.exception.AccountNotFoundException;
 import com.enonic.wem.core.command.CommandContext;
 import com.enonic.wem.core.command.CommandHandler;
+
+import com.enonic.cms.core.security.group.GroupEntity;
+import com.enonic.cms.core.security.group.GroupType;
+import com.enonic.cms.core.security.user.QualifiedUsername;
+import com.enonic.cms.core.security.user.UserEntity;
+import com.enonic.cms.core.security.userstore.UserStoreEntity;
+import com.enonic.cms.store.dao.GroupDao;
+import com.enonic.cms.store.dao.UserDao;
+import com.enonic.cms.store.dao.UserStoreDao;
 
 @Component
 public final class FindMembershipsHandler
     extends CommandHandler<FindMemberships>
 {
+    private GroupDao groupDao;
+
+    private UserDao userDao;
+
+    private UserStoreDao userStoreDao;
+
     public FindMembershipsHandler()
     {
         super( FindMemberships.class );
@@ -19,6 +45,124 @@ public final class FindMembershipsHandler
     public void handle( final CommandContext context, final FindMemberships command )
         throws Exception
     {
-        // TODO: Implement
+        final AccountKey account = command.getKey();
+
+        final String accountName = account.getLocalName();
+        final UserStoreEntity userStore = userStoreDao.findByName( account.getUserStore() );
+        if ( userStore == null )
+        {
+            throw new AccountNotFoundException( account );
+        }
+
+        final GroupEntity groupEntity;
+        if ( account.isUser() )
+        {
+            groupEntity = findUserGroup( userStore, accountName );
+        }
+        else
+        {
+            groupEntity = findGroupOrRole( userStore, accountName );
+        }
+
+        if ( groupEntity == null )
+        {
+            throw new AccountNotFoundException( account );
+        }
+
+        final Set<GroupEntity> groupMemberships = groupEntity.getMemberships( false );
+
+        final Set<AccountKey> members = membershipsToAccountKeys( groupMemberships, command.isIncludeTransitive() );
+
+        command.setResult( AccountKeySet.from( members ) );
+    }
+
+    private GroupEntity findUserGroup( final UserStoreEntity userStore, final String accountName )
+    {
+        final UserEntity user = userDao.findByQualifiedUsername( new QualifiedUsername( userStore.getName(), accountName ) );
+        if ( user == null )
+        {
+            return null;
+        }
+        return user.getUserGroup();
+    }
+
+    private GroupEntity findGroupOrRole( final UserStoreEntity userStore, final String groupName )
+    {
+        final List<GroupEntity> groups = groupDao.findByUserStoreKeyAndGroupname( userStore.getKey(), groupName, false );
+        if ( ( groups == null ) || groups.isEmpty() )
+        {
+            return null;
+        }
+        return groups.get( 0 );
+    }
+
+    private Set<AccountKey> membershipsToAccountKeys( final Set<GroupEntity> members, boolean includeTransitive )
+    {
+        final Set<AccountKey> accountSet = Sets.newHashSet();
+        for ( GroupEntity member : members )
+        {
+            final AccountType type = getAccountType( member );
+            final String name = getAccountName( member );
+            final AccountKey memberAccount = createAccountKey( type, member.getUserStore().getName(), name );
+            accountSet.add( memberAccount );
+            if ( includeTransitive )
+            {
+                final Set<AccountKey> transitiveAccountSet = membershipsToAccountKeys( member.getMemberships( false ), includeTransitive );
+                accountSet.addAll( transitiveAccountSet );
+            }
+        }
+        return accountSet;
+    }
+
+    private String getAccountName( final GroupEntity group )
+    {
+        if ( group.getType() == GroupType.USER )
+        {
+            return group.getUser().getName();
+        }
+        else
+        {
+            return group.getName();
+        }
+    }
+
+    private AccountKey createAccountKey( final AccountType type, final String userStore, final String localName )
+    {
+        return AccountKey.from( Joiner.on( ":" ).join( type.toString().toLowerCase(), userStore, localName ) );
+    }
+
+    private AccountType getAccountType( final GroupEntity groupEntity )
+    {
+        final GroupType type = groupEntity.getType();
+        if ( type == GroupType.USER )
+        {
+            return AccountType.USER;
+        }
+        else if ( groupEntity.isBuiltIn() )
+        {
+            return AccountType.ROLE;
+        }
+        else
+        {
+            return AccountType.GROUP;
+        }
+    }
+
+    @Autowired
+    public void setGroupDao( final GroupDao groupDao )
+    {
+        this.groupDao = groupDao;
+    }
+
+    @Autowired
+    public void setUserStoreDao( final UserStoreDao userStoreDao )
+    {
+        this.userStoreDao = userStoreDao;
+    }
+
+    @Autowired
+    public void setUserDao( final UserDao userDao )
+    {
+        this.userDao = userDao;
     }
 }
---
+++ modules/wem-core/src/test/java/com/enonic/wem/core/account/FindMembershipsHandlerTest.java
@@ -0,0 +1,237 @@
+package com.enonic.wem.core.account;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.google.common.collect.Sets;
+
+import com.enonic.wem.api.Client;
+import com.enonic.wem.api.account.AccountKey;
+import com.enonic.wem.api.account.AccountKeySet;
+import com.enonic.wem.api.command.Commands;
+import com.enonic.wem.api.exception.AccountNotFoundException;
+import com.enonic.wem.core.client.StandardClient;
+import com.enonic.wem.core.command.CommandInvokerImpl;
+
+import com.enonic.cms.core.security.group.GroupEntity;
+import com.enonic.cms.core.security.group.GroupKey;
+import com.enonic.cms.core.security.group.GroupType;
+import com.enonic.cms.core.security.user.QualifiedUsername;
+import com.enonic.cms.core.security.user.UserEntity;
+import com.enonic.cms.core.security.user.UserKey;
+import com.enonic.cms.core.security.user.UserType;
+import com.enonic.cms.core.security.userstore.UserStoreEntity;
+import com.enonic.cms.core.security.userstore.UserStoreKey;
+import com.enonic.cms.store.dao.GroupDao;
+import com.enonic.cms.store.dao.UserDao;
+import com.enonic.cms.store.dao.UserStoreDao;
+
+import static org.junit.Assert.*;
+
+public class FindMembershipsHandlerTest
+{
+    private static final String USERSTORE_KEY = "12345";
+
+    private Client client;
+
+    private UserDao userDao;
+
+    private GroupDao groupDao;
+
+    private UserStoreDao userStoreDao;
+
+    @Before
+    public void setUp()
+        throws Exception
+    {
+        userDao = Mockito.mock( UserDao.class );
+        groupDao = Mockito.mock( GroupDao.class );
+        userStoreDao = Mockito.mock( UserStoreDao.class );
+
+        final FindMembershipsHandler findMembershipsHandler = new FindMembershipsHandler();
+        findMembershipsHandler.setGroupDao( groupDao );
+        findMembershipsHandler.setUserDao( userDao );
+        findMembershipsHandler.setUserStoreDao( userStoreDao );
+
+        final StandardClient standardClient = new StandardClient();
+        final CommandInvokerImpl commandInvoker = new CommandInvokerImpl();
+        commandInvoker.setHandlers( findMembershipsHandler );
+        standardClient.setInvoker( commandInvoker );
+        client = standardClient;
+    }
+
+    @Test
+    public void testFindMembershipsUser()
+        throws Exception
+    {
+        final AccountKey account = AccountKey.from( "user:enonic:johndoe" );
+
+        // setup
+        final UserEntity user = createUser( "ASDD8F", account.getUserStore(), account.getLocalName() );
+        final GroupEntity group1 = createGroup( "10001", account.getUserStore(), "group1" );
+        final GroupEntity group2 = createGroup( "10002", account.getUserStore(), "group2" );
+        final GroupEntity role1 = createGroup( "10004", account.getUserStore(), "role1" );
+        Mockito.when( role1.isBuiltIn() ).thenReturn( true );
+
+        final Set<GroupEntity> memberships = Sets.newHashSet( group1, group2, role1 );
+        user.getUserGroup().setMemberships( memberships );
+
+        // exercise
+        final AccountKeySet members = client.execute( Commands.account().findMemberships().key( account ) );
+
+        // verify
+        assertNotNull( members );
+        assertEquals( 3, members.getSize() );
+        assertTrue( members.contains( AccountKey.from( "group:enonic:group1" ) ) );
+        assertTrue( members.contains( AccountKey.from( "group:enonic:group2" ) ) );
+        assertTrue( members.contains( AccountKey.from( "role:enonic:role1" ) ) );
+    }
+
+    @Test
+    public void testFindMembershipsGroup()
+        throws Exception
+    {
+        final AccountKey account = AccountKey.from( "group:enonic:devs" );
+
+        // setup
+        final GroupEntity group = createGroup( "ASDD8F", account.getUserStore(), account.getLocalName() );
+        final GroupEntity group1 = createGroup( "10001", account.getUserStore(), "group1" );
+        final GroupEntity group2 = createGroup( "10002", account.getUserStore(), "group2" );
+        final GroupEntity role1 = createGroup( "10004", account.getUserStore(), "role1" );
+        Mockito.when( role1.isBuiltIn() ).thenReturn( true );
+
+        final Set<GroupEntity> memberships = Sets.newHashSet( group1, group2, role1 );
+        group.setMemberships( memberships );
+
+        // exercise
+        final AccountKeySet members = client.execute( Commands.account().findMemberships().key( account ) );
+
+        // verify
+        assertNotNull( members );
+        assertEquals( 3, members.getSize() );
+        assertTrue( members.contains( AccountKey.from( "group:enonic:group1" ) ) );
+        assertTrue( members.contains( AccountKey.from( "group:enonic:group2" ) ) );
+        assertTrue( members.contains( AccountKey.from( "role:enonic:role1" ) ) );
+    }
+
+    @Test
+    public void testFindMembershipsUserIncludeTransitive()
+        throws Exception
+    {
+        final AccountKey account = AccountKey.from( "user:enonic:johndoe" );
+
+        // setup
+        final UserEntity user = createUser( "ASDD8F", account.getUserStore(), account.getLocalName() );
+        final GroupEntity group1 = createGroup( "10001", account.getUserStore(), "group1" );
+        final GroupEntity group2 = createGroup( "10002", account.getUserStore(), "group2" );
+        final GroupEntity role1 = createGroup( "10004", account.getUserStore(), "role1" );
+        Mockito.when( role1.isBuiltIn() ).thenReturn( true );
+
+        final Set<GroupEntity> group1Memberships = Sets.newHashSet( role1 );
+        group1.setMemberships( group1Memberships );
+
+        final Set<GroupEntity> memberships = Sets.newHashSet( group1, group2 );
+        user.getUserGroup().setMemberships( memberships );
+
+        // exercise
+        final AccountKeySet members = client.execute( Commands.account().findMemberships().key( account ).includeTransitive() );
+
+        // verify
+        assertNotNull( members );
+        assertEquals( 3, members.getSize() );
+        assertTrue( members.contains( AccountKey.from( "group:enonic:group1" ) ) );
+        assertTrue( members.contains( AccountKey.from( "group:enonic:group2" ) ) );
+        assertTrue( members.contains( AccountKey.from( "role:enonic:role1" ) ) );
+    }
+
+    @Test(expected = AccountNotFoundException.class)
+    public void testFindMembershipsMissingAccount()
+        throws Exception
+    {
+        final AccountKey groupAccount = AccountKey.from( "group:enonic:group1" );
+        createUserStore( groupAccount.getUserStore(), USERSTORE_KEY );
+
+        client.execute( Commands.account().findMemberships().key( groupAccount ) );
+    }
+
+    @Test(expected = AccountNotFoundException.class)
+    public void testFindMembershipsMissingUserstore()
+        throws Exception
+    {
+        final AccountKey groupAccount = AccountKey.from( "user:enonic:user1" );
+        client.execute( Commands.account().findMemberships().key( groupAccount ) );
+    }
+
+    private GroupEntity createGroup( final String key, final String userStore, final String name, final GroupEntity... members )
+        throws Exception
+    {
+        final UserStoreEntity userStoreEntity = createUserStore( userStore, USERSTORE_KEY );
+        final GroupEntity group = Mockito.mock( GroupEntity.class, Mockito.CALLS_REAL_METHODS );
+        group.setKey( new GroupKey( key ) );
+        group.setType( GroupType.USERSTORE_GROUP );
+        group.setUserStore( userStoreEntity );
+        group.setName( name );
+        group.setDescription( "Group " + name );
+        group.setDeleted( false );
+        group.setMemberships( Sets.<GroupEntity>newHashSet() );
+
+        final Set<GroupEntity> memberSet = new HashSet<GroupEntity>();
+        memberSet.addAll( Arrays.asList( members ) );
+        group.setMembers( memberSet );
+
+        mockAddGroupToUserStore( userStoreEntity, group );
+        return group;
+    }
+
+    private UserEntity createUser( final String key, final String userStore, final String name )
+        throws Exception
+    {
+        final UserEntity user = Mockito.mock( UserEntity.class, Mockito.CALLS_REAL_METHODS );
+        user.setKey( new UserKey( key ) );
+        user.setType( UserType.NORMAL );
+        user.setEmail( "user@email.com" );
+        user.setUserStore( createUserStore( userStore, USERSTORE_KEY ) );
+        user.setName( name );
+        user.setDisplayName( name + " User" );
+
+        final GroupEntity userGroup = createGroup( "U" + key, userStore, "userGroup" + key );
+        userGroup.setType( GroupType.USER );
+        user.setUserGroup( userGroup );
+
+        mockAddUserToDaoByQualifiedName( user );
+        return user;
+    }
+
+    private void mockAddUserToDaoByQualifiedName( final UserEntity user )
+    {
+        final QualifiedUsername qualifiedName = user.getQualifiedName();
+        Mockito.when( userDao.findByQualifiedUsername( Mockito.argThat( new IsQualifiedUsername( qualifiedName ) ) ) ).thenReturn( user );
+    }
+
+    private void mockAddGroupToUserStore( final UserStoreEntity userStore, final GroupEntity group )
+    {
+        final List<GroupEntity> userStoreResults = new ArrayList<GroupEntity>();
+        userStoreResults.add( group );
+        Mockito.when( groupDao.findByUserStoreKeyAndGroupname( userStore.getKey(), group.getName(), false ) ).thenReturn(
+            userStoreResults );
+    }
+
+    private UserStoreEntity createUserStore( final String name, final String userStoreKey )
+    {
+        final UserStoreEntity userStore = new UserStoreEntity();
+        userStore.setName( name );
+        userStore.setKey( new UserStoreKey( userStoreKey ) );
+
+        Mockito.when( userStoreDao.findByName( name ) ).thenReturn( userStore );
+
+        return userStore;
+    }
+
+}
