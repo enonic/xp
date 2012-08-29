e4bf5498b261611c58580d7324ce5df9dd1400ed
Viktar Fiodarau <vfi@enonic.com>
2012-08-29 14:11:07 +0300

    CMS-170 Improve userstore update rest service
    Improve userstore update rest service by implementing it in rest2 package. It should be under UserStoreResource class and the path for the update will be /admin/rest/userstore/<key>.

--- modules/wem-web/src/main/java/com/enonic/wem/web/rest2/resource/userstore/UserStoreResource.java
+++ modules/wem-web/src/main/java/com/enonic/wem/web/rest2/resource/userstore/UserStoreResource.java
@@ -2,14 +2,25 @@ package com.enonic.wem.web.rest2.resource.userstore;
 
 import java.util.List;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.enonic.esl.containers.ExtendedMap;
+import com.enonic.wem.web.rest2.service.userstore.UserStoreUpdateService;
+
+import com.enonic.cms.core.security.SecurityService;
+import com.enonic.cms.core.security.user.QualifiedUsername;
+import com.enonic.cms.core.security.user.User;
 import com.enonic.cms.core.security.userstore.UserStoreEntity;
 import com.enonic.cms.store.dao.UserStoreDao;
 
@@ -20,6 +31,12 @@ public class UserStoreResource
 {
     private UserStoreDao userStoreDao;
 
+    @Autowired
+    private SecurityService securityService;
+
+    @Autowired
+    private UserStoreUpdateService userStoreUpdateService;
+
     @GET
     public UserStoreResults getAll()
     {
@@ -27,9 +44,54 @@ public class UserStoreResource
         return new UserStoreResults( userStores );
     }
 
+    @POST
+    @Path("{key}")
+    public Response updateUserstore( @PathParam("key") String key, @FormParam("name") String name,
+                                     @FormParam("defaultUserstore") Boolean defaultUserstore, @FormParam("config") String config,
+                                     @FormParam("connector") String connector, @FormParam("administrators") List<String> administrators )
+    {
+        User user = securityService.getUser( new QualifiedUsername( "system", "admin" ) );
+        if ( user == null )
+        {
+            //Anonymous users can't create or update userstores
+            return Response.status( Response.Status.FORBIDDEN ).build();
+        }
+
+        UserStoreEntity duplicate = userStoreDao.findByName( name );
+        if ( duplicate != null && key.equals( duplicate.getKey().toString() ) )
+        {
+
+            //The userstore with such name already exists
+            return Response.status( Response.Status.NOT_ACCEPTABLE ).build();
+        }
+
+        ExtendedMap formItems = new ExtendedMap( 5 );
+        formItems.putString( UserStoreUpdateService.KEY, key );
+        formItems.putString( UserStoreUpdateService.NAME, name );
+        formItems.putBoolean( UserStoreUpdateService.DEFAULT_USERSTORE, defaultUserstore );
+        formItems.putString( UserStoreUpdateService.CONFIG, config );
+        if ( !StringUtils.isEmpty( connector ) )
+        {
+            formItems.putString( UserStoreUpdateService.CONFIG, connector );
+        }
+
+        userStoreUpdateService.updateUserStore( user, formItems, administrators );
+        return Response.ok().build();
+    }
+
     @Autowired
     public void setUserStoreDao( final UserStoreDao userStoreDao )
     {
         this.userStoreDao = userStoreDao;
+    }
+
+    public void setSecurityService( final SecurityService securityService )
+    {
+        this.securityService = securityService;
+    }
+
+    public void setUserStoreUpdateService( final UserStoreUpdateService userStoreUpdateService )
+    {
+        this.userStoreUpdateService = userStoreUpdateService;
     }
 }
--- modules/wem-web/src/test/java/com/enonic/wem/web/rest2/resource/userstore/UserStoreResourceTest.java
+++ modules/wem-web/src/test/java/com/enonic/wem/web/rest2/resource/userstore/UserStoreResourceTest.java
@@ -3,15 +3,24 @@ package com.enonic.wem.web.rest2.resource.userstore;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.core.Response;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import com.enonic.wem.web.rest2.resource.AbstractResourceTest;
+import com.enonic.wem.web.rest2.resource.account.IsQualifiedUsername;
+import com.enonic.wem.web.rest2.service.userstore.UserStoreUpdateService;
 
+import com.enonic.cms.core.security.SecurityService;
+import com.enonic.cms.core.security.user.QualifiedUsername;
+import com.enonic.cms.core.security.user.UserEntity;
 import com.enonic.cms.core.security.userstore.UserStoreEntity;
 import com.enonic.cms.core.security.userstore.UserStoreKey;
 import com.enonic.cms.store.dao.UserStoreDao;
+
+import static org.junit.Assert.*;
 
 public class UserStoreResourceTest
     extends AbstractResourceTest
@@ -19,14 +28,22 @@ public class UserStoreResourceTest
 
     private UserStoreResource userStoreResource;
 
+    private SecurityService securityService;
+
+    private UserStoreUpdateService userStoreUpdateService;
+
     private UserStoreDao userStoreDao;
 
     @Before
     public void setUp()
     {
         userStoreDao = Mockito.mock( UserStoreDao.class );
+        securityService = Mockito.mock( SecurityService.class );
+        userStoreUpdateService = Mockito.mock( UserStoreUpdateService.class );
         userStoreResource = new UserStoreResource();
         userStoreResource.setUserStoreDao( userStoreDao );
+        userStoreResource.setSecurityService( securityService );
+        userStoreResource.setUserStoreUpdateService( userStoreUpdateService );
     }
 
     @Test
@@ -36,6 +53,34 @@ public class UserStoreResourceTest
         Mockito.when( userStoreDao.findAll() ).thenReturn( createUserStoreEntityList() );
         UserStoreResults results = userStoreResource.getAll();
         assertJsonResult( "all_userstores.json", results );
+    }
+
+    @Test
+    public void testUpdateUserStore_anonimUserAccess()
+    {
+        Mockito.when( securityService.getUser(
+            Mockito.argThat( new IsQualifiedUsername( new QualifiedUsername( "system", "admin" ) ) ) ) ).thenReturn( null );
+        Response response = userStoreResource.updateUserstore( "1", "enonic", false, "", "", new ArrayList<String>() );
+        assertEquals( Response.Status.FORBIDDEN.getStatusCode(), response.getStatus() );
+    }
+
+    @Test
+    public void testUpdateUserStore_duplicateUserStore()
+    {
+        Mockito.when( securityService.getUser(
+            Mockito.argThat( new IsQualifiedUsername( new QualifiedUsername( "system", "admin" ) ) ) ) ).thenReturn( new UserEntity() );
+        Mockito.when( userStoreDao.findByName( "enonic" ) ).thenReturn( createUserStoreEntity( "1", "enonic" ) );
+        Response response = userStoreResource.updateUserstore( "1", "enonic", false, "", "", new ArrayList<String>() );
+        assertEquals( Response.Status.NOT_ACCEPTABLE.getStatusCode(), response.getStatus() );
+    }
+
+    @Test
+    public void testUpdateStore_OkCase()
+    {
+        Mockito.when( securityService.getUser(
+            Mockito.argThat( new IsQualifiedUsername( new QualifiedUsername( "system", "admin" ) ) ) ) ).thenReturn( new UserEntity() );
+        Response response = userStoreResource.updateUserstore( "1", "enonic", false, "", "", new ArrayList<String>() );
+        assertEquals( Response.Status.OK.getStatusCode(), response.getStatus() );
     }
 
     private List<UserStoreEntity> createUserStoreEntityList()
