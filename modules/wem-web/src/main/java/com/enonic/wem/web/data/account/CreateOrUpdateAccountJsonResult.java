b1af5ebb43c5d56f944875943a73526033181f6f
Alex Rodriguez <aro@enonic.com>
2012-09-05 12:24:33 +0200

    CMS-220 Create account createOrUpdate rpc handler using new API

---
+++ modules/wem-web/src/main/java/com/enonic/wem/web/data/account/CreateOrUpdateAccountJsonResult.java
@@ -0,0 +1,34 @@
+package com.enonic.wem.web.data.account;
+
+import org.codehaus.jackson.node.ObjectNode;
+
+import com.enonic.wem.web.json.result.JsonSuccessResult;
+
+final class CreateOrUpdateAccountJsonResult
+    extends JsonSuccessResult
+{
+    private final boolean created;
+
+    private CreateOrUpdateAccountJsonResult( final boolean created )
+    {
+        this.created = created;
+    }
+
+    public static CreateOrUpdateAccountJsonResult created()
+    {
+        return new CreateOrUpdateAccountJsonResult( true );
+    }
+
+    public static CreateOrUpdateAccountJsonResult updated()
+    {
+        return new CreateOrUpdateAccountJsonResult( false );
+    }
+
+    @Override
+    protected void serialize( final ObjectNode json )
+    {
+        json.put( "created", created );
+        json.put( "updated", !created );
+    }
+
+}
