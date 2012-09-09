cd611d1b993600e7a79d5b968d1d9704d0c2c446
Sten Roger Sandvik <srs@enonic.com>
2012-09-09 14:02:49 +0200

    Added placeholders for xml parse/serialize.

--- modules/wem-api/src/main/java/com/enonic/wem/api/userstore/config/UserStoreConfigParser.java
+++ modules/wem-api/src/main/java/com/enonic/wem/api/userstore/config/UserStoreConfigParser.java
@@ -1,5 +1,30 @@
 package com.enonic.wem.api.userstore.config;
 
-public class UserStoreConfigParser
+import java.io.StringReader;
+
+import org.jdom.Document;
+import org.jdom.input.SAXBuilder;
+
+public final class UserStoreConfigParser
 {
+    private final SAXBuilder builder;
+
+    public UserStoreConfigParser()
+    {
+        this.builder = new SAXBuilder();
+    }
+
+    public UserStoreConfig parseXml( final String xml )
+        throws Exception
+    {
+        final Document doc = this.builder.build( new StringReader( xml ) );
+        return parseXml( doc );
+    }
+
+    public UserStoreConfig parseXml( final Document xml )
+        throws Exception
+    {
+        // TODO: Implement parsing of UserStoreConfig
+        return null;
+    }
 }
--- modules/wem-api/src/main/java/com/enonic/wem/api/userstore/config/UserStoreConfigSerializer.java
+++ modules/wem-api/src/main/java/com/enonic/wem/api/userstore/config/UserStoreConfigSerializer.java
@@ -1,5 +1,30 @@
 package com.enonic.wem.api.userstore.config;
 
-public class UserStoreConfigSerializer
+import org.jdom.Document;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+
+public final class UserStoreConfigSerializer
 {
+    private final XMLOutputter out;
+
+    public UserStoreConfigSerializer()
+    {
+        this.out = new XMLOutputter();
+        this.out.setFormat( Format.getCompactFormat() );
+    }
+
+    public Document toXml( final UserStoreConfig config )
+        throws Exception
+    {
+        // TODO: Implement serializing of UserStoreConfig
+        return null;
+    }
+
+    public String toXmlString( final UserStoreConfig config )
+        throws Exception
+    {
+        final Document doc = toXml( config );
+        return this.out.outputString( doc );
+    }
 }
