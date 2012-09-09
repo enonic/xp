491f3ce5811d5de308c3a8023062e538f5743180
Sten Roger Sandvik <srs@enonic.com>
2012-09-09 13:11:21 +0200

    Added fields on userstoreconnector.

--- modules/wem-api/src/main/java/com/enonic/wem/api/userstore/connector/UserStoreConnector.java
+++ modules/wem-api/src/main/java/com/enonic/wem/api/userstore/connector/UserStoreConnector.java
@@ -4,6 +4,30 @@ public final class UserStoreConnector
 {
     private final String name;
 
+    private boolean createUser;
+
+    private boolean updateUser;
+
+    private boolean updatePassword;
+
+    private boolean deleteUser;
+
+    private boolean createGroup;
+
+    private boolean readGroup;
+
+    private boolean updateGroup;
+
+    private boolean deleteGroup;
+
+    private boolean groupsStoredRemote;
+
+    private boolean resurrectDeletedUsers;
+
+    private boolean resurrectDeletedGroups;
+
+    private String pluginClass;
+
     public UserStoreConnector( final String name )
     {
         this.name = name;
@@ -12,5 +36,125 @@ public final class UserStoreConnector
     public String getName()
     {
         return name;
+    }
+
+    public boolean isCreateUser()
+    {
+        return createUser;
+    }
+
+    public boolean isUpdateUser()
+    {
+        return updateUser;
+    }
+
+    public boolean isUpdatePassword()
+    {
+        return updatePassword;
+    }
+
+    public boolean isDeleteUser()
+    {
+        return deleteUser;
+    }
+
+    public boolean isCreateGroup()
+    {
+        return createGroup;
+    }
+
+    public boolean isReadGroup()
+    {
+        return readGroup;
+    }
+
+    public boolean isUpdateGroup()
+    {
+        return updateGroup;
+    }
+
+    public boolean isDeleteGroup()
+    {
+        return deleteGroup;
+    }
+
+    public boolean isGroupsStoredRemote()
+    {
+        return groupsStoredRemote;
+    }
+
+    public boolean isResurrectDeletedUsers()
+    {
+        return resurrectDeletedUsers;
+    }
+
+    public boolean isResurrectDeletedGroups()
+    {
+        return resurrectDeletedGroups;
+    }
+
+    public String getPluginClass()
+    {
+        return pluginClass;
+    }
+
+    public void setCreateUser( final boolean createUser )
+    {
+        this.createUser = createUser;
+    }
+
+    public void setUpdateUser( final boolean updateUser )
+    {
+        this.updateUser = updateUser;
+    }
+
+    public void setUpdatePassword( final boolean updatePassword )
+    {
+        this.updatePassword = updatePassword;
+    }
+
+    public void setDeleteUser( final boolean deleteUser )
+    {
+        this.deleteUser = deleteUser;
+    }
+
+    public void setCreateGroup( final boolean createGroup )
+    {
+        this.createGroup = createGroup;
+    }
+
+    public void setReadGroup( final boolean readGroup )
+    {
+        this.readGroup = readGroup;
+    }
+
+    public void setUpdateGroup( final boolean updateGroup )
+    {
+        this.updateGroup = updateGroup;
+    }
+
+    public void setDeleteGroup( final boolean deleteGroup )
+    {
+        this.deleteGroup = deleteGroup;
+    }
+
+    public void setGroupsStoredRemote( final boolean groupsStoredRemote )
+    {
+        this.groupsStoredRemote = groupsStoredRemote;
+    }
+
+    public void setResurrectDeletedUsers( final boolean resurrectDeletedUsers )
+    {
+        this.resurrectDeletedUsers = resurrectDeletedUsers;
+    }
+
+    public void setResurrectDeletedGroups( final boolean resurrectDeletedGroups )
+    {
+        this.resurrectDeletedGroups = resurrectDeletedGroups;
+    }
+
+    public void setPluginClass( final String pluginClass )
+    {
+        this.pluginClass = pluginClass;
     }
 }
