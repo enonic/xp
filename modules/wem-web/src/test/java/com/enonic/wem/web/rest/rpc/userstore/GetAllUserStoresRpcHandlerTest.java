c1cc9e56126027e57ec2157434bccbb0b3cc3870
Alex Rodriguez <aro@enonic.com>
2012-09-17 14:04:16 +0200

    CMS-268 Implement userstore_getAll rpc handler

---
+++ modules/wem-web/src/test/java/com/enonic/wem/web/rest/rpc/userstore/GetAllUserStoresRpcHandlerTest.java
@@ -0,0 +1,51 @@
+package com.enonic.wem.web.rest.rpc.userstore;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.enonic.wem.api.Client;
+import com.enonic.wem.api.command.userstore.FindAllUserStores;
+import com.enonic.wem.api.command.userstore.GetUserStores;
+import com.enonic.wem.api.userstore.UserStore;
+import com.enonic.wem.api.userstore.UserStoreName;
+import com.enonic.wem.api.userstore.UserStoreNames;
+import com.enonic.wem.api.userstore.UserStores;
+import com.enonic.wem.web.json.rpc.JsonRpcHandler;
+import com.enonic.wem.web.rest.rpc.AbstractRpcHandlerTest;
+
+public class GetAllUserStoresRpcHandlerTest
+    extends AbstractRpcHandlerTest
+
+{
+    private Client client;
+
+    @Override
+    protected JsonRpcHandler createHandler()
+        throws Exception
+    {
+        client = Mockito.mock( Client.class );
+        final GetAllUserStoreRpcHandler handler = new GetAllUserStoreRpcHandler();
+        handler.setClient( client );
+        return handler;
+    }
+
+    @Test
+    public void getConnectors()
+        throws Exception
+    {
+        final UserStoreName userStoreName1 = UserStoreName.from( "enonic" );
+        final UserStoreName userStoreName2 = UserStoreName.from( "system" );
+        final UserStoreNames userStoreNames = UserStoreNames.from( userStoreName1, userStoreName2 );
+        Mockito.when( client.execute( Mockito.isA( FindAllUserStores.class ) ) ).thenReturn( userStoreNames );
+
+        final UserStore userStore1 = new UserStore( userStoreName1 );
+        userStore1.setConnectorName( "ldap" );
+        final UserStore userStore2 = new UserStore( userStoreName2 );
+        userStore2.setConnectorName( "ad" );
+        final UserStores userStores = UserStores.from( userStore1, userStore2 );
+        Mockito.when( client.execute( Mockito.isA( GetUserStores.class ) ) ).thenReturn( userStores );
+
+        testSuccess( "userstore_getAll.json" );
+    }
+
+}
