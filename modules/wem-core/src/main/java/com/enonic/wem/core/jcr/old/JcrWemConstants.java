123a830f2124008f6c67e5921697aead86fec0d1
Alex Rodriguez <aro@enonic.com>
2012-09-25 11:38:06 +0200

    Updated JCR node type definitions.

--- modules/wem-core/src/main/java/com/enonic/wem/core/jcr/old/JcrWemConstants.java
+++ modules/wem-core/src/main/java/com/enonic/wem/core/jcr/old/JcrWemConstants.java
@@ -2,24 +2,24 @@ package com.enonic.wem.core.jcr.old;
 
 public class JcrWemConstants
 {
-    public static final String ENONIC_CMS_NAMESPACE = "http://www.enonic.com/cms";
+    public static final String ENONIC_CMS_NAMESPACE = "http://www.enonic.com/wem";
 
-    public static final String ENONIC_CMS_NAMESPACE_PREFIX = "cms";
+    public static final String ENONIC_CMS_NAMESPACE_PREFIX = "wem";
 
 
-    public static final String ROOT_NODE = "enonic";
+    public static final String ROOT_NODE = "wem";
 
-    public static final String USERSTORES_NODE = "userstores";
+    public static final String USERSTORES_NODE = "userStores";
 
-    public static final String USERSTORES_NODE_TYPE = "cms:userstores";
+    public static final String USERSTORES_NODE_TYPE = "wem:userStores";
 
-    public static final String USERSTORE_NODE_TYPE = "cms:userstore";
+    public static final String USERSTORE_NODE_TYPE = "wem:userStore";
 
     public static final String GROUPS_NODE = "groups";
 
-    public static final String GROUPS_NODE_TYPE = "cms:groups";
+    public static final String GROUPS_NODE_TYPE = "wem:groups";
 
-    public static final String GROUP_NODE_TYPE = "cms:group";
+    public static final String GROUP_NODE_TYPE = "wem:group";
 
     public static final String MEMBERS_NODE = "members";
 
@@ -27,17 +27,17 @@ public class JcrWemConstants
 
     public static final String USERS_NODE = "users";
 
-    public static final String USERS_NODE_TYPE = "cms:users";
+    public static final String USERS_NODE_TYPE = "wem:users";
 
-    public static final String USER_NODE_TYPE = "cms:user";
+    public static final String USER_NODE_TYPE = "wem:user";
 
     public static final String ROLES_NODE = "roles";
 
-    public static final String ROLES_NODE_TYPE = "cms:roles";
+    public static final String ROLES_NODE_TYPE = "wem:roles";
 
-    public static final String ROLE_NODE_TYPE = "cms:role";
+    public static final String ROLE_NODE_TYPE = "wem:role";
 
-    public static final String ACCOUNT_NODE_TYPE = "cms:account";
+    public static final String ACCOUNT_NODE_TYPE = "wem:account";
 
     public static final String USERSTORES_PATH = JcrWemConstants.ROOT_NODE + "/" + JcrWemConstants.USERSTORES_NODE + "/";
 
