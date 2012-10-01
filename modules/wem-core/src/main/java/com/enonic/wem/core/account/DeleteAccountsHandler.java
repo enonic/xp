933fc05b9ef723afbc12b5f05e7b2cd251737db4
Pavel Milkevich <pmi@enonic.com>
2012-10-01 16:29:40 +0300

    CMS-345 Use JCR in DeleteAccountsHandler

--- modules/wem-core/src/main/java/com/enonic/wem/core/account/DeleteAccountsHandler.java
+++ modules/wem-core/src/main/java/com/enonic/wem/core/account/DeleteAccountsHandler.java
@@ -1,7 +1,5 @@
 package com.enonic.wem.core.account;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,34 +7,17 @@ import org.springframework.transaction.annotation.Transactional;
 import com.enonic.wem.api.account.AccountKey;
 import com.enonic.wem.api.account.AccountKeys;
 import com.enonic.wem.api.command.account.DeleteAccounts;
+import com.enonic.wem.core.account.dao.AccountDao;
 import com.enonic.wem.core.command.CommandContext;
 import com.enonic.wem.core.command.CommandHandler;
 import com.enonic.wem.core.search.account.AccountSearchService;
-
-import com.enonic.cms.core.security.group.DeleteGroupCommand;
-import com.enonic.cms.core.security.group.GroupEntity;
-import com.enonic.cms.core.security.group.GroupSpecification;
-import com.enonic.cms.core.security.user.DeleteUserCommand;
-import com.enonic.cms.core.security.user.QualifiedUsername;
-import com.enonic.cms.core.security.user.UserEntity;
-import com.enonic.cms.core.security.user.UserSpecification;
-import com.enonic.cms.core.security.userstore.UserStoreEntity;
-import com.enonic.cms.core.security.userstore.UserStoreService;
-import com.enonic.cms.store.dao.GroupDao;
-import com.enonic.cms.store.dao.UserDao;
-import com.enonic.cms.store.dao.UserStoreDao;
 
 @Component
 public class DeleteAccountsHandler
     extends CommandHandler<DeleteAccounts>
 {
-    private GroupDao groupDao;
 
-    private UserDao userDao;
-
-    private UserStoreDao userStoreDao;
-
-    private UserStoreService userStoreService;
+    private AccountDao accountDao;
 
     private AccountSearchService searchService;
 
@@ -55,95 +36,19 @@ public class DeleteAccountsHandler
         int accountsDeleted = 0;
         for ( AccountKey accountKey : accountKeys )
         {
-            if ( deleteAccount( accountKey ) )
+            if ( this.accountDao.delete( context.getJcrSession(), accountKey ) )
             {
                 this.searchService.deleteIndex( accountKey.toString() );
                 accountsDeleted++;
             }
         }
-
         command.setResult( accountsDeleted );
     }
 
-    private boolean deleteAccount( final AccountKey account )
-    {
-        return account.isUser() ? deleteUser( account ) : deleteGroup( account );
-    }
-
-    private boolean deleteUser( final AccountKey userAccount )
-    {
-        final UserStoreEntity userStore = userStoreDao.findByName( userAccount.getUserStore() );
-        if ( userStore == null )
-        {
-            return false;
-        }
-
-        final QualifiedUsername qualifiedName = new QualifiedUsername( userStore.getName(), userAccount.getLocalName() );
-        final UserEntity userEntity = userDao.findByQualifiedUsername( qualifiedName );
-        if ( userEntity == null )
-        {
-            return false;
-        }
-
-        final UserEntity deleter = userDao.findBuiltInEnterpriseAdminUser(); // TODO get logged in user
-
-        final UserSpecification userSpec = UserSpecification.usingKey( userEntity.getKey() );
-        final DeleteUserCommand deleteUserCommand = new DeleteUserCommand( deleter.getKey(), userSpec );
-        userStoreService.deleteUser( deleteUserCommand );
-
-        return true;
-    }
-
-    private boolean deleteGroup( final AccountKey groupAccount )
-    {
-        final UserStoreEntity userStore = userStoreDao.findByName( groupAccount.getUserStore() );
-        if ( userStore == null )
-        {
-            return false;
-        }
-
-        final List<GroupEntity> groups = groupDao.findByUserStoreKeyAndGroupname( userStore.getKey(), groupAccount.getLocalName(), false );
-        if ( ( groups == null ) || groups.isEmpty() )
-        {
-            return false;
-        }
-
-        final UserEntity deleter = userDao.findBuiltInEnterpriseAdminUser(); // TODO get logged in user
-
-        final GroupEntity group =  groups.get( 0 ) ;
-        final GroupSpecification groupSpec = new GroupSpecification();
-        groupSpec.setKey( group.getGroupKey() );
-        groupSpec.setName( group.getName() );
-
-        final DeleteGroupCommand deleteGroupCommand = new DeleteGroupCommand( deleter, groupSpec );
-        deleteGroupCommand.setRespondWithException( true );
-        userStoreService.deleteGroup( deleteGroupCommand );
-
-        return true;
-    }
-
     @Autowired
-    public void setUserDao( final UserDao userDao )
+    public void setAccountDao( final AccountDao accountDao )
     {
-        this.userDao = userDao;
-    }
-
-    @Autowired
-    public void setUserStoreDao( final UserStoreDao userStoreDao )
-    {
-        this.userStoreDao = userStoreDao;
-    }
-
-    @Autowired
-    public void setGroupDao( final GroupDao groupDao )
-    {
-        this.groupDao = groupDao;
-    }
-
-    @Autowired
-    public void setUserStoreService( final UserStoreService userStoreService )
-    {
-        this.userStoreService = userStoreService;
+        this.accountDao = accountDao;
     }
 
     @Autowired
--- modules/wem-core/src/test/java/com/enonic/wem/core/account/DeleteAccountHandlerTest.java
+++ modules/wem-core/src/test/java/com/enonic/wem/core/account/DeleteAccountHandlerTest.java
@@ -1,51 +1,27 @@
 package com.enonic.wem.core.account;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import javax.jcr.Session;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import com.google.common.collect.Sets;
-
 import com.enonic.wem.api.account.AccountKey;
 import com.enonic.wem.api.account.AccountKeys;
 import com.enonic.wem.api.command.Commands;
 import com.enonic.wem.api.command.account.DeleteAccounts;
+import com.enonic.wem.core.account.dao.AccountDao;
 import com.enonic.wem.core.command.AbstractCommandHandlerTest;
 import com.enonic.wem.core.search.account.AccountSearchService;
-
-import com.enonic.cms.core.security.group.GroupEntity;
-import com.enonic.cms.core.security.group.GroupKey;
-import com.enonic.cms.core.security.group.GroupType;
-import com.enonic.cms.core.security.user.UserEntity;
-import com.enonic.cms.core.security.user.UserKey;
-import com.enonic.cms.core.security.user.UserType;
-import com.enonic.cms.core.security.userstore.UserStoreEntity;
-import com.enonic.cms.core.security.userstore.UserStoreKey;
-import com.enonic.cms.core.security.userstore.UserStoreService;
-import com.enonic.cms.store.dao.GroupDao;
-import com.enonic.cms.store.dao.UserDao;
-import com.enonic.cms.store.dao.UserStoreDao;
 
 import static org.junit.Assert.*;
 
 public class DeleteAccountHandlerTest
     extends AbstractCommandHandlerTest
 {
-    private static final String USERSTORE_KEY = "12345";
-
-    private UserDao userDao;
-
-    private GroupDao groupDao;
-
-    private UserStoreDao userStoreDao;
-
     private DeleteAccountsHandler handler;
+
+    private AccountDao accountDao;
 
     @Before
     public void setUp()
@@ -53,17 +29,11 @@ public class DeleteAccountHandlerTest
     {
         super.initialize();
 
-        userDao = Mockito.mock( UserDao.class );
-        groupDao = Mockito.mock( GroupDao.class );
-        userStoreDao = Mockito.mock( UserStoreDao.class );
-        UserStoreService userStoreService = Mockito.mock( UserStoreService.class );
+        accountDao = Mockito.mock( AccountDao.class );
         final AccountSearchService accountSearchService = Mockito.mock( AccountSearchService.class );
 
         handler = new DeleteAccountsHandler();
-        handler.setUserDao( userDao );
-        handler.setGroupDao( groupDao );
-        handler.setUserStoreDao( userStoreDao );
-        handler.setUserStoreService( userStoreService );
+        handler.setAccountDao( accountDao );
         handler.setSearchService( accountSearchService );
     }
 
@@ -74,21 +44,7 @@ public class DeleteAccountHandlerTest
         final AccountKey account1 = AccountKey.user( "enonic:joe" );
         final AccountKey account2 = AccountKey.group( "enonic:people" );
         final AccountKey account3 = AccountKey.role( "enonic:admin" );
-
-        // setup
-        final UserEntity admin = createUser( "10000", "system", "admin" );
-        Mockito.when( userDao.findBuiltInEnterpriseAdminUser() ).thenReturn( admin );
-
-        final UserEntity user = createUser( "ASDD8F", account1.getUserStore(), account1.getLocalName() );
-        assertNotNull( user );
-
-        final GroupEntity group1 = createGroup( "10001", account2.getUserStore(), account2.getLocalName() );
-        assertNotNull( group1 );
-
-        final GroupEntity role1 = createGroup( "10004", account3.getUserStore(), account3.getLocalName() );
-        assertNotNull( role1 );
-
-        Mockito.when( role1.isBuiltIn() ).thenReturn( true );
+        Mockito.when( accountDao.delete( Mockito.any( Session.class ), Mockito.any( AccountKey.class ) ) ).thenReturn( true );
 
         // exercise
         final DeleteAccounts command = Commands.account().delete().keys( AccountKeys.from( account1, account2, account3 ) );
@@ -101,6 +57,25 @@ public class DeleteAccountHandlerTest
     }
 
     @Test
+    public void deleteMissingAndExistingAccounts()
+        throws Exception
+    {
+        final AccountKey account1 = AccountKey.user( "enonic:joe" );
+        final AccountKey account2 = AccountKey.group( "enonic:people" );
+        final AccountKey account3 = AccountKey.role( "enonic:admin" );
+        Mockito.when( accountDao.delete( Mockito.any( Session.class ), Mockito.eq( account1 ) ) ).thenReturn( true );
+
+        // exercise
+        final DeleteAccounts command = Commands.account().delete().keys( AccountKeys.from( account1, account2, account3 ) );
+        this.handler.handle( this.context, command );
+        final Integer deletedCount = command.getResult();
+
+        // verify
+        assertNotNull( deletedCount );
+        assertEquals( 1, deletedCount.longValue() );
+    }
+
+    @Test
     public void deleteMissingAccounts()
         throws Exception
     {
@@ -108,8 +83,6 @@ public class DeleteAccountHandlerTest
         final AccountKey account2 = AccountKey.group( "enonic:people" );
         final AccountKey account3 = AccountKey.role( "enonic:admin" );
 
-        createUserStore( account1.getUserStore(), USERSTORE_KEY );
-
         // exercise
         final DeleteAccounts command = Commands.account().delete().keys( AccountKeys.from( account1, account2, account3 ) );
         this.handler.handle( this.context, command );
@@ -120,86 +93,4 @@ public class DeleteAccountHandlerTest
         assertEquals( 0, deletedCount.longValue() );
     }
 
-    @Test
-    public void deleteMissingAccountsAndUserStore()
-        throws Exception
-    {
-        final AccountKey account1 = AccountKey.user( "enonic:joe" );
-        final AccountKey account2 = AccountKey.group( "enonic:people" );
-        final AccountKey account3 = AccountKey.role( "enonic:admin" );
-
-        // exercise
-        final DeleteAccounts command = Commands.account().delete().keys( AccountKeys.from( account1, account2, account3 ) );
-        this.handler.handle( this.context, command );
-        final Integer deletedCount = command.getResult();
-
-        // verify
-        assertNotNull( deletedCount );
-        assertEquals( 0, deletedCount.longValue() );
-    }
-
-    private void mockAddUserToDaoByQualifiedName( final UserEntity user )
-    {
-        Mockito.when( userDao.findByQualifiedUsername( Mockito.argThat( new IsQualifiedUsername( user.getQualifiedName() ) ) ) ).thenReturn(
-            user );
-    }
-
-    private UserEntity createUser( final String key, final String userStore, final String name )
-        throws Exception
-    {
-        final UserEntity user = Mockito.mock( UserEntity.class, Mockito.CALLS_REAL_METHODS );
-        user.setKey( new UserKey( key ) );
-        user.setType( UserType.NORMAL );
-        user.setEmail( "user@email.com" );
-        user.setUserStore( createUserStore( userStore, USERSTORE_KEY ) );
-        user.setName( name );
-        user.setDisplayName( name + " User" );
-
-        final GroupEntity userGroup = createGroup( "U" + key, userStore, "userGroup" + key );
-        userGroup.setType( GroupType.USER );
-        user.setUserGroup( userGroup );
-
-        mockAddUserToDaoByQualifiedName( user );
-        return user;
-    }
-
-    private GroupEntity createGroup( final String key, final String userStore, final String name, final GroupEntity... members )
-        throws Exception
-    {
-        final UserStoreEntity userStoreEntity = createUserStore( userStore, USERSTORE_KEY );
-        final GroupEntity group = Mockito.mock( GroupEntity.class, Mockito.CALLS_REAL_METHODS );
-        group.setKey( new GroupKey( key ) );
-        group.setType( GroupType.USERSTORE_GROUP );
-        group.setUserStore( userStoreEntity );
-        group.setName( name );
-        group.setDescription( "Group " + name );
-        group.setDeleted( false );
-        group.setMemberships( Sets.<GroupEntity>newHashSet() );
-
-        final Set<GroupEntity> memberSet = new HashSet<GroupEntity>();
-        memberSet.addAll( Arrays.asList( members ) );
-        group.setMembers( memberSet );
-
-        mockAddGroupToUserStore( userStoreEntity, group );
-        return group;
-    }
-
-    private void mockAddGroupToUserStore( final UserStoreEntity userStore, final GroupEntity group )
-    {
-        final List<GroupEntity> userStoreResults = new ArrayList<GroupEntity>();
-        userStoreResults.add( group );
-        Mockito.when( groupDao.findByUserStoreKeyAndGroupname( userStore.getKey(), group.getName(), false ) ).thenReturn(
-            userStoreResults );
-    }
-
-    private UserStoreEntity createUserStore( final String name, final String userStoreKey )
-    {
-        final UserStoreEntity userStore = new UserStoreEntity();
-        userStore.setName( name );
-        userStore.setKey( new UserStoreKey( userStoreKey ) );
-
-        Mockito.when( userStoreDao.findByName( name ) ).thenReturn( userStore );
-
-        return userStore;
-    }
 }
