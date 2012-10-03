d12d088e06494c6c7330dbf48547e84719be3821
Pavel Milkevich <pmi@enonic.com>
2012-10-03 16:47:10 +0300

    CMS-370 Use JCR in CreateAccountHandler

--- modules/wem-core/src/main/java/com/enonic/wem/core/account/CreateAccountHandler.java
+++ modules/wem-core/src/main/java/com/enonic/wem/core/account/CreateAccountHandler.java
@@ -1,62 +1,29 @@
 package com.enonic.wem.core.account;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.enonic.wem.api.account.Account;
 import com.enonic.wem.api.account.AccountKey;
-import com.enonic.wem.api.account.AccountKeys;
 import com.enonic.wem.api.account.GroupAccount;
 import com.enonic.wem.api.account.UserAccount;
-import com.enonic.wem.api.account.profile.UserProfile;
 import com.enonic.wem.api.command.account.CreateAccount;
-import com.enonic.wem.api.exception.SystemException;
+import com.enonic.wem.core.account.dao.AccountDao;
 import com.enonic.wem.core.command.CommandContext;
 import com.enonic.wem.core.command.CommandHandler;
 import com.enonic.wem.core.search.account.AccountSearchService;
-
-import com.enonic.cms.core.security.SecurityService;
-import com.enonic.cms.core.security.group.GroupEntity;
-import com.enonic.cms.core.security.group.GroupKey;
-import com.enonic.cms.core.security.group.GroupType;
-import com.enonic.cms.core.security.group.StoreNewGroupCommand;
-import com.enonic.cms.core.security.user.QualifiedUsername;
-import com.enonic.cms.core.security.user.StoreNewUserCommand;
-import com.enonic.cms.core.security.user.UserEntity;
-import com.enonic.cms.core.security.user.UserType;
-import com.enonic.cms.core.security.userstore.UserStoreEntity;
-import com.enonic.cms.core.security.userstore.UserStoreKey;
-import com.enonic.cms.core.security.userstore.UserStoreParser;
-import com.enonic.cms.core.security.userstore.UserStoreService;
-import com.enonic.cms.core.user.field.UserFields;
-import com.enonic.cms.store.dao.GroupDao;
-import com.enonic.cms.store.dao.UserDao;
-import com.enonic.cms.store.dao.UserStoreDao;
 
 @Component
 public final class CreateAccountHandler
     extends CommandHandler<CreateAccount>
 {
-    private UserStoreDao userStoreDao;
-
-    private SecurityService securityService;
-
-    private UserStoreService userStoreService;
-
-    private GroupDao groupDao;
-
-    private UserDao userDao;
-
     private AccountSearchService searchService;
 
-    private final UserProfileTransformer userProfileTransformer;
+    private AccountDao accountDao;
 
     public CreateAccountHandler()
     {
         super( CreateAccount.class );
-        userProfileTransformer = new UserProfileTransformer();
     }
 
     @Override
@@ -65,170 +32,39 @@ public final class CreateAccountHandler
     {
         final Account account = command.getAccount();
         final AccountKey key = account.getKey();
+        boolean created = false;
+
         if ( key.isUser() )
         {
-            createUser( (UserAccount) account );
+            accountDao.createUser( context.getJcrSession(), (UserAccount) account );
+            created = true;
         }
         else if ( key.isGroup() )
         {
-
-            createGroup( (GroupAccount) account );
+            accountDao.createGroup( context.getJcrSession(), (GroupAccount) account );
+            created = true;
         }
-        this.searchService.index( account );
+        else if ( key.isRole() )
+        {
+            throw new IllegalArgumentException( "Roles are built-in and can't be created manually." );
+        }
 
+        if ( created )
+        {
+            this.searchService.index( account );
+        }
         command.setResult( key );
-    }
-
-    private void createUser( final UserAccount user )
-    {
-//        final UserEntity storer = securityService.getImpersonatedPortalUser();
-        final UserEntity storer = userDao.findBuiltInEnterpriseAdminUser(); // TODO get logged in user
-        final UserStoreEntity userStoreEntity = userStoreDao.findByName( user.getKey().getUserStore() );
-        if ( userStoreEntity == null )
-        {
-            throw new SystemException( "Userstore [{0}] not found", user.getKey().getUserStore() );
-        }
-        final String userStoreKey = userStoreEntity.getKey().toString();
-        final UserStoreEntity userStore = new UserStoreParser( userStoreDao ).parseUserStore( userStoreKey );
-
-        final StoreNewUserCommand storeNewUserCommand = new StoreNewUserCommand();
-        storeNewUserCommand.setUsername( user.getKey().getLocalName() );
-        storeNewUserCommand.setEmail( user.getEmail() );
-        final UserFields userFields = new UserFields();
-
-        if ( user.getDisplayName() == null )
-        {
-            storeNewUserCommand.setDisplayName( user.getKey().getLocalName() );
-        }
-        else
-        {
-            storeNewUserCommand.setDisplayName( user.getDisplayName() );
-        }
-        storeNewUserCommand.setPassword( "" );
-        storeNewUserCommand.setUserFields( userFields );
-
-        storeNewUserCommand.setType( UserType.NORMAL );
-        storeNewUserCommand.setUserStoreKey( userStore.getKey() );
-        storeNewUserCommand.setStorer( storer.getKey() );
-        storeNewUserCommand.setAllowAnyUserAccess( false );
-        final UserProfile profile = user.getProfile();
-        if ( profile != null )
-        {
-            storeNewUserCommand.setUserFields( userProfileTransformer.userProfileToUserFields( profile ) );
-        }
-
-        userStoreService.storeNewUser( storeNewUserCommand );
-    }
-
-    private void createGroup( final GroupAccount group )
-    {
-        final UserStoreEntity userStoreEntity = userStoreDao.findByName( group.getKey().getUserStore() );
-        if ( userStoreEntity == null )
-        {
-            throw new SystemException( "Userstore [{0}] not found", group.getKey().getUserStore() );
-        }
-        final String userStoreKey = userStoreEntity.getKey().toString();
-        final UserStoreEntity userStore = new UserStoreParser( userStoreDao ).parseUserStore( userStoreKey );
-
-//        final UserEntity runningUser = securityService.getImpersonatedPortalUser();
-        final UserEntity runningUser = userDao.findBuiltInEnterpriseAdminUser(); // TODO get logged in user
-
-        StoreNewGroupCommand storeNewGroupCommand = new StoreNewGroupCommand();
-        storeNewGroupCommand.setName( group.getKey().getLocalName() );
-        storeNewGroupCommand.setRestriced( true );
-        storeNewGroupCommand.setExecutor( runningUser );
-        storeNewGroupCommand.setDescription( group.getDisplayName() );
-        storeNewGroupCommand.setUserStoreKey( userStore.getKey() );
-        storeNewGroupCommand.setType( GroupType.USERSTORE_GROUP );
-        storeNewGroupCommand.setRespondWithException( false );
-
-        final AccountKeys members = group.getMembers();
-        if ( members != null )
-        {
-            for ( AccountKey member : members )
-            {
-                final GroupKey groupKey = memberAccountToGroupEntity( member );
-                if ( groupKey != null )
-                {
-                    storeNewGroupCommand.addMember( groupKey );
-                }
-            }
-        }
-
-        userStoreService.storeNewGroup( storeNewGroupCommand );
-    }
-
-    private GroupKey memberAccountToGroupEntity( final AccountKey accountKey )
-    {
-        if ( accountKey.isUser() )
-        {
-            return userAccountToGroupEntity( accountKey );
-        }
-        else
-        {
-            return groupAccountToGroupEntity( accountKey );
-        }
-    }
-
-    private GroupKey userAccountToGroupEntity( final AccountKey accountKey )
-    {
-        final UserStoreEntity userStoreEntity = userStoreDao.findByName( accountKey.getUserStore() );
-        if ( userStoreEntity == null )
-        {
-            return null;
-        }
-
-        final QualifiedUsername qualifiedUsername = new QualifiedUsername( userStoreEntity.getName(), accountKey.getLocalName() );
-        final UserEntity user = userDao.findByQualifiedUsername( qualifiedUsername );
-        return user == null ? null : user.getUserGroup().getGroupKey();
-    }
-
-    private GroupKey groupAccountToGroupEntity( final AccountKey accountKey )
-    {
-        final UserStoreEntity userStoreEntity = userStoreDao.findByName( accountKey.getUserStore() );
-        if ( userStoreEntity == null )
-        {
-            return null;
-        }
-
-        final UserStoreKey userStoreKey = userStoreEntity.getKey();
-        final List<GroupEntity> group = groupDao.findByUserStoreKeyAndGroupname( userStoreKey, accountKey.getLocalName(), false );
-        return group.isEmpty() ? null : group.get( 0 ).getGroupKey();
-    }
-
-    @Autowired
-    public void setUserStoreDao( final UserStoreDao userStoreDao )
-    {
-        this.userStoreDao = userStoreDao;
-    }
-
-    @Autowired
-    public void setSecurityService( final SecurityService securityService )
-    {
-        this.securityService = securityService;
-    }
-
-    @Autowired
-    public void setUserStoreService( final UserStoreService userStoreService )
-    {
-        this.userStoreService = userStoreService;
-    }
-
-    @Autowired
-    public void setGroupDao( final GroupDao groupDao )
-    {
-        this.groupDao = groupDao;
-    }
-
-    @Autowired
-    public void setUserDao( final UserDao userDao )
-    {
-        this.userDao = userDao;
     }
 
     @Autowired
     public void setSearchService( final AccountSearchService searchService )
     {
         this.searchService = searchService;
+    }
+
+    @Autowired
+    public void setAccountDao( final AccountDao accountDao )
+    {
+        this.accountDao = accountDao;
     }
 }
--- modules/wem-core/src/test/java/com/enonic/wem/core/account/CreateAccountHandlerTest.java
+++ modules/wem-core/src/test/java/com/enonic/wem/core/account/CreateAccountHandlerTest.java
@@ -1,23 +1,18 @@
 package com.enonic.wem.core.account;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Locale;
-import java.util.Set;
 import java.util.TimeZone;
+
+import javax.jcr.Session;
 
 import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Matchers;
 import org.mockito.Mockito;
 
-import com.google.common.collect.Sets;
-
 import com.enonic.wem.api.account.AccountKey;
-import com.enonic.wem.api.account.AccountKeys;
 import com.enonic.wem.api.account.GroupAccount;
+import com.enonic.wem.api.account.RoleAccount;
 import com.enonic.wem.api.account.UserAccount;
 import com.enonic.wem.api.account.profile.Address;
 import com.enonic.wem.api.account.profile.Addresses;
@@ -25,25 +20,9 @@ import com.enonic.wem.api.account.profile.Gender;
 import com.enonic.wem.api.account.profile.UserProfile;
 import com.enonic.wem.api.command.Commands;
 import com.enonic.wem.api.command.account.CreateAccount;
+import com.enonic.wem.core.account.dao.AccountDao;
 import com.enonic.wem.core.command.AbstractCommandHandlerTest;
 import com.enonic.wem.core.search.account.AccountSearchService;
-
-import com.enonic.cms.core.security.SecurityService;
-import com.enonic.cms.core.security.group.GroupEntity;
-import com.enonic.cms.core.security.group.GroupKey;
-import com.enonic.cms.core.security.group.GroupType;
-import com.enonic.cms.core.security.group.StoreNewGroupCommand;
-import com.enonic.cms.core.security.user.QualifiedUsername;
-import com.enonic.cms.core.security.user.StoreNewUserCommand;
-import com.enonic.cms.core.security.user.UserEntity;
-import com.enonic.cms.core.security.user.UserKey;
-import com.enonic.cms.core.security.user.UserType;
-import com.enonic.cms.core.security.userstore.UserStoreEntity;
-import com.enonic.cms.core.security.userstore.UserStoreKey;
-import com.enonic.cms.core.security.userstore.UserStoreService;
-import com.enonic.cms.store.dao.GroupDao;
-import com.enonic.cms.store.dao.UserDao;
-import com.enonic.cms.store.dao.UserStoreDao;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.atLeastOnce;
@@ -52,18 +31,9 @@ import static org.mockito.Mockito.verify;
 public class CreateAccountHandlerTest
     extends AbstractCommandHandlerTest
 {
-    private UserDao userDao;
-
-    private GroupDao groupDao;
-
-    private UserStoreDao userStoreDao;
-
-    private SecurityService securityService;
-
-    private UserStoreService userStoreService;
+    private AccountDao accountDao;
 
     private CreateAccountHandler handler;
-
 
     @Before
     public void setUp()
@@ -71,30 +41,18 @@ public class CreateAccountHandlerTest
     {
         super.initialize();
 
-        userDao = Mockito.mock( UserDao.class );
-        groupDao = Mockito.mock( GroupDao.class );
-        userStoreDao = Mockito.mock( UserStoreDao.class );
-        securityService = Mockito.mock( SecurityService.class );
-        userStoreService = Mockito.mock( UserStoreService.class );
+        accountDao = Mockito.mock( AccountDao.class );
         final AccountSearchService accountSearchService = Mockito.mock( AccountSearchService.class );
 
         handler = new CreateAccountHandler();
-        handler.setUserDao( userDao );
-        handler.setGroupDao( groupDao );
-        handler.setSecurityService( securityService );
-        handler.setUserStoreService( userStoreService );
-        handler.setUserStoreDao( userStoreDao );
+        handler.setAccountDao( accountDao );
         handler.setSearchService( accountSearchService );
     }
-
 
     @Test
     public void testCreateUser()
         throws Exception
     {
-        // setup
-        logInAdminUser();
-
         final UserAccount user = UserAccount.create( "enonic:user1" );
         user.setEmail( "user1@enonic.com" );
         user.setDisplayName( "The User #1" );
@@ -141,7 +99,7 @@ public class CreateAccountHandlerTest
         final AccountKey createdUserKey = command.getResult();
 
         // verify
-        verify( userStoreService, atLeastOnce() ).storeNewUser( Matchers.<StoreNewUserCommand>any() );
+        verify( accountDao, atLeastOnce() ).createUser( Mockito.any( Session.class ), Mockito.eq( user ) );
         assertNotNull( createdUserKey );
         assertTrue( createdUserKey.isUser() );
         assertEquals( user.getKey(), createdUserKey );
@@ -152,11 +110,8 @@ public class CreateAccountHandlerTest
         throws Exception
     {
         // setup
-        logInAdminUser();
-        createUserStore( "enonic" );
-
         final GroupAccount group = GroupAccount.create( "enonic:group1" );
-        group.setDisplayName( "The User #1" );
+        group.setDisplayName( "The group #1" );
 
         // exercise
         CreateAccount command = Commands.account().create().account( group );
@@ -164,109 +119,22 @@ public class CreateAccountHandlerTest
         final AccountKey createdGroupKey = command.getResult();
 
         // verify
-        verify( userStoreService, atLeastOnce() ).storeNewGroup( Matchers.<StoreNewGroupCommand>any() );
+        verify( accountDao, atLeastOnce() ).createGroup( Mockito.any( Session.class ), Mockito.eq( group ) );
         assertNotNull( createdGroupKey );
         assertTrue( createdGroupKey.isGroup() );
         assertEquals( group.getKey(), createdGroupKey );
     }
 
-    @Test
-    public void testCreateGroupWithMembers()
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateRole()
         throws Exception
     {
         // setup
-        logInAdminUser();
-        createUser( "enonic", "user1" );
-        createGroup( "enonic", "group2" );
-
-        final AccountKeys members = AccountKeys.from( "user:enonic:user1", "group:enonic:group2" );
-        final GroupAccount group = GroupAccount.create( "enonic:group1" );
-        group.setDisplayName( "The User #1" );
-        group.setMembers( members );
+        final RoleAccount role = RoleAccount.create( "enonic:role1" );
+        role.setDisplayName( "The role #1" );
 
         // exercise
-        CreateAccount command = Commands.account().create().account( group );
+        CreateAccount command = Commands.account().create().account( role );
         this.handler.handle( this.context, command );
-        final AccountKey createdGroupKey = command.getResult();
-
-        // verify
-        verify( userStoreService, atLeastOnce() ).storeNewGroup( Matchers.<StoreNewGroupCommand>any() );
-        assertNotNull( createdGroupKey );
-        assertTrue( createdGroupKey.isGroup() );
-        assertEquals( group.getKey(), createdGroupKey );
-    }
-
-    private void logInAdminUser()
-        throws Exception
-    {
-        final UserEntity loggedInUser = createUser( "enonic", "admin" );
-        Mockito.when( securityService.getImpersonatedPortalUser() ).thenReturn( loggedInUser );
-        Mockito.when( userDao.findBuiltInEnterpriseAdminUser() ).thenReturn( loggedInUser );
-    }
-
-    private GroupEntity createGroup( final String userStore, final String name )
-        throws Exception
-    {
-        final UserStoreEntity userStoreEntity = createUserStore( userStore );
-        final GroupEntity group = Mockito.mock( GroupEntity.class, Mockito.CALLS_REAL_METHODS );
-        final GroupKey key = new GroupKey( Integer.toString( Math.abs( name.hashCode() ) ) );
-
-        group.setKey( key );
-        group.setType( GroupType.USERSTORE_GROUP );
-        group.setUserStore( userStoreEntity );
-        group.setName( name );
-        group.setDescription( "Group " + name );
-        group.setDeleted( false );
-        group.setMemberships( Sets.<GroupEntity>newHashSet() );
-
-        final Set<GroupEntity> memberSet = new HashSet<GroupEntity>();
-        group.setMembers( memberSet );
-
-        mockAddGroupToUserStore( userStoreEntity, group );
-        return group;
-    }
-
-    private void mockAddGroupToUserStore( final UserStoreEntity userStore, final GroupEntity group )
-    {
-        final List<GroupEntity> userStoreResults = new ArrayList<GroupEntity>();
-        userStoreResults.add( group );
-        Mockito.when( groupDao.findByUserStoreKeyAndGroupname( userStore.getKey(), group.getName(), false ) ).thenReturn(
-            userStoreResults );
-    }
-
-    private UserEntity createUser( final String userStore, final String name )
-        throws Exception
-    {
-        final UserEntity user = Mockito.mock( UserEntity.class, Mockito.CALLS_REAL_METHODS );
-        final UserKey key = new UserKey( Integer.toString( Math.abs( name.hashCode() ) ) );
-
-        user.setKey( key );
-        user.setType( UserType.NORMAL );
-        user.setEmail( "user@email.com" );
-        user.setUserStore( createUserStore( userStore ) );
-        user.setName( name );
-        user.setDisplayName( "User " + name );
-
-        final QualifiedUsername qualifiedName = user.getQualifiedName();
-        Mockito.when( user.getQualifiedName() ).thenReturn( qualifiedName );
-        Mockito.when( userDao.findByQualifiedUsername( Mockito.argThat( new IsQualifiedUsername( qualifiedName ) ) ) ).thenReturn( user );
-
-        final GroupEntity userGroup = createGroup( userStore, "G" + user.getKey().toString() );
-        Mockito.when( user.getUserGroup() ).thenReturn( userGroup );
-
-        return user;
-    }
-
-    private UserStoreEntity createUserStore( final String name )
-    {
-        final UserStoreEntity userStore = Mockito.mock( UserStoreEntity.class, Mockito.CALLS_REAL_METHODS );
-        userStore.setName( name );
-        final UserStoreKey userStoreKey = new UserStoreKey( Math.abs( name.hashCode() ) );
-        userStore.setKey( userStoreKey );
-
-        Mockito.when( userStoreDao.findByKey( userStoreKey ) ).thenReturn( userStore );
-        Mockito.when( userStoreDao.findByName( name ) ).thenReturn( userStore );
-
-        return userStore;
     }
 }
