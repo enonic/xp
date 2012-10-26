ae16c94492eee7be0fe8042a32f810e20ed570ed
Pavel Milkevich <pmi@enonic.com>
2012-10-26 18:21:06 +0300

    CMS-466 New UI: Research how to do pluggable tiles in start menu. Added log in/out support and paging

--- modules/wem-webapp/src/main/webapp/dev/examples/app/view/StartMenu.js
+++ modules/wem-webapp/src/main/webapp/dev/examples/app/view/StartMenu.js
@@ -9,19 +9,27 @@ Ext.define('Admin.view.StartMenu', {
     hidden: true,
     hideMode: 'display',
     shadow: false,
+    width: '100%',
+
+    currentPage: 1,
+    pageSize: 7,
 
     tpl: new Ext.XTemplate('<div class="top clearfix">' +
-                           '<div class="logout"><img src="../html-templates/images/profile-image.png"/><a href="#">Log out</a></div>' +
-                           '<img src="../html-templates/images/enonic-logo.png"/>' +
+                           '<img class="logo" src="../html-templates/images/enonic-logo.png"/>' +
                            '<div class="search"><input type="text"></div>' +
-                           '</div>' +
+                           '<div class="user">' +
+                           '<tpl if="loggedUser"><img src="{[values.loggedUser.img]}"/><a class="logout" href="#">Log out</a>' +
+                           '<tpl else><a class="login" href="#">Log in</a></tpl>' +
+                           '</div></div>' +
 
                            '<div class="center clearfix">' +
-                           '<tpl for="items">{[this.renderTile(values)]}</tpl>' +
+                           '<tpl for="items">{[this.renderTile(xindex, values, parent)]}</tpl>' +
                            '</div>' +
 
-                           '<div class="controls-container clearfix">' +
-                           '<ul class="controls"><li class="checked"></li><li></li><li></li><li></li></ul>' +
+                           '<div class="pager-container clearfix">' +
+                           '<ul class="pager"><tpl for="pages" >' +
+                           '<li num="{[xindex]}" class="{[xindex == parent.currentPage ? \"current\" : \"\"]}"></li>' +
+                           '</tpl></ul>' +
                            '</div>' +
 
                            '<ul class="bottom clearfix">' +
@@ -39,16 +47,68 @@ Ext.define('Admin.view.StartMenu', {
             compiled: true,
             disableFormats: true,
 
-            renderTile: function (tile) {
-                return this.tileTpl.applyTemplate(tile);
+            renderTile: function (num, tile, menu) {
+                var page = menu.currentPage;
+                var size = menu.pageSize;
+                var start = (page - 1) * size;
+                var end = start + size;
+                if (num > start && num <= end) {
+                    return this.tileTpl.applyTemplate(tile);
+                } else {
+                    return "";
+                }
             }
         }
     ),
 
     initComponent: function () {
-
         this.callParent(arguments);
+        this.addEvents('login', 'logout', 'pagechange');
+        this.refresh();
+    },
 
+    refresh: function () {
+        var i;
+        var count = Math.ceil(this.items.length / this.pageSize);
+        this.pages = [];
+        for (i = 0; i < count; i++) {
+            this.pages.push(i);
+        }
         this.update(this);
+        if (this.rendered) {
+            this.bindListeners(this);
+        } else {
+            this.on('afterrender', this.bindListeners);
+        }
+
+    },
+
+    bindListeners: function (me) {
+        var login = me.el.down('a.login');
+        if (login) {
+            login.on('click', function () {
+                me.fireEvent("login", me);
+                me.refresh();
+            });
+        }
+        var logout = me.el.down('a.logout');
+        if (logout) {
+            logout.on('click', function () {
+                me.fireEvent('logout', me);
+                me.refresh();
+            });
+        }
+        var pageLinks = me.el.query('.pager li');
+        Ext.Array.each(pageLinks, function (li) {
+            li = Ext.fly(li);
+            if (!li.hasCls('current')) {
+                li.on('click', function (event, target) {
+                    me.currentPage = parseInt(target.getAttribute("num"));
+                    me.refresh();
+                    me.fireEvent('pagechange', me, me.currentPage);
+                });
+            }
+        });
     }
+
 });
