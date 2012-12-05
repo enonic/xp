f751e278e9c67b54994403f09c267db454a4a856
Alex Rodriguez <aro@enonic.com>
2012-12-05 12:35:38 +0100

    CMS-689 Refactor content form creation to handle data not defined in content type

--- modules/wem-webapp/src/main/webapp/admin/resources/app/view/contentManager/wizard/ContentDataPanel.js
+++ modules/wem-webapp/src/main/webapp/admin/resources/app/view/contentManager/wizard/ContentDataPanel.js
@@ -19,6 +19,7 @@ Ext.define('Admin.view.contentManager.wizard.ContentDataPanel', {
             padding: '10px 15px',
             items: []
         };
+        var dataNamesAdded = {};
         // set values in fields if editing existing content
         var editedContentValues = {};
         if (this.content && this.content.data) {
@@ -27,36 +28,53 @@ Ext.define('Admin.view.contentManager.wizard.ContentDataPanel', {
             });
         }
 
+        // add component fields defined in content type
         Ext.each(this.contentType.form, function (contentTypeItem) {
             var widgetItemType = me.parseItemType(contentTypeItem);
-            var contentTypeItemData = me.parseItemTypeData(contentTypeItem);
+            var contentTypeInput = me.parseItemTypeData(contentTypeItem);
             if (!widgetItemType) {
                 console.log('Unsupported input type', contentTypeItem);
                 return;
             }
-            var item = Ext.create({
-                xclass: "widget." + widgetItemType,
-                fieldLabel: contentTypeItemData.label,
-                name: contentTypeItemData.name,
-                itemId: contentTypeItemData.name,
-                cls: 'span-3',
-                listeners: {
-                    render: function (cmp) {
-                        Ext.tip.QuickTipManager.register({
-                            target: cmp.el,
-                            text: contentTypeItemData.helpText
-                        });
-                    }
-                },
-                value: editedContentValues[contentTypeItemData.name]
-            });
-
-            fieldSet.items.push(item);
+            var currentValue = editedContentValues[contentTypeInput.name];
+            var inputComponent = me.createInputComponent(widgetItemType, contentTypeInput.name, contentTypeInput.label,
+                contentTypeInput.helpText, currentValue);
+            fieldSet.items.push(inputComponent);
+            dataNamesAdded[contentTypeInput.name] = true;
         });
+
+        // add component fields for data items not defined in content type
+        if (this.content) { // in edit mode only (not for new)
+            Ext.each(this.content.data, function (dataItem) {
+                if (!dataNamesAdded[dataItem.name]) {
+                    var inputComponent = me.createInputComponent('textfield', dataItem.name, dataItem.name, '', dataItem.value);
+                    fieldSet.items.push(inputComponent);
+                }
+            });
+        }
 
         this.items = [fieldSet];
 
         this.callParent(arguments);
+    },
+
+    createInputComponent: function (widgetItemType, inputName, label, helpText, dataValue) {
+        return Ext.create({
+            xclass: "widget." + widgetItemType,
+            fieldLabel: label,
+            name: inputName,
+            itemId: inputName,
+            cls: 'span-3',
+            listeners: {
+                render: function (cmp) {
+                    Ext.tip.QuickTipManager.register({
+                        target: cmp.el,
+                        text: helpText
+                    });
+                }
+            },
+            value: dataValue
+        });
     },
 
     parseItemType: function (contentItem) {
