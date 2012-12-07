122e2bfbc96dd2d8837a87449aa58575899f8eeb
Thomas Andersen <tan@enonic.com>
2012-12-07 11:56:51 +0100

    CMS-704 Content Data Form: create submit data and submit \n- implement in content manager app

--- modules/wem-webapp/src/main/webapp/admin/resources/app/view/contentManager/wizard/ContentDataPanel.js
+++ modules/wem-webapp/src/main/webapp/admin/resources/app/view/contentManager/wizard/ContentDataPanel.js
@@ -2,101 +2,78 @@ Ext.define('Admin.view.contentManager.wizard.ContentDataPanel', {
     extend: 'Ext.form.Panel',
     alias: 'widget.contentDataPanel',
 
-    typeMapping: {
-        TextLine: "textfield",
-        TextArea: "textarea"
+    requires: [
+        'Admin.lib.formitem.FormItemSet',
+        'Admin.lib.formitem.HtmlArea',
+        'Admin.lib.formitem.Relation',
+        'Admin.lib.formitem.TextArea',
+        'Admin.lib.formitem.TextLine'
+    ],
+
+    mixins: {
+        formHelper: 'Admin.lib.formitem.FormHelper'
     },
 
+    layout: 'vbox',
+
     contentType: undefined,
+
     content: null, // content to be edited
+
+    autoDestroy: true,
+
+    jsonSubmit: true,
 
     initComponent: function () {
         var me = this;
+        me.items = [];
 
-        var fieldSet = {
-            xtype: 'fieldset',
-            title: 'A Separator',
-            padding: '10px 15px',
-            items: []
-        };
-        var dataNamesAdded = {};
-        // set values in fields if editing existing content
-        var editedContentValues = {};
-        if (this.content && this.content.data) {
-            Ext.each(this.content.data, function (dataItem) {
-                editedContentValues[dataItem.name] = dataItem.value;
-            });
-        }
+        console.time('RenderForm');
+        me.mixins.formHelper.addFormItems(me.contentType.form, me);
+        console.timeEnd('RenderForm');
 
-        // add component fields defined in content type
-        Ext.each(this.contentType.form, function (contentTypeItem) {
-            var widgetItemType = me.parseItemType(contentTypeItem);
-            var contentTypeInput = me.parseItemTypeData(contentTypeItem);
-            if (!widgetItemType) {
-                console.log('Unsupported input type', contentTypeItem);
-                return;
-            }
-            var currentValue = editedContentValues[contentTypeInput.name];
-            var inputComponent = me.createInputComponent(widgetItemType, contentTypeInput.name, contentTypeInput.label,
-                contentTypeInput.helpText, currentValue);
-            fieldSet.items.push(inputComponent);
-            dataNamesAdded[contentTypeInput.name] = true;
-        });
-
-        // add component fields for data items not defined in content type
-        if (this.content) { // in edit mode only (not for new)
-            Ext.each(this.content.data, function (dataItem) {
-                if (!dataNamesAdded[dataItem.name]) {
-                    var inputComponent = me.createInputComponent('textfield', dataItem.name, dataItem.name, '', dataItem.value);
-                    fieldSet.items.push(inputComponent);
-                }
-            });
-        }
-
-        this.items = [fieldSet];
-
-        this.callParent(arguments);
+        me.callParent(arguments);
     },
 
-    createInputComponent: function (widgetItemType, inputName, label, helpText, dataValue) {
-        return Ext.create({
-            xclass: "widget." + widgetItemType,
-            fieldLabel: label,
-            name: inputName,
-            itemId: inputName,
-            cls: 'span-3',
-            listeners: {
-                render: function (cmp) {
-                    Ext.tip.QuickTipManager.register({
-                        target: cmp.el,
-                        text: helpText
-                    });
-                }
-            },
-            value: dataValue
-        });
-    },
-
-    parseItemType: function (contentItem) {
-        var contentItemData = this.parseItemTypeData(contentItem);
-        if (contentItemData.type) {
-            return this.typeMapping[contentItemData.type.name];
-        }
-        return null;
-    },
-
-    parseItemTypeData: function (contentItem) {
-        var baseType;
-        for (baseType in contentItem) {
-            if (contentItem.hasOwnProperty(baseType)) {
-                return contentItem[baseType];
-            }
-        }
-        return null;
-    },
 
     getData: function () {
-        return this.getForm().getFieldValues();
+        return this.createFormData();
+    },
+
+
+    createFormData: function () {
+        var me = this,
+            formData = {},
+            formItems = me.items.items;
+
+        Ext.Array.each(formItems, function (formItem) {
+            if (formItem.getXType() === 'FormItemSet') {
+                me.addFormItemSetData(formItem, formData);
+            } else {
+                //console.log(formItem.name + ': ' + formItem.getValue());
+                formData[formItem.name] = formItem.getValue();
+            }
+        });
+
+        return formData;
+    },
+
+
+    addFormItemSetData: function (formItemSet, formData) {
+        //TODO: Recursive
+        var blocks = Ext.ComponentQuery.query('container[formItemSetBlock=true]', formItemSet),
+            formItemSetName = formItemSet.name,
+            blockIndex;
+
+        Ext.Array.each(blocks, function (block, index) {
+            blockIndex = index;
+            Ext.Array.each(block.items.items, function (item) {
+                if (item.cls !== 'header') {
+                    formData[formItemSetName + '[' + blockIndex + '].' + item.name] = item.getValue();
+                    //console.log(name + '[' + blockIndex + '].' + item.name + ': ' + item.getValue());
+                }
+            });
+        });
     }
 
 });
