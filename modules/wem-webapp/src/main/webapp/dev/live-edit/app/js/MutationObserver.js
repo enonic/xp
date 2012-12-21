1de17d7cb7ceb6f5239dbb7af16e90c64fe3f52f
Thomas Andersen <tan@enonic.com>
2012-12-21 19:11:35 +0100

    EXPERIMENTAL: Mutation Observer: also resize highlighter when mouse is over

--- modules/wem-webapp/src/main/webapp/dev/live-edit/app/js/MutationObserver.js
+++ modules/wem-webapp/src/main/webapp/dev/live-edit/app/js/MutationObserver.js
@@ -18,39 +18,63 @@
     // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
     proto.bindGlobalEvents = function () {
-        $(window).on('component:select', $.proxy(this.observe, this));
-        $(window).on('component:deselect', $.proxy(this.disconnect, this));
+        var me = this;
+        $(window).on('component:mouseover', $.proxy(me.observe, me));
+        $(window).on('component:mouseout', $.proxy(me.disconnect, me));
+        $(window).on('component:select', $.proxy(me.observe, me));
+        $(window).on('component:deselect', $.proxy(me.disconnect, me));
     };
 
 
-    proto.observe = function (event, $selectedComponent) {
+    proto.observe = function (event, $component) {
         var me = this;
 
-        var isAlreadyObserved = me.$observedComponent && me.$observedComponent[0] === $selectedComponent[0];
+        var isAlreadyObserved = me.$observedComponent && me.$observedComponent[0] === $component[0];
         if (isAlreadyObserved) {
             return;
         }
-        me.$observedComponent = $selectedComponent;
+        me.$observedComponent = $component;
 
         me.mutationObserver = new AdminLiveEdit.MutationSummary({
             callback: function (summaries) {
-                if (summaries && summaries[0]) {
-                    $(window).trigger('component:select', [$(summaries[0].target)]);
-                }
+                console.log('mutation!', summaries);
+                me.onMutate(summaries, event);
             },
-            rootNode: $selectedComponent[0],
+            rootNode: $component[0],
             queries: [{ all: true}]
         });
     };
 
 
-    proto.disconnect = function () {
-        console.info('disconnect mutation observer');
-        this.$observedComponent = null;
-        if (this.mutationObserver) {
-            this.mutationObserver.disconnect();
+    proto.onMutate = function (summaries, event) {
+        if (summaries && summaries[0]) {
+            var $targetComponent = $(summaries[0].target);
+            var targetComponentIsSelected = $targetComponent.hasClass('live-edit-selected-component');
+            var isMouseOverEventAndTargetComponentIsNotSelected = event.type === 'component:mouseover' && !targetComponentIsSelected;
+
+            if (isMouseOverEventAndTargetComponentIsNotSelected) {
+                $(window).trigger('component:mouseover', [$targetComponent]);
+            } else {
+                $(window).trigger('component:select', [$targetComponent]);
+            }
         }
     };
 
+
+    proto.disconnect = function (event) {
+        var targetComponentIsSelected = (this.$observedComponent && this.$observedComponent.hasClass('live-edit-selected-component'));
+        var isMouseOutEventAndComponentIsSelected = event.type === 'component:mouseout' && targetComponentIsSelected;
+        if (isMouseOutEventAndComponentIsSelected) {
+            return;
+        }
+
+        console.log('Mutation Observer disconnect');
+
+        this.$observedComponent = null;
+        if (this.mutationObserver) {
+            this.mutationObserver.disconnect();
+            this.mutationObserver = null;
+        }
+    };
 
 }($liveedit));
