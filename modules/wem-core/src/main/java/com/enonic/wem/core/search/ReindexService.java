c2aaec1afd9473c8943f52344cc4c7ad60dba485
Runar Myklebust <rmy@enonic.com>
2013-02-01 13:42:14 +0100

    CMS-918 Elasticsearch initialization cleanup
    Fixed missing memberships in reindex

--- modules/wem-core/src/main/java/com/enonic/wem/core/search/ReindexService.java
+++ modules/wem-core/src/main/java/com/enonic/wem/core/search/ReindexService.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 
 import javax.jcr.Session;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -22,6 +24,7 @@ public class ReindexService
 
     private AccountDao accountDao;
 
+    private final static Logger LOG = LoggerFactory.getLogger( ReindexService.class );
 
     public void reindexAccounts()
         throws Exception
@@ -32,9 +35,23 @@ public class ReindexService
 
         for ( AccountKey accountKey : accountKeys )
         {
-            final Account account = accountDao.findAccount( accountKey, session );
 
-            System.out.println( "indexing account: " + account.getDisplayName() );
+            final Account account;
+
+            if ( accountKey.isUser() )
+            {
+                account = accountDao.findUser( accountKey, true, false, session );
+            }
+            else if ( accountKey.isGroup() )
+            {
+                account = accountDao.findGroup( accountKey, true, session );
+            }
+            else
+            {
+                return;
+            }
+
+            LOG.info( "Reindex account: " + account.getDisplayName() );
 
             indexService.index( account );
         }
