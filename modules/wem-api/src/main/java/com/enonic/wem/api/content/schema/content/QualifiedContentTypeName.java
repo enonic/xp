c9925818bf51b901498c07998eb75dcf7c1279fa
Lasse Jul-Larsen <ljl@enonic.com>
2013-03-01 14:34:02 +0100

    CMS-1098 Additional File Content Types

--- modules/wem-api/src/main/java/com/enonic/wem/api/content/schema/content/QualifiedContentTypeName.java
+++ modules/wem-api/src/main/java/com/enonic/wem/api/content/schema/content/QualifiedContentTypeName.java
@@ -22,6 +22,31 @@ public final class QualifiedContentTypeName
 
     private static final QualifiedContentTypeName FILE = new QualifiedContentTypeName( Module.SYSTEM.getName(), "file" );
 
+    private static final QualifiedContentTypeName FILE_TEXT = new QualifiedContentTypeName( Module.SYSTEM.getName(), "text" );
+
+    private static final QualifiedContentTypeName FILE_DATA = new QualifiedContentTypeName( Module.SYSTEM.getName(), "data" );
+
+    private static final QualifiedContentTypeName FILE_AUDIO = new QualifiedContentTypeName( Module.SYSTEM.getName(), "audio" );
+
+    private static final QualifiedContentTypeName FILE_VIDEO = new QualifiedContentTypeName( Module.SYSTEM.getName(), "video" );
+
+    private static final QualifiedContentTypeName FILE_IMAGE = new QualifiedContentTypeName( Module.SYSTEM.getName(), "image" );
+
+    private static final QualifiedContentTypeName FILE_VECTOR = new QualifiedContentTypeName( Module.SYSTEM.getName(), "vector" );
+
+    private static final QualifiedContentTypeName FILE_ARCHIVE = new QualifiedContentTypeName( Module.SYSTEM.getName(), "archive" );
+
+    private static final QualifiedContentTypeName FILE_DOCUMENT = new QualifiedContentTypeName( Module.SYSTEM.getName(), "document" );
+
+    private static final QualifiedContentTypeName FILE_SPREADSHEET = new QualifiedContentTypeName( Module.SYSTEM.getName(), "spreadsheet" );
+
+    private static final QualifiedContentTypeName FILE_PRESENTATION =
+        new QualifiedContentTypeName( Module.SYSTEM.getName(), "presentation" );
+
+    private static final QualifiedContentTypeName FILE_CODE = new QualifiedContentTypeName( Module.SYSTEM.getName(), "code" );
+
+    private static final QualifiedContentTypeName FILE_EXECUTABLE = new QualifiedContentTypeName( Module.SYSTEM.getName(), "executable" );
+
     public QualifiedContentTypeName( final String qualifiedName )
     {
         super( qualifiedName );
@@ -72,6 +97,66 @@ public final class QualifiedContentTypeName
         return FILE.equals( this );
     }
 
+    public boolean isTextFile()
+    {
+        return FILE_TEXT.equals( this );
+    }
+
+    public boolean isDataFile()
+    {
+        return FILE_DATA.equals( this );
+    }
+
+    public boolean isAudioFile()
+    {
+        return FILE_AUDIO.equals( this );
+    }
+
+    public boolean isVideoFile()
+    {
+        return FILE_VIDEO.equals( this );
+    }
+
+    public boolean isImageFile()
+    {
+        return FILE_IMAGE.equals( this );
+    }
+
+    public boolean isVectorFile()
+    {
+        return FILE_VECTOR.equals( this );
+    }
+
+    public boolean isArchiveFile()
+    {
+        return FILE_ARCHIVE.equals( this );
+    }
+
+    public boolean isDocumentFile()
+    {
+        return FILE_DOCUMENT.equals( this );
+    }
+
+    public boolean isSpreadsheetFile()
+    {
+        return FILE_SPREADSHEET.equals( this );
+    }
+
+    public boolean isPresentationFile()
+    {
+        return FILE_PRESENTATION.equals( this );
+    }
+
+    public boolean isCodeFile()
+    {
+        return FILE_CODE.equals( this );
+    }
+
+    public boolean isExecutableFile()
+    {
+        return FILE_EXECUTABLE.equals( this );
+    }
+
     public static QualifiedContentTypeName space()
     {
         return SPACE;
@@ -106,4 +191,65 @@ public final class QualifiedContentTypeName
     {
         return FILE;
     }
+
+    public static QualifiedContentTypeName textFile()
+    {
+        return FILE_TEXT;
+    }
+
+    public static QualifiedContentTypeName dataFile()
+    {
+        return FILE_DATA;
+    }
+
+    public static QualifiedContentTypeName audioFile()
+    {
+        return FILE_AUDIO;
+    }
+
+    public static QualifiedContentTypeName videoFile()
+    {
+        return FILE_VIDEO;
+    }
+
+    public static QualifiedContentTypeName imageFile()
+    {
+        return FILE_IMAGE;
+    }
+
+    public static QualifiedContentTypeName vectorFile()
+    {
+        return FILE_VECTOR;
+    }
+
+    public static QualifiedContentTypeName archiveFile()
+    {
+        return FILE_ARCHIVE;
+    }
+
+    public static QualifiedContentTypeName documentFile()
+    {
+        return FILE_DOCUMENT;
+    }
+
+    public static QualifiedContentTypeName spreadsheetFile()
+    {
+        return FILE_SPREADSHEET;
+    }
+
+    public static QualifiedContentTypeName presentationFile()
+    {
+        return FILE_PRESENTATION;
+    }
+
+    public static QualifiedContentTypeName codeFile()
+    {
+        return FILE_CODE;
+    }
+
+    public static QualifiedContentTypeName executableFile()
+    {
+        return FILE_EXECUTABLE;
+    }
+
 }
--- modules/wem-core/src/main/java/com/enonic/wem/core/content/schema/content/ContentTypesInitializer.java
+++ modules/wem-core/src/main/java/com/enonic/wem/core/content/schema/content/ContentTypesInitializer.java
@@ -41,9 +41,47 @@ public class ContentTypesInitializer
 
     static final ContentType SHORTCUT = createSystemType( QualifiedContentTypeName.shortcut(), true, false );
 
-    static final ContentType FILE = createSystemType( QualifiedContentTypeName.file(), true, false );
+    static final ContentType FILE = createSystemType( QualifiedContentTypeName.file(), false, false );
 
-    private static final ContentType[] SYSTEM_TYPES = {SPACE, STRUCTURED, UNSTRUCTURED, FOLDER, PAGE, SHORTCUT, FILE};
+    static final ContentType FILE_TEXT =
+        createSystemType( QualifiedContentTypeName.textFile(), QualifiedContentTypeName.file(), true, false );
+
+    static final ContentType FILE_DATA =
+        createSystemType( QualifiedContentTypeName.dataFile(), QualifiedContentTypeName.file(), true, false );
+
+    static final ContentType FILE_AUDIO =
+        createSystemType( QualifiedContentTypeName.audioFile(), QualifiedContentTypeName.file(), true, false );
+
+    static final ContentType FILE_VIDEO =
+        createSystemType( QualifiedContentTypeName.videoFile(), QualifiedContentTypeName.file(), true, false );
+
+    static final ContentType FILE_IMAGE =
+        createSystemType( QualifiedContentTypeName.imageFile(), QualifiedContentTypeName.file(), true, false );
+
+    static final ContentType FILE_VECTOR =
+        createSystemType( QualifiedContentTypeName.vectorFile(), QualifiedContentTypeName.file(), true, false );
+
+    static final ContentType FILE_ARCHIVE =
+        createSystemType( QualifiedContentTypeName.archiveFile(), QualifiedContentTypeName.file(), true, false );
+
+    static final ContentType FILE_DOCUMENT =
+        createSystemType( QualifiedContentTypeName.documentFile(), QualifiedContentTypeName.file(), true, false );
+
+    static final ContentType FILE_SPREADSHEET =
+        createSystemType( QualifiedContentTypeName.spreadsheetFile(), QualifiedContentTypeName.file(), true, false );
+
+    static final ContentType FILE_PRESENTATION =
+        createSystemType( QualifiedContentTypeName.presentationFile(), QualifiedContentTypeName.file(), true, false );
+
+    static final ContentType FILE_CODE =
+        createSystemType( QualifiedContentTypeName.codeFile(), QualifiedContentTypeName.file(), true, false );
+
+    static final ContentType FILE_EXECUTABLE =
+        createSystemType( QualifiedContentTypeName.executableFile(), QualifiedContentTypeName.file(), true, false );
+
+    private static final ContentType[] SYSTEM_TYPES =
+        {SPACE, STRUCTURED, UNSTRUCTURED, FOLDER, PAGE, SHORTCUT, FILE, FILE_TEXT, FILE_DATA, FILE_AUDIO, FILE_VIDEO, FILE_IMAGE,
+            FILE_VECTOR, FILE_ARCHIVE, FILE_DOCUMENT, FILE_SPREADSHEET, FILE_PRESENTATION, FILE_CODE, FILE_EXECUTABLE};
 
     private static final String[] TEST_CONTENT_TYPES =
         {"demo-contenttype-htmlarea.json", "demo-contenttype-fieldset.json", "demo-contenttype-set.json", "demo-contenttype-blog.json",
@@ -105,16 +143,26 @@ public class ContentTypesInitializer
     private static ContentType createSystemType( final QualifiedContentTypeName qualifiedName, final boolean isFinal,
                                                  final boolean isAbstract )
     {
+        return createSystemType( qualifiedName, null, isFinal, isAbstract );
+    }
+
+    private static ContentType createSystemType( final QualifiedContentTypeName qualifiedName, final QualifiedContentTypeName superType,
+                                                 final boolean isFinal, final boolean isAbstract )
+    {
         final String displayName = WordUtils.capitalize( qualifiedName.getContentTypeName() );
         final String contentTypeName = qualifiedName.getContentTypeName();
-        return newContentType().
-            module( Module.SYSTEM.getName() ).
-            name( contentTypeName ).
-            displayName( displayName ).
-            setFinal( isFinal ).
-            setAbstract( isAbstract ).
-            icon( loadContentTypeIcon( qualifiedName ) ).
-            build();
+        final ContentType.Builder builder = newContentType();
+        builder.module( Module.SYSTEM.getName() );
+        builder.name( contentTypeName );
+        builder.displayName( displayName );
+        if ( superType != null )
+        {
+            builder.superType( superType );
+        }
+        builder.setFinal( isFinal );
+        builder.setAbstract( isAbstract );
+        builder.icon( loadContentTypeIcon( qualifiedName ) );
+        return builder.build();
     }
 
     private static Icon loadContentTypeIcon( final QualifiedContentTypeName qualifiedName )
