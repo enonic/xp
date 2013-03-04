0713b29768058af7357b8af13915270ce0d9a0e2
Alex Rodriguez <aro@enonic.com>
2013-03-04 10:50:20 +0100

    CMS-1054 Content Manager Search - Fix facet names

--- modules/wem-webapp/src/main/webapp/admin/resources/app/view/contentManager/FilterPanel.js
+++ modules/wem-webapp/src/main/webapp/admin/resources/app/view/contentManager/FilterPanel.js
@@ -6,15 +6,15 @@ Ext.define('Admin.view.contentManager.FilterPanel', {
     updateFacetCount: 'query',
     facetData: [
         {
-            "name": 'Sites',
+            "name": 'Space',
             "terms": [
                 { "name": 'Travel', "key": 'travel', "count": 2 },
                 { "name": 'Blueman', "key": 'blueman', "count": 1 },
-                { "name": 'Cityscape', "key": 'cityscape', "count": 3 },
+                { "name": 'Cityscape', "key": 'cityscape', "count": 3 }
             ]
         },
         {
-            "name": "Types",
+            "name": "Type",
             "terms": [
                 { "name": "News", "key": "news", "count": 7 },
                 { "name": "Article", "key": "article", "count": 4 }
@@ -27,7 +27,7 @@ Ext.define('Admin.view.contentManager.FilterPanel', {
                 { "name": "< 1d", "key": "day", "count": 3 },
                 { "name": "< 1w", "key": "week", "count": 6 },
                 { "name": "> [from date]", "key": "from", "count": 2 },
-                { "name": "< [to date]", "key": "to", "count": 0 },
+                { "name": "< [to date]", "key": "to", "count": 0 }
             ]
         }
     ]
