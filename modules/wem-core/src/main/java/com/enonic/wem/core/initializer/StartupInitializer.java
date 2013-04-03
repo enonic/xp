473f084ca6075c842bf8bc83f03e8e5b7b0b3fa4
Alex Rodriguez <aro@enonic.com>
2013-04-03 13:17:22 +0200

    Simplified sorting of InitializerTasks.

--- modules/wem-core/src/main/java/com/enonic/wem/core/initializer/StartupInitializer.java
+++ modules/wem-core/src/main/java/com/enonic/wem/core/initializer/StartupInitializer.java
@@ -1,13 +1,12 @@
 package com.enonic.wem.core.initializer;
 
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 
 import javax.inject.Inject;
 
-import org.springframework.core.annotation.Order;
+import org.springframework.core.annotation.AnnotationAwareOrderComparator;
 import org.springframework.stereotype.Component;
 
 import com.google.common.collect.Lists;
@@ -33,16 +32,7 @@ public final class StartupInitializer
     public void setTasks( final Set<InitializerTask> tasks )
     {
         final List<InitializerTask> sortedTaskList = Lists.newArrayList( tasks );
-        Collections.sort( sortedTaskList, new Comparator<InitializerTask>()
-        {
-            @Override
-            public int compare( final InitializerTask it1, final InitializerTask it2 )
-            {
-                final Order order1 = it1.getClass().getAnnotation( Order.class );
-                final Order order2 = it2.getClass().getAnnotation( Order.class );
-                return order1.value() - order2.value();
-            }
-        } );
+        Collections.sort( sortedTaskList, new AnnotationAwareOrderComparator() );
         this.tasks = sortedTaskList;
     }
 
