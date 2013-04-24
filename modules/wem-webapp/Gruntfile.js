7b18d64b6fca6eab94af66bc369ef5a05b874c4a
Lasse Jul-Larsen <ljl@enonic.com>
2013-04-24 14:14:35 +0200

    Fixed jslint errors

--- modules/wem-webapp/Gruntfile.js
+++ modules/wem-webapp/Gruntfile.js
@@ -1,39 +1,39 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-typescript');
     grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
     grunt.initConfig({
 
-		typescript: {
-        	api: {
-            	src: ['src/main/webapp/admin2/api/js/main.ts'],
-            	dest: 'src/main/webapp/admin2/api/js/all.js',
-            	options: {
-              		target: 'es5',
-				  	sourcemap: true,
-				  	declaration: true 
-            	}
-          	},
-        	space_manager: {
-            	src: ['src/main/webapp/admin2/apps/space-manager/js/main.ts'],
-            	dest: 'src/main/webapp/admin2/apps/space-manager/js/all.js',
-            	options: {
-              		target: 'es5',
-				  	sourcemap: true 
-            	}
-          	}
-			
-     	},
+        typescript: {
+            api: {
+                src: ['src/main/webapp/admin2/api/js/main.ts'],
+                dest: 'src/main/webapp/admin2/api/js/all.js',
+                options: {
+                    target: 'es5',
+                    sourcemap: true,
+                    declaration: true
+                }
+            },
+            space_manager: {
+                src: ['src/main/webapp/admin2/apps/space-manager/js/main.ts'],
+                dest: 'src/main/webapp/admin2/apps/space-manager/js/all.js',
+                options: {
+                    target: 'es5',
+                    sourcemap: true
+                }
+            }
 
-      	watch: {
-        	files: 'src/main/webapp/admin2/**/*.ts',
+        },
+
+        watch: {
+            files: 'src/main/webapp/admin2/**/*.ts',
             tasks: ['typescript']
-     	}
+        }
 
     });
-	
-	grunt.registerTask('default', 'watch');
-	
+
+    grunt.registerTask('default', 'watch');
+
 };
