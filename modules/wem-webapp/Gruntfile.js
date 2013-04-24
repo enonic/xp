ac84792b1abc8fb7d240ae8a90c73149d8bd1259
Sten Roger Sandvik <srs@enonic.com>
2013-04-24 13:39:32 +0200

    Added initial typescript build system based on grunt.

---
+++ modules/wem-webapp/Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	
+    grunt.initConfig({
+
+		typescript: {
+        	/*api: {
+            	src: ['src/api/Main.ts'],
+            	dest: 'target/api.js',
+            	options: {
+              		target: 'es5',
+				  	sourcemap: true,
+				  	declaration: true 
+            	}
+          	},*/
+        	space_manager: {
+            	src: ['src/main/webapp/admin2/apps/space-manager/js/main.ts'],
+            	dest: 'src/main/webapp/admin2/apps/space-manager/js/all.js',
+            	options: {
+              		target: 'es5',
+				  	sourcemap: true 
+            	}
+          	}
+			
+     	},
+
+      	watch: {
+        	files: 'src/main/webapp/admin2/**/*.ts',
+            tasks: ['typescript']
+     	}
+
+    });
+	
+	grunt.registerTask('default', 'watch');
+	
+};
