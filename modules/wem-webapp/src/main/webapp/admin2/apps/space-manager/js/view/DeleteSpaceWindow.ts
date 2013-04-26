e8464ff53bfcdff5c287cad020937afae794b8b3
Lasse Jul-Larsen <ljl@enonic.com>
2013-04-26 14:53:08 +0200

    CMS-1484 TypeScript: Convert DeleteSpacesDialog to TypeScript

--- modules/wem-webapp/src/main/webapp/admin2/apps/space-manager/js/view/DeleteSpaceWindow.ts
+++ modules/wem-webapp/src/main/webapp/admin2/apps/space-manager/js/view/DeleteSpaceWindow.ts
@@ -23,76 +23,109 @@ module admin.ui {
                 this.container.hide();
                 //TODO: Fire event
             };
-            this.container = Ext.create('Ext.container.Container', {
-                    border: false,
-                    floating: true,
-                    shadow: false,
-                    width: 500,
-                    modal: true,
-                    autoHeight: true,
-                    maxHeight: 600,
-                    cls: 'admin-window',
-                    closeAction: 'hide',
-                    padding: 20,
-                    items: <any[]>[
-                        {
-                            region: 'north',
-                            xtype: 'component',
-                            tpl: '<h2>{title}</h2><tpl if="subtitle != undefined"><p>{subtitle}</p></tpl>',
-                            data: {
-                                title: me.title
-                            },
-                            margin: '0 0 20 0'
-                        },
-                        {
-                            region: 'center',
-                            layout: {
-                                type: 'vbox',
-                                align: 'stretch'
-                            },
-                            border: false,
-                            items: {
-                                itemId: 'modalDialog',
-                                cls: 'dialog-info',
-                                xtype: 'component',
-                                border: false,
-                                height: 150,
-                                styleHtmlContent: true,
-                                tpl: me.template
-                            }
-                        },
-                        {
-                            region: 'south',
-                            margin: '20 0 0 0',
-                            border: false,
-                            layout: {
-                                type: 'hbox',
-                                pack: 'end'
-                            },
-                            defaults: {
-                                xtype: 'button',
-                                margin: '0 0 0 10'
-                            },
-                            items: <any[]>[
-                                {
-                                    text: 'Delete',
-                                    handler: (btn, evt) => {
-                                        btn.disable();
-                                        this.deleteHandler.doDelete(this.data, deleteCallback);
-                                    }
-                                },
-                                {
-                                    text: 'Cancel',
-                                    handler: function (btn, evt) {
-                                        me.container.hide();
-                                    }
-                                }
-                            ]
-                        }
-                    ]
-                }
 
-            );
+            var c = new Ext.container.Container();
+            c.border = false;
+            c.floating = true;
+            c.shadow = false;
+            c.width = 500;
+            c.modal = true;
+            c.autoHeight = true;
+            c.maxHeight = 600;
+            c.cls = 'admin-window';
+            c.padding = 20;
+
+            var header = new Ext.Component();
+            header.region = 'north';
+            header.tpl = '<h2>{title}</h2><tpl if="subtitle != undefined"><p>{subtitle}</p></tpl>';
+            header.data = { title: this.title };
+            header.margin = '0 0 20 0';
+
+            c.add(header);
+
+            var content = new Ext.Component();
+            content.itemId = 'modalDialog';
+            content.cls = 'dialog-info';
+            content.border = false;
+            content.height = 150;
+            content.styleHtmlContent = true;
+            content.tpl = this.template;
+
+            c.add(content);
+
+
+            this.container = c;
+
+            /*this.container = Ext.create('Ext.container.Container', {
+             border: false,
+             floating: true,
+             shadow: false,
+             width: 500,
+             modal: true,
+             autoHeight: true,
+             maxHeight: 600,
+             cls: 'admin-window',
+             closeAction: 'hide',
+             padding: 20,
+             items: <any[]>[
+             {
+             region: 'north',
+             xtype: 'component',
+             tpl: '<h2>{title}</h2><tpl if="subtitle != undefined"><p>{subtitle}</p></tpl>',
+             data: {
+             title: me.title
+             },
+             margin: '0 0 20 0'
+             },
+             {
+             region: 'center',
+             layout: {
+             type: 'vbox',
+             align: 'stretch'
+             },
+             border: false,
+             items: {
+             itemId: 'modalDialog',
+             cls: 'dialog-info',
+             xtype: 'component',
+             border: false,
+             height: 150,
+             styleHtmlContent: true,
+             tpl: me.template
+             }
+             },
+             {
+             region: 'south',
+             margin: '20 0 0 0',
+             border: false,
+             layout: {
+             type: 'hbox',
+             pack: 'end'
+             },
+             defaults: {
+             xtype: 'button',
+             margin: '0 0 0 10'
+             },
+             items: <any[]>[
+             {
+             text: 'Delete',
+             handler: (btn, evt) => {
+             btn.disable();
+             this.deleteHandler.doDelete(this.data, deleteCallback);
+             }
+             },
+             {
+             text: 'Cancel',
+             handler: function (btn, evt) {
+             me.container.hide();
+             }
+             }
+             ]
+             }
+             ]
+             }
+
+             );*/
         }
 
     ;
