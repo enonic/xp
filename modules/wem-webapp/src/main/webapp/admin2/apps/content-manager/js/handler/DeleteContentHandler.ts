8be7c87789337e66e6f4fa88c2cafef106d75d83
Thomas Andersen <tan@enonic.com>
2013-04-26 18:30:26 +0200

    CMS-1485: TypeScript: Convert DeleteContentsDialog to TypeScript - Prepare DeleteContentHandler

--- modules/wem-webapp/src/main/webapp/admin2/apps/content-manager/js/handler/DeleteContentHandler.ts
+++ modules/wem-webapp/src/main/webapp/admin2/apps/content-manager/js/handler/DeleteContentHandler.ts
@@ -1,23 +1,10 @@
 module admin.app.handler {
     export class DeleteContentHandler {
-        doDelete(spaces, callback:(obj, success, result) => void) {
+        doDelete(contentModels, callback:(obj, success, result) => void) {
             var me = this;
-            var spaceNames = Ext.Array.map([].concat(spaces), function (item) {
-                return item.get('name');
+            var contentKeys = Ext.Array.map([].concat(contentModels), function (item) {
+                return item.get('key');
             });
-
-            /*
-            ToDo: implement remote content_delete
-            Admin.lib.RemoteService.space_delete({
-                "spaceName": spaceNames
-            }, function (r) {
-                if (r) {
-                    callback(me, r.success, r);
-                } else {
-                    Ext.Msg.alert("Error", r ? r.error : "Unable to delete space.");
-                }
-            });
-            */
         }
     }
 }
