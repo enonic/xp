2218310e99478284fae41836c8b0b9cfbea8009b
Thomas Andersen <tan@enonic.com>
2013-04-30 10:30:34 +0200

    TypeScript: refactoring delete handlers  - expanding variable names and using arrow functions

--- modules/wem-webapp/src/main/webapp/admin2/apps/content-manager/js/handler/DeleteContentHandler.ts
+++ modules/wem-webapp/src/main/webapp/admin2/apps/content-manager/js/handler/DeleteContentHandler.ts
@@ -1,10 +1,21 @@
 module admin.app.handler {
+
     export class DeleteContentHandler {
-        doDelete(contentModels, callback:(obj, success, result) => void) {
-            var me = this;
-            var contentKeys = Ext.Array.map([].concat(contentModels), function (item) {
-                return item.get('key');
+
+        doDelete(contentModels, callback:(thisArg, success, result) => void) {
+            var contentPaths = Ext.Array.map([].concat(contentModels), function (item) {
+                return item.get('path');
             });
+
+            Admin.lib.RemoteService.content_delete({'contentPaths': contentPaths }, (response) => {
+                if (response) {
+                    callback.call(this, response.success, response.failures);
+                } else {
+                    Ext.Msg.alert('Error', response ? response.error : 'Internal error occured.');
+                }
+            });
+
         }
+
     }
 }
--- modules/wem-webapp/src/main/webapp/admin2/apps/space-manager/js/handler/DeleteSpacesHandler.ts
+++ modules/wem-webapp/src/main/webapp/admin2/apps/space-manager/js/handler/DeleteSpacesHandler.ts
@@ -1,20 +1,20 @@
 module admin.app.handler {
+
     export class DeleteSpacesHandler {
-        doDelete(spaces, callback:(obj, success, result) => void) {
-            var me = this;
-            var spaceNames = Ext.Array.map([].concat(spaces), function (item) {
+
+        doDelete(spaceModels, callback:(thisArg, success, result) => void) {
+            var spaceNames = Ext.Array.map([].concat(spaceModels), (item) => {
                 if (!item) {
                     console.error('No spaces selected');
                 }
                 return item.get('name');
             });
-            Admin.lib.RemoteService.space_delete({
-                "spaceName": spaceNames
-            }, function (r) {
-                if (r) {
-                    callback(me, r.success, r);
+
+            Admin.lib.RemoteService.space_delete({ 'spaceName': spaceNames}, function (response) {
+                if (response) {
+                    callback.call(this, response.success, response);
                 } else {
-                    Ext.Msg.alert("Error", r ? r.error : "Unable to delete space.");
+                    Ext.Msg.alert('Error', response ? response.error : 'Unable to delete space.');
                 }
             });
         }
