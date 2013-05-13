bf14b8002592fbbfcc586be628e987adbd100620
Lasse Jul-Larsen <ljl@enonic.com>
2013-05-13 10:53:00 +0200

    Deleted BaseFilterPanel and BaseTreeGridPanel

--- modules/wem-webapp/src/main/webapp/admin2/apps/space-manager/js/main.ts
+++ modules/wem-webapp/src/main/webapp/admin2/apps/space-manager/js/main.ts
@@ -27,7 +27,6 @@
 
 ///<reference path='view/DeleteSpaceWindow.ts' />
 
-///<reference path='view/BaseTreeGridPanel.ts' />
 ///<reference path='view/TreeGridPanel.ts' />
 
 ///<reference path='view/ContextMenu.ts' />
@@ -41,7 +40,6 @@
 ///<reference path='view/TopBarMenu.ts' />
 ///<reference path='view/TopBar.ts' />
 ///<reference path='view/TabPanel.ts' />
-///<reference path='view/BaseFilterPanel.ts' />
 ///<reference path='view/FilterPanel.ts' />
 ///<reference path='view/BrowseToolbar.ts' />
 
