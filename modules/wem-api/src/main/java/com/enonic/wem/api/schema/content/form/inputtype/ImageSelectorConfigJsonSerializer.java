ba89e0ead0ec2c0ecc52bd021a8da2e5205cb75d
Aliaksei Biaskosty <abiaskosty@scnsoft.com>
2013-05-28 15:05:09 +0300

    CMS-1493 Content Manager - Image input type

--- modules/wem-api/src/main/java/com/enonic/wem/api/schema/content/form/inputtype/ImageSelectorConfigJsonSerializer.java
+++ modules/wem-api/src/main/java/com/enonic/wem/api/schema/content/form/inputtype/ImageSelectorConfigJsonSerializer.java
@@ -7,16 +7,16 @@ import org.codehaus.jackson.node.ObjectNode;
 
 import com.enonic.wem.api.schema.relationship.QualifiedRelationshipTypeName;
 
-import static com.enonic.wem.api.schema.content.form.inputtype.ImageConfig.newImageConfig;
+import static com.enonic.wem.api.schema.content.form.inputtype.ImageSelectorConfig.newImageSelectorConfig;
 
 
-public class ImageConfigJsonSerializer
-    extends AbstractInputTypeConfigJsonSerializer<ImageConfig>
+public class ImageSelectorConfigJsonSerializer
+    extends AbstractInputTypeConfigJsonSerializer<ImageSelectorConfig>
 {
-    public static final ImageConfigJsonSerializer DEFAULT = new ImageConfigJsonSerializer();
+    public static final ImageSelectorConfigJsonSerializer DEFAULT = new ImageSelectorConfigJsonSerializer();
 
     @Override
-    public JsonNode serializeConfig( final ImageConfig config, final ObjectMapper objectMapper )
+    public JsonNode serializeConfig( final ImageSelectorConfig config, final ObjectMapper objectMapper )
     {
         final ObjectNode jsonConfig = objectMapper.createObjectNode();
         if ( config.getRelationshipType() != null )
@@ -31,9 +31,9 @@ public class ImageConfigJsonSerializer
     }
 
     @Override
-    public ImageConfig parseConfig( final JsonNode inputTypeConfigNode )
+    public ImageSelectorConfig parseConfig( final JsonNode inputTypeConfigNode )
     {
-        final ImageConfig.Builder builder = newImageConfig();
+        final ImageSelectorConfig.Builder builder = newImageSelectorConfig();
         final JsonNode relationshipTypeNode = inputTypeConfigNode.get( "relationshipType" );
         if ( relationshipTypeNode != null && !relationshipTypeNode.isNull() )
         {
