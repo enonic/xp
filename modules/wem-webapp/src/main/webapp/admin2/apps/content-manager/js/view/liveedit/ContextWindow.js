91a3e91fa39201caeaf7aa59428ef4c37ee9d92a
Thomas Andersen <tan@enonic.com>
2013-05-31 16:37:32 +0200

    CMS-1577 Live Edit:Context window - implement basic framework prototype - Window frame - Moved to dev folder for better development workflow

--- modules/wem-webapp/src/main/webapp/admin2/apps/content-manager/js/view/liveedit/ContextWindow.js
+++ modules/wem-webapp/src/main/webapp/admin2/apps/content-manager/js/view/liveedit/ContextWindow.js
@@ -1,32 +1,73 @@
 Ext.define('Admin.view.contentManager.liveedit.ContextWindow', {
     extend: 'Ext.container.Container',
     alias: 'widget.liveEditContextWindow',
-
+    modal: false,
+    width: 320,
+    height: 518,
     border: false,
     floating: true,
     shadow: false,
-    width: 500,
-    modal: true,
-    autoHeight: true,
-    maxHeight: 600,
-    cls: 'admin-window',
-    closeAction: 'hide',
-    padding: 20,
+    draggable: true,
+    constrain: true,
+    cls: 'admin-context-window',
 
     initComponent: function () {
-
         var me = this;
+        me.items = [
+            me.createTitleBar()
+        ];
 
-
-        if (!this.items) {
-            this.items = [];
-        }
-
+        this.enableDrag();
         this.callParent(arguments);
     },
 
+    createTitleBar: function () {
+        var me = this;
+        return Ext.create('Ext.container.Container', {
+            cls: 'admin-context-window-title-bar',
+            layout: {
+                type: 'hbox',
+                align: 'stretch'
+            },
+            items: [
+                me.createMenuButton(),
+                me.createTitleText(),
+                me.createToggle()
+            ]
+        });
+    },
 
-    doShow: function (model) {
+    createMenuButton: function () {
+        return Ext.create('Ext.Component', {
+            cls: 'admin-context-window-menu icon-reorder',
+            width: 30
+        });
+    },
+
+    createTitleText: function () {
+        return Ext.create('Ext.Component', {
+            cls: 'admin-context-window-title-text',
+            html: 'Title',
+            flex: 3
+        });
+    },
+
+    createToggle: function () {
+        return Ext.create('Ext.Component', {
+            cls: 'admin-context-window-toggle icon-chevron-down',
+            width: 30
+        });
+    },
+
+    enableDrag: function () {
+        var me = this;
+        this.draggable = {
+            delegate: '.admin-context-window-title-text'
+        };
+        this.constrain = true;
+    },
+
+    doShow: function () {
         this.show();
     },
 
