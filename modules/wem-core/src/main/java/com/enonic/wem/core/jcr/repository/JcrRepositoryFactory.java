c8c0e7d5840e5fe16e35d6e7e913b3d407eb1bcd
Alex Rodriguez <aro@enonic.com>
2013-06-04 15:04:56 +0200

    CMS-1599 Upgrade to OAK 0.7

--- modules/wem-core/src/main/java/com/enonic/wem/core/jcr/repository/JcrRepositoryFactory.java
+++ modules/wem-core/src/main/java/com/enonic/wem/core/jcr/repository/JcrRepositoryFactory.java
@@ -6,18 +6,20 @@ import javax.jcr.Repository;
 import org.apache.jackrabbit.mk.api.MicroKernel;
 import org.apache.jackrabbit.oak.Oak;
 import org.apache.jackrabbit.oak.jcr.RepositoryImpl;
-import org.apache.jackrabbit.oak.plugins.commit.AnnotatingConflictHandler;
 import org.apache.jackrabbit.oak.plugins.commit.ConflictValidatorProvider;
-import org.apache.jackrabbit.oak.plugins.index.CompositeIndexHookProvider;
-import org.apache.jackrabbit.oak.plugins.index.IndexHookManager;
-import org.apache.jackrabbit.oak.plugins.index.p2.Property2IndexHookProvider;
-import org.apache.jackrabbit.oak.plugins.index.p2.Property2IndexProvider;
+import org.apache.jackrabbit.oak.plugins.commit.JcrConflictHandler;
+import org.apache.jackrabbit.oak.plugins.index.nodetype.NodeTypeIndexProvider;
+import org.apache.jackrabbit.oak.plugins.index.property.PropertyIndexEditorProvider;
+import org.apache.jackrabbit.oak.plugins.index.property.PropertyIndexProvider;
 import org.apache.jackrabbit.oak.plugins.name.NameValidatorProvider;
 import org.apache.jackrabbit.oak.plugins.name.NamespaceValidatorProvider;
-import org.apache.jackrabbit.oak.plugins.nodetype.DefaultTypeEditor;
-import org.apache.jackrabbit.oak.plugins.nodetype.RegistrationValidatorProvider;
-import org.apache.jackrabbit.oak.plugins.nodetype.TypeValidatorProvider;
+import org.apache.jackrabbit.oak.plugins.nodetype.RegistrationEditorProvider;
+import org.apache.jackrabbit.oak.plugins.nodetype.TypeEditorProvider;
 import org.apache.jackrabbit.oak.plugins.nodetype.write.InitialContent;
+import org.apache.jackrabbit.oak.plugins.version.VersionEditorProvider;
+import org.apache.jackrabbit.oak.security.SecurityProviderImpl;
+import org.apache.jackrabbit.oak.spi.commit.EditorHook;
+import org.apache.jackrabbit.oak.spi.security.SecurityProvider;
 
 import com.google.inject.Provider;
 
@@ -41,18 +43,22 @@ public final class JcrRepositoryFactory
     public void afterPropertiesSet()
         throws Exception
     {
+        final SecurityProvider securityProvider = new SecurityProviderImpl();
         final Oak oak = new Oak( this.microKernel );
         oak.with( new InitialContent() );
-        oak.with( new DefaultTypeEditor() );
+        oak.with( JcrConflictHandler.JCR_CONFLICT_HANDLER );
+        oak.with( new EditorHook( new VersionEditorProvider() ) );
         oak.with( new NameValidatorProvider() );
         oak.with( new NamespaceValidatorProvider() );
-        oak.with( new TypeValidatorProvider() );
-        oak.with( new RegistrationValidatorProvider() );
+        oak.with( new TypeEditorProvider() );
+        oak.with( new RegistrationEditorProvider() );
         oak.with( new ConflictValidatorProvider() );
-        oak.with( new AnnotatingConflictHandler() );
-        oak.with( new Property2IndexProvider() );
-        oak.with( IndexHookManager.of( new CompositeIndexHookProvider( new Property2IndexHookProvider() ) ) );
-        this.repository = new RepositoryImpl( oak.createContentRepository(), null, null );
+        oak.with( new PropertyIndexEditorProvider() );
+        oak.with( new PropertyIndexProvider() );
+        oak.with( new NodeTypeIndexProvider() );
+        oak.with( securityProvider );
+
+        this.repository = new RepositoryImpl( oak.createContentRepository(), oak.getExecutorService(), securityProvider );
     }
 
     @Inject
