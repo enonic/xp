85b3f4d0b8d7c20f7d3ece022b664cd8b2fa6d0d
Sten Roger Sandvik <srs@enonic.com>
2013-06-17 10:31:37 +0200

    Moved ping into generic status resource.

--- modules/wem-web/src/main/java/com/enonic/wem/admin/rest/resource/status/StatusResource.java
+++ modules/wem-web/src/main/java/com/enonic/wem/admin/rest/resource/status/StatusResource.java
@@ -3,12 +3,13 @@ package com.enonic.wem.admin.rest.resource.status;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("status")
 public final class StatusResource
 {
     @GET
-    @Produces("application/json")
+    @Produces(MediaType.APPLICATION_JSON)
     public StatusResult getStatus()
     {
         return new StatusResult();
