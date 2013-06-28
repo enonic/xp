333b31a55dc69b1ac0bd2b76fa9a512c7fd0baa9
Aliaksei Biaskosty <abiaskosty@scnsoft.com>
2013-06-28 10:52:11 +0300

    CMS-1826 Add type definitions for system_getSystemInfo in RemoteService.ts

--- modules/wem-webapp/src/main/webapp/admin2/api/js/lib/RemoteService.ts
+++ modules/wem-webapp/src/main/webapp/admin2/api/js/lib/RemoteService.ts
@@ -168,6 +168,15 @@ module api_remote {
         schemas:Schema[];
     }
 
+    export interface RemoteCallSystemGetSystemInfoParams {
+    }
+
+    export interface RemoteCallSystemGetSystemInfoResult extends RemoteCallResultBase {
+        installationName: string;
+        version: string;
+        title: string;
+    }
+
     export interface RemoteCallMixinGetParams {
         format:string;
         mixin:string;
@@ -289,7 +298,7 @@ module api_remote {
         contentType_tree (params:RemoteCallGetContentTypeTreeParams, callback:(result:RemoteCallGetContentTypeTreeResult)=>void):void;
         schema_tree (params, callback):void;
         schema_list (params:RemoteCallSchemaListParams, callback:(result:RemoteCallSchemaListResult)=>void):void;
-        system_getSystemInfo (params, callback):void;
+        system_getSystemInfo (params:RemoteCallSystemGetSystemInfoParams, callback:(result:RemoteCallSystemGetSystemInfoResult)=>void):void;
         mixin_get (params:RemoteCallMixinGetParams, callback:(result:RemoteCallMixinGetResult)=>void):void;
         mixin_createOrUpdate (params:RemoteCallMixinCreateOrUpdateParams, callback:(result:RemoteCallMixinCreateOrUpdateResult)=>void):void;
         mixin_delete (params:RemoteCallMixinDeleteParams, callback:(result:RemoteCallMixinDeleteResult)=>void):void;
@@ -452,7 +461,7 @@ module api_remote {
             console.log(params, callback);
         }
 
-        system_getSystemInfo(params, callback):void {
+        system_getSystemInfo(params:RemoteCallSystemGetSystemInfoParams, callback:(result:RemoteCallSystemGetSystemInfoResult)=>void):void {
             console.log(params, callback);
         }
 
