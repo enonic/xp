621a985eab402c636d7d4f0c7d2845f1a24a59b7
Raman Yafimchyk <rya@enonic.com>
2013-07-22 18:27:44 +0300

    CMS-1765: Convert util_getLocales to use value objects

--- modules/wem-web/src/main/java/com/enonic/wem/admin/rest/resource/util/LocaleResource.java
+++ modules/wem-web/src/main/java/com/enonic/wem/admin/rest/resource/util/LocaleResource.java
@@ -1,11 +1,36 @@
 package com.enonic.wem.admin.rest.resource.util;
 
+import java.util.Locale;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.enonic.wem.admin.rest.resource.AbstractResource;
+import com.enonic.wem.admin.rest.resource.util.model.LocaleListJson;
+import com.enonic.wem.core.locale.LocaleService;
+
 @Path("util/locale")
 @Produces(MediaType.APPLICATION_JSON)
 public class LocaleResource
+    extends AbstractResource
 {
+    private LocaleService localeService;
+
+    @GET
+    public LocaleListJson list()
+    {
+        final Locale[] locales = this.localeService.getLocales();
+        final LocaleListJson result = new LocaleListJson( locales );
+
+        return result;
+    }
+
+    @Inject
+    public void setLocaleService( final LocaleService localeService )
+    {
+        this.localeService = localeService;
+    }
 }
---
+++ modules/wem-web/src/main/java/com/enonic/wem/admin/rest/resource/util/model/LocaleListJson.java
@@ -0,0 +1,32 @@
+package com.enonic.wem.admin.rest.resource.util.model;
+
+import java.util.List;
+import java.util.Locale;
+
+import com.google.common.collect.ImmutableList;
+
+public class LocaleListJson
+{
+    private final ImmutableList<LocaleJson> list;
+
+    public LocaleListJson( final Locale[] locales )
+    {
+        final ImmutableList.Builder<LocaleJson> builder = ImmutableList.builder();
+        for ( final Locale locale : locales )
+        {
+            builder.add( new LocaleJson( locale ) );
+        }
+
+        this.list = builder.build();
+    }
+
+    public int getTotal()
+    {
+        return this.list.size();
+    }
+
+    public List<LocaleJson> getLocales()
+    {
+        return this.list;
+    }
+}
