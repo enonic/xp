bf627b16d209e234f0efeda7d47fe9fbeca84327
Jørund Vier Skriubakken <jvs@enonic.com>
2013-08-12 21:29:43 +0200

    CMS-1953 Create initial classes for building a simple form with one TextLine input Added property isBuiltIn to InputType

--- modules/wem-webapp/src/main/webapp/admin2/api/js/schema/content/form/InputType.ts
+++ modules/wem-webapp/src/main/webapp/admin2/api/js/schema/content/form/InputType.ts
@@ -4,12 +4,19 @@ module api_schema_content_form{
 
         private name:string;
 
+        private builtIn:bool;
+
         constructor(json:any) {
             this.name = json.name;
+            this.builtIn = json.builIn;
         }
 
         getName():string {
             return this.name;
+        }
+
+        isBuiltIn():bool {
+            return this.builtIn;
         }
     }
 }
