051a2b0bb73e4953ab01aca6aacf46954323af6f
Jørund Vier Skriubakken <jvs@enonic.com>
2013-08-13 10:00:27 +0200

    Made DialogButton not show tooltip (if it have shortcut)

--- modules/wem-webapp/src/main/webapp/admin2/api/js/ui/dialog/DialogButton.ts
+++ modules/wem-webapp/src/main/webapp/admin2/api/js/ui/dialog/DialogButton.ts
@@ -3,7 +3,7 @@ module api_ui_dialog{
     export class DialogButton extends api_ui.ActionButton {
 
         constructor(action:api_ui.Action) {
-            super("DialogButton", action);
+            super("DialogButton", action, false);
         }
     }
 }
