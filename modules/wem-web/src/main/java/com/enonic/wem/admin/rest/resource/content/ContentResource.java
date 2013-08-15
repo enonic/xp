d23be8df01ef62e3490d4271977d2d295fdd13f2
Raman Yafimchyk <rya@enonic.com>
2013-08-15 15:59:41 +0300

    CMS-1817: Convert content_get to use value objects

--- modules/wem-web/src/main/java/com/enonic/wem/admin/rest/resource/content/ContentResource.java
+++ modules/wem-web/src/main/java/com/enonic/wem/admin/rest/resource/content/ContentResource.java
@@ -1,11 +1,101 @@
 package com.enonic.wem.admin.rest.resource.content;
 
+import java.util.List;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.google.common.base.Joiner;
+import com.sun.jersey.api.NotFoundException;
+
+import com.enonic.wem.admin.rest.resource.AbstractResource;
+import com.enonic.wem.admin.rest.resource.content.model.ContentListJson;
+import com.enonic.wem.api.command.Commands;
+import com.enonic.wem.api.command.content.GetContentVersion;
+import com.enonic.wem.api.command.content.GetContents;
+import com.enonic.wem.api.content.Content;
+import com.enonic.wem.api.content.ContentIds;
+import com.enonic.wem.api.content.ContentPath;
+import com.enonic.wem.api.content.ContentPaths;
+import com.enonic.wem.api.content.Contents;
+import com.enonic.wem.api.content.versioning.ContentVersionId;
 
 @Path("content")
 public class ContentResource
+    extends AbstractResource
 {
-    // get() @GET
+    @GET
+    public ContentListJson get( @QueryParam("path") final String path,
+                                @QueryParam("version") final Integer version,
+                                @QueryParam("contentIds") final List<String> contentIds )
+    {
+        if ( StringUtils.isNotEmpty( path ) )
+        {
+            final ContentPath contentPath = ContentPath.from( path );
+            return handleGetContentByPath( contentPath, version );
+        }
+        else
+        {
+            final String[] stringOfPathIds = contentIds.toArray( new String[contentIds.size()] );
+            return handleGetContentByIds( ContentIds.from( stringOfPathIds ) );
+        }
+    }
+
+    private ContentListJson handleGetContentByPath( final ContentPath contentPath, final Integer version )
+    {
+        final Content content;
+
+        if ( version == null )
+        {
+            content = findContent( contentPath );
+        }
+        else
+        {
+            content = findContentVersion( contentPath, ContentVersionId.of( version ) );
+        }
+
+        if ( content != null )
+        {
+            return new ContentListJson( content );
+        }
+        throw new NotFoundException( String.format( "Content [%s] was not found", contentPath ) );
+    }
+
+    private Content findContent( final ContentPath contentPath )
+    {
+        final GetContents getContents = Commands.content().get();
+        getContents.selectors( ContentPaths.from( contentPath ) );
+
+        final Contents contents = client.execute( getContents );
+        return contents.isNotEmpty() ? contents.first() : null;
+    }
+
+    private Content findContentVersion( final ContentPath contentPath, final ContentVersionId versionId )
+    {
+        final GetContentVersion getContentVersion = Commands.content().getVersion();
+        getContentVersion.selector( contentPath ).version( versionId );
+        return client.execute( getContentVersion );
+    }
+
+    private ContentListJson handleGetContentByIds( final ContentIds ids )
+    {
+        final GetContents getContents = Commands.content().get();
+        getContents.selectors( ids );
+
+        final Contents contents = client.execute( getContents );
+
+        if ( contents.isNotEmpty() )
+        {
+            return new ContentListJson( contents );
+        }
+
+        final String missing = Joiner.on( "," ).join( ids );
+        throw new NotFoundException( String.format( "Contents [%s] not found", missing ) );
+    }
+
     // list() @GET
     // delete() @POST
     // create() @POST
---
+++ modules/wem-web/src/main/java/com/enonic/wem/admin/rest/resource/content/model/DataSetJson.java
@@ -0,0 +1,54 @@
+package com.enonic.wem.admin.rest.resource.content.model;
+
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
+import com.enonic.wem.api.content.data.Data;
+import com.enonic.wem.api.content.data.DataSet;
+
+public class DataSetJson
+    extends AbstractDataJson
+{
+    private final ImmutableList<DataJson> list;
+
+    private final DataSet dataSet;
+
+    public DataSetJson( final DataSet dataSet )
+    {
+        this.dataSet = dataSet;
+
+        final ImmutableList.Builder<DataJson> builder = ImmutableList.builder();
+        for ( final Data data : dataSet )
+        {
+            builder.add( new DataJson( data ) );
+        }
+
+        this.list = builder.build();
+    }
+
+    public List<DataJson> getData()
+    {
+        return list;
+    }
+
+    public String getName()
+    {
+        return dataSet.getName();
+    }
+
+    public String getPath()
+    {
+        return dataSet.getPath().toString();
+    }
+
+    public String getType()
+    {
+        return DataSet.class.getSimpleName();
+    }
+
+    public DataSetJson getValue()
+    {
+        return new DataSetJson( dataSet );
+    }
+}
