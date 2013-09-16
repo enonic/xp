8b827a6985a243ece11ac85171c62d40eae13301
Sten Roger Sandvik <srs@enonic.com>
2013-09-16 14:29:46 +0200

    Added admin-ui module. This is not in use yet, but shows an example on how to use karma + jasmine.

---
+++ modules/wem-admin-ui/Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function (grunt) {
+
+    grunt.loadNpmTasks('grunt-ts');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-karma');
+
+    grunt.initConfig({
+
+            ts: {
+                api: {
+                    src: ['src/main/js/main.ts'],
+                    out: 'target/main/all.js',
+                    options: {
+                        // target: 'es5',
+                        sourcemap: true,
+                        declaration: true
+                    }
+                }
+            },
+
+            karma: {
+                options: {
+                    browsers: ['PhantomJS'],
+                    files: [
+                        'target/main/**/*.js',
+                        'src/test/**/*.js'
+                    ],
+                    frameworks: ['jasmine'],
+                    preprocessors: {
+                        '**/main/**/*.js': 'coverage'
+                    },
+                    reporters: ['dots', 'coverage'],
+                    coverageReporter: {
+                        type: 'html',
+                        dir: 'target/coverage/'
+                    }
+                },
+                dev: {
+                    singleRun: true
+                }
+            }
+
+        }
+    );
+
+    grunt.registerTask('all', ['ts', 'karma']);
+
+};
