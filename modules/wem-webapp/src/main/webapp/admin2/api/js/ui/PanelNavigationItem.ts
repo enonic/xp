44c15db089e117cc90540ffa0bdf5a71bfedb918
Jørund Vier Skriubakken <jvs@enonic.com>
2013-09-18 14:07:30 +0200

    CMS-1688 Create TabBar/WizardTabBar Removed not used methods in PanelNavigationItem

--- modules/wem-webapp/src/main/webapp/admin2/api/js/ui/PanelNavigationItem.ts
+++ modules/wem-webapp/src/main/webapp/admin2/api/js/ui/PanelNavigationItem.ts
@@ -2,25 +2,7 @@ module api_ui {
 
     export interface PanelNavigationItem {
 
-        setIndex(value:number);
-
         getIndex():number;
-
-        setLabel(value:string);
-
-        getLabel():string;
-
-        setActive(value:boolean);
-
-        isActive():boolean;
-
-        setVisible(value:boolean);
-
-        isVisible():boolean;
-
-        setRemovable(value:boolean);
-
-        isRemovable():boolean;
 
     }
 }
