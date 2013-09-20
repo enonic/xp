1cd117cd8763050897430f668803c7ad07ff0b97
Lasse Jul-Larsen <ljl@enonic.com>
2013-09-20 15:54:53 +0200

    CMS-2068 TypeScript - Context Window: Create Iframe mask

--- modules/wem-webapp/src/main/webapp/admin2/apps/content-manager/js/app/contextwindow/DraggingMask.ts
+++ modules/wem-webapp/src/main/webapp/admin2/apps/content-manager/js/app/contextwindow/DraggingMask.ts
@@ -12,8 +12,12 @@ module app_contextwindow {
         show() {
             var maskedHeight = this.maskedItem.getHTMLElement().offsetHeight;
             var maskedWidth = this.maskedItem.getHTMLElement().offsetWidth;
+            var maskedTop = this.maskedItem.getHTMLElement().offsetTop;
+            var maskedLeft = this.maskedItem.getHTMLElement().offsetLeft;
             this.getEl().setHeight(maskedHeight + "px");
             this.getEl().setWidth(maskedWidth + "px");
+            this.getEl().setTop(maskedTop + "px");
+            this.getEl().setLeft(maskedLeft + "px");
 
             jQuery(this.getHTMLElement()).fadeIn(200);
         }
