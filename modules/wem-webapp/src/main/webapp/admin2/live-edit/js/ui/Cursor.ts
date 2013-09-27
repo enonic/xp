741e377ba0f3b473823f201629f0e64ec5b80aaa
Thomas Andersen <tan@enonic.com>
2013-09-27 09:36:41 +0200

    CMS-2150 Re-design drag'n drop helper box - Hide cursor properly during drag/drop life cycle

--- modules/wem-webapp/src/main/webapp/admin2/live-edit/js/ui/Cursor.ts
+++ modules/wem-webapp/src/main/webapp/admin2/live-edit/js/ui/Cursor.ts
@@ -5,8 +5,20 @@ module LiveEdit.ui {
 
     export class Cursor extends LiveEdit.ui.Base {
 
+        bodyElement:JQuery;
+
+        defaultBodyCursor:string;
+
         constructor() {
             super();
+
+            this.bodyElement = $('body');
+
+            // Cache any user set body@style cursor in order to restore it later.
+            // Not 100% as the cursor can change any time during the page's life cycle.
+            // $.css('cursor') should be avoided here used as it uses window.getComputedStyle()
+            this.defaultBodyCursor = this.bodyElement[0].style.cursor;
+
             this.registerGlobalListeners();
         }
 
@@ -14,19 +26,23 @@ module LiveEdit.ui {
             $(window).on('mouseOverComponent.liveEdit', (event:JQueryEventObject, component) => this.update(component));
             $(window).on('selectComponent.liveEdit', (event:JQueryEventObject, component) => this.update(component));
             $(window).on('mouseOutComponent.liveEdit', () => this.reset());
+            $(window).on('sortableStart.liveEdit', () => this.hide());
+            $(window).on('sortableStop.liveEdit', () => this.reset());
         }
 
         private update(component:LiveEdit.component.Component):void {
-            var body:JQuery = $('body');
+            this.bodyElement.css('cursor', component.getComponentType().getCursor());
+        }
 
-            body.css('cursor', component.getComponentType().getCursor());
+        private hide():void {
+            this.bodyElement.css('cursor', 'none');
         }
 
         private reset():void {
             if(LiveEdit.component.DragDropSort.isDragging()) {
                 return;
             }
-            $('body').css('cursor', 'default');
+            this.bodyElement.css('cursor', this.defaultBodyCursor || '');
         }
 
     }
