352bb4cc23b68bcbbd88bbf7f6d9501d6144d5c0
Sten Roger Sandvik <srs@enonic.com>
2013-10-03 19:53:48 +0200

    Moved admin2 to admin.

--- modules/wem-web/src/main/java/com/enonic/wem/admin/rest/RestServletModule.java
+++ modules/wem-web/src/main/java/com/enonic/wem/admin/rest/RestServletModule.java
@@ -9,9 +9,9 @@ final class RestServletModule
     protected void configureServlets()
     {
         // Main rest path (should be the only one).
-        serve( "/admin2/rest/*" ).with( RestServlet.class );
+        serve( "/admin/rest/*" ).with( RestServlet.class );
 
         // Temporary rest path's (should be deleted before production).
-        serve( "/dev/rest/*", "/old-admin/rest/*", "/admin2/apps/space-manager/rest/*" ).with( RestServlet.class );
+        serve( "/dev/rest/*", "/old-admin/rest/*", "/admin/apps/space-manager/rest/*" ).with( RestServlet.class );
     }
 }
