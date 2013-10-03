9555d07e2014d13f27c509c9675f20d3bc7f9252
Sten Roger Sandvik <srs@enonic.com>
2013-10-03 19:34:37 +0200

    Moved admin to old-admin for historic purposes.

--- modules/wem-web/src/main/java/com/enonic/wem/admin/rest/RestServletModule.java
+++ modules/wem-web/src/main/java/com/enonic/wem/admin/rest/RestServletModule.java
@@ -9,7 +9,7 @@ final class RestServletModule
     protected void configureServlets()
     {
         // Main rest path (should be the only one).
-        serve( "/admin/rest/*" ).with( RestServlet.class );
+        serve( "/admin2/rest/*" ).with( RestServlet.class );
 
         // Temporary rest path's (should be deleted before production).
         serve( "/dev/rest/*", "/old-admin/rest/*", "/admin2/apps/space-manager/rest/*" ).with( RestServlet.class );
