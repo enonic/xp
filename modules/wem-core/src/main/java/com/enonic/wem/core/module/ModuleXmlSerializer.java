3c410b86ad8e2672e07730572241e7c32967231f
Viktar Fiodarau <vfi@enonic.com>
2013-10-17 17:05:40 +0300

    CMS-2239 Implement XML serializer/deserializer for Module
    Implemented serialization/deserialization for Module and written unit tests for it.

--- modules/wem-core/src/main/java/com/enonic/wem/core/module/ModuleXmlSerializer.java
+++ modules/wem-core/src/main/java/com/enonic/wem/core/module/ModuleXmlSerializer.java
@@ -1,12 +1,19 @@
 package com.enonic.wem.core.module;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
 
 import com.enonic.wem.api.module.Module;
+import com.enonic.wem.api.module.ModuleKey;
+import com.enonic.wem.api.module.ModuleVersion;
+import com.enonic.wem.api.schema.content.QualifiedContentTypeName;
+import com.enonic.wem.api.schema.content.form.Form;
+import com.enonic.wem.api.schema.content.form.FormItem;
+import com.enonic.wem.core.schema.content.serializer.FormItemsXmlSerializer;
 import com.enonic.wem.core.support.serializer.XmlParsingException;
 import com.enonic.wem.core.support.util.JdomHelper;
 
@@ -37,6 +44,37 @@ public final class ModuleXmlSerializer
     private void generate( final Module module, final Element moduleEl )
     {
         moduleEl.addContent( new Element( "display-name" ).setText( module.getDisplayName() ) );
+        moduleEl.addContent( new Element( "info" ).setText( module.getInfo() ) );
+        moduleEl.addContent( new Element( "url" ).setText( module.getUrl() ) );
+        moduleEl.addContent( new Element( "key" ).setText( module.getModuleKey().toString() ) );
+
+        Element vendor = new Element( "vendor" );
+        vendor.addContent( new Element( "name" ).setText( module.getVendorName() ) );
+        vendor.addContent( new Element( "url" ).setText( module.getVendorUrl() ) );
+        moduleEl.addContent( vendor );
+
+        Element dependencies = new Element( "dependencies" );
+        dependencies.setAttribute( "system-min-version", String.valueOf( module.getMinSystemVersion() ) );
+        dependencies.setAttribute( "system-max-version", String.valueOf( module.getMaxSystemVersion() ) );
+
+        for ( ModuleKey moduleKey : module.getModuleDependencies() )
+        {
+            dependencies.addContent( new Element( "module" ).setText( moduleKey.toString() ) );
+        }
+
+        for ( QualifiedContentTypeName qualifiedName : module.getContentTypeDependencies() )
+        {
+            dependencies.addContent( new Element( "content-type" ).setText( qualifiedName.toString() ) );
+        }
+        moduleEl.addContent( dependencies );
+
+        Element form = new Element( "form" ).setAttribute( "name", "config" );
+        if ( module.getConfig() != null )
+        {
+            new FormItemsXmlSerializer().serialize( module.getConfig(), form );
+        }
+        moduleEl.addContent( form );
+
     }
 
     public void toModule( final String xml, final Module.Builder moduleBuilder )
@@ -58,6 +96,39 @@ public final class ModuleXmlSerializer
     {
         final String displayName = moduleEl.getChildText( "display-name" );
         moduleBuilder.
-            displayName( displayName );
+            displayName( displayName ).info( moduleEl.getChildText( "info" ) ).moduleKey(
+            ModuleKey.from( moduleEl.getChildText( "key" ) ) ).
+            url( moduleEl.getChildText( "url" ) ).vendorName( moduleEl.getChild( "vendor" ).
+            getChildText( "name" ) ).vendorUrl( moduleEl.getChild( "vendor" ).getChildText( "url" ) );
+
+        final Element dependenciesEl = moduleEl.getChild( "dependencies" );
+        if ( dependenciesEl != null )
+        {
+            moduleBuilder.maxSystemVersion( ModuleVersion.from( dependenciesEl.getAttributeValue( "system-max-version" ) ) ).
+                minSystemVersion( ModuleVersion.from( dependenciesEl.getAttributeValue( "system-min-version" ) ) );
+            for ( Element child : (List<Element>) dependenciesEl.getChildren() )
+            {
+                switch ( child.getName() )
+                {
+                    case "module":
+                        moduleBuilder.addModuleDependency( ModuleKey.from( child.getText() ) );
+                        break;
+                    case "content-type":
+                        moduleBuilder.addContentTypeDependency( QualifiedContentTypeName.from( child.getText() ) );
+                        break;
+                }
+            }
+        }
+
+        Iterable<FormItem> formItems = new FormItemsXmlSerializer().parse( moduleEl.getChild( "form" ) );
+
+        Form.Builder form = Form.newForm();
+
+        for ( FormItem formItem : formItems )
+        {
+            form.addFormItem( formItem );
+        }
+
+        moduleBuilder.config( form.build() );
     }
 }
