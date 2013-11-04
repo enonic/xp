b6a6b3110ca2843df2ae76b5cfd411fbf18fde32
Jørund Vier Skriubakken <jvs@enonic.com>
2013-11-04 20:25:41 +0100

    CMS-2372 Introduce blobstore

--- modules/wem-core/src/main/java/com/enonic/wem/core/CoreModule.java
+++ modules/wem-core/src/main/java/com/enonic/wem/core/CoreModule.java
@@ -3,10 +3,12 @@ package com.enonic.wem.core;
 import com.google.inject.AbstractModule;
 
 import com.enonic.wem.core.account.AccountModule;
+import com.enonic.wem.core.blobstore.BlobStoreModule;
 import com.enonic.wem.core.client.ClientModule;
 import com.enonic.wem.core.config.ConfigModule;
 import com.enonic.wem.core.content.ContentModule;
 import com.enonic.wem.core.entity.EntityModule;
+import com.enonic.wem.core.hazelcast.HazelcastModule;
 import com.enonic.wem.core.home.HomeModule;
 import com.enonic.wem.core.index.IndexModule;
 import com.enonic.wem.core.initializer.InitializerModule;
@@ -33,6 +35,7 @@ public final class CoreModule
         install( new JcrModule() );
         install( new InitializerModule() );
         install( new ClientModule() );
+        install( new BlobStoreModule() );
         install( new EntityModule() );
         install( new AccountModule() );
         install( new ContentModule() );
@@ -44,8 +47,7 @@ public final class CoreModule
         install( new ResourceModule() );
         install( new ModuleModule() );
 
-        // Add this to enable hazelcast
-        // install( new HazelcastModule() );
+        install( new HazelcastModule() );
 
         // TODO: Move to plugin. Need some service starting system first.
         install( new MigrateModule() );
