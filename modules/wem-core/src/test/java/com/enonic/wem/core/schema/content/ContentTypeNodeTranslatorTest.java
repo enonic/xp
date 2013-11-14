00cf79d3667e1fe9870cf63adc5175011c855967
Runar Myklebust <rmy@enonic.com>
2013-11-14 10:04:38 +0100

    CMS-2078 Let ContentType be persisted as an Node
    Added some tests

--- modules/wem-core/src/test/java/com/enonic/wem/core/schema/content/ContentTypeNodeTranslatorTest.java
+++ modules/wem-core/src/test/java/com/enonic/wem/core/schema/content/ContentTypeNodeTranslatorTest.java
@@ -6,17 +6,28 @@ import com.enonic.wem.api.command.Commands;
 import com.enonic.wem.api.command.entity.CreateNode;
 import com.enonic.wem.api.command.schema.content.CreateContentType;
 import com.enonic.wem.api.data.Data;
+import com.enonic.wem.api.data.DataPath;
 import com.enonic.wem.api.data.DataSet;
 import com.enonic.wem.api.data.Property;
+import com.enonic.wem.api.data.RootDataSet;
+import com.enonic.wem.api.data.Value;
+import com.enonic.wem.api.entity.EntityId;
+import com.enonic.wem.api.entity.Node;
+import com.enonic.wem.api.entity.NodePath;
 import com.enonic.wem.api.form.Form;
 import com.enonic.wem.api.form.Input;
 import com.enonic.wem.api.form.inputtype.InputTypes;
+import com.enonic.wem.api.schema.SchemaId;
+import com.enonic.wem.api.schema.content.ContentType;
 
 import static com.enonic.wem.core.schema.content.ContentTypeNodeTranslator.ABSTRACT_PROPERTY;
 import static com.enonic.wem.core.schema.content.ContentTypeNodeTranslator.ALLOW_CHILD_CONTENT_PROPERTY;
 import static com.enonic.wem.core.schema.content.ContentTypeNodeTranslator.BUILT_IN_PROPERTY;
+import static com.enonic.wem.core.schema.content.ContentTypeNodeTranslator.CONTENT_DISPLAY_NAME_SCRIPT_PROPERTY;
+import static com.enonic.wem.core.schema.content.ContentTypeNodeTranslator.DISPLAY_NAME_PROPERTY;
 import static com.enonic.wem.core.schema.content.ContentTypeNodeTranslator.FINAL_PROPERTY;
 import static com.enonic.wem.core.schema.content.ContentTypeNodeTranslator.FORMITEMS_FULL_PATH;
+import static com.enonic.wem.core.schema.content.ContentTypeNodeTranslator.SUPER_TYPE_PROPERTY;
 import static org.junit.Assert.*;
 
 public class ContentTypeNodeTranslatorTest
@@ -91,6 +102,55 @@ public class ContentTypeNodeTranslatorTest
         assertNotNull( input );
     }
 
+    @Test
+    public void fromNode_nodeProperties()
+        throws Exception
+    {
+        final NodePath parentPath = NodePath.newPath( "my/test/parent" ).build();
+        Node node = Node.newNode().
+            id( EntityId.from( "1" ) ).
+            name( "my-name" ).
+            build();
+
+        final ContentType contentType = translator.fromNode( node );
+
+        assertEquals( new SchemaId( "1" ), contentType.getId() );
+        assertEquals( ContentTypesInitializer.UNSTRUCTURED, contentType.getSuperType() );
+    }
+
+
+    @Test
+    public void fromNode_rootDataSet()
+        throws Exception
+    {
+
+        RootDataSet rootDataSet = new RootDataSet();
+        rootDataSet.addProperty( DataPath.from( ALLOW_CHILD_CONTENT_PROPERTY ), new Value.String( "true" ) );
+        rootDataSet.addProperty( DataPath.from( BUILT_IN_PROPERTY ), new Value.String( "true" ) );
+        rootDataSet.addProperty( DataPath.from( FINAL_PROPERTY ), new Value.String( "true" ) );
+        rootDataSet.addProperty( DataPath.from( ABSTRACT_PROPERTY ), new Value.String( "true" ) );
+        rootDataSet.addProperty( DataPath.from( DISPLAY_NAME_PROPERTY ), new Value.String( "myDisplayName" ) );
+        rootDataSet.addProperty( DataPath.from( CONTENT_DISPLAY_NAME_SCRIPT_PROPERTY ), new Value.String( "myDisplayNameScript" ) );
+        rootDataSet.addProperty( DataPath.from( SUPER_TYPE_PROPERTY ), new Value.String( "my-super-type" ) );
+
+        final NodePath parentPath = NodePath.newPath( "my/test/parent" ).build();
+        Node node = Node.newNode().
+            id( EntityId.from( "1" ) ).
+            name( "my-name" ).
+            rootDataSet( rootDataSet ).
+            build();
+
+        final ContentType contentType = translator.fromNode( node );
+
+        assertEquals( new SchemaId( "1" ), contentType.getId() );
+        assertEquals( "my-super-type", contentType.getSuperType().getContentTypeName().toString() );
+        assertEquals( true, contentType.allowChildContent() );
+        assertEquals( true, contentType.isFinal() );
+        assertEquals( true, contentType.isBuiltIn() );
+        assertEquals( true, contentType.isAbstract() );
+        assertEquals( "myDisplayNameScript", contentType.getContentDisplayNameScript() );
+        assertEquals( "myDisplayName", contentType.getDisplayName() );
+    }
 
     private void verifyPropertyValue( final String myExpectedValue, final CreateNode createNode, final String propertyNameToCheck )
     {
