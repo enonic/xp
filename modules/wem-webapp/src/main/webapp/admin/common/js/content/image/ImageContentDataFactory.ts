3130745ea5596007170ef360a1efb2a91045ef0c
Jørund Vier Skriubakken <jvs@enonic.com>
2013-11-27 07:51:01 +0100

    CMS-2492 Resolve big problem with saving attachments (images) to content (continue from previous sprints) Added ImageContentDataFactory for having a unified way of creating an Image ContentData

---
+++ modules/wem-webapp/src/main/webapp/admin/common/js/content/image/ImageContentDataFactory.ts
@@ -0,0 +1,31 @@
+module api_content_image{
+
+    export class ImageContentDataFactory {
+
+        private image:api_content.AttachmentName;
+
+        private mimeType:string;
+
+        public setImage(value:api_content.AttachmentName):ImageContentDataFactory {
+            this.image = value;
+            return this;
+        }
+
+        public setMimeType(value:string):ImageContentDataFactory {
+            this.mimeType = value;
+            return this;
+        }
+
+        public create():api_content.ContentData {
+            var contentData = new api_content.ContentData();
+
+            var imageValue = new api_data.Value( this.image.getFileName(), api_data.ValueTypes.STRING );
+            var mimeTypeValue = new api_data.Value( this.mimeType, api_data.ValueTypes.STRING );
+
+            contentData.addData(api_data.Property.fromNameValue("image", imageValue ));
+            contentData.addData(api_data.Property.fromNameValue("mimeType", mimeTypeValue));
+            return contentData;
+        }
+
+    }
+}
