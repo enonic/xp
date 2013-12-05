3be02ce69ea3e3843e04f336f05d889beee28a75
Jørund Vier Skriubakken <jvs@enonic.com>
2013-12-05 20:25:22 +0100

    CMS-2331 Design new input type for Region Added page and site to ContentJson

--- modules/wem-webapp/src/main/webapp/admin/common/js/content/json/ContentJson.ts
+++ modules/wem-webapp/src/main/webapp/admin/common/js/content/json/ContentJson.ts
@@ -5,5 +5,9 @@ module api_content_json{
         data: api_data_json.DataTypeWrapperJson[];
 
         form: api_form_json.FormJson;
+
+        site: api_content_site_json.SiteJson;
+
+        page: api_content_page_json.PageJson;
     }
 }
