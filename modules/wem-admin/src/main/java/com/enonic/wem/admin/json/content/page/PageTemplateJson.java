94f9c353872aeded5c6d72629ea94469959d26c2
Jørund Vier Skriubakken <jvs@enonic.com>
2013-12-05 20:22:40 +0100

    CMS-2331 Design new input type for Region Added REST API method for getting a PageTemplate

--- modules/wem-admin/src/main/java/com/enonic/wem/admin/json/content/page/PageTemplateJson.java
+++ modules/wem-admin/src/main/java/com/enonic/wem/admin/json/content/page/PageTemplateJson.java
@@ -2,9 +2,12 @@ package com.enonic.wem.admin.json.content.page;
 
 import java.util.List;
 
+import com.google.common.collect.ImmutableList;
+
 import com.enonic.wem.admin.json.data.DataJson;
 import com.enonic.wem.admin.json.data.RootDataSetJson;
 import com.enonic.wem.api.content.page.PageTemplate;
+import com.enonic.wem.api.schema.content.ContentTypeName;
 
 
 public class PageTemplateJson
@@ -12,14 +15,27 @@ public class PageTemplateJson
 {
     private final RootDataSetJson configJson;
 
+    private final ImmutableList<String> canRender;
+
     public PageTemplateJson( final PageTemplate template )
     {
         super( template );
         this.configJson = new RootDataSetJson( template.getConfig() );
+        ImmutableList.Builder<String> canRenderBuilder = new ImmutableList.Builder<>();
+        for ( ContentTypeName contentTypeName : template.getCanRender() )
+        {
+            canRenderBuilder.add( contentTypeName.toString() );
+        }
+        this.canRender = canRenderBuilder.build();
     }
 
     public List<DataJson> getConfig()
     {
         return configJson.getSet();
+    }
+
+    public List<String> getCanRender()
+    {
+        return canRender;
     }
 }
--- modules/wem-admin/src/main/java/com/enonic/wem/admin/rest/resource/content/page/PageTemplateResource.java
+++ modules/wem-admin/src/main/java/com/enonic/wem/admin/rest/resource/content/page/PageTemplateResource.java
@@ -1,15 +1,36 @@
 package com.enonic.wem.admin.rest.resource.content.page;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import com.enonic.wem.admin.json.content.page.PageTemplateJson;
 import com.enonic.wem.admin.rest.resource.AbstractResource;
+import com.enonic.wem.admin.rest.resource.Result;
+import com.enonic.wem.api.content.page.PageTemplate;
+import com.enonic.wem.api.content.page.PageTemplateKey;
+
+import static com.enonic.wem.api.command.Commands.page;
 
 @Path("content/page/template")
 @Produces(MediaType.APPLICATION_JSON)
 public class PageTemplateResource
     extends AbstractResource
 {
-    // TODO
+    @GET
+    public Result getByKey( @QueryParam("key") final String pageTemplateKeyAsString )
+    {
+        try
+        {
+            final PageTemplateKey pageTemplateKey = PageTemplateKey.from( pageTemplateKeyAsString );
+            final PageTemplate pageTemplate = client.execute( page().template().page().getByKey().key( pageTemplateKey ) );
+            return Result.result( new PageTemplateJson( pageTemplate ) );
+        }
+        catch ( Exception e )
+        {
+            return Result.exception( e );
+        }
+    }
 }
