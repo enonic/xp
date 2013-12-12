6a79d27f6dd1d687c49288f81032115188c77506
Jørund Vier Skriubakken <jvs@enonic.com>
2013-12-12 07:42:55 +0100

    CMS-2586 Introduce Attachment on Entity

---
+++ modules/wem-api/src/main/java/com/enonic/wem/api/entity/Attachment.java
@@ -0,0 +1,85 @@
+package com.enonic.wem.api.entity;
+
+
+import com.enonic.wem.api.blob.BlobKey;
+
+public final class Attachment
+{
+    private final String name;
+
+    private final long size;
+
+    private final String mimeType;
+
+    private final BlobKey blobKey;
+
+    private Attachment( final Builder builder )
+    {
+        this.name = builder.name;
+        this.size = builder.size;
+        this.mimeType = builder.mimeType;
+        this.blobKey = builder.blobKey;
+    }
+
+    public String name()
+    {
+        return name;
+    }
+
+    public long size()
+    {
+        return size;
+    }
+
+    public String mimeType()
+    {
+        return mimeType;
+    }
+
+    public BlobKey blobKey()
+    {
+        return blobKey;
+    }
+
+    public final class Builder
+    {
+        private String name;
+
+        private long size;
+
+        private String mimeType;
+
+        private BlobKey blobKey;
+
+        public Builder name( final String value )
+        {
+            this.name = value;
+            return this;
+        }
+
+        public Builder size( final long value )
+        {
+            this.size = value;
+            return this;
+        }
+
+        public Builder mimeType( final String value )
+        {
+            this.mimeType = value;
+            return this;
+        }
+
+        public Builder blobKey( final BlobKey value )
+        {
+            this.blobKey = value;
+            return this;
+        }
+
+        public Attachment build()
+        {
+            return new Attachment( this );
+        }
+    }
+
+
+}
---
+++ modules/wem-api/src/main/java/com/enonic/wem/api/entity/Attachments.java
@@ -0,0 +1,81 @@
+package com.enonic.wem.api.entity;
+
+
+import java.util.Collection;
+
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+
+import com.enonic.wem.api.support.AbstractImmutableEntityList;
+
+public class Attachments
+    extends AbstractImmutableEntityList<Attachment>
+{
+    private final ImmutableMap<String, Attachment> attachmentByName;
+
+    private Attachments( final ImmutableList<Attachment> list )
+    {
+        super( list );
+
+        this.attachmentByName = Maps.uniqueIndex( list, new ToNameFunction() );
+    }
+
+    public Attachment getAttachment( final String name )
+    {
+        return attachmentByName.get( name );
+    }
+
+    public static Attachments empty()
+    {
+        final ImmutableList<Attachment> list = ImmutableList.of();
+        return new Attachments( list );
+    }
+
+    public static Attachments from( final Attachment... contents )
+    {
+        return new Attachments( ImmutableList.copyOf( contents ) );
+    }
+
+    public static Attachments from( final Iterable<? extends Attachment> contents )
+    {
+        return new Attachments( ImmutableList.copyOf( contents ) );
+    }
+
+    public static Attachments from( final Collection<? extends Attachment> contents )
+    {
+        return new Attachments( ImmutableList.copyOf( contents ) );
+    }
+
+    private final static class ToNameFunction
+        implements Function<Attachment, String>
+    {
+        @Override
+        public String apply( final Attachment value )
+        {
+            return value.name();
+        }
+    }
+
+    public static Builder builder()
+    {
+        return new Builder();
+    }
+
+    public static class Builder
+    {
+        private ImmutableList.Builder<Attachment> builder = ImmutableList.builder();
+
+        public Builder add( Attachment content )
+        {
+            builder.add( content );
+            return this;
+        }
+
+        public Attachments build()
+        {
+            return new Attachments( builder.build() );
+        }
+    }
+}
