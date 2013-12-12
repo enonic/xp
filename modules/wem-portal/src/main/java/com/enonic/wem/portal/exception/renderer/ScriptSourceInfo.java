7c92e501e0c3cc8397fdc6194aadd7753b95f381
Sten Roger Sandvik <srs@enonic.com>
2013-12-12 01:43:59 +0100

    Fixed tabbing in error source view.

--- modules/wem-portal/src/main/java/com/enonic/wem/portal/exception/renderer/ScriptSourceInfo.java
+++ modules/wem-portal/src/main/java/com/enonic/wem/portal/exception/renderer/ScriptSourceInfo.java
@@ -3,7 +3,9 @@ package com.enonic.wem.portal.exception.renderer;
 import java.io.IOException;
 import java.util.List;
 
+import com.google.common.base.Function;
 import com.google.common.base.Splitter;
+import com.google.common.collect.Collections2;
 import com.google.common.collect.Lists;
 
 import com.enonic.wem.portal.script.loader.ScriptSource;
@@ -48,12 +50,32 @@ public final class ScriptSourceInfo
     public List<String> getLines()
         throws IOException
     {
+        final List<String> allLines = getAllLines();
+        final List<String> subList = sliceLines( allLines );
+        return Lists.newArrayList( Collections2.transform( subList, new TabToSpaces() ) );
+    }
+
+    private List<String> getAllLines()
+    {
         final String str = this.source.getScriptAsString();
         final Iterable<String> allLines = Splitter.onPattern( "\r?\n" ).split( str );
-        final List<String> lineList = Lists.newArrayList( allLines );
+        return Lists.newArrayList( allLines );
+    }
 
+    private List<String> sliceLines( final List<String> all )
+    {
         final int firstLine = Math.max( 0, this.line - NUM_DELTA_LINES );
-        final int lastLine = Math.min( lineList.size(), this.line + NUM_DELTA_LINES );
-        return lineList.subList( firstLine, lastLine );
+        final int lastLine = Math.min( all.size(), this.line + NUM_DELTA_LINES );
+        return all.subList( firstLine, lastLine );
+    }
+
+    private final class TabToSpaces
+        implements Function<String, String>
+    {
+        @Override
+        public String apply( final String input )
+        {
+            return input.replaceAll( "\t", "    " );
+        }
     }
 }
