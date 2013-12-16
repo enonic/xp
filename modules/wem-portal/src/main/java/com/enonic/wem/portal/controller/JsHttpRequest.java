b198a143bae1b5adaa50b4264e1a107ea4526913
Alex Rodriguez <aro@enonic.com>
2013-12-16 14:16:45 +0100

    CMS-2610 Implement rendering of content using javascript. Fixed dependency conflict with Mustache.

--- modules/wem-portal/src/main/java/com/enonic/wem/portal/controller/JsHttpRequest.java
+++ modules/wem-portal/src/main/java/com/enonic/wem/portal/controller/JsHttpRequest.java
@@ -1,18 +1,28 @@
 package com.enonic.wem.portal.controller;
 
+import javax.ws.rs.core.MultivaluedMap;
+
 import com.sun.jersey.api.core.HttpRequestContext;
 
 public final class JsHttpRequest
 {
     private final HttpRequestContext raw;
 
+    private final MultivaluedMap<String, String> queryParameters;
+
     public JsHttpRequest( final HttpRequestContext raw )
     {
         this.raw = raw;
+        this.queryParameters = raw.getQueryParameters();
     }
 
     public String getMethod()
     {
         return this.raw.getMethod();
+    }
+
+    public String param( final String name )
+    {
+        return queryParameters.getFirst( name );
     }
 }
