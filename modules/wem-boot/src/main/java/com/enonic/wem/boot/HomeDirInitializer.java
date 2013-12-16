ebd7188dc55791b025e50ef87fad55ac0640833a
Sten Roger Sandvik <srs@enonic.com>
2013-12-16 14:25:45 +0100

    Added population of home dir.

---
+++ modules/wem-boot/src/main/java/com/enonic/wem/boot/HomeDirInitializer.java
@@ -0,0 +1,70 @@
+package com.enonic.wem.boot;
+
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.Set;
+
+import javax.servlet.ServletContext;
+
+import com.enonic.wem.core.home.HomeDir;
+
+final class HomeDirInitializer
+{
+    private final static String PATH_PREFIX = "/WEB-INF/home";
+
+    private final ServletContext context;
+
+    private final HomeDir homeDir;
+
+    public HomeDirInitializer( final ServletContext context, final HomeDir homeDir )
+    {
+        this.context = context;
+        this.homeDir = homeDir;
+    }
+
+    public void initialize()
+        throws Exception
+    {
+        copyAll( this.homeDir.toFile(), PATH_PREFIX );
+    }
+
+    private void copyAll( final File to, final String from )
+        throws Exception
+    {
+        copyFile( to, from );
+        copyDir( to, from );
+    }
+
+    private void copyFile( final File to, final String from )
+        throws Exception
+    {
+        final InputStream in = this.context.getResourceAsStream( from );
+        if ( in == null )
+        {
+            return;
+        }
+
+        if ( to.exists() )
+        {
+            return;
+        }
+
+        Files.copy( in, to.toPath() );
+    }
+
+    private void copyDir( final File to, final String from )
+        throws Exception
+    {
+        final Set<String> paths = this.context.getResourcePaths( from );
+        if ( paths == null )
+        {
+            return;
+        }
+
+        for ( final String path : paths )
+        {
+            copyAll( new File( this.homeDir.toFile(), path.substring( PATH_PREFIX.length() ) ), path );
+        }
+    }
+}
---
+++ modules/wem-webapp/src/main/webapp/WEB-INF/home/README.txt
@@ -0,0 +1,2 @@
+This folder contains a sample home that is right now popluated every time on startup. When the product is done, this will only be
+popluated when home directory does not exist.
