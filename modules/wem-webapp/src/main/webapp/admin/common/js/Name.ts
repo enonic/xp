b81c050a7570da7cb9f08819362f6ef77708e952
Jørund Vier Skriubakken <jvs@enonic.com>
2014-01-13 10:08:59 +0100

    CMS-2762 Make ImageSelector set a valid name of embedded image content Fixed problems with last commit

--- modules/wem-webapp/src/main/webapp/admin/common/js/Name.ts
+++ modules/wem-webapp/src/main/webapp/admin/common/js/Name.ts
@@ -2,15 +2,17 @@ module api {
 
     export class Name {
 
+        private static NAME_REG_EXP = new RegExp("^[_a-z0-9]([a-z0-9_\\-\\.])*$");
+
         private value:string;
 
         constructor(name:string) {
-            var nameRegExp = new RegExp("^[_a-z0-9]([a-z0-9_\\-\\.])*$");
-            api.util.assert(!name, "Name cannot be null");
 
-            api.util.assert(!name.trim(),"Name cannot be empty");
+            api.util.assertNotNull(name, "Name cannot be null");
 
-            api.util.assert(nameRegExp.test(name),
+            api.util.assert(!api.util.isStringEmpty(name),"Name cannot be empty");
+
+            api.util.assert(Name.NAME_REG_EXP.test(name),
                 "A name can only start with lower case latin letters or digit, and further consist of the same, digits or the following special chars: _-.: " + name);
 
             this.value = name;
