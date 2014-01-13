44890a8a549548867e9a905d0d5bae168c7f5327
Pavel Milkevich <pmi@enonic.com>
2014-01-13 16:43:58 +0300

    quick fix for the imports

--- modules/wem-webapp/src/main/webapp/admin/common/js/query/expr/_module.ts
+++ modules/wem-webapp/src/main/webapp/admin/common/js/query/expr/_module.ts
@@ -1,17 +1,17 @@
 ///<reference path='Expression.ts' />
 ///<reference path='ConstraintExpr.ts' />
-
 ///<reference path='CompareExpr.ts' />
-///<reference path='DynamicConstraintExpr.ts' />
-///<reference path='DynamicOrderExpr.ts' />
-///<reference path='FieldExpr.ts' />
-///<reference path='FieldOrderExpr.ts' />
-///<reference path='FunctionExpr.ts' />
-///<reference path='LogicalExpr.ts' />
-///<reference path='NotExpr.ts' />
 ///<reference path='OrderExpr.ts' />
 ///<reference path='QueryExpr.ts' />
 ///<reference path='ValueExpr.ts' />
+///<reference path='FunctionExpr.ts' />
+///<reference path='FieldExpr.ts' />
+
+///<reference path='DynamicConstraintExpr.ts' />
+///<reference path='DynamicOrderExpr.ts' />
+///<reference path='FieldOrderExpr.ts' />
+///<reference path='LogicalExpr.ts' />
+///<reference path='NotExpr.ts' />
 
 ///<reference path='CompareOperator.ts' />
 ///<reference path='LogicalOperator.ts' />
