5fabbb401af8ea57551f9c72f4db162cd0dcba74
Lasse Jul-Larsen <ljl@enonic.com>
2014-01-13 14:27:16 +0100

    CMS-2778 Create RichComboBox for PageTemplate-s

--- modules/wem-webapp/src/main/webapp/admin/apps/content-manager/js/app/wizard/page/_module.ts
+++ modules/wem-webapp/src/main/webapp/admin/apps/content-manager/js/app/wizard/page/_module.ts
@@ -1,4 +1,2 @@
-///<reference path='PageTemplateSelectedOptionsView.ts' />
-///<reference path='PageTemplateSelectedOptionView.ts' />
 ///<reference path='PageTemplateSelectorForm.ts' />
 ///<reference path='PageWizardStepForm.ts' />
