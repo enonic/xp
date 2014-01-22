2dabb838733360fb0cc24899c99a38c5ef3cb4bb
Jørund Vier Skriubakken <jvs@enonic.com>
2014-01-22 22:08:24 +0100

    CMS-2845 Rendering - Provide access to page data when content is created Added shortcut to save action, since clicking on save button in Wizard is not blocked

--- modules/wem-webapp/src/main/webapp/admin/common/js/app/wizard/SaveAction.ts
+++ modules/wem-webapp/src/main/webapp/admin/common/js/app/wizard/SaveAction.ts
@@ -3,7 +3,7 @@ module api.app.wizard {
     export class SaveAction extends api.ui.Action {
 
         constructor(wizardPanel: WizardPanel<any>) {
-            super("Save");
+            super("Save", "mod+alt+s");
 
             this.addExecutionListener(() => {
                 wizardPanel.saveChanges().
