69c88e3b849750d348616a673241d1f5b2e6a4d4
Aliaksei Biaskosty <abiaskosty@scnsoft.com>
2014-01-24 10:02:22 +0300

    Fix for default icon in ModuleComboBox
    default icon changed to puzzle.png

--- modules/wem-webapp/src/main/webapp/admin/common/js/module/ModuleComboBox.ts
+++ modules/wem-webapp/src/main/webapp/admin/common/js/module/ModuleComboBox.ts
@@ -13,7 +13,7 @@ module api.module {
         optionFormatter(row:number, cell:number, moduleInst:api.module.ModuleSummary, columnDef:any, dataContext:api.ui.combobox.Option<api.module.ModuleSummary>):string {
             var img = new api.dom.ImgEl();
             img.setClass("icon");
-            img.getEl().setSrc(api.util.getAdminUri("common/images/icons/icoMoon/32x32/earth.png"));
+            img.getEl().setSrc(api.util.getAdminUri("common/images/icons/icoMoon/32x32/puzzle.png"));
 
             var contentSummary = new api.dom.DivEl();
             contentSummary.setClass("item-summary");
@@ -52,7 +52,7 @@ module api.module {
 
         resolveIconUrl(content:api.module.ModuleSummary):string
         {
-            return api.util.getAdminUri("common/images/icons/icoMoon/128x128/earth.png");
+            return api.util.getAdminUri("common/images/icons/icoMoon/128x128/puzzle.png");
         }
 
         resolveTitle(content:api.module.ModuleSummary):string
