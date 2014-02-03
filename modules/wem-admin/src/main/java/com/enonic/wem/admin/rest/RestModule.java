a849be3953d85633c9493776c4b73226354c3c6b
Sten Roger Sandvik <srs@enonic.com>
2014-02-03 12:32:40 +0100

    CMS-2920 Remove unused admin jsonrpc methods.

--- modules/wem-admin/src/main/java/com/enonic/wem/admin/rest/RestModule.java
+++ modules/wem-admin/src/main/java/com/enonic/wem/admin/rest/RestModule.java
@@ -3,13 +3,7 @@ package com.enonic.wem.admin.rest;
 import com.google.inject.AbstractModule;
 
 import com.enonic.wem.admin.jsonrpc.JsonRpcHandlerBinder;
-import com.enonic.wem.admin.rpc.relationship.CreateRelationshipRpcHandler;
-import com.enonic.wem.admin.rpc.relationship.GetRelationshipRpcHandler;
-import com.enonic.wem.admin.rpc.relationship.UpdateRelationshipPropertiesRpcHandler;
 import com.enonic.wem.admin.rpc.system.GetSystemInfoRpcHandler;
-import com.enonic.wem.admin.rpc.util.GetCountriesRpcHandler;
-import com.enonic.wem.admin.rpc.util.GetLocalesRpcHandler;
-import com.enonic.wem.admin.rpc.util.GetTimeZonesRpcHandler;
 
 public final class RestModule
     extends AbstractModule
@@ -19,14 +13,6 @@ public final class RestModule
     {
         final JsonRpcHandlerBinder handlers = JsonRpcHandlerBinder.from( binder() );
 
-        handlers.add( GetCountriesRpcHandler.class );
-        handlers.add( GetLocalesRpcHandler.class );
-        handlers.add( GetTimeZonesRpcHandler.class );
-
         handlers.add( GetSystemInfoRpcHandler.class );
-
-        handlers.add( CreateRelationshipRpcHandler.class );
-        handlers.add( GetRelationshipRpcHandler.class );
-        handlers.add( UpdateRelationshipPropertiesRpcHandler.class );
     }
 }
--- modules/wem-webapp/src/main/webapp/admin/common/js/remote/util/RemoteSystemService.ts
+++ modules/wem-webapp/src/main/webapp/admin/common/js/remote/util/RemoteSystemService.ts
@@ -1,45 +1,23 @@
 module api.remote.util {
 
-    export var RemoteSystemService:RemoteSystemServiceInterface;
+    export var RemoteSystemService: RemoteSystemServiceInterface;
 
     export interface RemoteSystemServiceInterface {
-        util_getCountries (params:GetCountriesParams, success:(result:GetCountriesResult)=>void,
-                           failure?:(result:api.remote.FailureResult)=>void):void;
-        util_getLocales (params:GetLocalesParams, success:(result:GetLocalesResult)=>void,
-                         failure?:(result:api.remote.FailureResult)=>void):void;
-        util_getTimeZones (params:GetTimeZonesParams, success:(result:GetTimeZonesResult)=>void,
-                           failure?:(result:api.remote.FailureResult)=>void):void;
-        system_getSystemInfo (params:SystemGetSystemInfoParams, success:(result:SystemGetSystemInfoResult)=>void,
-                              failure?:(result:api.remote.FailureResult)=>void):void;
+        system_getSystemInfo (params: SystemGetSystemInfoParams, success: (result: SystemGetSystemInfoResult)=>void,
+                              failure?: (result: api.remote.FailureResult)=>void):void;
     }
 
     class RemoteSystemServiceImpl extends api.remote.BaseRemoteService implements RemoteSystemServiceInterface {
 
         constructor() {
-            var methods:string[] = [
-                "util.getCountries", "util.getLocales", "util.getTimeZones",
+            var methods: string[] = [
                 "system.getSystemInfo"
             ];
             super('api.remote.util.RemoteSystemService', methods);
         }
 
-        util_getCountries(params:GetCountriesParams, success:(result:GetCountriesResult)=>void,
-                          failure?:(result:api.remote.FailureResult)=>void):void {
-            console.log(params, success, failure);
-        }
-
-        util_getLocales(params:GetLocalesParams, success:(result:GetLocalesResult)=>void,
-                        failure?:(result:api.remote.FailureResult)=>void):void {
-            console.log(params, success, failure);
-        }
-
-        util_getTimeZones(params:GetTimeZonesParams, success:(result:GetTimeZonesResult)=>void,
-                          failure?:(result:api.remote.FailureResult)=>void):void {
-            console.log(params, success, failure);
-        }
-
-        system_getSystemInfo(params:SystemGetSystemInfoParams, success:(result:SystemGetSystemInfoResult)=>void,
-                             failure?:(result:api.remote.FailureResult)=>void):void {
+        system_getSystemInfo(params: SystemGetSystemInfoParams, success: (result: SystemGetSystemInfoResult)=>void,
+                             failure?: (result: api.remote.FailureResult)=>void): void {
             console.log(params, success, failure);
         }
     }
