7cb923f641a00574ad679f907d24399b433a4d28
Sten Roger Sandvik <srs@enonic.com>
2014-02-03 08:03:57 +0100

    CMS-2913 Implement wem:component thymeleaf processor.

---
+++ modules/wem-portal/src/main/java/com/enonic/wem/portal/view/thymeleaf/ComponentProcessor.java
@@ -0,0 +1,45 @@
+package com.enonic.wem.portal.view.thymeleaf;
+
+import java.util.List;
+
+import org.thymeleaf.Arguments;
+import org.thymeleaf.dom.Element;
+import org.thymeleaf.dom.Node;
+import org.thymeleaf.processor.attr.AbstractFragmentHandlingAttrProcessor;
+
+import com.google.common.collect.Lists;
+
+final class ComponentProcessor
+    extends AbstractFragmentHandlingAttrProcessor
+{
+    public ComponentProcessor()
+    {
+        super( "component" );
+    }
+
+    @Override
+    public int getPrecedence()
+    {
+        return 100;
+    }
+
+    @Override
+    protected boolean getRemoveHostNode( final Arguments arguments, final Element element, final String attributeName,
+                                         final String attributeValue )
+    {
+        return false;
+    }
+
+    @Override
+    protected List<Node> computeFragment( final Arguments arguments, final Element element, final String attributeName,
+                                          final String attributeValue )
+    {
+        final List<Node> list = Lists.newArrayList();
+
+        final Element elem = new Element( "div" );
+        elem.setAttribute( "wem:component", attributeValue );
+        list.add( elem );
+
+        return list;
+    }
+}
--- modules/wem-portal/src/main/java/com/enonic/wem/portal/view/thymeleaf/ThymeleafDialect.java
+++ modules/wem-portal/src/main/java/com/enonic/wem/portal/view/thymeleaf/ThymeleafDialect.java
@@ -1,10 +1,22 @@
 package com.enonic.wem.portal.view.thymeleaf;
 
+import java.util.Set;
+
+import org.thymeleaf.processor.IProcessor;
 import org.thymeleaf.standard.StandardDialect;
+
+import com.google.common.collect.Sets;
 
 final class ThymeleafDialect
     extends StandardDialect
 {
+    public ThymeleafDialect()
+    {
+        final Set<IProcessor> processors = Sets.newHashSet();
+        processors.add( new ComponentProcessor() );
+        setAdditionalProcessors( processors );
+    }
+
     @Override
     public String getPrefix()
     {
