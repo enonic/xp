f9dd276587ca5ac51f2f620278f70046f39e88f3
Runar Myklebust <rmy@enonic.com>
2014-02-24 10:03:21 +0100

    CMS-2368 Fix orderby for properties with several values
    Added tests

--- modules/wem-core/src/test/java/com/enonic/wem/core/index/elastic/indexsource/XContentBuilderFactoryTest.java
+++ modules/wem-core/src/test/java/com/enonic/wem/core/index/elastic/indexsource/XContentBuilderFactoryTest.java
@@ -1,5 +1,6 @@
 package com.enonic.wem.core.index.elastic.indexsource;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
@@ -7,12 +8,20 @@ import org.elasticsearch.common.collect.Tuple;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentHelper;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.joda.time.DateTime;
 import org.junit.Test;
 
+import com.enonic.wem.api.data.Value;
 import com.enonic.wem.core.index.Index;
 import com.enonic.wem.core.index.IndexConstants;
 import com.enonic.wem.core.index.IndexType;
 import com.enonic.wem.core.index.document.IndexDocument;
+import com.enonic.wem.core.index.document.IndexDocumentDateItem;
+import com.enonic.wem.core.index.document.IndexDocumentGeoPointItem;
+import com.enonic.wem.core.index.document.IndexDocumentItemPath;
+import com.enonic.wem.core.index.document.IndexDocumentNumberItem;
+import com.enonic.wem.core.index.document.IndexDocumentOrderbyItem;
+import com.enonic.wem.core.index.document.IndexDocumentStringItem;
 import com.enonic.wem.core.index.elastic.XContentBuilderFactory;
 
 import static org.junit.Assert.*;
@@ -42,21 +51,44 @@ public class XContentBuilderFactoryTest
     }
 
     @Test
+    public void multiple_orderby_entries_gives_one()
+    {
+        IndexDocument indexDocument = IndexDocument.newIndexDocument().
+            index( Index.NODB ).
+            indexType( IndexType.NODE ).
+            addEntry( new IndexDocumentStringItem( IndexDocumentItemPath.from( "myField" ), "myValue1" ) ).
+            addEntry( new IndexDocumentStringItem( IndexDocumentItemPath.from( "myField" ), "myValue2" ) ).
+            addEntry( new IndexDocumentOrderbyItem( IndexDocumentItemPath.from( "myField" ), "myOrderByValue1" ) ).
+            addEntry( new IndexDocumentOrderbyItem( IndexDocumentItemPath.from( "myField" ), "myOrderByValue2" ) ).
+            build();
+
+        final XContentBuilder xContentBuilder = XContentBuilderFactory.create( indexDocument );
+
+        final Map<String, Object> objectMap = getObjectMap( xContentBuilder );
+
+        final Object myString = objectMap.get( "myfield" );
+        assertTrue( myString instanceof ArrayList );
+        assertTrue( ( (ArrayList) myString ).size() == 2 );
+        final Object myOrder = objectMap.get( "myfield._orderby" );
+        assertTrue( myOrder instanceof String );
+    }
+
+    @Test
     public void create_given_indexdocument_arrayvalues()
         throws Exception
     {
-        /*
-        IndexDocument2 indexDocument = IndexDocument2.newIndexDocument().
+
+        IndexDocument indexDocument = IndexDocument.newIndexDocument().
             index( Index.NODB ).
             indexType( IndexType.NODE ).
-            addEntry( new IndexDocumentStringItem( "myField", "myValue1" ) ).
-            addEntry( new IndexDocumentStringItem( "myField", "myValue2" ) ).
-            addEntry( new IndexDocumentNumberItem( "myNumericField", 1.0 ) ).
-            addEntry( new IndexDocumentNumberItem( "myNumericField", 2.0 ) ).
-            addEntry( new IndexDocumentDateItem( "myDateField", DateTime.now() ) ).
-            addEntry( new IndexDocumentDateItem( "myDateField", DateTime.now() ) ).
-            addEntry( new IndexDocumentGeoPointItem( "myGeoPoint", new Value.GeoPoint( "80,80" ) ) ).
-            addEntry( new IndexDocumentGeoPointItem( "myGeoPoint", new Value.GeoPoint( "81,81" ) ) ).
+            addEntry( new IndexDocumentStringItem( IndexDocumentItemPath.from( "myField" ), "myValue1" ) ).
+            addEntry( new IndexDocumentStringItem( IndexDocumentItemPath.from( "myField" ), "myValue2" ) ).
+            addEntry( new IndexDocumentNumberItem( IndexDocumentItemPath.from( "myNumericField" ), 1.0 ) ).
+            addEntry( new IndexDocumentNumberItem( IndexDocumentItemPath.from( "myNumericField" ), 2.0 ) ).
+            addEntry( new IndexDocumentDateItem( IndexDocumentItemPath.from( "myDateField" ), DateTime.now() ) ).
+            addEntry( new IndexDocumentDateItem( IndexDocumentItemPath.from( "myDateField" ), DateTime.now() ) ).
+            addEntry( new IndexDocumentGeoPointItem( IndexDocumentItemPath.from( "myGeoPoint" ), new Value.GeoPoint( "80,80" ) ) ).
+            addEntry( new IndexDocumentGeoPointItem( IndexDocumentItemPath.from( "myGeoPoint" ), new Value.GeoPoint( "81,81" ) ) ).
             build();
 
         final XContentBuilder xContentBuilder = XContentBuilderFactory.create( indexDocument );
@@ -74,7 +106,7 @@ public class XContentBuilderFactoryTest
 
         final Collection<Object> geoPointObjectValue = getObjectValue( "mygeopoint._geopoint", objectMap );
         assertEquals( 2, geoPointObjectValue.size() );
-   */
+
     }
 
 
