89218b8e7951eff8b598df8067e87a397051697e
Alex Rodriguez <aro@enonic.com>
2014-02-27 07:24:58 +0100

    CMS-3001 Display ComboBox for selecting descriptor in each page component. Reverted combobox in image inspection panel.

--- modules/wem-webapp/src/main/webapp/admin/apps/content-manager/js/app/contextwindow/ImageInspectionPanel.ts
+++ modules/wem-webapp/src/main/webapp/admin/apps/content-manager/js/app/contextwindow/ImageInspectionPanel.ts
@@ -6,40 +6,47 @@ module app.contextwindow {
     export class ImageInspectionPanel extends PageComponentInspectionPanel<api.content.page.image.ImageComponent> {
 
         private imageComponent: api.content.page.image.ImageComponent;
-        private imageDescriptors: {
-            [key: string]: ImageDescriptor;
-        };
+        private descriptorComboBox: api.content.page.image.ImageDescriptorComboBox;
 
         constructor(liveFormPanel: app.wizard.LiveFormPanel, siteTemplate: SiteTemplate) {
             super("live-edit-font-icon-image", liveFormPanel, siteTemplate);
-            this.imageDescriptors = {};
             this.initElements();
         }
 
         private initElements() {
-            var getImageDescriptorsRequest = new api.content.page.image.GetImageDescriptorsByModulesRequest(this.getSiteTemplate().getModules());
-            getImageDescriptorsRequest.sendAndParse().done((results: ImageDescriptor[]) => {
-                results.forEach((imageDescriptor: ImageDescriptor) => {
-                    this.imageDescriptors[imageDescriptor.getKey().toString()] = imageDescriptor;
+            var descriptorHeader = new api.dom.H6El();
+            descriptorHeader.setText("Descriptor:");
+            descriptorHeader.addClass("descriptor-header");
+            this.appendChild(descriptorHeader);
+
+            var imageDescriptorsRequest = new api.content.page.image.GetImageDescriptorsByModulesRequest(this.getSiteTemplate().getModules());
+            var imageDescriptorLoader = new api.content.page.image.ImageDescriptorLoader(imageDescriptorsRequest);
+            this.descriptorComboBox = new api.content.page.image.ImageDescriptorComboBox(imageDescriptorLoader);
+
+            var onDescriptorsLoaded = () => {
+                this.descriptorComboBox.setDescriptor(this.getLiveFormPanel().getDefaultImageDescriptor().getKey());
+                this.descriptorComboBox.removeLoadedListener(onDescriptorsLoaded); // execute only on the first loaded event
+            };
+            this.descriptorComboBox.addLoadedListener(onDescriptorsLoaded);
+
+            this.descriptorComboBox.addOptionSelectedListener((option: api.ui.combobox.Option<ImageDescriptor>) => {
+                if (this.imageComponent) {
+                    var selectedDescriptor = option.displayValue.getKey();
+                    this.imageComponent.setDescriptor(selectedDescriptor);
+                }
                 });
-            });
+            this.appendChild(this.descriptorComboBox);
         }
         
-        private getDescriptor(key: api.content.page.DescriptorKey): ImageDescriptor {
-            return this.imageDescriptors[key.toString()];
-        }
-
         setImageComponent(component: api.content.page.image.ImageComponent) {
             this.setComponent(component);
             this.imageComponent = component;
 
             var descriptorKey = component.getDescriptor();
             if (descriptorKey) {
-                var imageDescriptor = this.getDescriptor(descriptorKey);
-                if (!imageDescriptor) {
-                    console.warn('Image descriptor not found' + descriptorKey);
-                    return;
-                }
+                this.descriptorComboBox.setDescriptor(descriptorKey);
+                var imageDescriptorOption: api.ui.combobox.Option<ImageDescriptor> = this.descriptorComboBox.getSelectedData()[0];
+                var imageDescriptor = imageDescriptorOption.displayValue;
                 this.setupComponentForm(component, imageDescriptor);
             }
         }
