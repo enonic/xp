ef30e8fa96f8383927c10854626cb59da5a4a0fc
Pavel Milkevich <pmi@enonic.com>
2014-03-05 15:29:39 +0300

    CMS-3050 Ensure that all kinds of content of a WizardStepPanel gets a minimum margin/padding of 10px
    - renamed step-panel to wizard-step-deck-panel

--- modules/wem-webapp/src/main/webapp/admin/common/js/app/wizard/WizardStepDeckPanel.ts
+++ modules/wem-webapp/src/main/webapp/admin/common/js/app/wizard/WizardStepDeckPanel.ts
@@ -3,7 +3,7 @@ module api.app.wizard {
     export class WizardStepDeckPanel extends api.ui.NavigatedDeckPanel {
 
         constructor(navigator:WizardStepNavigator) {
-            super(navigator, "step-panel");
+            super(navigator, "wizard-step-deck-panel");
         }
 
     }
