0760a5a4c91e80f341b413d0c8bad8c1e97928ee
Raman Yafimchyk <rya@enonic.com>
2014-03-06 12:04:25 +0300

    CMS-3059 Content Browse Detail Pane - Fix better "empty" message

--- modules/wem-webapp/src/main/webapp/admin/common/js/app/browse/ItemsSelectionPanel.ts
+++ modules/wem-webapp/src/main/webapp/admin/common/js/app/browse/ItemsSelectionPanel.ts
@@ -8,8 +8,8 @@ module api.app.browse {
         private messageForNoSelection = "You are wasting this space - select something!";
 
         constructor() {
-            super("empty-space");
-            this.getEl().setInnerHtml(this.messageForNoSelection);
+            super();
+            this.getEl().addClass('empty-space').setInnerHtml(this.messageForNoSelection);
         }
 
         setItems(items: BrowseItem<M>[]) {
@@ -36,6 +36,7 @@ module api.app.browse {
             }
 
             if (this.items.length == 0) {
+                this.removeClass('empty-space');
                 this.removeChildren();
             }
 
@@ -60,7 +61,7 @@ module api.app.browse {
             this.items.splice(index, 1);
 
             if (this.items.length == 0) {
-                this.getEl().setInnerHtml(this.messageForNoSelection);
+                this.getEl().addClass('empty-space').setInnerHtml(this.messageForNoSelection);
             }
 
             this.notifyDeselected(item);
