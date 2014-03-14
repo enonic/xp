c4b01226afcc6e23f3041aa1d9156364b8116026
Sten Roger Sandvik <srs@enonic.com>
2014-03-14 10:56:36 +0100

    Added evaluation of expression in thymeleaf component processor.

--- modules/wem-portal/src/main/java/com/enonic/wem/portal/view/thymeleaf/ComponentProcessor.java
+++ modules/wem-portal/src/main/java/com/enonic/wem/portal/view/thymeleaf/ComponentProcessor.java
@@ -3,10 +3,14 @@ package com.enonic.wem.portal.view.thymeleaf;
 import java.util.List;
 
 import org.thymeleaf.Arguments;
+import org.thymeleaf.Configuration;
 import org.thymeleaf.dom.Comment;
 import org.thymeleaf.dom.Element;
 import org.thymeleaf.dom.Node;
 import org.thymeleaf.processor.attr.AbstractFragmentHandlingAttrProcessor;
+import org.thymeleaf.standard.expression.IStandardExpression;
+import org.thymeleaf.standard.expression.IStandardExpressionParser;
+import org.thymeleaf.standard.expression.StandardExpressions;
 
 import com.google.common.collect.Lists;
 
@@ -35,8 +39,16 @@ final class ComponentProcessor
     protected List<Node> computeFragment( final Arguments arguments, final Element element, final String attributeName,
                                           final String attributeValue )
     {
+        final Configuration configuration = arguments.getConfiguration();
+        final IStandardExpressionParser expressionParser = StandardExpressions.getExpressionParser( configuration );
+
+        final IStandardExpression expression = expressionParser.parseExpression( configuration, arguments, attributeValue );
+        final Object result = expression.execute( configuration, arguments );
+
+        final String componentPath = ( result == null ? "" : result.toString() );
+
         final List<Node> list = Lists.newArrayList();
-        final Comment comment = new Comment( "# COMPONENT " + attributeValue + " " );
+        final Comment comment = new Comment( "# COMPONENT " + componentPath + " " );
         list.add( comment );
         return list;
     }
