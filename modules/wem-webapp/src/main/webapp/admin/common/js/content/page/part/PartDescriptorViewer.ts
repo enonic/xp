5e26cb2a7ed77b6bb88ff4ad10012e51cfeb8568
Alex Rodriguez <aro@enonic.com>
2014-03-20 09:42:11 +0100

    CMS-3116 Live Edit - Add part icon to part selector combobox. Minor changes.

--- modules/wem-webapp/src/main/webapp/admin/common/js/content/page/part/PartDescriptorViewer.ts
+++ modules/wem-webapp/src/main/webapp/admin/common/js/content/page/part/PartDescriptorViewer.ts
@@ -8,14 +8,14 @@ module api.content.page.part {
             super();
             this.namesAndIconView = new api.app.NamesAndIconViewBuilder().
                 setSize(api.app.NamesAndIconViewSize.small).build();
+            this.namesAndIconView.setIconUrl(api.util.getAdminUri('common/images/icons/icoMoon/32x32/puzzle.png'));
             this.appendChild(this.namesAndIconView);
         }
 
         setObject(partDescriptor: PartDescriptor) {
             super.setObject(partDescriptor);
             this.namesAndIconView.setMainName(partDescriptor.getDisplayName()).
-                setSubName(partDescriptor.getName().toString()).
-                setIconUrl(api.util.getAdminUri('common/images/icons/icoMoon/32x32/puzzle.png'));
+                setSubName(partDescriptor.getName().toString());
         }
 
         getPreferredHeight(): number {
