ebf1ef65a4471398ccf53a14951dab93444d1fb2
Alex Rodriguez <aro@enonic.com>
2014-03-21 13:12:10 +0100

    CMS-2971 Ensure it's possible to render a Component not existing in the Page's template. Unit test.

--- modules/wem-portal/src/test/java/com/enonic/wem/portal/postprocess/instruction/ComponentInstructionTest.java
+++ modules/wem-portal/src/test/java/com/enonic/wem/portal/postprocess/instruction/ComponentInstructionTest.java
@@ -9,9 +9,14 @@ import com.enonic.wem.api.account.UserKey;
 import com.enonic.wem.api.content.Content;
 import com.enonic.wem.api.content.ContentId;
 import com.enonic.wem.api.content.ContentPath;
+import com.enonic.wem.api.content.page.ComponentName;
+import com.enonic.wem.api.content.page.DescriptorKey;
 import com.enonic.wem.api.content.page.Page;
+import com.enonic.wem.api.content.page.PageComponent;
 import com.enonic.wem.api.content.page.PageComponentService;
+import com.enonic.wem.api.content.page.PageDescriptorKey;
 import com.enonic.wem.api.content.page.PageRegions;
+import com.enonic.wem.api.content.page.PageTemplate;
 import com.enonic.wem.api.content.page.PageTemplateKey;
 import com.enonic.wem.api.content.page.part.PartDescriptorKey;
 import com.enonic.wem.api.content.page.region.Region;
@@ -19,6 +24,7 @@ import com.enonic.wem.api.content.site.Site;
 import com.enonic.wem.api.data.Property;
 import com.enonic.wem.api.data.RootDataSet;
 import com.enonic.wem.api.data.Value;
+import com.enonic.wem.api.module.ModuleKey;
 import com.enonic.wem.api.rendering.Renderable;
 import com.enonic.wem.api.schema.content.ContentTypeName;
 import com.enonic.wem.portal.controller.JsContext;
@@ -31,7 +37,9 @@ import static com.enonic.wem.api.content.page.part.PartComponent.newPartComponen
 import static com.enonic.wem.api.content.page.region.Region.newRegion;
 import static junit.framework.Assert.assertEquals;
 import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ComponentInstructionTest
 {
@@ -54,6 +62,48 @@ public class ComponentInstructionTest
 
         final String outputHtml = instruction.evaluate( context, "COMPONENT myRegion/myPartComponent" );
         assertEquals( "<b>part content</b>", outputHtml );
+    }
+
+    @Test
+    public void testInstructionRenderByName()
+        throws Exception
+    {
+        final RendererFactory rendererFactory = newRendererFactory( "<b>part content</b>" );
+        final PageComponentService pageComponentService = Mockito.mock( PageComponentService.class );
+
+        final PageComponent<? extends DescriptorKey> component = createPartComponent();
+        doReturn( component ).when( pageComponentService ).getByName( isA( ModuleKey.class ), isA( ComponentName.class ) );
+        final ComponentInstruction instruction = new ComponentInstruction( rendererFactory, pageComponentService );
+
+        final JsHttpResponse resp = new JsHttpResponse();
+        resp.setPostProcess( true );
+        final JsContext context = new JsContext();
+        context.setResponse( resp );
+        Content content = createPage( "content-id", "content-name", "content-type" );
+        context.setContent( content );
+        Content siteContent = createSite( "site-id", "site-name", "content-type" );
+        context.setSiteContent( siteContent );
+        PageTemplate pageTemplate = createPageTemplate();
+        context.setPageTemplate( pageTemplate );
+
+        final String outputHtml = instruction.evaluate( context, "COMPONENT module:myPartComponent" );
+        assertEquals( "<b>part content</b>", outputHtml );
+    }
+
+    private PageTemplate createPageTemplate()
+    {
+        return PageTemplate.newPageTemplate().
+            key( PageTemplateKey.from( "mymodule|my-page" ) ).
+            descriptor( PageDescriptorKey.from( "mymodule-1.0.0:mypagetemplate" ) ).
+            build();
+    }
+
+    private PageComponent<? extends DescriptorKey> createPartComponent()
+    {
+        return newPartComponent().
+            name( "myPartComponent" ).
+            descriptor( PartDescriptorKey.from( "mymodule-1.0.0:myparttemplate" ) ).
+            build();
     }
 
     private Content createPage( final String id, final String name, final String contentTypeName )
@@ -124,7 +174,7 @@ public class ComponentInstructionTest
                 return Response.ok( renderResult ).build();
             }
         };
-        Mockito.when( rendererFactory.getRenderer( isA( Renderable.class ) ) ).thenReturn( renderer );
+        when( rendererFactory.getRenderer( isA( Renderable.class ) ) ).thenReturn( renderer );
         return rendererFactory;
     }
 }
