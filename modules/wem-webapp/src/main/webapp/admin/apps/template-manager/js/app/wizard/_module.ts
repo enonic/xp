388ad6882f4e40197272ccffcfba388c87ce5dc4
Aliaksei Biaskosty <abiaskosty@scnsoft.com>
2014-04-01 13:20:25 +0300

    CMS-3263 Content Wizard - Add dummy steps

--- modules/wem-webapp/src/main/webapp/admin/apps/template-manager/js/app/wizard/_module.ts
+++ modules/wem-webapp/src/main/webapp/admin/apps/template-manager/js/app/wizard/_module.ts
@@ -1,7 +1,4 @@
-///<reference path='ComponentsWizardStepForm.ts' />
-///<reference path='ContentWizardStepForm.ts' />
 ///<reference path='SiteTemplateWizardStepForm.ts' />
-///<reference path='SummaryWizardStepForm.ts' />
 ///<reference path='SiteTemplateWizardPanel.ts' />
 ///<reference path='SiteTemplateWizardToolbar.ts' />
 
