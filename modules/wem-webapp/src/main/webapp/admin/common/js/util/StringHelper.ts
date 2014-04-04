16d2be5c7b66eb1d2696dd78629be887a9b127f4
Mikita Taukachou <mta@enonic.com>
2014-04-04 16:16:05 +0300

    CMS-3242 Live Edit - Automatically rename components when configuring from empty mode
    Replaced formCleanString() with removeInvalidChars() util function.

--- modules/wem-webapp/src/main/webapp/admin/common/js/util/StringHelper.ts
+++ modules/wem-webapp/src/main/webapp/admin/common/js/util/StringHelper.ts
@@ -13,16 +13,8 @@ module api.util {
         return str.replace(/(?:^|\s)\S/g, function(ch) { return ch.toUpperCase(); });
     }
 
-    export function formCleanString(str: string) {
-        var result:string = "",
-            ch:RegExpExecArray;
-        var regex = /[\w,-,_]+/g;
-
-        while (ch = regex.exec(str)) {
-            result += ch.toString();
-        }
-
-        return result;
+    export function removeInvalidChars(str: string) {
+        return str.replace(/\s/g, "");
     }
 
     export function isStringEmpty(str: string) {
