99706668d3d5237660d67ef47f2c76ed31abb0f5
Sten Roger Sandvik <srs@enonic.com>
2014-04-07 17:09:51 +0200

    CMS-3341 Implement generic module source access. Binding SourceResolver to guice.

--- modules/wem-core/src/main/java/com/enonic/wem/core/module/ModuleModule.java
+++ modules/wem-core/src/main/java/com/enonic/wem/core/module/ModuleModule.java
@@ -6,6 +6,8 @@ import com.google.inject.AbstractModule;
 
 import com.enonic.wem.api.module.ModuleService;
 import com.enonic.wem.core.command.CommandBinder;
+import com.enonic.wem.core.module.source.SourceResolver;
+import com.enonic.wem.core.module.source.SourceResolverImpl;
 
 public final class ModuleModule
     extends AbstractModule
@@ -14,6 +16,7 @@ public final class ModuleModule
     protected void configure()
     {
         bind( ModuleService.class ).to( ModuleServiceImpl.class ).in( Singleton.class );
+        bind( SourceResolver.class ).to( SourceResolverImpl.class ).in( Singleton.class );
 
         final CommandBinder commands = CommandBinder.from( binder() );
         commands.add( GetModuleResourceHandler.class );
