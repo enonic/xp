2b72c1c4e8c1d9628b6a187298c10aaa3fde564a
Pavel Milkevich <pmi@enonic.com>
2014-04-09 17:46:26 +0300

    CMS-3329 Wizard - Save keyboard shortcut often gets "lost"

--- modules/wem-webapp/src/main/webapp/admin/live-edit/js/main.ts
+++ modules/wem-webapp/src/main/webapp/admin/live-edit/js/main.ts
@@ -55,8 +55,8 @@ function notifyImageUploaded(event: api.ui.ImageUploadedEvent) {
     })
 }
 
-function getComponentByPath(path:string): LiveEdit.component.Component {
-    return LiveEdit.component.Component.fromJQuery($('[data-live-edit-component="'+ path +'"]'));
+function getComponentByPath(path: string): LiveEdit.component.Component {
+    return LiveEdit.component.Component.fromJQuery($('[data-live-edit-component="' + path + '"]'));
 }
 
 (function ($) {
@@ -87,18 +87,39 @@ function getComponentByPath(path:string): LiveEdit.component.Component {
         $(window).unload(() => console.log('Clean up any css classes etc. that live edit / sortable has added'));
 
         //TODO: move this somewhere logical
-        $(window).on('componentLoaded.liveEdit', (event, component:LiveEdit.component.Component) => {
+        $(window).on('componentLoaded.liveEdit', (event, component: LiveEdit.component.Component) => {
             if (component.getComponentType().getType() == LiveEdit.component.Type.LAYOUT) {
                 LiveEdit.component.dragdropsort.DragDropSort.createSortableLayout(component);
             }
         })
     });
 
-//    Prevent the user from clicking on things
-//    This needs more work as we want them to click on Live Edit ui stuff.
     $(document).ready(() => {
+        // Prevent the user from clicking on things
+        // This needs more work as we want them to click on Live Edit ui stuff.
         $(document).on('mousedown', 'btn, button, a, select, input', (event) => {
             event.preventDefault();
+        });
+
+
+        // Notify parent frame if any modifier except shift is pressed
+        // For the parent shortcuts to work if the inner iframe has focus
+        $(document).on("keypress keydown keyup", (event) => {
+
+            if ((event.metaKey || event.ctrlKey || event.altKey) && event.keyCode) {
+                $(parent.document).simulate(event.type, {
+                    bubbles: event.bubbles,
+                    cancelable: event.cancelable,
+                    view: parent,
+                    ctrlKey: event.ctrlKey,
+                    altKey: event.altKey,
+                    shiftKey: event.shiftKey,
+                    metaKey: event.metaKey,
+                    keyCode: event.keyCode,
+                    charCode: event.charCode
+                });
+                return false;
+            }
         });
     });
 
