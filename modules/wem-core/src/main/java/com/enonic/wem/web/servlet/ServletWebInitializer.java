9e19b5c936c43714c0cbce76c05ed3c624ff5fbf
Sten Roger Sandvik <srs@enonic.com>
2014-04-09 13:43:43 +0200

    Added shallow etag filter.

--- modules/wem-core/src/main/java/com/enonic/wem/web/servlet/ServletWebInitializer.java
+++ modules/wem-core/src/main/java/com/enonic/wem/web/servlet/ServletWebInitializer.java
@@ -1,8 +1,13 @@
 package com.enonic.wem.web.servlet;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 
 import com.enonic.wem.web.WebInitializer;
+import com.enonic.wem.web.etag.ShallowEtagFilter;
 
 final class ServletWebInitializer
     implements WebInitializer
@@ -11,5 +16,10 @@ final class ServletWebInitializer
     public void initialize( final ServletContext context )
     {
         context.addListener( new RequestContextListener() );
+
+        final FilterRegistration.Dynamic reg = context.addFilter( "etag", new ShallowEtagFilter() );
+
+        final EnumSet<DispatcherType> dispatcherTypes = EnumSet.allOf( DispatcherType.class );
+        reg.addMappingForUrlPatterns( dispatcherTypes, false, "/admin/*" );
     }
 }
