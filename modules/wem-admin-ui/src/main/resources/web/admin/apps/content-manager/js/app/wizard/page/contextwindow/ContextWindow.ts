42052138c912545ffbc0d17eeb5ad5a132c3e73b
Mikita Taukachou <mta@enonic.com>
2014-05-07 17:07:43 +0300

    CMS-3417 Split Screen - Set "pinned" mode dynamically
    Moved PinButton class and less styles to the separate files.

--- modules/wem-admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/wizard/page/contextwindow/ContextWindow.ts
+++ modules/wem-admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/wizard/page/contextwindow/ContextWindow.ts
@@ -154,18 +154,4 @@ module app.wizard.page.contextwindow {
             }
         }
     }
-
-    export class PinButton extends api.ui.Button {
-        constructor(contextWindow: ContextWindow) {
-            super("");
-            this.addClass("pin-button icon-pushpin");
-            this.setActive(contextWindow.isPinned());
-
-            this.onClicked((event: MouseEvent) => {
-                contextWindow.setDynamicPinning(false);
-                contextWindow.setPinned(!contextWindow.isPinned());
-                this.setActive(contextWindow.isPinned());
-            });
-        }
-    }
 }
--- modules/wem-admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/wizard/page/contextwindow/_module.ts
+++ modules/wem-admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/wizard/page/contextwindow/_module.ts
@@ -1,3 +1,4 @@
 ///<reference path='EmulatorGrid.ts' />
 ///<reference path='EmulatorPanel.ts' />
 ///<reference path='ContextWindow.ts' />
+///<reference path='PinButton.ts' />
