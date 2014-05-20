355692cf89d29cb5c71c40c1b3bb244b3e3a3dbe
Sten Roger Sandvik <srs@enonic.com>
2014-05-20 07:32:53 +0200

    CMS-3561 Make admin module work in OSGi. Broke down StartupInitializer into interface/implementation.

--- modules/wem-admin/src/main/java/com/enonic/wem/admin/rest/resource/tools/ToolsResource.java
+++ modules/wem-admin/src/main/java/com/enonic/wem/admin/rest/resource/tools/ToolsResource.java
@@ -6,28 +6,13 @@ import javax.ws.rs.Produces;
 
 import com.google.inject.Inject;
 
-import com.enonic.wem.core.elasticsearch.ElasticsearchIndexService;
-import com.enonic.wem.core.index.Index;
 import com.enonic.wem.core.initializer.StartupInitializer;
 
 @Path("tools")
 public final class ToolsResource
 {
-    private ElasticsearchIndexService indexService;
-
-    private StartupInitializer startupInitializer;
-
     @Inject
-    public void setIndexService( final ElasticsearchIndexService indexService )
-    {
-        this.indexService = indexService;
-    }
-
-    @Inject
-    public void setStartupInitializer( final StartupInitializer startupInitializer )
-    {
-        this.startupInitializer = startupInitializer;
-    }
+    protected StartupInitializer startupInitializer;
 
     @GET
     @Path("cleanData")
@@ -35,14 +20,7 @@ public final class ToolsResource
     public String cleanData()
         throws Exception
     {
-        this.indexService.deleteIndex( Index.NODB );
-        this.indexService.createIndex( Index.NODB );
-        this.indexService.deleteIndex( Index.STORE );
-        this.indexService.createIndex( Index.STORE );
-        this.indexService.deleteIndex( Index.WORKSPACE );
-        this.indexService.createIndex( Index.WORKSPACE );
-
-        this.startupInitializer.initialize();
+        this.startupInitializer.initialize( true );
         return "Done.";
     }
 }
--- modules/wem-core/src/main/java/com/enonic/wem/core/initializer/StartupInitializer.java
+++ modules/wem-core/src/main/java/com/enonic/wem/core/initializer/StartupInitializer.java
@@ -1,31 +1,7 @@
 package com.enonic.wem.core.initializer;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-
-import javax.inject.Inject;
-
-import com.google.common.collect.Lists;
-
-public final class StartupInitializer
+public interface StartupInitializer
 {
-    private final List<InitializerTask> tasks;
-
-    @Inject
-    public StartupInitializer( final Set<InitializerTask> tasks )
-    {
-        final List<InitializerTask> sortedTaskList = Lists.newArrayList( tasks );
-        Collections.sort( sortedTaskList );
-        this.tasks = sortedTaskList;
-    }
-
-    public void initialize()
-        throws Exception
-    {
-        for ( final InitializerTask task : this.tasks )
-        {
-            task.initialize();
-        }
-    }
+    public void initialize( boolean reinit )
+        throws Exception;
 }
