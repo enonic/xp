37a7e3d6d77c6a6c5642c9b346d4e2cd57c3f5e6
Jørund Vier Skriubakken <jvs@enonic.com>
2014-05-20 16:17:04 +0200

    CMS-3557 LiveEdit - Create reusable class between admin and live-edit representing selectable item in Page Replaced usage of Component.getComponentType with getType in DuplicateMenuItem

--- modules/wem-admin-ui/src/main/resources/web/admin/live-edit/js/ui/contextmenu/menuitem/DuplicateMenuItem.ts
+++ modules/wem-admin-ui/src/main/resources/web/admin/live-edit/js/ui/contextmenu/menuitem/DuplicateMenuItem.ts
@@ -24,7 +24,7 @@ module LiveEdit.ui.contextmenu.menuitem {
 
         private onDuplicateComponent() {
             var component = this.menu.selectedComponent;
-            var placeholder = LiveEdit.component.ComponentPlaceholder.fromComponent(LiveEdit.component.Type[component.getComponentType().getName().toUpperCase()]);
+            var placeholder = LiveEdit.component.ComponentPlaceholder.fromComponent(LiveEdit.component.Type[component.getType().getShortName().toUpperCase()]);
             placeholder.getEl().insertAfterEl(component);
             placeholder.init();
             placeholder.showLoadingSpinner();
