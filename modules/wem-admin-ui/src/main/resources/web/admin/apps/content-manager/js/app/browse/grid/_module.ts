61e9ba01c15576c5339694943047eebfdf551a6d
Mikita Taukachou <mta@enonic.com>
2014-06-10 16:40:30 +0300

    CMS-3592 Introduce GridToolbar with SelectAll and ClearSelection actions
    Made GridToolbar to implement default toolbar.
    Added toolbar Actions.
    Fixed bug with css row selection on scrollbar appearing.

--- modules/wem-admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/browse/grid/_module.ts
+++ modules/wem-admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/browse/grid/_module.ts
@@ -2,5 +2,6 @@
 ///<reference path='ContentGridStore.ts' />
 ///<reference path='ContentGridPanel2.ts' />
 ///<reference path='GridToolbar.ts' />
+///<reference path='GridActions.ts' />
 ///<reference path='ContentGridCache2.ts' />
 ///<reference path='ContentGridCacheItem2.ts' />
