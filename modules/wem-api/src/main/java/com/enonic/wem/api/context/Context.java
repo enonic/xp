1167ba1cc5b6952749723322c378f95bf34e1e67
Runar Myklebust <rmy@enonic.com>
2014-06-16 18:25:41 +0200

    CMS-3686 Versioning and workspace: Clean up dependencies in NodeCommands
    Ops, fixed failing test

--- modules/wem-api/src/main/java/com/enonic/wem/api/context/Context.java
+++ modules/wem-api/src/main/java/com/enonic/wem/api/context/Context.java
@@ -15,4 +15,33 @@ public class Context
     {
         return workspace;
     }
+
+
+    @Override
+    public boolean equals( final Object o )
+    {
+        if ( this == o )
+        {
+            return true;
+        }
+        if ( !( o instanceof Context ) )
+        {
+            return false;
+        }
+
+        final Context context = (Context) o;
+
+        if ( workspace != null ? !workspace.equals( context.workspace ) : context.workspace != null )
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return workspace != null ? workspace.hashCode() : 0;
+    }
 }
