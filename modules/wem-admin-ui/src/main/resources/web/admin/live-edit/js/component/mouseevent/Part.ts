c783f4ff75c667ae16a1c05a63b946e7299694da
Jørund Vier Skriubakken <jvs@enonic.com>
2014-06-18 08:09:18 +0200

    Removed not needed method renderEmptyPlaceholders in LiveEdit.component.mouseevent.Part

--- modules/wem-admin-ui/src/main/resources/web/admin/live-edit/js/component/mouseevent/Part.ts
+++ modules/wem-admin-ui/src/main/resources/web/admin/live-edit/js/component/mouseevent/Part.ts
@@ -8,34 +8,9 @@ module LiveEdit.component.mouseevent {
 
             this.componentCssSelectorFilter = PartItemType.get().getConfig().getCssSelector();
 
-            this.renderEmptyPlaceholders();
             this.attachMouseOverEvent();
             this.attachMouseOutEvent();
             this.attachClickEvent();
         }
-
-        private renderEmptyPlaceholders(): void {
-            var parts = this.getAll(),
-                part: JQuery;
-            parts.each((i) => {
-                part = wemjq(parts[i]);
-                if (this.isPartEmpty(part)) {
-                    this.appendEmptyPlaceholder(part);
-                }
-            });
-        }
-
-        private appendEmptyPlaceholder(part: JQuery): void {
-            var placeholder: JQuery = wemjq('<div/>', {
-                'class': 'live-edit-empty-part-placeholder',
-                'html': 'Empty Part'
-            });
-            part.append(placeholder);
-        }
-
-        private isPartEmpty(part: JQuery): Boolean {
-            return wemjq(part).children().length === 0;
-        }
-
     }
 }
