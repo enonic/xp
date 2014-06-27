de0898fd0ccab53272ca33058fa3c0576edcf75e
Alex Rodriguez <aro@enonic.com>
2014-06-27 12:06:58 +0200

    Updated ModuleTreeGrid after changes on base TreeGrid.

--- modules/wem-admin-ui/src/main/resources/web/admin/apps/module-manager/js/app/browse/ModuleTreeGrid.ts
+++ modules/wem-admin-ui/src/main/resources/web/admin/apps/module-manager/js/app/browse/ModuleTreeGrid.ts
@@ -1,58 +1,65 @@
 module app.browse {
 
     import GridColumn = api.ui.grid.GridColumn;
+    import GridColumnBuilder = api.ui.grid.GridColumnBuilder;
 
     import ModuleSummary = api.module.ModuleSummary;
     import ModuleSummaryViewer = api.module.ModuleSummaryViewer;
     import TreeGrid = api.app.browse.treegrid.TreeGrid;
+    import TreeNode = api.app.browse.treegrid.TreeNode;
+    import TreeGridBuilder = api.app.browse.treegrid.TreeGridBuilder;
     import DateTimeFormatter = api.app.browse.treegrid.DateTimeFormatter;
 
 
     export class ModuleTreeGrid extends TreeGrid<ModuleSummary> {
 
         constructor() {
-            super({showToolbar: true}, "module-grid");
+            super(new TreeGridBuilder<ModuleSummary>().
+                    setColumns([
+                        new GridColumnBuilder<TreeNode<ModuleSummary>>().
+                            setName("Name").
+                            setId("displayName").
+                            setField("displayName").
+                            setFormatter(this.defaultNameFormatter).
+                            setMinWidth(250).
+                            build(),
 
-            var nameFormatter = (row: number, cell: number, value: any, columnDef: any, item: ModuleSummary) => {
-                var viewer = new ModuleSummaryViewer();
-                viewer.setObject(item);
-                return viewer.toString();
-            };
+                        new GridColumnBuilder<TreeNode<ModuleSummary>>().
+                            setName("ModifiedTime").
+                            setId("modifiedTime").
+                            setField("modifiedTime").
+                            setWidth(150).
+                            setMinWidth(150).
+                            setFormatter(DateTimeFormatter.format).
+                            build() ,
 
-            var column1 = <GridColumn<any>> {
-                name: "Name",
-                id: "displayName",
-                field: "displayName",
-                formatter: nameFormatter,
-                minWidth: 250
-            };
-            var column2 = <GridColumn<any>> {
-                name: "ModifiedTime",
-                id: "modifiedTime",
-                field: "modifiedTime",
-                formatter: DateTimeFormatter.format,
-                width: 150,
-                minWidth: 150
-            };
-            var column3 = <GridColumn<any>> {
-                name: "Version",
-                id: "version",
-                field: "version",
-                maxWidth: 70,
-                minWidth: 50
-            };
-            var column4 = <GridColumn<any>> {
-                name: "State",
-                id: "state",
-                field: "state",
-                minWidth: 80
-            };
+                        new GridColumnBuilder<TreeNode<ModuleSummary>>().
+                            setName("Version").
+                            setId("version").
+                            setField("version").
+                            setMaxWidth(70).
+                            setMinWidth(50).
+                            build() ,
 
-            this.setColumns([column1, column2, column3, column4]);
+                        new GridColumnBuilder<TreeNode<ModuleSummary>>().
+                            setName("State").
+                            setId("state").
+                            setField("state").
+                            setMinWidth(80).
+                            build()
+
+                    ]).prependClasses("module-grid")
+            );
         }
 
         fetchChildren(parent?: ModuleSummary): Q.Promise<ModuleSummary[]> {
             return new api.module.ListModuleRequest().sendAndParse();
+        }
+
+        private defaultNameFormatter(row: number, cell: number, value: any, columnDef: any, item: ModuleSummary) {
+            var viewer = new ModuleSummaryViewer();
+            viewer.setObject(item);
+            return viewer.toString();
         }
     }
 }
