2a2e945514fcc25534a14de1c139e92fbff837a1
Sten Roger Sandvik <srs@enonic.com>
2014-07-01 14:18:41 +0200

    Added action test.

---
+++ modules/wem-admin-ui/src/main/resources/web/dev/test/common/js/ui/ActionTest.js
@@ -0,0 +1,31 @@
+describe("Tests Action functions", function () {
+
+    it("test getLabel", function () {
+        var action = new api.ui.Action('My Action');
+        expect(action.getLabel()).toBe('My Action');
+    });
+
+    it("test addPropertyChangeListener is invoked on enabled", function () {
+        var action = new api.ui.Action('My Action');
+
+        action.setEnabled(true);
+        expect(action.isEnabled()).toBe(true);
+
+        action.onPropertyChanged(function (action) {
+            expect(action.isEnabled()).toBe(false);
+        });
+
+        action.setEnabled(false);
+    });
+
+    it("test addPropertyChangeListener is invoked on label", function () {
+        var action = new api.ui.Action('My Action');
+
+        action.onPropertyChanged(function (action) {
+            expect(action.getLabel()).toBe('Changed label');
+        });
+
+        action.setLabel('Changed label');
+    });
+
+});
