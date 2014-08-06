e84ba05454ec729e7d76d0fba139abaee1365e1f
Lasse Jul-Larsen <ljl@enonic.com>
2014-08-06 13:43:41 +0200

    CMS-3874 Remove ExtJS

--- modules/wem-admin-ui/src/main/resources/web/admin/common/js/_module.ts
+++ modules/wem-admin-ui/src/main/resources/web/admin/common/js/_module.ts
@@ -1,8 +1,6 @@
 // require ExtJs as long as it is used for implementation
 ///<reference path='lib/ExtJs.d.ts' />
 
-///<reference path='plugin/fileupload/_module.ts' />
-
 ///<reference path='lib/Mousetrap.d.ts' />
 ///<reference path='lib/jquery.d.ts' />
 ///<reference path='lib/jquery.resize.d.ts' />
@@ -121,7 +119,7 @@
 ///<reference path='app/_module.ts' />
 ///<reference path='app/browse/_module.ts' />
 ///<reference path='app/browse/filter/_module.ts' />
-///<reference path='app/browse/grid/_module.ts' />
+
 ///<reference path='app/delete/_module.ts' />
 ///<reference path='app/view/_module.ts' />
 ///<reference path='app/wizard/_module.ts' />
