0ed17f8e310c497ced0b518a2a4ff5b08a842c2d
Jørund Vier Skriubakken <jvs@enonic.com>
2014-08-13 09:54:16 +0200

    CMS-3939 Make input type Tag

---
+++ modules/wem-admin-ui/src/main/resources/web/admin/common/js/content/inputtype/tag/ContentTagSuggester.ts
@@ -0,0 +1,78 @@
+module api.content.inputtype.tag {
+
+    import ContentQuery = api.content.query.ContentQuery;
+    import ContentQueryRequest = api.content.ContentQueryRequest;
+    import QueryExpr = api.query.expr.QueryExpr;
+    import CompareExpr = api.query.expr.CompareExpr;
+    import FieldExpr = api.query.expr.FieldExpr;
+    import CompareOperator = api.query.expr.CompareOperator;
+    import ValueExpr = api.query.expr.ValueExpr;
+    import DataPath = api.data.DataPath;
+    import Value = api.data.Value;
+    import ValueTypes = api.data.ValueTypes;
+
+    export class ContentTagSuggesterBuilder {
+
+        contentType: api.schema.content.ContentTypeName;
+
+        dataPath: DataPath;
+
+        setContentType(value: api.schema.content.ContentTypeName): ContentTagSuggesterBuilder {
+            this.contentType = value;
+            return this;
+        }
+
+        setDataPath(value: DataPath): ContentTagSuggesterBuilder {
+            this.dataPath = value;
+            return this;
+        }
+
+        build(): ContentTagSuggester {
+            return new ContentTagSuggester(this);
+        }
+    }
+
+    export class ContentTagSuggester implements api.ui.tags.TagSuggester {
+
+        private contentType: api.schema.content.ContentTypeName;
+
+        private dataPath: DataPath;
+
+        constructor(builder: ContentTagSuggesterBuilder) {
+            this.contentType = builder.contentType;
+            this.dataPath = builder.dataPath;
+        }
+
+        suggest(value: string): Q.Promise<string[]> {
+            var deferred = Q.defer<string[]>();
+
+            var suggestions: string[] = [];
+
+            var query = new ContentQuery();
+            query.setContentTypeNames([this.contentType]);
+            query.setSize(10);
+
+            var queryExpr = new QueryExpr(new CompareExpr(new FieldExpr(this.dataPath.toString()), CompareOperator.LIKE,
+                [new ValueExpr(new Value(value, ValueTypes.STRING))]));
+            query.setQueryExpr(queryExpr);
+
+            var queryRequest = new ContentQueryRequest(query);
+            queryRequest.setExpand(api.rest.Expand.FULL);
+            queryRequest.sendAndParse().
+                then((contentQueryResult: ContentQueryResult<api.content.Content,api.content.json.ContentJson>) => {
+                    var contents = contentQueryResult.getContents();
+                    contents.forEach((content) => {
+                        var contentData = content.getContentData();
+                        var property = contentData.getPropertyFromDataPath(this.dataPath);
+                        suggestions.push(property.getString());
+                    });
+
+                    deferred.resolve(suggestions);
+                }).catch((reason: any) => {
+                    api.DefaultErrorHandler.handle(reason);
+                }).done();
+
+            return deferred.promise;
+        }
+    }
+}
---
+++ modules/wem-admin-ui/src/main/resources/web/admin/common/js/content/inputtype/tag/Tag.ts
@@ -0,0 +1,153 @@
+module api.content.inputtype.tag {
+
+    import DataPath = api.data.DataPath;
+
+    export class Tag extends api.dom.DivEl implements api.form.inputtype.InputTypeView {
+
+        private tags: api.ui.tags.Tags;
+
+        private tagSuggester: ContentTagSuggester;
+
+        private valueAddedListeners: {(event: api.form.inputtype.ValueAddedEvent) : void}[] = [];
+
+        private valueRemovedListeners: {(event: api.form.inputtype.ValueRemovedEvent) : void}[] = [];
+
+        constructor(config: api.content.inputtype.ContentInputTypeViewConfig<any>) {
+            super("tag");
+            this.addClass("input-type-view");
+            var dataPath = this.resolveDataPath(config);
+            this.tagSuggester = new ContentTagSuggesterBuilder().
+                setDataPath(dataPath).
+                build();
+
+            var tagsBuilder = new api.ui.tags.TagsBuilder().
+                setTagSuggester(this.tagSuggester).
+                setMaxTags(config.input.getOccurrences().getMaximum());
+            this.tags = tagsBuilder.build();
+            this.appendChild(this.tags);
+        }
+
+        private resolveDataPath(config: api.content.inputtype.ContentInputTypeViewConfig<any>): DataPath {
+            if (config.parentDataPath) {
+                return api.data.DataPath.fromParent(config.parentDataPath, api.data.DataPathElement.fromString(config.input.getName()));
+            }
+            else {
+                return new api.data.DataPath([api.data.DataPathElement.fromString(config.input.getName())], false);
+            }
+        }
+
+        availableSizeChanged() {
+
+        }
+
+        getElement(): api.dom.Element {
+            return this;
+        }
+
+        isManagingAdd(): boolean {
+            return true;
+        }
+
+        newInitialValue(): api.data.Value {
+            return null;
+        }
+
+        layout(input: api.form.Input, properties: api.data.Property[]) {
+
+            this.tags.clearTags();
+            properties.forEach((property) => {
+                this.tags.addTag(property.getString());
+            });
+
+            this.tags.onTagAdded((event) => {
+                this.notifyValueAdded(new api.data.Value(event.getValue(), api.data.ValueTypes.STRING));
+            });
+
+            this.tags.onTagRemoved((event) => {
+                this.notifyValueRemoved(event.getIndex());
+            });
+        }
+
+        getValues(): api.data.Value[] {
+
+            this.tags
+            return null;
+        }
+
+        getAttachments(): api.content.attachment.Attachment[] {
+            return [];
+        }
+
+        validate(silent: boolean = true): api.form.inputtype.InputValidationRecording {
+
+            var recording = new api.form.inputtype.InputValidationRecording();
+            // TODO
+            return recording;
+        }
+
+        onValueAdded(listener: (event: api.form.inputtype.ValueAddedEvent) => void) {
+            this.valueAddedListeners.push(listener);
+        }
+
+        unValueAdded(listener: (event: api.form.inputtype.ValueAddedEvent) => void) {
+            this.valueAddedListeners.filter((currentListener: (event: api.form.inputtype.ValueAddedEvent)=>void) => {
+                return listener == currentListener;
+            });
+        }
+
+        private notifyValueAdded(value: api.data.Value) {
+            var event = new api.form.inputtype.ValueAddedEvent(value);
+            this.valueAddedListeners.forEach((listener: (event: api.form.inputtype.ValueAddedEvent)=>void) => {
+                listener(event);
+            });
+        }
+
+        onValueChanged(listener: (event: api.form.inputtype.ValueChangedEvent) => void) {
+            // A tag value never changes
+        }
+
+        unValueChanged(listener: (event: api.form.inputtype.ValueChangedEvent) => void) {
+            // A tag value never changes
+        }
+
+        onValueRemoved(listener: (event: api.form.inputtype.ValueRemovedEvent) => void) {
+            this.valueRemovedListeners.push(listener);
+        }
+
+        unValueRemoved(listener: (event: api.form.inputtype.ValueRemovedEvent) => void) {
+            this.valueRemovedListeners.filter((currentListener: (event: api.form.inputtype.ValueRemovedEvent)=>void) => {
+                return listener == currentListener;
+            });
+        }
+
+        private notifyValueRemoved(index: number) {
+
+        }
+
+        onValidityChanged(listener: (event: api.form.inputtype.InputValidityChangedEvent)=>void) {
+        }
+
+        unValidityChanged(listener: (event: api.form.inputtype.InputValidityChangedEvent)=>void) {
+
+        }
+
+        private notifyValidityChanged(event: api.form.inputtype.InputValidityChangedEvent) {
+
+        }
+
+        giveFocus(): boolean {
+            return false;
+        }
+
+        onEditContentRequest(listener: (content: api.content.ContentSummary) => void) {
+            // Have to use stub here because it doesn't extend BaseInputTypeView
+        }
+
+        unEditContentRequest(listener: (content: api.content.ContentSummary) => void) {
+            // Have to use stub here because it doesn't extend BaseInputTypeView
+        }
+
+    }
+
+    api.form.inputtype.InputTypeManager.register(new api.Class("Tag", Tag));
+}
