36f0b5f8aecedc58027208c6dba1742de9e07965
Aliaksei Biaskosty <abiaskosty@scnsoft.com>
2014-08-20 10:54:23 +0300

    CMS-3960 Let api.ui.tags.Tags use TagSuggester

--- modules/wem-admin-ui/src/main/resources/web/admin/common/js/content/form/inputtype/tag/ContentTagSuggester.ts
+++ modules/wem-admin-ui/src/main/resources/web/admin/common/js/content/form/inputtype/tag/ContentTagSuggester.ts
@@ -45,7 +45,7 @@ module api.content.form.inputtype.tag {
 
         suggest(value: string): Q.Promise<string[]> {
 
-            var fieldName = this.dataPath.getParentPath().asRelative().toString() + this.dataPath.getLastElement().getName();
+            var fieldName = 'contentdata' + this.dataPath.getParentPath().toString() + this.dataPath.getLastElement().getName();
 
             var queryExpr = new QueryExpr(new CompareExpr(new FieldExpr(fieldName), CompareOperator.LIKE,
                 [new ValueExpr(new Value(value, ValueTypes.STRING))]));
@@ -59,12 +59,20 @@ module api.content.form.inputtype.tag {
             queryRequest.setExpand(api.rest.Expand.FULL);
             return queryRequest.sendAndParse().
                 then((contentQueryResult: ContentQueryResult<api.content.Content,api.content.json.ContentJson>) => {
-                    var contents = contentQueryResult.getContents();
-                    return contents.map((content: api.content.Content): string => {
-                        var contentData = content.getContentData();
-                        var property = contentData.getPropertyFromDataPath(this.dataPath);
-                        return property.getString();
+                    var values: string[] = [];
+                    contentQueryResult.getContents().forEach((content: api.content.Content) => {
+                        var dataSet = this.dataPath.getParentPath().isRoot() ?
+                                      content.getContentData() :
+                                      content.getContentData().getDataSetFromDataPath(this.dataPath);
+                        var properties = dataSet.getPropertiesByName(this.dataPath.getLastElement().getName());
+                        properties.forEach((property: api.data.Property) => {
+                            var tag = property.getValue().asString();
+                            if (tag.indexOf(value) >= 0 && values.indexOf(tag) < 0) {
+                                values.push(tag);
+                            }
+                        });
                     });
+                    return values;
                 }).catch((reason: any) => {
                     api.DefaultErrorHandler.handle(reason);
                     return [];
