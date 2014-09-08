18380706c87e015cabd8959007605b2d5cd687e0
Sten Roger Sandvik <srs@enonic.com>
2014-09-08 09:21:30 +0200

    Fixed some tests.

---
+++ modules/wem-itests/src/test/java/com/enonic/wem/itests/api/ApiBundleTest.java
@@ -0,0 +1,29 @@
+package com.enonic.wem.itests.api;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.ops4j.pax.exam.Option;
+
+import junit.framework.Assert;
+
+import com.enonic.wem.itests.AbstractOsgiTest;
+
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+
+public class ApiBundleTest
+    extends AbstractOsgiTest
+{
+    @Override
+    protected void options( final List<Option> options )
+    {
+        super.options( options );
+        options.add( mavenBundle( "com.enonic.wem", "wem-api" ) );
+    }
+
+    @Test
+    public void testBundle()
+    {
+        Assert.assertTrue( true );
+    }
+}
