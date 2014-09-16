0cce257e5ea3b41dcf974b0caea2602d1d822d98
Sten Roger Sandvik <srs@enonic.com>
2014-09-16 18:05:35 +0200

    Added numeric utils from lucene.

--- modules/wem-core/src/main/java/com/enonic/wem/core/index/document/OrderbyValueResolver.java
+++ modules/wem-core/src/main/java/com/enonic/wem/core/index/document/OrderbyValueResolver.java
@@ -58,12 +58,12 @@ class OrderbyValueResolver
 
     private static String doubleToPrefixCoded( double val )
     {
-        return longToPrefixCoded( org.apache.lucene.util.NumericUtils.doubleToSortableLong( val ) );
+        return longToPrefixCoded( IndexNumericUtils.doubleToSortableLong( val ) );
     }
 
     private static String floatToPrefixCoded( float val )
     {
-        return intToPrefixCoded( org.apache.lucene.util.NumericUtils.floatToSortableInt( val ) );
+        return intToPrefixCoded( IndexNumericUtils.floatToSortableInt( val ) );
     }
 
     private static String longToPrefixCoded( final long val )
@@ -73,7 +73,7 @@ class OrderbyValueResolver
 
     private static String longToPrefixCoded( final long val, final int shift )
     {
-        final char[] buffer = new char[org.apache.lucene.util.NumericUtils.BUF_SIZE_LONG];
+        final char[] buffer = new char[IndexNumericUtils.BUF_SIZE_LONG];
         final int len = longToPrefixCoded( val, shift, buffer );
         return new String( buffer, 0, len );
     }
@@ -85,7 +85,7 @@ class OrderbyValueResolver
             throw new IllegalArgumentException( "Illegal shift value, must be 0..63" );
         }
         int nChars = ( 63 - shift ) / 7 + 1, len = nChars + 1;
-        buffer[0] = (char) ( org.apache.lucene.util.NumericUtils.SHIFT_START_LONG + shift );
+        buffer[0] = (char) ( IndexNumericUtils.SHIFT_START_LONG + shift );
         long sortableBits = val ^ 0x8000000000000000L;
         sortableBits >>>= shift;
         while ( nChars >= 1 )
@@ -106,7 +106,7 @@ class OrderbyValueResolver
 
     private static String intToPrefixCoded( final int val, final int shift )
     {
-        final char[] buffer = new char[org.apache.lucene.util.NumericUtils.BUF_SIZE_INT];
+        final char[] buffer = new char[IndexNumericUtils.BUF_SIZE_INT];
         final int len = intToPrefixCoded( val, shift, buffer );
         return new String( buffer, 0, len );
     }
@@ -118,7 +118,7 @@ class OrderbyValueResolver
             throw new IllegalArgumentException( "Illegal shift value, must be 0..31" );
         }
         int nChars = ( 31 - shift ) / 7 + 1, len = nChars + 1;
-        buffer[0] = (char) ( org.apache.lucene.util.NumericUtils.SHIFT_START_INT + shift );
+        buffer[0] = (char) ( IndexNumericUtils.SHIFT_START_INT + shift );
         int sortableBits = val ^ 0x80000000;
         sortableBits >>>= shift;
         while ( nChars >= 1 )
