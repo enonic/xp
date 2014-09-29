ff8f14bed77d7780dd2b4f6055c1e6b821dfef3f
Sten Roger Sandvik <srs@enonic.com>
2014-09-29 10:06:53 +0200

    CMS-4161 Require should only be used within a module. Fixed tests.

--- modules/wem-script/src/test/resources/mymodule/resolve/resolve-test.js
+++ modules/wem-script/src/test/resources/mymodule/resolve/resolve-test.js
@@ -1,7 +1,7 @@
 var assert = Java.type('org.junit.Assert');
 
 var relative = resolve('./test.html').toString();
-assert.assertEquals('mymodule-1.0.0:/resolve/test.html', relative);
+assert.assertEquals('mymodule:/resolve/test.html', relative);
 
 var absolute = resolve('/test.html').toString();
-assert.assertEquals('mymodule-1.0.0:/test.html', absolute);
+assert.assertEquals('mymodule:/test.html', absolute);
