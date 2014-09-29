fa19f7aafe1f6b1f4a8e9daa12f906e12f073110
Lasse Jul-Larsen <ljl@enonic.com>
2014-09-29 10:29:41 +0200

    CMS-4147 Stop modules from being deselected after start/stop

--- modules/wem-admin-ui/src/main/resources/web/admin/apps/module-manager/js/app/view/ModuleItemStatisticsPanel.ts
+++ modules/wem-admin-ui/src/main/resources/web/admin/apps/module-manager/js/app/view/ModuleItemStatisticsPanel.ts
@@ -9,6 +9,7 @@ module app.view {
         private moduleDataContainer: api.dom.DivEl;
         private moduleActions: api.ui.Action[] = [];
         private actionMenu: api.ui.menu.ActionMenu;
+        private currentItem: api.app.view.ViewItem<api.module.ModuleSummary>;
 
         constructor() {
             super("module-item-statistics-panel");
@@ -28,10 +29,22 @@ module app.view {
             this.moduleDataContainer = new api.dom.DivEl("module-data-container");
             this.appendChild(this.moduleDataContainer);
 
+            app.browse.StartModuleEvent.on(() => {
+                this.actionMenu.setLabel("Started");
+            });
 
+            app.browse.StopModuleEvent.on(() => {
+                this.actionMenu.setLabel("Stopped");
+                console.log("changing label from event stop");
+            });
         }
 
         setItem(item: api.app.view.ViewItem<api.module.ModuleSummary>) {
+            if (this.currentItem && this.currentItem.getPath() == item.getPath()) {
+                return;
+            }
+            this.currentItem = item;
+
             super.setItem(item);
             this.actionMenu.setLabel(api.util.StringHelper.capitalize(item.getModel().getState()));
 
