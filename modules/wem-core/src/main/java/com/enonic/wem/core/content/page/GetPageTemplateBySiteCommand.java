b63c9d54dc56d56c6ef0072fbf2abb27084a52df
vbradnitski <vbradnitski@scnsoft.com>
2014-10-03 15:37:47 +0300

    CMS-4203 Remove Site Template - Make proper commands for all methods in PageTemplateServiceImpl

---
+++ modules/wem-core/src/main/java/com/enonic/wem/core/content/page/GetPageTemplateBySiteCommand.java
@@ -0,0 +1,55 @@
+package com.enonic.wem.core.content.page;
+
+import com.enonic.wem.api.content.Content;
+import com.enonic.wem.api.content.ContentId;
+import com.enonic.wem.api.content.ContentPath;
+import com.enonic.wem.api.content.ContentService;
+import com.enonic.wem.api.content.FindContentByParentParams;
+import com.enonic.wem.api.content.FindContentByParentResult;
+import com.enonic.wem.api.content.page.PageTemplate;
+import com.enonic.wem.api.content.page.PageTemplates;
+import com.enonic.wem.api.context.Context;
+
+final class GetPageTemplateBySiteCommand
+{
+    private Context context;
+
+    private ContentService contentService;
+
+    private ContentId siteId;
+
+    public PageTemplates execute()
+    {
+        final PageTemplates.Builder pageTemplatesBuilder = PageTemplates.newPageTemplates();
+        final Content site = contentService.getById( siteId, context );
+        final ContentPath pageTemplatesFolderPath = ContentPath.from( site.getPath(), "templates" );
+        final FindContentByParentResult result =
+            contentService.findByParent( FindContentByParentParams.create().parentPath( pageTemplatesFolderPath ).build(), context );
+        for ( final Content content : result.getContents() )
+        {
+            if ( content instanceof PageTemplate )
+            {
+                pageTemplatesBuilder.add( (PageTemplate) content );
+            }
+        }
+        return pageTemplatesBuilder.build();
+    }
+
+    public GetPageTemplateBySiteCommand site( final ContentId siteId )
+    {
+        this.siteId = siteId;
+        return this;
+    }
+
+    public GetPageTemplateBySiteCommand context( final Context context )
+    {
+        this.context = context;
+        return this;
+    }
+
+    public GetPageTemplateBySiteCommand contentService( final ContentService contentService )
+    {
+        this.contentService = contentService;
+        return this;
+    }
+}
