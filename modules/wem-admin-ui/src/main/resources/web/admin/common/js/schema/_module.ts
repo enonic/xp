a0613aec0506b56a300039a7b72ef2c97605fb5c
Alex Rodriguez <aro@enonic.com>
2014-10-16 13:56:25 +0200

    Deleted unused Schema TypeScript classes.

--- modules/wem-admin-ui/src/main/resources/web/admin/common/js/schema/_module.ts
+++ modules/wem-admin-ui/src/main/resources/web/admin/common/js/schema/_module.ts
@@ -1,11 +1,6 @@
 ///<reference path="SchemaJson.ts"/>
-///<reference path="SchemaDeleteJson.ts"/>
-///<reference path="SchemaDeleteResult.ts"/>
 
 ///<reference path="Schema.ts"/>
 
 ///<reference path="SchemaIconUrlResolver.ts"/>
 
-///<reference path="SchemaCreatedEvent.ts"/>
-///<reference path="SchemaUpdatedEvent.ts"/>
-///<reference path="SchemaDeletedEvent.ts"/>
