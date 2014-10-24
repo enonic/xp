cf32fedf3840550c08a57ddf45de0b56efe34563
Alex Rodriguez <aro@enonic.com>
2014-10-24 13:14:33 +0200

    CMS-4276 Implement rest service for listing userstores. Updated JSON response to align with the other REST resources.

--- modules/wem-admin/src/main/java/com/enonic/wem/admin/rest/resource/security/SecurityResource.java
+++ modules/wem-admin/src/main/java/com/enonic/wem/admin/rest/resource/security/SecurityResource.java
@@ -1,16 +1,12 @@
 package com.enonic.wem.admin.rest.resource.security;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import com.enonic.wem.admin.rest.resource.security.json.UserStoreJson;
+import com.enonic.wem.admin.rest.resource.security.json.UserStoresJson;
 import com.enonic.wem.api.security.SecurityService;
-import com.enonic.wem.api.security.UserStore;
 import com.enonic.wem.api.security.UserStores;
 
 
@@ -24,16 +20,10 @@ public class SecurityResource
 
     @GET
     @Path("list")
-    public List<UserStoreJson> getUserStores()
+    public UserStoresJson getUserStores()
     {
         UserStores userStores = securityService.getUserStores();
-        List<UserStoreJson> userStoresJson = new ArrayList<>();
-        List<UserStore> list = userStores.getList();
-        for ( UserStore userStore : list )
-        {
-            userStoresJson.add( new UserStoreJson( userStore ) );
-        }
-        return userStoresJson;
+        return new UserStoresJson( userStores );
     }
 
     public void setSecurityService( final SecurityService securityService )
--- modules/wem-admin/src/main/java/com/enonic/wem/admin/rest/resource/security/json/UserStoresJson.java
+++ modules/wem-admin/src/main/java/com/enonic/wem/admin/rest/resource/security/json/UserStoresJson.java
@@ -4,54 +4,27 @@ package com.enonic.wem.admin.rest.resource.security.json;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-
 import com.enonic.wem.api.security.UserStore;
-import com.enonic.wem.api.security.UserStoreKey;
 import com.enonic.wem.api.security.UserStores;
 
-public class UserStoresJson
+public final class UserStoresJson
 {
-    private final UserStores userStores;
-
-    private List<UserStoreJson> userStoresJson;
+    private final List<UserStoreJson> userStoresJson;
 
     public UserStoresJson( final UserStores userStores )
     {
-        this.userStores = userStores;
-
+        this.userStoresJson = new ArrayList<>();
         if ( userStores != null )
         {
-            userStoresJson = new ArrayList<>();
-            List<UserStore> list = userStores.getList();
-            for ( UserStore userStore : list )
+            for ( UserStore userStore : userStores )
             {
                 userStoresJson.add( new UserStoreJson( userStore ) );
             }
         }
-        else
-        {
-            userStoresJson = null;
-        }
     }
 
-    @JsonCreator
-    public UserStoresJson( final List<UserStoreJson> userStoresJson )
+    public List<UserStoreJson> getUserStores()
     {
-
-        this.userStoresJson = userStoresJson;
-        List<UserStore> storeList = new ArrayList<>();
-        for ( UserStoreJson us : userStoresJson )
-        {
-            storeList.add( UserStore.newUserStore().displayName( us.getDisplayName() ).key( new UserStoreKey( us.getKey() ) ).build() );
-        }
-
-        this.userStores = UserStores.from( storeList );
-    }
-
-
-    public UserStores getUserStores()
-    {
-        return userStores;
+        return userStoresJson;
     }
 }
