9aba23700f4e435730fd1f16765c27594c20a072
Sergei Gavrusev <sig@enonic.com>
2014-10-27 12:37:01 +0300

    CMS-4277 Implement rest service for listing principals - minor changes

--- modules/wem-admin/src/main/java/com/enonic/wem/admin/rest/resource/security/SecurityResource.java
+++ modules/wem-admin/src/main/java/com/enonic/wem/admin/rest/resource/security/SecurityResource.java
@@ -67,7 +67,6 @@ public class SecurityResource
         for ( E t : en.getEnumConstants() )
         {
             results.add( t.toString() );
-            System.out.println( t );
         }
 
         return results;
