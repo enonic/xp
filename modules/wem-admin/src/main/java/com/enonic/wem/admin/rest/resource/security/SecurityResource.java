5a8062e00ad9b99bdd954c73d6d97bc0623a14a0
Alex Rodriguez <aro@enonic.com>
2014-11-19 13:15:34 +0100

    CMS-4457 Implement REST method getPrincipalById. Restrict key parameter to valid principal key pattern.

--- modules/wem-admin/src/main/java/com/enonic/wem/admin/rest/resource/security/SecurityResource.java
+++ modules/wem-admin/src/main/java/com/enonic/wem/admin/rest/resource/security/SecurityResource.java
@@ -75,7 +75,7 @@ public final class SecurityResource
     }
 
     @GET
-    @Path("principals/{key}")
+    @Path("principals/{key: ([^:]+):(user|group|role):([^:]+)}")
     public PrincipalJson getPrincipalByKey( @PathParam("key") final String keyParam )
     {
         final PrincipalKey principalKey = PrincipalKey.from( keyParam );
