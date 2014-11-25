73e43f1c70be490863b378c322fa0da85d5df69f
Jørund Vier Skriubakken <jvs@enonic.com>
2014-11-25 18:08:11 +0100

    CMS-4511 Prepare node export tasks

--- modules/wem-api/src/main/java/com/enonic/wem/api/xml/model/XmlNode.java
+++ modules/wem-api/src/main/java/com/enonic/wem/api/xml/model/XmlNode.java
@@ -4,9 +4,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlSchemaType;
 import javax.xml.bind.annotation.XmlType;
-import javax.xml.datatype.XMLGregorianCalendar;
 
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlType(name = "node")
@@ -15,20 +13,6 @@ public final class XmlNode
 {
     @XmlElement
     private String id;
-
-    @XmlElement(name = "created-time")
-    @XmlSchemaType(name = "dateTime")
-    private XMLGregorianCalendar createdTime;
-
-    @XmlElement(name = "modified-time")
-    @XmlSchemaType(name = "dateTime")
-    private XMLGregorianCalendar modifiedTime;
-
-    @XmlElement
-    private String creator;
-
-    @XmlElement
-    private String modifier;
 
     @XmlElement
     private XmlDataSet data;
@@ -41,46 +25,6 @@ public final class XmlNode
     public void setId( String value )
     {
         this.id = value;
-    }
-
-    public XMLGregorianCalendar getCreatedTime()
-    {
-        return createdTime;
-    }
-
-    public void setCreatedTime( XMLGregorianCalendar value )
-    {
-        this.createdTime = value;
-    }
-
-    public XMLGregorianCalendar getModifiedTime()
-    {
-        return modifiedTime;
-    }
-
-    public void setModifiedTime( XMLGregorianCalendar value )
-    {
-        this.modifiedTime = value;
-    }
-
-    public String getCreator()
-    {
-        return creator;
-    }
-
-    public void setCreator( String value )
-    {
-        this.creator = value;
-    }
-
-    public String getModifier()
-    {
-        return modifier;
-    }
-
-    public void setModifier( String value )
-    {
-        this.modifier = value;
     }
 
     public XmlDataSet getData()
