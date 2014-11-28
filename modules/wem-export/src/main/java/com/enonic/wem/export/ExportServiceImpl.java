8238eea237dcc3083f384308e61ed581fbda555e
Sten Roger Sandvik <srs@enonic.com>
2014-11-28 07:55:27 +0100

    Added some schema for export.

--- modules/wem-export/src/main/java/com/enonic/wem/export/ExportServiceImpl.java
+++ modules/wem-export/src/main/java/com/enonic/wem/export/ExportServiceImpl.java
@@ -1,9 +1,13 @@
 package com.enonic.wem.export;
 
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 import com.enonic.wem.api.export.ExportService;
 import com.enonic.wem.api.node.NodePath;
 import com.enonic.wem.api.node.NodeService;
 
+@Component(immediate = true)
 public class ExportServiceImpl
     implements ExportService
 {
@@ -15,6 +19,7 @@ public class ExportServiceImpl
 
     }
 
+    @Reference
     public void setNodeService( final NodeService nodeService )
     {
         this.nodeService = nodeService;
