c54835cd7d4e80a04161010c16b9e7f93bebf6e4
Jørund Vier Skriubakken <jvs@enonic.com>
2014-12-11 06:41:28 +0100

    CMS-4637 Media Types - Introduce ContentResource.createMedia

--- modules/wem-admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/browse/SortContentEvent.ts
+++ modules/wem-admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/browse/SortContentEvent.ts
@@ -2,11 +2,11 @@ module app.browse {
 
     export class SortContentEvent extends BaseContentModelEvent {
 
-        static on(handler: (event: ShowPreviewEvent) => void) {
+        static on(handler: (event: SortContentEvent) => void) {
             api.event.Event.bind(api.ClassHelper.getFullName(this), handler);
         }
 
-        static un(handler?: (event: ShowPreviewEvent) => void) {
+        static un(handler?: (event: SortContentEvent) => void) {
             api.event.Event.unbind(api.ClassHelper.getFullName(this), handler);
         }
     }
