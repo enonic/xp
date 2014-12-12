9a2a5739c236fb8c2404de80d0d34df036de1bc6
Sten Roger Sandvik <srs@enonic.com>
2014-12-12 11:55:28 +0100

    CMS-4647 Implement simple status page

---
+++ modules/wem-admin/src/main/java/com/enonic/wem/admin/rest/resource/status/InfoResource.java
@@ -0,0 +1,54 @@
+package com.enonic.wem.admin.rest.resource.status;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.Lists;
+
+import com.enonic.wem.admin.status.GCStatusInfoBuilder;
+import com.enonic.wem.admin.status.JVMStatusInfoBuilder;
+import com.enonic.wem.admin.status.MemoryStatusInfoBuilder;
+import com.enonic.wem.admin.status.OSStatusInfoBuilder;
+import com.enonic.wem.admin.status.StatusInfoBuilder;
+import com.enonic.wem.servlet.jaxrs.JaxRsComponent;
+
+@Path("status")
+public final class InfoResource
+    implements JaxRsComponent
+{
+    private final List<StatusInfoBuilder> infoBuilders;
+
+    public InfoResource()
+    {
+        this.infoBuilders = Lists.newArrayList();
+        this.infoBuilders.add( new OSStatusInfoBuilder() );
+        this.infoBuilders.add( new JVMStatusInfoBuilder() );
+        this.infoBuilders.add( new MemoryStatusInfoBuilder() );
+        this.infoBuilders.add( new GCStatusInfoBuilder() );
+        Collections.sort( this.infoBuilders );
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getStatus()
+    {
+        return createStatus().toString();
+    }
+
+    private ObjectNode createStatus()
+    {
+        final ObjectNode node = JsonNodeFactory.instance.objectNode();
+        for ( final StatusInfoBuilder builder : this.infoBuilders )
+        {
+            builder.addInfo( node );
+        }
+        return node;
+    }
+}
