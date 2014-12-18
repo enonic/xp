2ddb5b4c204f54074ce3045ca58a0b773ae37646
Sten Roger Sandvik <srs@enonic.com>
2014-12-18 08:49:19 +0100

    Added more nashorn diag.

--- modules/wem-admin/src/main/java/com/enonic/wem/admin/status/NashornStatusInfoBuilder.java
+++ modules/wem-admin/src/main/java/com/enonic/wem/admin/status/NashornStatusInfoBuilder.java
@@ -1,5 +1,8 @@
 package com.enonic.wem.admin.status;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import com.enonic.wem.script.NashornDiag;
@@ -16,11 +19,22 @@ public final class NashornStatusInfoBuilder
     public void build( final ObjectNode json )
     {
         json.put( "scriptEngine", NashornDiag.getScriptEngine() );
-        // json.put( "newScriptObject", NashornDiag.newScriptObject() );
-        // json.put( "newScriptArray", NashornDiag.newScriptArray() );
-        // json.put( "getCurrentGlobal", NashornDiag.getCurrentGlobal() );
-        // json.put( "findGlobalObject", NashornDiag.findGlobalObject() );
-        // json.put( "setCurrentGlobal", NashornDiag.setCurrentGlobal() );
+
+        try
+        {
+            json.put( "newScriptObject", NashornDiag.newScriptObject() );
+            json.put( "newScriptArray", NashornDiag.newScriptArray() );
+            json.put( "getCurrentGlobal", NashornDiag.getCurrentGlobal() );
+            json.put( "findGlobalObject", NashornDiag.findGlobalObject() );
+            json.put( "setCurrentGlobal", NashornDiag.setCurrentGlobal() );
+        }
+        catch ( final Exception e )
+        {
+            final StringWriter out = new StringWriter();
+            e.printStackTrace( new PrintWriter( out ) );
+            json.put( "error", out.toString() );
+        }
+
         json.set( "contextDiag", NashornDiag.getOtherInfo() );
     }
 }
