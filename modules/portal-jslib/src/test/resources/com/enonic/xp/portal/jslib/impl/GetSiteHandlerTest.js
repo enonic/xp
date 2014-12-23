78077c011d785c0bccaa6d41aa7e34e991e1f6eb
Alex Rodriguez <aro@enonic.com>
2014-12-23 13:34:27 +0100

    CMS-4739 Implement portal.getSite script method

---
+++ modules/portal-jslib/src/test/resources/com/enonic/xp/portal/jslib/impl/GetSiteHandlerTest.js
@@ -0,0 +1,61 @@
+var expectedJson = {
+    "_id": "100123",
+    "_name": "my-content",
+    "_path": "/my-content",
+    "data": {
+        "moduleConfig": [{
+            "config": [{
+                "Field": [42]
+            }],
+            "moduleKey": ["mymodule"]
+        }]
+    },
+    "draft": false,
+    "hasChildren": false,
+    "metadata": {},
+    "moduleConfigs": {
+        "mymodule": {
+            "Field": [42]
+        }
+    },
+    "page": {},
+    "type": "system:unstructured"
+};
+
+exports.getCurrentSite = function () {
+    var result = execute('portal.getSite');
+
+    assert.assertJson(expectedJson, result);
+};
+
+exports.getById = function () {
+    var result = execute('portal.getSite', {
+        key: '100123'
+    });
+
+    assert.assertJson(expectedJson, result);
+};
+
+exports.getByPath = function () {
+    var result = execute('portal.getSite', {
+        key: '/a/b/mycontent'
+    });
+
+    assert.assertJson(expectedJson, result);
+};
+
+exports.getById_notFound = function () {
+    var result = execute('portal.getSite', {
+        key: '123456'
+    });
+
+    assert.assertNull(result);
+};
+
+exports.getByPath_notFound = function () {
+    var result = execute('portal.getSite', {
+        key: '/a/b/mycontent'
+    });
+
+    assert.assertNull(result);
+};
