6efe89ca6d13702fcc700a8af6f42f538af54e12
Jørund Vier Skriubakken <jvs@enonic.com>
2015-01-02 07:06:02 +0100

    CMS-4769 Media Types - Introduce AttachmentUrlBuilder

--- modules/portal-api/src/test/java/com/enonic/xp/portal/url/AttachmentUrlBuilderTest.java
+++ modules/portal-api/src/test/java/com/enonic/xp/portal/url/AttachmentUrlBuilderTest.java
@@ -8,11 +8,51 @@ public class AttachmentUrlBuilderTest
     extends AbstractUrlBuilderTest
 {
     @Test
-    public void createUrl()
+    public void createUrl_with_nothing_specified()
     {
         final AttachmentUrlBuilder urlBuilder = builders.attachmentUrl();
 
-        assertEquals( "/root/portal/live/stage/some/path", urlBuilder.toString() );
+        assertEquals( "/root/portal/live/stage/some/path/_/attachment", urlBuilder.toString() );
+    }
+    
+    @Test
+    public void createUrl_with_name()
+    {
+        final AttachmentUrlBuilder urlBuilder = builders.attachmentUrl().name( "mycv.pdf" );
+
+        assertEquals( "/root/portal/live/stage/some/path/_/attachment/mycv.pdf", urlBuilder.toString() );
+    }
+
+    @Test
+    public void createUrl_with_label()
+    {
+        final AttachmentUrlBuilder urlBuilder = builders.attachmentUrl().label( "source" );
+
+        assertEquals( "/root/portal/live/stage/some/path/_/attachment/source", urlBuilder.toString() );
+    }
+
+    @Test
+    public void createUrl_with_mediaId_and_nothing_specified()
+    {
+        final AttachmentUrlBuilder urlBuilder = builders.attachmentUrl().mediaId( "123abc" );
+
+        assertEquals( "/root/portal/live/stage/some/path/_/attachment/id/123abc", urlBuilder.toString() );
+    }
+
+    @Test
+    public void createUrl_with_mediaId_and_name()
+    {
+        final AttachmentUrlBuilder urlBuilder = builders.attachmentUrl().mediaId( "123abc" ).name( "mycv.pdf" );
+
+        assertEquals( "/root/portal/live/stage/some/path/_/attachment/id/123abc/mycv.pdf", urlBuilder.toString() );
+    }
+
+    @Test
+    public void createUrl_with_mediaId_and_label()
+    {
+        final AttachmentUrlBuilder urlBuilder = builders.attachmentUrl().mediaId( "123abc" ).label( "source" );
+
+        assertEquals( "/root/portal/live/stage/some/path/_/attachment/id/123abc/source", urlBuilder.toString() );
     }
 }
 
