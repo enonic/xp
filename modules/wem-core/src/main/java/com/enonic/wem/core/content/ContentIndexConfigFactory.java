8fabda81fbe0c80cbf0d0270af1d889aaf73f511
Runar Myklebust <rmy@enonic.com>
2015-01-04 22:07:37 +0100

    CMS-4776 Fix indexing of media content-types on update

--- modules/wem-core/src/main/java/com/enonic/wem/core/content/ContentIndexConfigFactory.java
+++ modules/wem-core/src/main/java/com/enonic/wem/core/content/ContentIndexConfigFactory.java
@@ -7,7 +7,6 @@ import com.enonic.wem.api.data.PropertyPath;
 import com.enonic.wem.api.index.IndexConfig;
 import com.enonic.wem.api.index.IndexConfigDocument;
 import com.enonic.wem.api.index.PatternIndexConfigDocument;
-import com.enonic.wem.api.schema.content.ContentTypeName;
 
 import static com.enonic.wem.core.content.ContentPropertyNames.ATTACHMENT;
 import static com.enonic.wem.core.content.ContentPropertyNames.DATA;
@@ -22,6 +21,16 @@ class ContentIndexConfigFactory
 {
     public static IndexConfigDocument create( final CreateContentParams params )
     {
+        return doCreateIndexConfig();
+    }
+
+    public static IndexConfigDocument create( final Content content )
+    {
+        return doCreateIndexConfig();
+    }
+
+    private static IndexConfigDocument doCreateIndexConfig()
+    {
         final PatternIndexConfigDocument.Builder config = PatternIndexConfigDocument.create().
             analyzer( ContentConstants.CONTENT_DEFAULT_ANALYZER ).
             add( PAGE, IndexConfig.NONE ).
@@ -31,42 +40,11 @@ class ContentIndexConfigFactory
             add( DATA, IndexConfig.BY_TYPE ).
             add( TYPE, IndexConfig.MINIMAL ).
             add( ATTACHMENT, IndexConfig.NONE ).
+            add( PropertyPath.from( DATA, METADATA ), IndexConfig.NONE ).
+            add( PropertyPath.from( DATA, METADATA, "media" ), IndexConfig.MINIMAL ).
             defaultConfig( IndexConfig.BY_TYPE );
-
-        final ContentTypeName typeName = params.getType();
-
-        if ( typeName.isMedia() || typeName.isDescendantOfMedia() )
-        {
-            setFiltersForMediaData( typeName, config );
-        }
 
         return config.build();
     }
 
-    private static void setFiltersForMediaData( final ContentTypeName contentTypeName, PatternIndexConfigDocument.Builder builder )
-    {
-        // Extend with separate stuff for different contentTypeNames
-        builder.add( PropertyPath.from( DATA, METADATA ), IndexConfig.NONE );
-        builder.add( PropertyPath.from( DATA, METADATA, "media" ), IndexConfig.MINIMAL );
-    }
-
-    public static IndexConfigDocument create( final Content content )
-    {
-        final PatternIndexConfigDocument.Builder config = PatternIndexConfigDocument.create().
-            analyzer( ContentConstants.CONTENT_DEFAULT_ANALYZER ).
-            add( PAGE, IndexConfig.NONE ).
-            add( SITE, IndexConfig.NONE ).
-            add( DRAFT, IndexConfig.NONE ).
-            add( FORM, IndexConfig.NONE ).
-            add( DATA, IndexConfig.BY_TYPE ).
-            add( TYPE, IndexConfig.MINIMAL ).
-            defaultConfig( IndexConfig.BY_TYPE );
-
-        if ( content.getType().isMedia() )
-        {
-            setFiltersForMediaData( content.getType(), config );
-        }
-
-        return config.build();
-    }
 }
