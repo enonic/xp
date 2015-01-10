94a49c49043e7b210029eac4e7cf97e726ae6785
Pavel Milkevich <pmi@enonic.com>
2015-01-10 12:04:00 +0300

    Fix for empty screen at /admin

--- modules/wem-admin/src/main/java/com/enonic/wem/admin/app/AppHtmlHandler.java
+++ modules/wem-admin/src/main/java/com/enonic/wem/admin/app/AppHtmlHandler.java
@@ -3,10 +3,10 @@ package com.enonic.wem.admin.app;
 import java.net.URL;
 import java.util.Map;
 
+import javax.ws.rs.core.UriInfo;
+
 import com.google.common.collect.Maps;
 import com.samskivert.mustache.Template;
-
-import com.enonic.xp.web.servlet.ServletRequestUrlHelper;
 
 final class AppHtmlHandler
 {
@@ -18,9 +18,9 @@ final class AppHtmlHandler
         this.template = MustacheCompiler.getInstance().compile( url );
     }
 
-    public String render( final String app )
+    public String render( final String app, UriInfo uriInfo )
     {
-        final String baseUri = ServletRequestUrlHelper.createUri( "" );
+        final String baseUri = uriInfo.getBaseUri().toString();
 
         final Map<String, Object> model = Maps.newHashMap();
         model.put( "app", app );
--- modules/wem-admin/src/main/java/com/enonic/wem/admin/app/MainResource.java
+++ modules/wem-admin/src/main/java/com/enonic/wem/admin/app/MainResource.java
@@ -6,7 +6,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import com.enonic.xp.web.jaxrs.JaxRsComponent;
 
@@ -14,6 +16,9 @@ import com.enonic.xp.web.jaxrs.JaxRsComponent;
 public final class MainResource
     implements JaxRsComponent
 {
+    @Context
+    UriInfo uriInfo;
+
     private final AppHtmlHandler appHtmlHandler;
 
     private final ResourceHandler resourceHandler;
@@ -44,7 +49,7 @@ public final class MainResource
     @Produces("text/html")
     public String getAdminApp( @QueryParam("app") @DefaultValue("app-launcher") final String app )
     {
-        return this.appHtmlHandler.render( app );
+        return this.appHtmlHandler.render( app, uriInfo );
     }
 
     public void setResourceLocator( final ResourceLocator resourceLocator )
