897011a2e8a1838501e8158647b24a855438f24c
Sten Roger Sandvik <srs@enonic.com>
2015-01-12 14:53:53 +0100

    Removed some unused view code.

--- modules/portal-api/src/main/java/com/enonic/xp/portal/view/ViewProcessor.java
+++
@@ -1,8 +0,0 @@
-package com.enonic.xp.portal.view;
-
-public interface ViewProcessor
-{
-    public String getName();
-
-    public String process( ViewModel viewModel );
-}
--- modules/portal-api/src/main/java/com/enonic/xp/portal/view/ViewService.java
+++
@@ -1,6 +0,0 @@
-package com.enonic.xp.portal.view;
-
-public interface ViewService
-{
-    public String process( ViewModel viewModel );
-}
