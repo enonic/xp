743cc692b50809eb575be8773d86d996cb2c5400
Bradnitski Viachaslau <vbradnitski@scnsoft.com>
2015-02-02 12:14:50 +0100

    CMS-5033 Small Task - Image content type - Add extra fields to content-type image

--- modules/core-impl/src/main/java/com/enonic/wem/core/content/ImageFormDataBuilder.java
+++ modules/core-impl/src/main/java/com/enonic/wem/core/content/ImageFormDataBuilder.java
@@ -1,12 +1,9 @@
 package com.enonic.wem.core.content;
 
-
 import com.enonic.wem.api.data.PropertyTree;
 
 final class ImageFormDataBuilder
 {
-    private String mimeType;
-
     private String image;
 
     ImageFormDataBuilder image( final String name )
@@ -15,15 +12,8 @@ final class ImageFormDataBuilder
         return this;
     }
 
-    ImageFormDataBuilder mimeType( final String mimeType )
-    {
-        this.mimeType = mimeType;
-        return this;
-    }
-
     void build( PropertyTree data )
     {
         data.setString( "media", image );
-        data.setString( "mimeType", mimeType );
     }
 }
