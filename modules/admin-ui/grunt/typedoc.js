41ac87555055c07dd8aa318859198a53ddc26a81
Bradnitski Viachaslau <vbradnitski@scnsoft.com>
2015-02-12 13:00:13 +0100

    CMS-5178 Small taks - Admin UI - Rename app title from wem to xp

--- modules/admin-ui/grunt/typedoc.js
+++ modules/admin-ui/grunt/typedoc.js
@@ -5,7 +5,7 @@ module.exports = {
     common: {
         options: {
             out: './target/typedoc',
-            name: 'Enonic WEM Admin UI Api'
+            name: 'Enonic XP Admin UI Api'
         },
         src: [baseDir + '/common/js/_module.ts']
     }
