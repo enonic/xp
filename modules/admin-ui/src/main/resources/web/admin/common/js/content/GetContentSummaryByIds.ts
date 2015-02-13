d43ed63664b37f5c2e8eb7303f8f85f9ef413934
Alex Rodriguez <aro@enonic.com>
2015-02-13 10:31:35 +0100

    CMS-5215 Wizard - Not possible to edit a content if relationship has been broken

--- modules/admin-ui/src/main/resources/web/admin/common/js/content/GetContentSummaryByIds.ts
+++ modules/admin-ui/src/main/resources/web/admin/common/js/content/GetContentSummaryByIds.ts
@@ -9,13 +9,27 @@ module api.content {
         }
 
         get(): wemQ.Promise<api.content.ContentSummary[]> {
+            var deferred = wemQ.defer<api.content.ContentSummary[]>();
 
             var allPromises = (this.ids || []).map((contentId: ContentId) => {
-
                 return new api.content.GetContentSummaryByIdRequest(contentId).sendAndParse();
             });
+            var settledPromises: wemQ.Promise<wemQ.PromiseState<api.content.ContentSummary>[]> = wemQ.allSettled(allPromises);
 
-            return wemQ.all(allPromises);
+            settledPromises.spread((...contentResults: wemQ.PromiseState<api.content.ContentSummary>[])=> {
+                if (!contentResults) {
+                    deferred.resolve([]);
+                    return;
+                }
+
+                var contents: api.content.ContentSummary[] = contentResults.
+                    filter(contentResult => contentResult.state === 'fulfilled').
+                    map(contentResult => contentResult.value);
+
+                deferred.resolve(contents);
+            }).done();
+
+            return deferred.promise;
         }
 
     }
