1948a122bf98b54051a2fc075398f44fb6d985d7
Sten Roger Sandvik <srs@enonic.com>
2015-02-14 19:58:58 +0100

    Removed unused code.

--- modules/portal-script/src/main/java/com/enonic/wem/script/internal/bean/JsObjectConverter.java
+++ modules/portal-script/src/main/java/com/enonic/wem/script/internal/bean/JsObjectConverter.java
@@ -13,9 +13,7 @@ import jdk.nashorn.internal.objects.Global;
 import jdk.nashorn.internal.objects.NativeArray;
 
 import com.enonic.wem.script.internal.serializer.ScriptMapGenerator;
-import com.enonic.wem.script.internal.serializer.ScriptMapGenerator2;
 import com.enonic.wem.script.serializer.MapSerializable;
-import com.enonic.wem.script.serializer.MapSerializable2;
 
 public final class JsObjectConverter
 {
@@ -24,11 +22,6 @@ public final class JsObjectConverter
         if ( value instanceof MapSerializable )
         {
             return toJs( (MapSerializable) value );
-        }
-
-        if ( value instanceof MapSerializable2 )
-        {
-            return toJs( (MapSerializable2) value );
         }
 
         if ( value instanceof List )
@@ -55,13 +48,6 @@ public final class JsObjectConverter
         final ScriptMapGenerator generator = new ScriptMapGenerator();
         value.serialize( generator );
         return generator.getRoot();
-    }
-
-    private static Object toJs( final MapSerializable2 value )
-    {
-        final ScriptMapGenerator2 generator = new ScriptMapGenerator2();
-        value.serialize( generator );
-        return generator.getModel();
     }
 
     private static Object toJs( final List list )
--- modules/portal-script/src/main/java/com/enonic/wem/script/serializer/MapSerializable2.java
+++
@@ -1,6 +0,0 @@
-package com.enonic.wem.script.serializer;
-
-public interface MapSerializable2
-{
-    public void serialize( MapGenerator2 gen );
-}
