d1bf12280866774accdacf0813f9ed6a3c11775b
Sten Roger Sandvik <srs@enonic.com>
2015-02-23 18:03:27 +0100

    Removed unused file.

--- modules/core-repo/src/main/java/com/enonic/wem/repo/internal/entity/relationship/dao/RelationshipDao.java
+++
@@ -1,9 +0,0 @@
-package com.enonic.wem.repo.internal.entity.relationship.dao;
-
-
-public class RelationshipDao
-{
-    /* TODO:
-    *   Store relationships next to items (not inside it) as: itemRelationships
-    */
-}
