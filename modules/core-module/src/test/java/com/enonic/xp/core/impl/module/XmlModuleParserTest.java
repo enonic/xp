1c0a3fb325648a7e6c3a57da3328e4ed09bdee40
Sten Roger Sandvik <srs@enonic.com>
2015-03-29 23:50:05 +0200

    XP-232 Refactor XML parsing using validation.

--- modules/core-module/src/test/java/com/enonic/xp/core/impl/module/XmlModuleParserTest.java
+++ modules/core-module/src/test/java/com/enonic/xp/core/impl/module/XmlModuleParserTest.java
@@ -7,16 +7,16 @@ import com.enonic.xp.module.ModuleKey;
 import com.enonic.xp.module.ModuleVersion;
 import com.enonic.xp.support.SerializingTestHelper;
 
-public class ModuleXmlBuilderTest
+public class XmlModuleParserTest
 {
     private final SerializingTestHelper serializingTestHelper;
 
-    private final ModuleXmlBuilder xmlBuilder;
+    private final XmlModuleParser parser;
 
-    public ModuleXmlBuilderTest()
+    public XmlModuleParserTest()
     {
         this.serializingTestHelper = new SerializingTestHelper( this, true );
-        this.xmlBuilder = new ModuleXmlBuilder();
+        this.parser = new XmlModuleParser();
     }
 
     private String loadTestXml( final String fileName )
@@ -28,11 +28,14 @@ public class ModuleXmlBuilderTest
     public void testModuleXmlDeserialization()
     {
         final String xml = loadTestXml( "serialized-module.xml" );
+
         final ModuleImpl module = new ModuleImpl();
         module.moduleKey = ModuleKey.from( "mymodule" );
         module.moduleVersion = ModuleVersion.from( "1.0.0" );
 
-        this.xmlBuilder.toModule( xml, module );
+        this.parser.source( xml );
+        this.parser.module( module );
+        this.parser.parse();
 
         Assert.assertNotNull( module.config );
         Assert.assertNotNull( module.metaSteps );
