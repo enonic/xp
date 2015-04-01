65c77ae134ee5d00b48702d3851df7bef0ed9d79
Pavel Milkevich <pmi@enonic.com>
2015-04-01 11:08:13 +0300

    XP-7 Live Edit - Go through all components used in live edit and make sure they don't depend on parent styles
    - also removed live edit's font-icon.less duplicating the api's icons.less

--- modules/admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/wizard/page/contextwindow/ContextWindowToggler.ts
+++ modules/admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/wizard/page/contextwindow/ContextWindowToggler.ts
@@ -3,8 +3,8 @@ module app.wizard.page.contextwindow {
     export class ContextWindowToggler extends api.ui.button.Button {
 
         constructor() {
-            super("");
-            this.addClass("icon-menu3 icon-medium toggler");
+            super();
+            this.addClass("toggle-button icon-menu3 icon-medium");
             this.setActive(false);
 
             ShowLiveEditEvent.on(() => {
