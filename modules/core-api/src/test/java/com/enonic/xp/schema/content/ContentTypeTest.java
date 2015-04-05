ebe33b374f80e4d32e64a16a57d5085b85812622
CorbenDallas <sts@enonic.com>
2015-04-05 21:02:10 +0300

    XP-244 Add more unit tests for schema package in API - Covered > 85% lines of codes of schema package and its internal packages with unit tests

--- modules/core-api/src/test/java/com/enonic/xp/schema/content/ContentTypeTest.java
+++ modules/core-api/src/test/java/com/enonic/xp/schema/content/ContentTypeTest.java
@@ -1,18 +1,27 @@
 package com.enonic.xp.schema.content;
 
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
+import com.enonic.xp.content.ContentName;
 import com.enonic.xp.form.FieldSet;
+import com.enonic.xp.form.Form;
 import com.enonic.xp.form.FormItemSet;
 import com.enonic.xp.form.inputtype.InputTypes;
 import com.enonic.xp.schema.content.ContentType;
 import com.enonic.xp.schema.content.ContentTypeName;
+import com.enonic.xp.security.PrincipalKey;
 
 import static com.enonic.xp.form.FormItemSet.newFormItemSet;
 import static com.enonic.xp.form.Input.newInput;
 import static com.enonic.xp.schema.content.ContentType.newContentType;
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ContentTypeTest
 {
@@ -91,4 +100,143 @@ public class ContentTypeTest
         assertEquals( "top-set.inner-set.myInnerInput",
                       contentType.form().getInput( "top-set.inner-set.myInnerInput" ).getPath().toString() );
     }
+
+    @Test
+    public void contentTypeBuilder()
+    {
+        ContentType.Builder builder = newContentType().name( ContentTypeName.media() ).form( ContentTypeForms.MEDIA_DEFAULT )
+            .setAbstract().setFinal().allowChildContent( true ).setBuiltIn().contentDisplayNameScript( "contentDisplayNameScript" )
+            .metadata( null ).displayName( "displayName" ).description( "description" ).modifiedTime( Instant.now() )
+            .createdTime( Instant.now() ).creator( PrincipalKey.ofAnonymous() ).modifier( PrincipalKey.ofAnonymous() );
+        ContentType contentType1 = builder.build();
+        ContentType contentType2 = newContentType(contentType1).build();
+        assertEquals( contentType1.getName(), contentType2.getName() );
+        assertEquals( contentType1.form(), contentType2.form() );
+        assertEquals( contentType1.isAbstract(), contentType2.isAbstract() );
+        assertEquals( contentType1.isFinal(), contentType2.isFinal() );
+        assertEquals( contentType1.allowChildContent(), contentType2.allowChildContent() );
+        assertEquals( contentType1.isBuiltIn(), contentType2.isBuiltIn() );
+        assertEquals( contentType1.getContentDisplayNameScript(), contentType2.getContentDisplayNameScript() );
+        assertEquals( contentType1.getDisplayName(), contentType2.getDisplayName() );
+        assertEquals( contentType1.getDescription(), contentType2.getDescription() );
+        assertEquals( contentType1.getModifiedTime(), contentType2.getModifiedTime() );
+        assertEquals( contentType1.getCreatedTime(), contentType2.getCreatedTime() );
+        assertEquals( contentType1.getCreator(), contentType2.getModifier() );
+        assertEquals( contentType1.toString(), contentType2.toString() );
+    }
+
+    @Test
+    public void contentTypeDeletedEvent()
+    {
+        ContentTypeDeletedEvent ctdEvent = new ContentTypeDeletedEvent(ContentTypeName.media());
+        assertTrue(ContentTypeName.media().equals( ctdEvent.getName() ));
+    }
+
+    @Test
+    public void contentTypeFilter()
+    {
+        ContentTypeFilter.Builder builder = ContentTypeFilter.newContentFilter().allowContentType( ContentTypeName.media() )
+            .allowContentType( ContentTypeName.from( "mymodule:my_type" ) )
+            .allowContentTypes( ContentTypeNames.from( ContentTypeName.archiveMedia() ) )
+            .defaultDeny().denyContentType( ContentTypeName.audioMedia() )
+            .denyContentTypes( ContentTypeNames.from( ContentTypeName.documentMedia() ) )
+            .allowContentType( "mymodule:my_type1" )
+            .denyContentType( "mymodule:my_type2" );
+        ContentTypeFilter ctFilter = builder.build();
+        ContentTypeFilter ctFilter1 = builder.build();
+        assertTrue( ctFilter.isContentTypeAllowed( ContentTypeName.from( "mymodule:my_type" ) ) );
+        assertTrue( ctFilter.isContentTypeAllowed( ContentTypeName.from( "mymodule:my_type1" ) ) );
+        assertTrue( ctFilter.isContentTypeAllowed( ContentTypeName.archiveMedia() ) );
+        assertFalse( ctFilter.isContentTypeAllowed( ContentTypeName.from( "mymodule:my_type2" ) ) );
+        assertFalse( ctFilter.isContentTypeAllowed( ContentTypeName.audioMedia() ) );
+        assertFalse( ctFilter.isContentTypeAllowed( ContentTypeName.documentMedia() ) );
+        assertTrue( ctFilter.equals( ( ctFilter1 ) ) );
+        assertFalse( ctFilter.equals( ( builder ) ) );
+    }
+
+    @Test
+    public void contentTypes()
+    {
+        ContentType.Builder builder = newContentType().name( ContentTypeName.media() ).form( ContentTypeForms.PAGE_TEMPLATE )
+            .setAbstract().setFinal().allowChildContent( true ).setBuiltIn().contentDisplayNameScript( "contentDisplayNameScript" )
+            .metadata( null ).displayName( "displayName" ).description( "description" ).modifiedTime( Instant.now() )
+            .createdTime( Instant.now() ).creator( PrincipalKey.ofAnonymous() ).modifier( PrincipalKey.ofAnonymous() );
+        ContentType contentType = builder.build();
+        ContentTypes contentTypes = ContentTypes.newContentTypes().add(contentType).build();
+        assertTrue( contentTypes.getNames().contains( ContentTypeName.media() ) );
+        assertTrue(ContentTypes.empty().getSize() == 0);
+        assertTrue(ContentTypes.from( contentType ).getSize() == 1);
+        assertNotNull( contentTypes.getContentType( contentType.getName() ) );
+    }
+
+    @Test
+    public void contentTypeUpdatedEvent()
+    {
+        Instant now = Instant.now();
+        ContentTypeUpdatedEvent event = new ContentTypeUpdatedEvent(ContentTypeName.media(), now);
+        assertTrue( ContentTypeName.media().equals( event.getName() ) );
+        assertEquals( now, event.getModifiedTime() );
+    }
+
+    @Test
+    public void getAllContentTypesParams()
+    {
+        GetAllContentTypesParams params = new GetAllContentTypesParams();
+        params.inlineMixinsToFormItems(true);
+        assertTrue( params.isInlineMixinsToFormItems() );
+    }
+
+    @Test
+    public void getChildContentTypesParams()
+    {
+        GetChildContentTypesParams params1 = new GetChildContentTypesParams();
+        params1.parentName( ContentTypeName.archiveMedia() );
+        params1.validate();
+        GetChildContentTypesParams params2 = new GetChildContentTypesParams();
+        params2.parentName( ContentTypeName.archiveMedia() );
+        assertEquals( params1.getParentName(), params2.getParentName() );
+        assertEquals( params1, params2 );
+        assertTrue( params1.hashCode() == params2.hashCode() );
+        assertTrue( params1.equals( params1 ) );
+        assertFalse( params1.equals( null ) );
+    }
+
+    @Test
+    public void getContentTypeParams()
+    {
+        GetContentTypeParams params1 = GetContentTypeParams.from( ContentTypeName.archiveMedia() );
+        GetContentTypeParams params2 = GetContentTypeParams.from( ContentTypeName.archiveMedia() );
+        GetContentTypeParams params3 = GetContentTypeParams.from( ContentTypeName.archiveMedia() );
+        params1.inlineMixinsToFormItems( true );
+        params1.validate();
+        assertFalse( params2.isInlineMixinsToFormItems() );
+        assertFalse( params2.equals( params1 ) );
+        assertFalse( params2.hashCode() == params1.hashCode() );
+        assertTrue( params2.getContentTypeName().equals( params1.getContentTypeName() ) );
+        assertEquals( params1, params1 );
+        params1.contentTypeName( "test" );
+        assertNotEquals( params1, params2 );
+        assertEquals( params2, params3 );
+    }
+
+    @Test
+    public void getContentTypesParams()
+    {
+        List<ContentTypeName> list = new ArrayList<ContentTypeName>() {{add(ContentTypeName.audioMedia());}};
+        GetContentTypesParams params1 = new GetContentTypesParams();
+        params1.contentTypeNames( ContentTypeNames.newContentTypeNames().add( ContentTypeName.archiveMedia() ).addAll( list ).build() );
+        GetContentTypesParams params2 = new GetContentTypesParams();
+        params2.contentTypeNames( ContentTypeNames.empty() );
+        params2.inlineMixinsToFormItems( true );
+        params1.validate();
+        assertTrue( params1.getContentTypeNames().getSize() == 2 );
+        assertFalse( params1.hashCode() == params2.hashCode() );
+        assertEquals( params1, params1 );
+        assertNotEquals( params1.isInlineMixinsToFormItems(), params2.isInlineMixinsToFormItems() );
+        assertNotEquals( params1, null );
+    }
+
+
+
+
 }
