2f97ba91a87dc5d4a92424c781ff343227f3446b
Alex Shklianko <asl@enonic.com>
2015-04-07 14:00:34 +0300

    XP-210 Recursive Delete - Support recursive delete
    -Enabled delete button also for items with children
    -Updated Delete dialogue according to design (re-used bulk selection design from detail view)
    -Dropped grey background for bulk items in detail view too (made these lists exactly the same)
    -Added a totalcount of how many items will be deleted in the delete button of the modal window.
    -Made delete button use red color
    
    -BONUS: also found issue with mask spinner and resolved it

--- modules/admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/remove/ContentDeleteDialog.ts
+++ modules/admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/remove/ContentDeleteDialog.ts
@@ -1,48 +1,28 @@
 module app.remove {
 
     import ContentIconUrlResolver = api.content.ContentIconUrlResolver;
+    import BrowseItem = api.app.browse.BrowseItem;
+    import SelectionItem = api.app.browse.SelectionItem;
+    import ContentSummary = api.content.ContentSummary;
+    import ContentPath = api.content.ContentPath;
+    import DialogButton = api.ui.dialog.DialogButton;
 
     export class ContentDeleteDialog extends api.app.remove.DeleteDialog {
 
-        private contentToDelete: api.content.ContentSummary[];
+        private selectedItems: SelectionItem<ContentSummary>[];
+
+        private deleteButton: DialogButton;
 
         constructor() {
             super("Content");
 
-            this.setDeleteAction(new ContentDeleteDialogAction());
+            this.deleteButton = this.setDeleteAction(new ContentDeleteDialogAction());
 
             this.getDeleteAction().onExecuted(() => {
 
-                var deleteRequest = new api.content.DeleteContentRequest();
-                for (var i = 0; i < this.contentToDelete.length; i++) {
-                    deleteRequest.addContentPath(this.contentToDelete[i].getPath());
-                }
-
-                deleteRequest.sendAndParse().then((result: api.content.DeleteContentResult) => {
-
-                    var paths = [],
-                        deletedContents: api.content.ContentSummary[] = [];
-
-                    for (var i = 0; i < result.getDeleted().length; i++) {
-                        var path = result.getDeleted()[i];
-                        paths.push(path);
-
-                        this.contentToDelete.forEach((content: api.content.ContentSummary) => {
-                            if (content.getPath().equals(path)) {
-                                deletedContents.push(content);
-                            }
-                        })
-                    }
-
+                this.createDeleteRequest().sendAndParse().then((result: api.content.DeleteContentResult) => {
                     this.close();
-
-                    if (result.getDeleted().length > 0) {
-                        api.notify.showFeedback('Content [' + paths.map((path)=>path.toString()).join(', ') + '] deleted!');
-                    } else {
-                        var reason = result.getDeleteFailures().length > 0 ? result.getDeleteFailures()[0].getReason() : '';
-                        api.notify.showWarning('Content could not be deleted. ' + reason);
-                    }
-
+                    this.showDeleteResult(result);
                 }).catch((reason: any) => {
                     if (reason && reason.message) {
                         api.notify.showError(reason.message);
@@ -55,17 +35,120 @@ module app.remove {
             this.addCancelButtonToBottom();
         }
 
-        setContentToDelete(contentModels: api.content.ContentSummary[]) {
-            this.contentToDelete = contentModels;
+        setContentToDelete(contents: ContentSummary[]) {
 
-            var deleteViewers: api.content.ContentSummaryViewer[] = [];
-            for (var i in contentModels) {
-                var content = contentModels[i];
-                var deleteViewer = new api.content.ContentSummaryViewer();
-                deleteViewer.setObject(content);
-                deleteViewers.push(deleteViewer);
+            this.selectedItems = [];
+
+            contents.forEach((content: ContentSummary) => {
+                this.selectedItems.push(this.createSelectionItemForDelete(content));
+            });
+
+            this.renderSelectedItems(this.selectedItems);
+
+            this.countItemsToDeleteAndUpdateButtonCounter();
+        }
+
+        private indexOf(item: SelectionItem<ContentSummary>): number {
+            for (var i = 0; i < this.selectedItems.length; i++) {
+                if (item.getBrowseItem().getPath() == this.selectedItems[i].getBrowseItem().getPath()) {
+                    return i;
+                }
             }
-            this.setDeleteViewers(deleteViewers);
+            return -1;
+        }
+
+        private createSelectionItemForDelete(content: ContentSummary): SelectionItem<ContentSummary> {
+
+            var deleteItemViewer = new api.content.ContentSummaryViewer();
+            deleteItemViewer.setObject(content);
+
+            var browseItem = new BrowseItem<ContentSummary>(content).
+                setId(content.getId()).
+                setDisplayName(content.getDisplayName()).
+                setPath(content.getPath().toString()).
+                setIconUrl(new ContentIconUrlResolver().setContent(content).resolve());
+
+            var selectionItem = new SelectionItem(deleteItemViewer, browseItem, () => {
+                var index = this.indexOf(selectionItem);
+                if (index < 0) {
+                    return;
+                }
+
+                this.selectedItems[index].remove();
+                this.selectedItems.splice(index, 1);
+
+                if (this.selectedItems.length == 0) {
+                    this.close();
+                }
+                else {
+                    this.countItemsToDeleteAndUpdateButtonCounter();
+                }
+            });
+
+            return selectionItem;
+        }
+
+        private countItemsToDeleteAndUpdateButtonCounter() {
+            this.cleanDeleteButtonText();
+            this.showLoadingSpinner();
+
+            this.createRequestForCountingItemsToDelete().sendAndParse().then((itemsToDeleteCounter: number) => {
+                this.hideLoadingSpinner();
+                this.updateDeleteButtonCounter(itemsToDeleteCounter);
+            }).finally(() => {
+                this.hideLoadingSpinner();
+            }).done();
+        }
+
+
+        private createRequestForCountingItemsToDelete(): api.content.CountContentsWithDescendantsRequest {
+            var countContentChildrenRequest = new api.content.CountContentsWithDescendantsRequest();
+            for (var j = 0; j < this.selectedItems.length; j++) {
+                countContentChildrenRequest.addContentPath(ContentPath.fromString(this.selectedItems[j].getBrowseItem().getPath()));
+            }
+
+            return countContentChildrenRequest;
+        }
+
+        private createDeleteRequest(): api.content.DeleteContentRequest {
+            var deleteRequest = new api.content.DeleteContentRequest();
+            for (var i = 0; i < this.selectedItems.length; i++) {
+                deleteRequest.addContentPath(ContentPath.fromString(this.selectedItems[i].getBrowseItem().getPath()));
+            }
+
+            return deleteRequest;
+        }
+
+        private showDeleteResult(result: api.content.DeleteContentResult) {
+            var paths = [];
+
+            for (var i = 0; i < result.getDeleted().length; i++) {
+                var path = result.getDeleted()[i];
+                paths.push(path);
+            }
+
+            if (result.getDeleted().length > 0) {
+                api.notify.showFeedback('Content [' + paths.map((path)=>path.toString()).join(', ') + '] deleted!');
+            } else {
+                var reason = result.getDeleteFailures().length > 0 ? result.getDeleteFailures()[0].getReason() : '';
+                api.notify.showWarning('Content could not be deleted. ' + reason);
+            }
+        }
+
+        private updateDeleteButtonCounter(count: number) {
+            this.deleteButton.setLabel("Delete (" + count + ")");
+        }
+
+        private showLoadingSpinner() {
+            this.deleteButton.addClass("spinner");
+        }
+
+        private hideLoadingSpinner() {
+            this.deleteButton.removeClass("spinner");
+        }
+
+        private cleanDeleteButtonText() {
+            this.deleteButton.setLabel("Delete ");
         }
     }
 }
