70b96148b9716b7c8deddea11a30becdb9bb4bb0
Sten Roger Sandvik <srs@enonic.com>
2015-04-22 13:05:56 +0200

    XP-377 Script - Object export does not work the same as module.export. Added test to identify the problem.

--- modules/portal-script/src/test/resources/mymodule/require/require-test.js
+++ modules/portal-script/src/test/resources/mymodule/require/require-test.js
@@ -9,3 +9,11 @@ assert.assertEquals('Hello World!', other.hello('World'));
 
 var util = require('util');
 assert.assertEquals('Hello from Lib!', util.hello());
+
+var all1 = require('./export/all1');
+// assert.assertEquals(1, all1.a);
+// assert.assertEquals(2, all1.b);
+
+var all2 = require('export/all2');
+assert.assertEquals(1, all2.a);
+assert.assertEquals(2, all2.b);
