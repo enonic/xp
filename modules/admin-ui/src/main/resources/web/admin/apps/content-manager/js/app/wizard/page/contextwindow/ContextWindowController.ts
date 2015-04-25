49512f9d056e1852565e0c32cb1b18ac8b66e39b
Pavel Milkevich <pmi@enonic.com>
2015-04-25 15:43:44 +0300

    XP-329 Live Edit - Improved consistency of Context Window Cycle button

--- modules/admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/wizard/page/contextwindow/ContextWindowController.ts
+++ modules/admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/wizard/page/contextwindow/ContextWindowController.ts
@@ -14,33 +14,55 @@ module app.wizard.page.contextwindow {
             this.contextWindow = contextWindow;
             this.contextWindowToggler = contextWindowToggler;
 
-            this.contextWindowToggler.onClicked((event: MouseEvent) => {
-                var active = !this.contextWindowToggler.isActive();
-                this.contextWindowToggler.setActive(active);
-
-                if (active) {
+            this.contextWindowToggler.onActiveChanged((isActive: boolean) => {
+                if (isActive) {
                     this.contextWindow.slideIn();
                 } else {
                     this.contextWindow.slideOut();
                 }
             });
 
-            this.contextWindow.onShown(() => {
-                if (this.contextWindow.isFloating()) {
-                    this.contextWindow.slideOut();
-                    this.contextWindowToggler.setActive(false);
-                } else {
-                    this.contextWindow.slideIn();
-                    this.contextWindowToggler.setActive(true);
-                }
-            });
+            var liveEditShownHandler = () => {
+                this.contextWindowToggler.setEnabled(true);
+                this.activateContextWindowForLargeScreen();
+                this.slideContextWindowIfNecessary();
+            };
 
-            this.contextWindow.onDisplayModeChanged(() => {
-                if (!this.contextWindow.isFloating() && !this.contextWindowToggler.isActive() && this.contextWindow.isShown()) {
-                    this.contextWindow.slideOut();
-                }
-            });
+            var liveEditHiddenHandler = () => {
+                this.contextWindowToggler.setEnabled(false);
+            };
+
+            app.wizard.ShowLiveEditEvent.on(liveEditShownHandler);
+            app.wizard.ShowSplitEditEvent.on(liveEditShownHandler);
+            app.wizard.ShowContentFormEvent.on(liveEditHiddenHandler);
+
+            this.contextWindow.onShown(() => this.activateContextWindowForLargeScreen());
         }
+
+        public slideContextWindowIfNecessary() {
+            // triggered by screen resize or split change
+            if (this.contextWindowToggler.isActive()) {
+                // context window should be shown anyway when toggler is active
+                return;
+            }
+            if (this.contextWindow.isShown() && !this.contextWindow.canAutoSlide()) {
+                // hide if it's shown and can not auto slide
+                this.contextWindow.slideOut();
+            } else if (!this.contextWindow.isShown() && this.contextWindow.canAutoSlide() && this.contextWindow.isInspecting()) {
+                // show if it's not shown, but inspecting and can auto slide
+                this.contextWindow.slideIn();
+            }
+        }
+
+        private activateContextWindowForLargeScreen() {
+            // triggered on show, or split change
+            if (this.contextWindow.isFloating()) {
+                this.contextWindowToggler.setActive(false);
+            } else {
+                this.contextWindowToggler.setActive(true);
+            }
+        }
+
     }
 
 }
--- modules/admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/wizard/page/contextwindow/ContextWindowToggler.ts
+++ modules/admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/wizard/page/contextwindow/ContextWindowToggler.ts
@@ -2,21 +2,42 @@ module app.wizard.page.contextwindow {
 
     export class ContextWindowToggler extends api.ui.button.Button {
 
+        private activeListeners: {(isActive: boolean): void}[] = [];
+
         constructor() {
             super();
             this.addClass("toggle-button icon-menu3 icon-medium");
             this.setActive(false);
 
-            ShowLiveEditEvent.on(() => {
-                this.setEnabled(true);
-            });
+            this.onClicked((event: MouseEvent) => {
+                if (this.isEnabled()) {
+                    this.setActive(!this.isActive());
+                }
+            })
+        }
 
-            ShowSplitEditEvent.on(() => {
-                this.setEnabled(true);
-            });
+        setActive(value: boolean) {
+            this.toggleClass('active', value);
+            this.notifyActiveChanged(value);
+        }
 
-            ShowContentFormEvent.on(() => {
-                this.setEnabled(false);
+        isActive() {
+            return this.hasClass("active");
+        }
+
+        onActiveChanged(listener: (isActive: boolean) => void) {
+            this.activeListeners.push(listener);
+        }
+
+        unActiveChanged(listener: (isActive: boolean) => void) {
+            this.activeListeners = this.activeListeners.filter((curr) => {
+                return curr !== listener;
+            })
+        }
+
+        private notifyActiveChanged(isActive: boolean) {
+            this.activeListeners.forEach((listener) => {
+                listener(isActive);
             });
         }
     }
