30ae620af1a8f55a4a60990682747482b461fa40
gri <gri@enonic.com>
2015-04-30 10:51:11 +0200

    XP-343 Validate content property tree for create

--- modules/core-api/src/main/java/com/enonic/xp/form/inputtype/SingleSelector.java
+++ modules/core-api/src/main/java/com/enonic/xp/form/inputtype/SingleSelector.java
@@ -5,7 +5,9 @@ import org.apache.commons.lang.StringUtils;
 
 import com.enonic.xp.data.Property;
 import com.enonic.xp.data.Value;
+import com.enonic.xp.data.ValueTypes;
 import com.enonic.xp.form.BreaksRequiredContractException;
+import com.enonic.xp.form.InvalidTypeException;
 
 final class SingleSelector
     extends InputType
@@ -35,6 +37,16 @@ final class SingleSelector
         if ( StringUtils.isBlank( stringValue ) )
         {
             throw new BreaksRequiredContractException( property, this );
+        }
+    }
+
+    @Override
+    public void checkTypeValidity( final Property property )
+        throws InvalidTypeException
+    {
+        if ( !ValueTypes.STRING.equals( property.getType() ) )
+        {
+            throw new InvalidTypeException( property, ValueTypes.STRING );
         }
     }
 
--- modules/core-api/src/main/java/com/enonic/xp/form/inputtype/Time.java
+++ modules/core-api/src/main/java/com/enonic/xp/form/inputtype/Time.java
@@ -7,6 +7,7 @@ import com.enonic.xp.data.Property;
 import com.enonic.xp.data.Value;
 import com.enonic.xp.data.ValueTypes;
 import com.enonic.xp.form.BreaksRequiredContractException;
+import com.enonic.xp.form.InvalidTypeException;
 
 @Beta
 public class Time
@@ -22,6 +23,16 @@ public class Time
         throws BreaksRequiredContractException
     {
 
+    }
+
+    @Override
+    public void checkTypeValidity( final Property property )
+        throws InvalidTypeException
+    {
+        if ( !ValueTypes.LOCAL_TIME.equals( property.getType() ) )
+        {
+            throw new InvalidTypeException( property, ValueTypes.LOCAL_TIME );
+        }
     }
 
     @Override
