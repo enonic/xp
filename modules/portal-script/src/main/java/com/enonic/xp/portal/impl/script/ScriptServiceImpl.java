575c9ba2ac4c5d9cdf7504324baaa4eae705edbb
Sten Roger Sandvik <srs@enonic.com>
2015-05-13 12:06:01 +0200

    XP-486 Local ScriptEngine per module

--- modules/portal-script/src/main/java/com/enonic/xp/portal/impl/script/ScriptServiceImpl.java
+++ modules/portal-script/src/main/java/com/enonic/xp/portal/impl/script/ScriptServiceImpl.java
@@ -1,6 +1,7 @@
 package com.enonic.xp.portal.impl.script;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
 
 import javax.script.ScriptEngine;
 
@@ -11,6 +12,10 @@ import org.osgi.service.component.annotations.ReferencePolicy;
 
 import com.google.common.collect.Maps;
 
+import com.enonic.xp.event.Event;
+import com.enonic.xp.event.EventListener;
+import com.enonic.xp.module.ModuleKey;
+import com.enonic.xp.module.ModuleUpdatedEvent;
 import com.enonic.xp.portal.impl.script.invoker.CommandInvokerImpl;
 import com.enonic.xp.portal.impl.script.util.NashornHelper;
 import com.enonic.xp.portal.script.ScriptExports;
@@ -19,21 +24,21 @@ import com.enonic.xp.portal.script.ScriptValue;
 import com.enonic.xp.portal.script.command.CommandHandler;
 import com.enonic.xp.resource.ResourceKey;
 
-@Component(immediate = true)
+@Component(immediate = true, service = {ScriptService.class, EventListener.class})
 public final class ScriptServiceImpl
-    implements ScriptService
+    implements ScriptService, EventListener
 {
-    private final ScriptEngine engine;
-
     private final CommandInvokerImpl invoker;
 
     private final Map<String, Object> globalMap;
 
+    private final ConcurrentMap<ModuleKey, ScriptExecutor> executors;
+
     public ScriptServiceImpl()
     {
-        this.engine = NashornHelper.getScriptEngine( "-strict" );
         this.invoker = new CommandInvokerImpl();
         this.globalMap = Maps.newHashMap();
+        this.executors = Maps.newConcurrentMap();
     }
 
     public void addGlobalVariable( final String key, final Object value )
@@ -44,15 +49,11 @@ public final class ScriptServiceImpl
     @Override
     public ScriptExports execute( final ResourceKey script )
     {
-        final ScriptExecutorImpl executor = new ScriptExecutorImpl();
-        executor.setEngine( this.engine );
-        executor.setInvoker( this.invoker );
-        executor.setGlobalMap( this.globalMap );
-        executor.setScript( script );
+        final ScriptExecutor executor = getExecutor( script.getModule() );
 
-        final Object exports = executor.executeMain();
+        final Object exports = executor.executeRequire( script );
         final ScriptValue value = executor.newScriptValue( exports );
-        return new ScriptExportsImpl( script, value );
+        return new ScriptExportsImpl( script, value, exports );
     }
 
     @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
@@ -64,5 +65,36 @@ public final class ScriptServiceImpl
     public void removeHandler( final CommandHandler handler )
     {
         this.invoker.unregister( handler );
+    }
+
+    private ScriptExecutor getExecutor( final ModuleKey key )
+    {
+        return this.executors.computeIfAbsent( key, this::createExecutor );
+    }
+
+    private ScriptExecutor createExecutor( final ModuleKey key )
+    {
+        final ScriptEngine engine = NashornHelper.getScriptEngine( "-strict" );
+
+        final ScriptExecutorImpl executor = new ScriptExecutorImpl();
+        executor.setEngine( engine );
+        executor.setInvoker( this.invoker );
+        executor.setGlobalMap( this.globalMap );
+        executor.initialize();
+        return executor;
+    }
+
+    private void invalidate( final ModuleKey key )
+    {
+        this.executors.remove( key );
+    }
+
+    @Override
+    public void onEvent( final Event event )
+    {
+        if ( event instanceof ModuleUpdatedEvent )
+        {
+            invalidate( ( (ModuleUpdatedEvent) event ).getModuleKey() );
+        }
     }
 }
