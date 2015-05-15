e96b89b81ce3bd7d6db98d5a33d8bdb91c81a32b
gri <gri@enonic.com>
2015-05-15 13:22:41 +0200

    XP-58 Add order-value to script command reorderChildren

--- modules/admin-ui/src/main/resources/web/admin/common/js/content/json/ReorderChildContentsJson.ts
+++ modules/admin-ui/src/main/resources/web/admin/common/js/content/json/ReorderChildContentsJson.ts
@@ -2,6 +2,8 @@ module api.content.json {
 
     export interface ReorderChildContentsJson extends SetOrderUpdateJson {
 
+        childOrder: ChildOrderJson;
+
         reorderChildren: ReorderChildContentJson[];
 
     }
