1214c76fee4db72446d3e2d49405a60bf2013394
Glenn Ricaud <gri@enonic.com>
2015-06-03 16:43:37 +0200

    XP-660 Change format of site.xml

--- modules/core-module/src/main/java/com/enonic/xp/core/impl/module/XmlModuleParser.java
+++ modules/core-module/src/main/java/com/enonic/xp/core/impl/module/XmlModuleParser.java
@@ -1,5 +1,6 @@
 package com.enonic.xp.core.impl.module;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,6 +14,16 @@ import com.enonic.xp.xml.parser.XmlObjectParser;
 final class XmlModuleParser
     extends XmlObjectParser<XmlModuleParser>
 {
+    private static final String ROOT_TAG_NAME = "site";
+
+    private static final String CONFIG_TAG_NAME = "config";
+
+    private static final String CONTENT_TAG_NAME = "content";
+
+    private static final String META_STEP_TAG_NAME = "meta-step";
+
+    private static final String MIXIN_ATTRIBUTE_NAME = "mixin";
+
     private ModuleImpl module;
 
     public XmlModuleParser module( final ModuleImpl module )
@@ -25,23 +36,33 @@ final class XmlModuleParser
     protected void doParse( final DomElement root )
         throws Exception
     {
-        assertTagName( root, "module" );
+        assertTagName( root, ROOT_TAG_NAME );
 
         final XmlFormMapper formMapper = new XmlFormMapper( this.module.moduleKey );
-        this.module.config = formMapper.buildForm( root.getChild( "config" ) );
+        this.module.config = formMapper.buildForm( root.getChild( CONFIG_TAG_NAME ) );
 
         this.module.metaSteps = MixinNames.from( parseMetaSteps( root ) );
     }
 
     private List<MixinName> parseMetaSteps( final DomElement root )
     {
-        return root.getChildren( "x-data" ).stream().map( this::toMixinName ).collect( Collectors.toList() );
+        final DomElement content = root.getChild( CONTENT_TAG_NAME );
+
+        if ( content == null )
+        {
+            return Collections.emptyList();
+        }
+        else
+        {
+            return content.getChildren( META_STEP_TAG_NAME ).stream().map( this::toMixinName ).collect( Collectors.toList() );
+        }
+
     }
 
-    private MixinName toMixinName( final DomElement root )
+    private MixinName toMixinName( final DomElement metaStep )
     {
         final ModuleRelativeResolver resolver = new ModuleRelativeResolver( this.module.getKey() );
-        final String name = root.getAttribute( "mixin" );
+        final String name = metaStep.getAttribute( MIXIN_ATTRIBUTE_NAME );
         return resolver.toMixinName( name );
     }
 }
--- modules/core-module/src/test/java/com/enonic/xp/core/impl/module/XmlModuleParserTest.java
+++ modules/core-module/src/test/java/com/enonic/xp/core/impl/module/XmlModuleParserTest.java
@@ -37,7 +37,24 @@ public class XmlModuleParserTest
         this.parser.module( module );
         this.parser.parse();
 
-        Assert.assertNotNull( module.config );
-        Assert.assertNotNull( module.metaSteps );
+        Assert.assertEquals( 1, module.config.getFormItems().size() );
+        Assert.assertEquals( 2, module.metaSteps.getSize() );
+    }
+
+    @Test
+    public void testEmptyModuleXmlDeserialization()
+    {
+        final String xml = loadTestXml( "empty-site.xml" );
+
+        final ModuleImpl module = new ModuleImpl();
+        module.moduleKey = ModuleKey.from( "mymodule" );
+        module.moduleVersion = ModuleVersion.from( "1.0.0" );
+
+        this.parser.source( xml );
+        this.parser.module( module );
+        this.parser.parse();
+
+        Assert.assertEquals( 0, module.config.getFormItems().size() );
+        Assert.assertEquals( 0, module.metaSteps.getSize() );
     }
 }
