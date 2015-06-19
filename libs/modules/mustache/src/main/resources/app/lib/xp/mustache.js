330d7aa9975ed431886ff7d13b323447d6fa7e96
Sten Roger Sandvik <srs@enonic.com>
2015-06-19 12:49:03 +0200

    XP-848 Refactor mustache-lib to implement ScriptBean.

--- libs/modules/mustache/src/main/resources/app/lib/xp/mustache.js
+++ libs/modules/mustache/src/main/resources/app/lib/xp/mustache.js
@@ -1,4 +1,4 @@
-var service = __.getBean('com.enonic.xp.lib.mustache.MustacheService');
+var service = __.newBean('com.enonic.xp.lib.mustache.MustacheService');
 
 exports.render = function (view, model) {
     var processor = service.newProcessor();
