200cbc51d68ce9025f3ce5b695cbe15cb5368f96
Sten Roger Sandvik <srs@enonic.com>
2015-06-19 13:07:46 +0200

    XP-851 Rename system-lib to content-lib.

---
+++ libs/modules/content/src/main/java/com/enonic/xp/lib/content/ContentServiceWrapper.java
@@ -0,0 +1,43 @@
+package com.enonic.xp.lib.content;
+
+import com.enonic.xp.content.ContentService;
+
+public final class ContentServiceWrapper
+{
+    private ContentService contentService;
+
+    public GetContentHandler newGetContent()
+    {
+        return new GetContentHandler( this.contentService );
+    }
+
+    public GetChildContentHandler newGetContentChildren()
+    {
+        return new GetChildContentHandler( this.contentService );
+    }
+
+    public QueryContentHandler newQueryContent()
+    {
+        return new QueryContentHandler( this.contentService );
+    }
+
+    public DeleteContentHandler newDeleteContent()
+    {
+        return new DeleteContentHandler( this.contentService );
+    }
+
+    public CreateContentHandler newCreateContent()
+    {
+        return new CreateContentHandler( this.contentService );
+    }
+
+    public ModifyContentHandler newModifyContent()
+    {
+        return new ModifyContentHandler( this.contentService );
+    }
+
+    public void setContentService( final ContentService contentService )
+    {
+        this.contentService = contentService;
+    }
+}
---
+++ libs/modules/content/src/main/java/com/enonic/xp/lib/content/ModifyContentHandler.java
@@ -0,0 +1,195 @@
+package com.enonic.xp.lib.content;
+
+import java.util.Map;
+import java.util.function.Function;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.enonic.xp.content.Content;
+import com.enonic.xp.content.ContentEditor;
+import com.enonic.xp.content.ContentId;
+import com.enonic.xp.content.ContentNotFoundException;
+import com.enonic.xp.content.ContentPath;
+import com.enonic.xp.content.ContentService;
+import com.enonic.xp.content.EditableContent;
+import com.enonic.xp.content.ExtraData;
+import com.enonic.xp.content.ExtraDatas;
+import com.enonic.xp.content.UpdateContentParams;
+import com.enonic.xp.convert.Converters;
+import com.enonic.xp.data.PropertyTree;
+import com.enonic.xp.lib.content.mapper.ContentMapper;
+import com.enonic.xp.module.ModuleKey;
+import com.enonic.xp.schema.content.ContentTypeName;
+import com.enonic.xp.schema.mixin.MixinName;
+
+public final class ModifyContentHandler
+    extends BaseContextHandler
+{
+    private final ContentService contentService;
+
+    private String key;
+
+    private Function<Object[], Object> editor;
+
+    public ModifyContentHandler( final ContentService contentService )
+    {
+        this.contentService = contentService;
+    }
+
+    @Override
+    protected Object doExecute()
+    {
+        final String key = checkRequired( "key", this.key );
+        final Function<Object[], Object> editor = checkRequired( "editor", this.editor );
+
+        final Content existingContent = getExistingContent( key );
+        if ( existingContent == null )
+        {
+            return null;
+        }
+
+        final UpdateContentParams params = new UpdateContentParams();
+        params.contentId( existingContent.getId() );
+        params.editor( newContentEditor( editor, existingContent ) );
+
+        final Content result = this.contentService.update( params );
+        return result != null ? new ContentMapper( result ) : null;
+    }
+
+    private Content getExistingContent( final String key )
+    {
+        try
+        {
+            if ( !key.startsWith( "/" ) )
+            {
+                return this.contentService.getById( ContentId.from( key ) );
+            }
+            else
+            {
+                return this.contentService.getByPath( ContentPath.from( key ) );
+            }
+        }
+        catch ( final ContentNotFoundException e )
+        {
+            return null;
+        }
+    }
+
+    private ContentEditor newContentEditor( final Function<Object[], Object> func, final Content existingContent )
+    {
+        return edit -> {
+            final Object value = func.apply( new Object[]{new ContentMapper( edit.source )} );
+            if ( value instanceof Map )
+            {
+                updateContent( edit, (Map) value, existingContent );
+            }
+        };
+    }
+
+    private void updateContent( final EditableContent target, final Map<?, ?> map, final Content existingContent )
+    {
+        final String displayName = Converters.convert( map.get( "displayName" ), String.class );
+        if ( displayName != null )
+        {
+            target.displayName = displayName;
+        }
+
+        final Object data = map.get( "data" );
+        if ( data instanceof Map )
+        {
+            target.data = createPropertyTree( (Map) data, existingContent.getType() );
+        }
+
+        final Object extraData = map.get( "x" );
+        if ( extraData instanceof Map )
+        {
+            target.extraDatas = createExtraDatas( (Map) extraData );
+        }
+    }
+
+    private PropertyTree createPropertyTree( final Map<?, ?> value, final ContentTypeName contentTypeName )
+    {
+        if ( value == null )
+        {
+            return null;
+        }
+
+        return this.contentService.translateToPropertyTree( createJson( value ), contentTypeName );
+    }
+
+    private PropertyTree createPropertyTree( final Map<?, ?> value, final MixinName mixinName )
+    {
+        if ( value == null )
+        {
+            return null;
+        }
+
+        return this.contentService.translateToPropertyTree( createJson( value ), mixinName );
+    }
+
+    private JsonNode createJson( final Map<?, ?> value )
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.valueToTree( value );
+    }
+
+    private ExtraDatas createExtraDatas( final Map<String, Object> value )
+    {
+        if ( value == null )
+        {
+            return null;
+        }
+
+        final ExtraDatas.Builder extradatasBuilder = ExtraDatas.builder();
+        for ( final String modulePrefix : value.keySet() )
+        {
+            final ModuleKey moduleKey = ExtraData.fromModulePrefix( modulePrefix );
+            final Object metadatasObject = value.get( modulePrefix );
+            if ( !( metadatasObject instanceof Map ) )
+            {
+                continue;
+            }
+
+            final Map<String, Object> metadatas = (Map<String, Object>) metadatasObject;
+
+            for ( final String metadataName : metadatas.keySet() )
+            {
+                final MixinName mixinName = MixinName.from( moduleKey, metadataName );
+                final ExtraData item = createExtraData( mixinName, metadatas.get( metadataName ) );
+                if ( item != null )
+                {
+                    extradatasBuilder.add( item );
+                }
+            }
+        }
+
+        return extradatasBuilder.build();
+    }
+
+
+    private ExtraData createExtraData( final MixinName mixinName, final Object value )
+    {
+        if ( value instanceof Map )
+        {
+            final PropertyTree propertyTree = createPropertyTree( (Map) value, mixinName );
+
+            if ( propertyTree != null )
+            {
+                return new ExtraData( mixinName, propertyTree );
+            }
+        }
+
+        return null;
+    }
+
+    public void setKey( final String key )
+    {
+        this.key = key;
+    }
+
+    public void setEditor( final Function<Object[], Object> editor )
+    {
+        this.editor = editor;
+    }
+}
