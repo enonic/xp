1bf4b19c50ceac7c3280a723e1870cb6e5893f60
vbradnitski <vbradnitski@scnsoft.com>
2015-06-22 19:07:32 +0300

    XP-830 content with type 'Image Selector': Image not removed, when image opened for edit and 'delete' button on toolbar was pressed

--- modules/admin-ui/src/main/resources/web/admin/common/js/content/DeleteContentResult.ts
+++ modules/admin-ui/src/main/resources/web/admin/common/js/content/DeleteContentResult.ts
@@ -3,17 +3,17 @@ module api.content {
 
     export class DeleteContentResult {
 
-        private deleteSuccess: string[];
+        private deleteSuccess: DeleteContentResultSuccess[];
         private deletePending: string[];
         private deleteFailures: DeleteContentResultFailure[];
 
-        constructor(success: string[], pending: string[], failures: DeleteContentResultFailure[]) {
+        constructor(success: DeleteContentResultSuccess[], pending: string[], failures: DeleteContentResultFailure[]) {
             this.deleteSuccess = !!success ? success : [];
             this.deleteFailures = !!failures ? failures : [];
             this.deletePending = !!pending ? pending : [];
         }
 
-        getDeleted(): string[] {
+        getDeleted(): DeleteContentResultSuccess[] {
             return this.deleteSuccess;
         }
 
@@ -27,32 +27,47 @@ module api.content {
 
         static fromJson(json: DeleteContentResultJson): DeleteContentResult {
             if (json.successes) {
-                var success: string[] = json.successes.map((success) => success.name);
+                var success: DeleteContentResultSuccess[] = json.successes.
+                    map((success) => new DeleteContentResultSuccess(success.id, success.name));
             }
             if (json.pendings) {
                 var pending: string[] = json.pendings.map((pending) => pending.name);
             }
             if (json.failures) {
                 var failure: DeleteContentResultFailure[] = json.failures.
-                    map((failure) => new DeleteContentResultFailure(failure.name, failure.reason));
+                    map((failure) => new DeleteContentResultFailure(failure.id, failure.name, failure.reason));
             }
             return new DeleteContentResult(success, pending, failure);
         }
 
     }
 
-    export class DeleteContentResultFailure {
+    export class DeleteContentResultSuccess {
 
+        private id: string;
         private name: string;
-        private reason: string;
 
-        constructor(name: string, reason: string) {
+        constructor(id: string, name: string) {
+            this.id = id;
             this.name = name;
-            this.reason = reason;
         }
 
-        getPath(): string {
+        getId(): string {
+            return this.id;
+        }
+
+        getName(): string {
             return this.name;
+        }
+    }
+
+    export class DeleteContentResultFailure extends DeleteContentResultSuccess {
+
+        private reason: string;
+
+        constructor(id: string, name: string, reason: string) {
+            super(id, name);
+            this.reason = reason;
         }
 
         getReason(): string {
