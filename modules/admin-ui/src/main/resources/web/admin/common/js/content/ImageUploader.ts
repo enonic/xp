8b26a9bcb07cf745fcd35653384ab645a72affe5
Alex Shklianko <asl@enonic.com>
2015-06-29 14:41:32 +0300

    XP-864 Image Selector :"HTTP Status 500 - undefined: javax/xml/stream/XMLEventFactory" message appears, when *docx or *xlsx uploaded
    -Restricted allowed file extensions for image uploader to 'jpg,jpeg,gif,png'

--- modules/admin-ui/src/main/resources/web/admin/common/js/content/ImageUploader.ts
+++ modules/admin-ui/src/main/resources/web/admin/common/js/content/ImageUploader.ts
@@ -21,11 +21,18 @@ module api.content {
         private originalHeight: number;
         private originalWidth: number;
 
-        private scaleWidth: boolean = false; // parameter states if width of the image must be preferred over its height during resolving
+        private scaleWidth: boolean; // parameter states if width of the image must be preferred over its height during resolving
 
         constructor(config: ImageUploaderConfig) {
+            if (config.allowTypes == undefined) {
+                config.allowTypes = [
+                    {title: 'Image files', extensions: 'jpg,jpeg,gif,png'}
+                ];
+            }
+
             super(config);
 
+            this.scaleWidth = false;
             this.imageEditors = [];
             this.focalEditModeListeners = [];
             this.cropEditModeListeners = [];
