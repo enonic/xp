978f33b8019c6ea3547bde306fda46f5f705a5ba
sshklianko <sts@enonic.com>
2015-07-01 15:18:41 +0300

    XP-918 Implement ExportCommand in Java  - Implemented

--- modules/toolbox/src/main/java/com/enonic/xp/toolbox/repo/ExportCommand.java
+++ modules/toolbox/src/main/java/com/enonic/xp/toolbox/repo/ExportCommand.java
@@ -1,14 +1,41 @@
 package com.enonic.xp.toolbox.repo;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import io.airlift.airline.Command;
+import io.airlift.airline.Option;
+
+import com.enonic.xp.toolbox.util.JsonHelper;
 
 @Command(name = "export", description = "Export node from a branch in a repository.")
 public final class ExportCommand
     extends RepoCommand
 {
+    public static final String EXPORT_REST_PATH = "/admin/rest/export/export";
+
+    @Option(name = "-t", description = "Target directory to save export.", required = true)
+    public String targetDir;
+
+    @Option(name = "-s", description = "Path of data to export. Format: <repo-name>:<branch-name>:<node-path>.", required = true)
+    public String sourceRepoPath;
+
+    @Option(name = "-i", description = "Flag that includes ids in data when exporting.", required = false)
+    public boolean exportWithIds = false;
+
     @Override
     protected void execute()
         throws Exception
     {
+        final String result = postRequest( EXPORT_REST_PATH, createJsonRequest() );
+        System.out.println( result );
+    }
+
+    private ObjectNode createJsonRequest()
+    {
+        final ObjectNode json = JsonHelper.newObjectNode();
+        json.put( "sourceRepoPath", this.sourceRepoPath );
+        json.put( "targetDirectory", this.targetDir );
+        json.put( "exportWithIds", this.exportWithIds );
+        return json;
     }
 }
