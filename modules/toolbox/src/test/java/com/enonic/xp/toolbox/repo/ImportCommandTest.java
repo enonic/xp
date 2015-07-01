19e0d8e319e6f521ca0556c8cf2aa813b2b6f0bf
sshklianko <sts@enonic.com>
2015-07-01 18:42:29 +0300

    XP-937 Create tests for tool commands  - Implemented

---
+++ modules/toolbox/src/test/java/com/enonic/xp/toolbox/repo/ImportCommandTest.java
@@ -0,0 +1,49 @@
+package com.enonic.xp.toolbox.repo;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.base.Charsets;
+import com.squareup.okhttp.mockwebserver.RecordedRequest;
+
+import com.enonic.xp.toolbox.util.JsonHelper;
+
+import static org.junit.Assert.*;
+
+public class ImportCommandTest
+    extends RepoCommandTest
+{
+    @Test
+    public void testImport()
+        throws Exception
+    {
+        final ImportCommand command = new ImportCommand();
+        configure( command );
+        command.sourceDir = "source-directory";
+        command.targetRepoPath = "<draft>:<master>:<root/node1>";
+
+        addResponse( createResponseJson() );
+
+        command.run();
+
+        final RecordedRequest request = takeRequest();
+        assertEquals( "POST", request.getMethod() );
+        assertEquals( "/admin/rest/export/import", request.getPath() );
+        assertEquals( JsonHelper.serialize( createRequestJson() ), request.getBody().readString( Charsets.UTF_8 ) );
+    }
+
+    private ObjectNode createRequestJson()
+    {
+        final ObjectNode json = JsonHelper.newObjectNode();
+        json.put( "sourceDirectory", "source-directory" );
+        json.put( "targetRepoPath", "<draft>:<master>:<root/node1>" );
+        json.put( "importWithIds", false );
+        return json;
+    }
+
+    private ObjectNode createResponseJson()
+    {
+        // Should probably be populated with mock data.
+        return JsonHelper.newObjectNode();
+    }
+}
