987473ed393cdbe463ff7126501cf4e45ed0c111
Glenn Ricaud <gri@enonic.com>
2015-08-18 13:54:57 +0200

    XP-1279 Implement getSiteConfig in lib-portal

---
+++ modules/lib-portal/src/test/java/com/enonic/xp/lib/portal/current/GetCurrentSiteConfigScriptTest.java
@@ -0,0 +1,36 @@
+package com.enonic.xp.lib.portal.current;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.enonic.xp.lib.portal.TestDataFixtures;
+import com.enonic.xp.site.Site;
+import com.enonic.xp.testing.script.ScriptTestSupport;
+
+public class GetCurrentSiteConfigScriptTest
+    extends ScriptTestSupport
+{
+    @Before
+    public void setUp()
+    {
+        setupRequest();
+    }
+
+    @Test
+    public void currentSite()
+        throws Exception
+    {
+        final Site site = TestDataFixtures.newSite();
+        this.portalRequest.setSite( site );
+
+        runTestFunction( "/test/getCurrentSiteConfig-test.js", "currentSite" );
+    }
+
+    @Test
+    public void noCurrentSite()
+        throws Exception
+    {
+        this.portalRequest.setSite( null );
+        runTestFunction( "/test/getCurrentSiteConfig-test.js", "noCurrentSite" );
+    }
+}
