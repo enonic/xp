7ae4e6b8d82fbed55e2785b7b354a3283f275d07
Alex Shklianko <asl@enonic.com>
2015-08-20 14:32:16 +0300

    XP-1282 Rendering without Template - Adjustments to Live Edit inspect panel

--- modules/admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/wizard/page/contextwindow/inspect/page/PageTemplateOptionViewer.ts
+++ modules/admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/wizard/page/contextwindow/inspect/page/PageTemplateOptionViewer.ts
@@ -11,9 +11,20 @@ module app.wizard.page.contextwindow.inspect.page {
         }
 
         resolveSubName(object: PageTemplateOption, relativePath: boolean = false): string {
-            return !!object.getPageTemplate() ? object.getPageTemplate().getDisplayName() == "Customized" ? "Set up your own page" :
-                                                    object.getPageTemplate().getPath().toString() :
-                                                        "(" + object.getPageModel().getDefaultPageTemplate().getDisplayName().toString() + ")";
+            if (!!object.getPageTemplate()) {
+                if (object.getPageTemplate().getDisplayName() == "Customized") {
+                    return "Set up your own page";
+                }
+                else {
+                    return object.getPageTemplate().getPath().toString();
+                }
+            }
+            else if (!!object.getPageModel().getDefaultPageTemplate()) {
+                return "(" + object.getPageModel().getDefaultPageTemplate().getDisplayName().toString() + ")";
+            }
+            else {
+                return "( no default template found )";
+            }
         }
 
         resolveIconClass(object: PageTemplateOption): string {
