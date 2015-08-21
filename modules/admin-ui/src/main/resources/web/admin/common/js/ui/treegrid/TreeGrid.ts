4ec4cecc06a5b971e5336499ebd48c1f76619c77
sshklianko <sts@enonic.com>
2015-08-21 11:41:03 +0300

    XP-1092 Widget Panel - Add support for detail panel in mobile mode - Added separate panel for mobile preview with widgets panel

--- modules/admin-ui/src/main/resources/web/admin/common/js/ui/treegrid/TreeGrid.ts
+++ modules/admin-ui/src/main/resources/web/admin/common/js/ui/treegrid/TreeGrid.ts
@@ -140,6 +140,9 @@ module api.ui.treegrid {
                         this.active = true;
                         this.root.clearStashedSelection();
                         this.grid.selectRow(data.row);
+                        if (!elem.hasClass("sort-dialog-trigger")) {
+                            new api.content.TreeGridItemClickedEvent().fire();
+                        }
                     }
 
                     this.manageRowClick(elem, data);
