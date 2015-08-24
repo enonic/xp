d911af9c9b51d4fb60f05e04479cec7312539eca
sshklianko <sts@enonic.com>
2015-08-24 18:53:22 +0300

    XP-1092 Widget Panel - Add support for detail panel in mobile mode - Added separate panel for mobile preview with widgets panel

---
+++ modules/admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/view/MobileContentItemStatisticsPanel.ts
@@ -0,0 +1,121 @@
+module app.view {
+
+    import Panel = api.ui.panel.Panel;
+    import TabMenuItemBuilder = api.ui.tab.TabMenuItemBuilder;
+    import WidgetsPanelToggleButton = app.view.widget.WidgetsPanelToggleButton;
+    import WidgetsPanel = app.view.widget.WidgetsPanel;
+    import WidgetView = app.view.widget.WidgetView;
+    import ViewItem = api.app.view.ViewItem;
+    import ContentSummary = api.content.ContentSummary;
+    import StringHelper = api.util.StringHelper;
+    import ResponsiveManager = api.ui.responsive.ResponsiveManager;
+    import ResponsiveItem = api.ui.responsive.ResponsiveItem;
+    import MobileContentTreeGridActions = app.browse.action.MobileContentTreeGridActions;
+    import MobileContentBrowseToolbar = app.browse.MobileContentBrowseToolbar;
+
+    export class MobileContentItemStatisticsPanel extends api.app.view.ItemStatisticsPanel<api.content.ContentSummary> {
+
+        private itemHeader: api.dom.DivEl = new api.dom.DivEl("mobile-content-item-statistics-header");
+        private headerLabel: api.dom.SpanEl = new api.dom.SpanEl();
+
+        private previewPanel: ContentItemPreviewPanel;
+        private widgetsPanel: WidgetsPanel = new WidgetsPanel(false, false);
+        private widgetsToggleButton: app.view.widget.MobileWidgetsPanelToggleButton;
+
+        private mobileBrowseActions: MobileContentTreeGridActions;
+        private toolbar: MobileContentBrowseToolbar;
+
+        constructor(mobileBrowseActions: MobileContentTreeGridActions) {
+            super("mobile-content-item-statistics-panel");
+
+            this.mobileBrowseActions = mobileBrowseActions;
+
+            this.setDoOffset(false);
+
+            this.initHeader();
+
+            this.initPreviewPanel();
+
+            this.initWidgetsPanel();
+
+            this.initToolbar();
+
+            this.onRendered(() => {
+                this.slideAllOut();
+            });
+
+            ResponsiveManager.onAvailableSizeChanged(this, (item: ResponsiveItem) => {
+                this.slideAllOut();
+            });
+        }
+
+        private initToolbar() {
+            this.toolbar = new MobileContentBrowseToolbar(this.mobileBrowseActions);
+            this.appendChild(this.toolbar);
+        }
+
+        private initHeader() {
+            this.itemHeader.appendChild(this.headerLabel);
+            this.widgetsToggleButton = new app.view.widget.MobileWidgetsPanelToggleButton(this.widgetsPanel);
+            var backButton = new api.dom.DivEl("back-button");
+            backButton.onClicked((event) => {
+                this.slideAllOut();
+            });
+            this.itemHeader.appendChild(backButton);
+            this.itemHeader.appendChild(this.widgetsToggleButton);
+
+            this.appendChild(this.itemHeader);
+
+        }
+
+        private initWidgetsPanel() {
+            this.appendChild(this.widgetsPanel);
+        }
+
+        private initPreviewPanel() {
+            this.previewPanel = new ContentItemPreviewPanel();
+            this.previewPanel.setDoOffset(false);
+            this.appendChild(this.previewPanel);
+        }
+
+        setItem(item: ViewItem<ContentSummary>) {
+            if (!item.equals(this.getItem())) {
+                super.setItem(item);
+                this.previewPanel.setItem(item);
+                this.widgetsPanel.setItem(item);
+                this.setName(this.makeDisplayName(item));
+            }
+            this.slideIn();
+        }
+
+        private makeDisplayName(item: ViewItem<ContentSummary>): string {
+            return StringHelper.isEmpty(item.getDisplayName()) ? StringHelper.escapeHtml("<Unnamed " +
+                                                                                         this.convertName(item.getModel().getType().getLocalName() +
+                                                                                         "") + ">") : item.getDisplayName();
+        }
+
+        private convertName(name: string): string {
+            return StringHelper.capitalize(name.replace(/-/g, " ").trim());
+        }
+
+        setName(name: string) {
+            this.headerLabel.setHtml(name);
+        }
+
+        slideAllOut() {
+            this.slideOut();
+            this.widgetsPanel.slideOut();
+            this.widgetsToggleButton.removeClass("expanded");
+        }
+
+
+        slideOut() {
+            this.getEl().setLeftPx(-this.getEl().getWidthWithBorder());
+        }
+
+        slideIn() {
+            this.getEl().setLeftPx(0);
+        }
+    }
+
+}
