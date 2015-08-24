e810658aee84186cf352249420a6b2df7457ffa8
Alex Shklianko <asl@enonic.com>
2015-08-24 17:07:58 +0300

    XP-1328 'Confirm Navigation' dialog appears when a site without a template opened for edit
    - WindowDOM onbeforeunload event was handled improperly - each time event was overriden by calling "WindowDOM.get().asWiindow().onbeforeunload = (event) => {...}" instead of maintaining array of listeners of event

--- modules/admin-ui/src/main/resources/web/admin/live-edit/js/LiveEditPage.ts
+++ modules/admin-ui/src/main/resources/web/admin/live-edit/js/LiveEditPage.ts
@@ -77,15 +77,16 @@ module LiveEdit {
 
         private registerGlobalListeners(): void {
 
-            api.dom.WindowDOM.get().asWindow().onbeforeunload = (event) => {
+            api.dom.WindowDOM.get().onBeforeUnload((event) => {
                 if (!this.skipNextReloadConfirmation) {
                     var message = "This will close this wizard!";
                     (event || window.event)['returnValue'] = message;
                     return message;
                 }
-            };
+            });
 
-            api.dom.WindowDOM.get().asWindow().onunload = (event) => {
+            api.dom.WindowDOM.get().onUnload((event) => {
+
                 if (!this.skipNextReloadConfirmation) {
                     new api.liveedit.PageUnloadedEvent(this.pageView).fire();
                     // do remove to trigger model unbinding
@@ -93,7 +94,7 @@ module LiveEdit {
                     this.skipNextReloadConfirmation = false;
                 }
                 this.pageView.remove();
-            };
+            });
 
             api.liveedit.ComponentLoadedEvent.on((event: api.liveedit.ComponentLoadedEvent) => {
 
