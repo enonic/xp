291b5e9bc3af47787bf2eaaf63025356056caa0b
Sten Roger Sandvik <srs@enonic.com>
2015-08-26 13:46:55 +0200

    XP-1366 Support for logging arrays in scripts.

--- modules/script-impl/src/test/resources/myapplication/logging/format-test.js
+++ modules/script-impl/src/test/resources/myapplication/logging/format-test.js
@@ -2,15 +2,6 @@ var assert = Java.type('org.junit.Assert');
 
 var json1 = {};
 
-var json2 = {
-    a: 1,
-    b: [1, 2],
-    c: {
-        a: 1,
-        b: [1, 2]
-    }
-};
-
 var s1 = log.format('simple message');
 assert.assertEquals('(/logging/format-test.js) simple message', s1);
 
@@ -19,6 +10,15 @@ assert.assertEquals('(/logging/format-test.js) param 1 and 2', s2);
 
 var s3 = log.format('empty json %s', JSON.stringify(json1));
 assert.assertEquals('(/logging/format-test.js) empty json {}', s3);
+
+var json2 = {
+    a: 1,
+    b: [1, 2],
+    c: {
+        a: 1,
+        b: [1, 2]
+    }
+};
 
 var s4 = log.format('complex json %s', JSON.stringify(json2));
 s4 = s4.replace(/\n/g, '');
@@ -29,3 +29,18 @@ assert.assertEquals('(/logging/format-test.js) empty json {}', s5);
 
 var s6 = log.format('complex json %s', json2);
 assert.assertEquals('(/logging/format-test.js) complex json {"a":1,"b":[1,2],"c":{"a":1,"b":[1,2]}}', s6);
+
+var array1 = [];
+
+var s7 = log.format('empty array %s', array1);
+assert.assertEquals('(/logging/format-test.js) empty array []', s7);
+
+var array2 = [1, 2];
+
+var s8 = log.format('simple array %s', array2);
+assert.assertEquals('(/logging/format-test.js) simple array [1,2]', s8);
+
+var array3 = [1, json1];
+
+var s9 = log.format('complex array %s', array3);
+assert.assertEquals('(/logging/format-test.js) complex array [1,{}]', s9);
--- modules/script-impl/src/test/resources/myapplication/logging/log-test.js
+++ modules/script-impl/src/test/resources/myapplication/logging/log-test.js
@@ -1,15 +1,19 @@
 log.debug('debug message');
 log.debug('debug message %s', true);
 log.debug('debug message %s %s', true, {a: 3});
+log.debug('debug message %s', [1,2]);
 
 log.info('info message');
 log.info('info message %s', true);
 log.info('info message %s %s', true, {a: 3});
+log.info('info message %s', [1,2]);
 
 log.warning('warning message');
 log.warning('warning message %s', true);
 log.warning('warning message %s %s', true, {a: 3});
+log.warning('warning message %s', [1,2]);
 
 log.error('error message');
 log.error('error message %s', true);
 log.error('error message %s %s', true, {a: 3});
+log.error('error message %s', [1,2]);
