bef295d384e425a4f6c0612c570c75f00af69bed
Alex Shklianko <asl@enonic.com>
2015-08-27 17:02:33 +0300

    XP-1365 LiveEdit - Fix positioning of context menu

--- modules/admin-ui/src/main/resources/web/admin/common/js/liveedit/ItemViewContextMenu.ts
+++ modules/admin-ui/src/main/resources/web/admin/common/js/liveedit/ItemViewContextMenu.ts
@@ -4,9 +4,13 @@ module api.liveedit {
 
         private title: ItemViewContextMenuTitle;
         private menu: api.ui.menu.ContextMenu;
+        private arrow: ItemViewContextMenuArrow;
 
         constructor(menuTitle: ItemViewContextMenuTitle, actions: api.ui.Action[]) {
             super('item-view-context-menu bottom');
+
+            this.arrow = new ItemViewContextMenuArrow(this);
+            this.appendChild(this.arrow);
 
             this.title = menuTitle;
             if (this.title) {
@@ -69,7 +73,7 @@ module api.liveedit {
         }
 
         showAt(x: number, y: number) {
-            this.menu.showAt.call(this, x - this.getEl().getWidth() / 2, y);
+            this.menu.showAt.call(this, this.updateXToStayWithinFrame(x), this.updateYToStayWithinFrame(y));
         }
 
         moveBy(dx: number, dy: number) {
@@ -88,6 +92,90 @@ module api.liveedit {
         private stopDrag(dragListener: (e: MouseEvent) => void, upListener: (e: MouseEvent) => void) {
             api.dom.Body.get().unMouseMove(dragListener);
             api.dom.Body.get().unMouseUp(upListener);
+        }
+
+        private updateXToStayWithinFrame(x: number): number {
+            var pageView = wemjq(this.getHTMLElement()).closest(".page-view");
+            var minDistFromFrameBorder = (pageView.outerWidth(true) - pageView.innerWidth()) / 2;
+
+            if (this.oveflowsLeftFrameBorder(x)) {
+                this.arrow.updateArrowXPosition(this.arrow.overflowsLeftFrameBorder(x, minDistFromFrameBorder) ? ItemViewContextMenuArrow.width : x - minDistFromFrameBorder);
+                return minDistFromFrameBorder;
+            }
+            else if (this.oveflowsRightFrameBorder(x)) {
+                var arrowPos = this.getEl().getWidth() - (pageView.outerWidth(true) - x) + minDistFromFrameBorder;
+                this.arrow.updateArrowXPosition(this.arrow.overflowsRightFrameBorder(arrowPos, minDistFromFrameBorder) ? this.getEl().getWidth() - ItemViewContextMenuArrow.width : arrowPos );
+                return pageView.outerWidth(true) - this.getEl().getWidth() - minDistFromFrameBorder;
+            }
+            else {
+                this.arrow.resetXPosition();
+                return x - this.getEl().getWidth() / 2;
+            }
+        }
+
+        private updateYToStayWithinFrame(y:number): number {
+            if (this.oveflowsBottom(y)) {
+                this.arrow.putArrowToBottomOfMenu();
+                return y - this.getEl().getHeight() - ItemViewContextMenuArrow.height;
+            } else {
+                this.arrow.putArrowToTopOfMenu();
+                return y + ItemViewContextMenuArrow.height;
+            }
+
+        }
+
+        private oveflowsLeftFrameBorder(x:number): boolean {
+            return (x - this.getEl().getWidth() / 2) < 0;
+        }
+
+        private oveflowsRightFrameBorder(x:number): boolean {
+            return (x + this.getEl().getWidth()) > window.innerWidth;
+        }
+
+        private oveflowsBottom(y:number): boolean {
+            return (y + this.getEl().getHeight() + ItemViewContextMenuArrow.height + 1) > (wemjq(window).scrollTop() + window.innerHeight);
+        }
+
+    }
+
+    export class ItemViewContextMenuArrow extends api.dom.DivEl {
+
+        static height: number = 7; //height of pseudo element
+
+        static width: number = 7; //width of pseudo element
+
+        private contextMenu: ItemViewContextMenu;
+
+        constructor(contextMenu: ItemViewContextMenu) {
+            super("item-view-context-menu-arrow bottom");
+
+            this.contextMenu = contextMenu;
+        }
+
+        putArrowToBottomOfMenu() {
+            this.removeClass("bottom");
+            this.addClass("top");
+        }
+
+        putArrowToTopOfMenu() {
+            this.removeClass("top");
+            this.addClass("bottom");
+        }
+
+        updateArrowXPosition(x:number) {
+            this.getEl().setLeftPx(x);
+        }
+
+        resetXPosition() {
+            this.getEl().setLeft("");
+        }
+
+        overflowsLeftFrameBorder(x:number, minimalDistFromFrameBorder: number): boolean {
+            return (minimalDistFromFrameBorder + ItemViewContextMenuArrow.width) > x;
+        }
+
+        overflowsRightFrameBorder(arrowPos: number, minimalDistFromFrameBorder: number): boolean {
+            return (this.contextMenu.getEl().getWidth() - ItemViewContextMenuArrow.width) < arrowPos;
         }
     }
 
