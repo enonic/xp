7d6b2eba090d913249e275968a8b542abcb9991b
Sten Roger Sandvik <srs@enonic.com>
2015-09-01 22:24:53 +0200

    XP-1418 Implement skeleton lib-auth project.

---
+++ modules/lib-auth/src/main/resources/site/lib/xp/auth.js
@@ -0,0 +1,13 @@
+exports.login = function (user, password) {
+    // TODO: Implement
+};
+
+exports.logout = function () {
+    // TODO: Implement
+};
+
+exports.getUser = function () {
+    // TODO: Implement
+};
+
+
