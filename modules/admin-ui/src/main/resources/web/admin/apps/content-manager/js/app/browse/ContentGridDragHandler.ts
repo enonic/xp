479491b6b891643bb6e1e0b9a13deb286195490e
Alex Shklianko <asl@enonic.com>
2015-09-02 12:24:55 +0300

    XP-1429 Issue, that described in already fixed XP-834, reproduced again. One selenium test failed due it
    -updated rows moving handling to properly react on beforemoverows event

--- modules/admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/browse/ContentGridDragHandler.ts
+++ modules/admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/browse/ContentGridDragHandler.ts
@@ -72,6 +72,12 @@ module app.browse {
         private prev = 0;
 
         handleBeforeMoveRows(event: Event, data: DragEventData) {
+            // no point in moving before or after itself
+            if (data.rows[0] == data.insertBefore || data.rows[0] == data.insertBefore - 1) {
+                event.stopPropagation();
+                return false;
+            }
+
             if (!this.draggableItem) {
                 this.handleDragStart();
             }
@@ -102,7 +108,7 @@ module app.browse {
         }
 
 
-        handleMoveRows(event: Event, args: DragEventData) {
+        handleMoveRows(event: Event, args: DragEventData) {debugger;
             var dataView = this.contentGrid.getGrid().getDataView();
             var draggableRow = args.rows[0];
 
