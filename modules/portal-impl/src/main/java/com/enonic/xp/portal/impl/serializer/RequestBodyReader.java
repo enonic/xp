3564b0c676bbba2bfd2e2bbc0409ed3f7d59a38a
Sten Roger Sandvik <srs@enonic.com>
2015-09-14 14:59:13 +0200

    XP-1436 Allow posting of text based body to controller.

--- modules/portal-impl/src/main/java/com/enonic/xp/portal/impl/serializer/RequestBodyReader.java
+++ modules/portal-impl/src/main/java/com/enonic/xp/portal/impl/serializer/RequestBodyReader.java
@@ -2,21 +2,29 @@ package com.enonic.xp.portal.impl.serializer;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.io.CharStreams;
 import com.google.common.net.MediaType;
 
 public final class RequestBodyReader
 {
-    private final static ObjectMapper MAPPER = new ObjectMapper();
-
     private final static MediaType JSON = MediaType.JSON_UTF_8.withoutParameters();
 
     public static Object readBody( final HttpServletRequest req )
         throws Exception
     {
         final String type = req.getContentType();
-        if ( ( type == null ) || !isText( MediaType.parse( type ) ) )
+        if ( type == null )
+        {
+            return null;
+        }
+
+        return readBody( req, MediaType.parse( type ) );
+    }
+
+    private static Object readBody( final HttpServletRequest req, final MediaType type )
+        throws Exception
+    {
+        if ( !isText( type ) )
         {
             return null;
         }
