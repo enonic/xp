b20fb13040ec48966d1d4236451ba1d3e494f619
Mikita Taukachou <mta@enonic.com>
2015-09-15 17:49:58 +0300

    XP-1519 Admin UI - Improve loading of background image
    Moved background url to `launcher.less`.

--- modules/admin-ui/src/main/resources/web/admin/apps/app-launcher/js/app/home/HomeMainContainer.ts
+++ modules/admin-ui/src/main/resources/web/admin/apps/app-launcher/js/app/home/HomeMainContainer.ts
@@ -17,7 +17,8 @@ module app.home {
         constructor(builder: HomeMainContainerBuilder) {
             super('home-main-container');
 
-            this.appendChild(new api.ui.image.LazyImage("/admin/common/images/background-1920.jpg"));
+            var lazyImage = new api.ui.image.LazyImage();
+            this.appendChild(lazyImage);
 
             this.getEl().setAttribute("tabindex", "100"); //Need tabindex to be able to focus element
 
@@ -40,6 +41,11 @@ module app.home {
             this.centerPanel.addToLoginPanel(this.linksContainer);
 
             this.appendChild(this.centerPanel);
+
+            this.onAdded(() => {
+                // See `.home-main-container .lazy-image` content property
+                lazyImage.setSrc(lazyImage.getEl().getComputedProperty("content").slice(1, -1));
+            });
 
             LogOutEvent.on(() => {
                 new api.security.auth.LogoutRequest().sendAndParse().then(() => {
