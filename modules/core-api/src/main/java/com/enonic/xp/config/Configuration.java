6366b0a99d3dd1b3ac791e3d3b56221233e2d8d5
Sten Roger Sandvik <srs@enonic.com>
2015-09-16 12:16:31 +0200

    XP-1592 Make Configuration object an interface.

--- modules/core-api/src/main/java/com/enonic/xp/config/Configuration.java
+++ modules/core-api/src/main/java/com/enonic/xp/config/Configuration.java
@@ -2,102 +2,19 @@ package com.enonic.xp.config;
 
 import java.util.Map;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
-
-import com.enonic.xp.convert.Converters;
-
-public final class Configuration
+public interface Configuration
 {
-    private final ImmutableMap<String, String> map;
+    String get( String key );
 
-    private Configuration( final ImmutableMap<String, String> map )
-    {
-        this.map = map;
-    }
+    boolean exists( String key );
 
-    public String get( final String key )
-    {
-        return this.map.get( key );
-    }
+    String getOrDefault( String key, String defValue );
 
-    public String getOrDefault( final String key, final String defValue )
-    {
-        final String value = get( key );
-        return value != null ? value : defValue;
-    }
+    <T> T get( String key, Class<T> type );
 
-    public <T> T get( final String key, final Class<T> type )
-    {
-        final String value = get( key );
-        return value != null ? Converters.convertOrNull( value, type ) : null;
-    }
+    <T> T getOrDefault( String key, Class<T> type, T defValue );
 
-    public <T> T getOrDefault( final String key, final Class<T> type, final T defValue )
-    {
-        final T value = get( key, type );
-        return value != null ? value : defValue;
-    }
+    Configuration subConfig( String prefix );
 
-    public boolean exists( final String key )
-    {
-        return this.map.containsKey( key );
-    }
-
-    public Configuration subConfig( final String prefix )
-    {
-        final Builder builder = create();
-        for ( final Map.Entry<String, String> entry : this.map.entrySet() )
-        {
-            final String key = entry.getKey();
-            if ( key.startsWith( prefix ) )
-            {
-                builder.add( key.substring( prefix.length() ), entry.getValue() );
-            }
-        }
-
-        return builder.build();
-    }
-
-    public Map<String, String> asMap()
-    {
-        return this.map;
-    }
-
-    public static Builder create()
-    {
-        return new Builder();
-    }
-
-    public final static class Builder
-    {
-        private final Map<String, String> map;
-
-        public Builder()
-        {
-            this.map = Maps.newHashMap();
-        }
-
-        public Builder add( final String key, final String value )
-        {
-            this.map.put( key, value );
-            return this;
-        }
-
-        public Builder addAll( final Configuration config )
-        {
-            return addAll( config.map );
-        }
-
-        public Builder addAll( final Map<String, String> map )
-        {
-            this.map.putAll( map );
-            return this;
-        }
-
-        public Configuration build()
-        {
-            return new Configuration( ImmutableMap.copyOf( this.map ) );
-        }
-    }
+    Map<String, String> asMap();
 }
---
+++ modules/core-api/src/test/java/com/enonic/xp/config/ConfigBuilderTest.java
@@ -0,0 +1,148 @@
+package com.enonic.xp.config;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Properties;
+
+import org.junit.Test;
+
+import com.google.common.collect.Maps;
+
+import static org.junit.Assert.*;
+
+public class ConfigBuilderTest
+{
+    @Test
+    public void testEmpty()
+    {
+        final Configuration config = ConfigBuilder.create().build();
+
+        assertNotNull( config );
+        assertTrue( config.asMap().isEmpty() );
+    }
+
+    @Test
+    public void testAdd()
+    {
+        final Configuration config = ConfigBuilder.create().
+            add( "key1", "value1" ).
+            add( "  key2  ", "  value2  " ).
+            add( "key3", 11 ).
+            add( "key4", null ).
+            build();
+
+        assertNotNull( config );
+        assertEquals( "value1", config.get( "key1" ) );
+        assertEquals( "value2", config.get( "key2" ) );
+        assertEquals( "11", config.get( "key3" ) );
+        assertEquals( 3, config.asMap().size() );
+    }
+
+    @Test
+    public void testAddAll_map()
+    {
+        final Map<String, String> source = Maps.newHashMap();
+        source.put( "key1", "value1" );
+        source.put( "key2", "value2" );
+
+        final Configuration config = ConfigBuilder.create().
+            addAll( source ).
+            add( "key3", "value3" ).
+            build();
+
+        assertNotNull( config );
+        assertEquals( true, config.exists( "key1" ) );
+        assertEquals( true, config.exists( "key2" ) );
+        assertEquals( true, config.exists( "key3" ) );
+
+        final Map<String, String> map = config.asMap();
+        assertEquals( 3, map.size() );
+    }
+
+    @Test
+    public void testAddAll_properties()
+    {
+        final Properties properties = new Properties();
+        properties.put( "key1", "value1" );
+        properties.put( "key2", "value2" );
+
+        final Configuration config = ConfigBuilder.create().
+            addAll( properties ).
+            add( "key3", "value3" ).
+            build();
+
+        assertNotNull( config );
+        assertEquals( true, config.exists( "key1" ) );
+        assertEquals( true, config.exists( "key2" ) );
+        assertEquals( true, config.exists( "key3" ) );
+
+        final Map<String, String> map = config.asMap();
+        assertEquals( 3, map.size() );
+    }
+
+    @Test
+    public void testAddAll_dictionary()
+    {
+        final Dictionary<String, String> dictionary = new Hashtable<>();
+        dictionary.put( "key1", "value1" );
+        dictionary.put( "key2", "value2" );
+
+        final Configuration config = ConfigBuilder.create().
+            addAll( dictionary ).
+            add( "key3", "value3" ).
+            build();
+
+        assertNotNull( config );
+        assertEquals( true, config.exists( "key1" ) );
+        assertEquals( true, config.exists( "key2" ) );
+        assertEquals( true, config.exists( "key3" ) );
+
+        final Map<String, String> map = config.asMap();
+        assertEquals( 3, map.size() );
+    }
+
+    @Test
+    public void testAddConfig()
+    {
+        final Configuration config1 = ConfigBuilder.create().
+            add( "key1", "value1" ).
+            add( "key2", "value2" ).
+            build();
+
+        final Configuration config2 = ConfigBuilder.create().
+            addAll( config1 ).
+            add( "key3", "value3" ).
+            build();
+
+        assertNotNull( config2 );
+        assertEquals( true, config2.exists( "key1" ) );
+        assertEquals( true, config2.exists( "key2" ) );
+        assertEquals( true, config2.exists( "key3" ) );
+
+        final Map<String, String> map = config2.asMap();
+        assertEquals( 3, map.size() );
+    }
+
+    @Test
+    public void testLoad()
+    {
+        final Configuration config1 = ConfigBuilder.create().
+            load(getClass(), "ConfigLoaderTest.properties").
+            add( "key3", "value3" ).
+            build();
+
+        final Configuration config2 = ConfigBuilder.create().
+            addAll( config1 ).
+            add( "key3", "value3" ).
+            build();
+
+        assertNotNull( config2 );
+        assertEquals( true, config2.exists( "key1" ) );
+        assertEquals( true, config2.exists( "key2" ) );
+        assertEquals( true, config2.exists( "key3" ) );
+
+        final Map<String, String> map = config2.asMap();
+        assertEquals( 3, map.size() );
+    }
+}
