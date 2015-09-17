637f4685591108ac86f0a8d3346eb30a7e55ba9f
Pavel Milkevich <pmi@enonic.com>
2015-09-17 11:45:26 +0300

    XP-1597 Fix tests that fail on Windows

--- modules/core-api/src/test/java/com/enonic/xp/util/StringTemplateTest.java
+++ modules/core-api/src/test/java/com/enonic/xp/util/StringTemplateTest.java
@@ -1,9 +1,12 @@
 package com.enonic.xp.util;
 
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import org.junit.Test;
 
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
 import com.google.common.collect.Maps;
 
 import static org.junit.Assert.*;
@@ -57,12 +60,18 @@ public class StringTemplateTest
         final StringTemplate template = StringTemplate.load( getClass(), getClass().getSimpleName() + ".txt" );
         final String value = template.apply( model );
 
-        assertEquals( "This is a file test.\n" + "1, {{two}}, 3\n", value );
+        assertEquals( "This is a file test.\n" + "1, {{two}}, 3\n", normalizeString( value ) );
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void loadFromResource_notFound()
     {
         StringTemplate.load( getClass(), "unknown.txt" );
+    }
+
+    private String normalizeString( final String text )
+    {
+        final Iterable<String> lines = Splitter.on( Pattern.compile( "(\r\n|\n|\r)" ) ).trimResults().split( text );
+        return Joiner.on( "\n" ).join( lines );
     }
 }
