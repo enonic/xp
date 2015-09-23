d05914745ad8dd9a3f351271f380430df572f531
Glenn Ricaud <gri@enonic.com>
2015-09-23 10:49:08 +0200

    XP-1619 Dynamic configuration of elasticsearch

--- modules/core/core-elasticsearch/src/test/java/com/enonic/xp/elasticsearch/impl/NodeSettingsBuilderTest.java
+++ modules/core/core-elasticsearch/src/test/java/com/enonic/xp/elasticsearch/impl/NodeSettingsBuilderTest.java
@@ -41,8 +41,7 @@ public class NodeSettingsBuilderTest
 
         assertNotNull( settings );
         assertEquals( 15, settings.getAsMap().size() );
-
-        // TODO: Check if settings is OK
+        assertSettings( System.getProperty( "xp.home" ) + "/repo/index", settings );
     }
 
     @Test
@@ -55,9 +54,27 @@ public class NodeSettingsBuilderTest
 
         assertNotNull( settings );
         assertEquals( 15, settings.getAsMap().size() );
-
-        // TODO: Check if settings is OK
-
-        assertEquals( "/to/some/other/path", settings.get( "path" ) );
+        assertSettings( "/to/some/other/path", settings );
     }
+
+    private void assertSettings( String pathValue, final Settings settings )
+    {
+        assertEquals( "local-node", settings.get( "name" ) );
+        assertEquals( "false", settings.get( "client" ) );
+        assertEquals( "true", settings.get( "data" ) );
+        assertEquals( "true", settings.get( "local" ) );
+        assertEquals( "true", settings.get( "http.enabled" ) );
+        assertEquals( "mycluster", settings.get( "cluster.name" ) );
+        assertEquals( "127.0.0.1", settings.get( "network.host" ) );
+        assertEquals( "false", settings.get( "discovery.zen.ping.multicast.enabled" ) );
+        assertEquals( "false", settings.get( "cluster.routing.allocation.disk.threshold_enabled" ) );
+        assertEquals( pathValue, settings.get( "path" ) );
+        assertEquals( pathValue + "/data", settings.get( "path.data" ) );
+        assertEquals( pathValue + "/work", settings.get( "path.work" ) );
+        assertEquals( pathValue + "/conf", settings.get( "path.conf" ) );
+        assertEquals( pathValue + "/logs", settings.get( "path.logs" ) );
+        assertEquals( pathValue + "/plugins", settings.get( "path.plugins" ) );
+    }
+
+
 }
