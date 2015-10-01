930652561de2f9984c8848a7e5f2ac79702f4696
Runar Myklebust <rmy@enonic.com>
2015-10-01 17:45:53 +0200

    XP-1733 Improve path-cache
    We wont use loading-cache for now, this has to be rewritten for cluster anyway.

--- modules/core/core-repo/src/main/java/com/enonic/wem/repo/internal/cache/PathCacheImpl.java
+++ modules/core/core-repo/src/main/java/com/enonic/wem/repo/internal/cache/PathCacheImpl.java
@@ -1,30 +1,18 @@
 package com.enonic.wem.repo.internal.cache;
 
+import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
 
 public class PathCacheImpl
     implements PathCache
 {
-    private final LoadingCache<CachePath, String> pathCache;
+    private final Cache<CachePath, String> pathCache;
 
     public PathCacheImpl()
     {
-        pathCache = CacheBuilder.newBuilder().maximumSize( 10000 ).build( createCacheLoader() );
-    }
-
-    private final CacheLoader<CachePath, String> createCacheLoader()
-    {
-        return new CacheLoader<CachePath, String>()
-        {
-            @Override
-            public String load( final CachePath key )
-                throws Exception
-            {
-                return null;
-            }
-        };
+        pathCache = CacheBuilder.newBuilder().
+            maximumSize( 10000 ).
+            build();
     }
 
     @Override
