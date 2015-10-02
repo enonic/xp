259c01595eb944b6c3c1193015b197fddf0a85fc
Alan Semenov <ase@enonic.com>
2015-10-02 12:58:08 +0200

    * Removed tabbed panel

--- modules/admin/admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/view/ContentItemVersionsPanel.ts
+++ modules/admin/admin-ui/src/main/resources/web/admin/apps/content-manager/js/app/view/ContentItemVersionsPanel.ts
@@ -6,51 +6,27 @@ module app.view {
     export class ContentItemVersionsPanel extends api.ui.panel.Panel {
 
         private item: ViewItem<ContentSummary>;
-
-        private deckPanel: api.ui.panel.NavigatedDeckPanel;
-        private activeGrid: ContentVersionsTreeGrid;
         private allGrid: ContentVersionsTreeGrid;
 
         constructor() {
             super("content-item-versions-panel");
 
-            var navigator = new api.ui.tab.TabBar();
-            this.deckPanel = new api.ui.panel.NavigatedDeckPanel(navigator);
-            this.deckPanel.setDoOffset(false);
-            this.appendChild(navigator);
-            this.appendChild(this.deckPanel);
-
-            navigator.onNavigationItemSelected((event: api.ui.NavigatorEvent) => {
-                this.setItem(this.item);
-            });
-
-
             this.allGrid = new AllContentVersionsTreeGrid();
-
-            this.deckPanel.addNavigablePanel(new api.ui.tab.TabBarItemBuilder().setLabel('All Versions').setAddLabelTitleAttribute(false).build(),
-                this.allGrid, true);
-
-            this.activeGrid = new ActiveContentVersionsTreeGrid();
-
-            this.deckPanel.addNavigablePanel(new api.ui.tab.TabBarItemBuilder().setLabel('Active Versions').setAddLabelTitleAttribute(false).build(),
-                this.activeGrid);
-
+            this.appendChild(this.allGrid);
         }
 
         public setItem(item: ViewItem<ContentSummary>) {
             this.item = item;
             if (this.item) {
-                var panel = <ContentVersionsTreeGrid>this.deckPanel.getPanelShown();
-                if (panel.getContentId() != this.item.getModel().getContentId()) {
-                    (<ContentVersionsTreeGrid>this.deckPanel.getPanelShown()).setContentId(item.getModel().getContentId());
+                if (this.allGrid.getContentId() != this.item.getModel().getContentId()) {
+                    this.allGrid.setContentId(item.getModel().getContentId());
                 }
             }
         }
 
         public ReRenderActivePanel() {
             if (this.item) {
-                var panel = <ContentVersionsTreeGrid>this.deckPanel.getPanelShown();
-                panel.render();
+                this.allGrid.render();
             }
         }
 
