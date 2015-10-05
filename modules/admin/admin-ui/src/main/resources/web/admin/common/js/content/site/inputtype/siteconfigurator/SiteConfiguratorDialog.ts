5918e2904ee4a583197436d77551d5bdd9e1bc27
sshklianko <sts@enonic.com>
2015-10-05 14:53:17 +0300

    XP-1714 SiteConfig modal window - Fix dropdowns cut off by dialog window edge - Added listener on dialog's dropdown, that checks if expanded dropdown is below visible dialog area that scrolls to expanded list.

--- modules/admin/admin-ui/src/main/resources/web/admin/common/js/content/site/inputtype/siteconfigurator/SiteConfiguratorDialog.ts
+++ modules/admin/admin-ui/src/main/resources/web/admin/common/js/content/site/inputtype/siteconfigurator/SiteConfiguratorDialog.ts
@@ -57,6 +57,41 @@ module api.content.site.inputtype.siteconfigurator {
             return dialogHeader;
         }
 
+        private subscribeOnDropdownExpand() {
+
+            wemjq.each(wemjq(this.getHTMLElement()).find('.dropdown-handle'), (key, value) => {
+                console.log(this);
+                wemjq(value).click(() => {
+                    if (wemjq(value).hasClass("down")) {
+                        var optionsContainer = wemjq(value).nextAll(".options-container").get(0);
+                        this.scrollToExpandedDropdownIfNotVisible(optionsContainer);
+                    }
+                });
+            });
+        }
+
+        private scrollToExpandedDropdownIfNotVisible(dropdown: HTMLElement) {
+            if (dropdown) {
+                var optionsContainerElement: api.dom.Element = api.dom.Element.fromHtmlElement(dropdown);
+                setTimeout(() => {
+                    var expandedDropdownBottomPosition = this.getAbsoluteBottomPosition(optionsContainerElement),
+                        siteDialogBottomPosition = this.getAbsoluteBottomPosition(this),
+                        dialogBottomAreaHeight = this.getButtonRow().getEl().getHeightWithMargin() + this.getEl().getMarginBottom();
+                    if (expandedDropdownBottomPosition > (siteDialogBottomPosition - dialogBottomAreaHeight)) {
+                        this.getContentPanel().getEl().setScrollTop(this.getContentPanel().getEl().getScrollTop() +
+                                                                    optionsContainerElement.getEl().getHeightWithBorder());
+                    }
+                }, 500);
+            }
+        }
+
+        private getAbsoluteBottomPosition(element: api.dom.Element): number {
+            var el = element.getEl(),
+                bottomPosition: number = (el.getOffset().top +
+                                          el.getHeightWithBorder());
+            return bottomPosition;
+        }
+
         show() {
             api.dom.Body.get().appendChild(this);
             super.show();
@@ -65,6 +100,10 @@ module api.content.site.inputtype.siteconfigurator {
                 this.centerMyself();
                 wemjq(this.getHTMLElement()).find('input[type=text],textarea,select').first().focus();
             }, 100);
+
+            setTimeout(() => {
+                this.subscribeOnDropdownExpand();
+            }, 1500);
         }
 
         close() {
