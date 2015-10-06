9f31cd577d805232a14069237f401dd2a3ce681b
Alex Shklianko <asl@enonic.com>
2015-10-06 17:56:27 +0300

    XP-1757 Make require look for index.js in a folder

---
+++ modules/script/script-impl/src/test/java/com/enonic/xp/script/impl/function/RequireFunctionTest.java
@@ -0,0 +1,16 @@
+package com.enonic.xp.script.impl.function;
+
+
+import org.junit.Test;
+
+import com.enonic.xp.script.impl.AbstractScriptTest;
+
+public class RequireFunctionTest extends AbstractScriptTest
+{
+    @Test
+    public void testDefaultResourceExecuted()
+        throws Exception
+    {
+        runTestScript( "site/function/default.js" );//index.js supposed to be run
+    }
+}
