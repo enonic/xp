1a5b48b417caea1fad8f0d56fd4414c73e01903f
sshklianko <sts@enonic.com>
2015-10-21 12:17:06 +0300

    XP-1892 Add FilterDescriptors to site.xml - Added descriptors and adjusted tests

---
+++ modules/core/core-api/src/main/java/com/enonic/xp/site/filter/FilterDescriptors.java
@@ -0,0 +1,31 @@
+package com.enonic.xp.site.filter;
+
+import com.google.common.collect.ImmutableList;
+
+import com.enonic.xp.support.AbstractImmutableEntityList;
+
+public class FilterDescriptors
+    extends AbstractImmutableEntityList<FilterDescriptor>
+{
+
+    private FilterDescriptors( final ImmutableList<FilterDescriptor> list )
+    {
+        super( list );
+    }
+
+    public static FilterDescriptors empty()
+    {
+        final ImmutableList<FilterDescriptor> list = ImmutableList.of();
+        return new FilterDescriptors( list );
+    }
+
+    public static FilterDescriptors from( final FilterDescriptor... FilterDescriptors )
+    {
+        return new FilterDescriptors( ImmutableList.copyOf( FilterDescriptors ) );
+    }
+
+    public static FilterDescriptors from( final Iterable<FilterDescriptor> FilterDescriptors )
+    {
+        return new FilterDescriptors( ImmutableList.copyOf( FilterDescriptors ) );
+    }
+}
