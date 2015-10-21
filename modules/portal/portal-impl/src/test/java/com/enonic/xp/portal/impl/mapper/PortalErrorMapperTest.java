85233c673c61c54ce990304ae2d64e08fd333932
Mikita Taukachou <mta@enonic.com>
2015-10-21 16:09:13 +0300

    XP-1898 Implement PortalError and PortalErrorMapper
    Implemented `PortalError` and `PortalErrorMapper` classes and tests.

---
+++ modules/portal/portal-impl/src/test/java/com/enonic/xp/portal/impl/mapper/PortalErrorMapperTest.java
@@ -0,0 +1,95 @@
+package com.enonic.xp.portal.impl.mapper;
+
+import java.net.URL;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import com.enonic.xp.app.ApplicationKey;
+import com.enonic.xp.exception.NotFoundException;
+import com.enonic.xp.portal.PortalError;
+import com.enonic.xp.portal.PortalRequest;
+import com.enonic.xp.portal.impl.ContentFixtures;
+import com.enonic.xp.script.serializer.JsonMapGenerator;
+import com.enonic.xp.script.serializer.MapSerializable;
+import com.enonic.xp.web.HttpMethod;
+import com.enonic.xp.web.HttpStatus;
+
+public class PortalErrorMapperTest
+{
+    private PortalError error;
+
+    private final ObjectMapper mapper;
+
+    public PortalErrorMapperTest()
+    {
+        this.mapper = new ObjectMapper();
+        this.mapper.enable( SerializationFeature.INDENT_OUTPUT );
+        this.mapper.enable( SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS );
+        this.mapper.enable( SerializationFeature.WRITE_NULL_MAP_VALUES );
+    }
+
+    private void assertJson( final String name, final MapSerializable value )
+        throws Exception
+    {
+        final String resource = "/" + getClass().getName().replace( '.', '/' ) + "-" + name + ".json";
+        final URL url = getClass().getResource( resource );
+
+        Assert.assertNotNull( "File [" + resource + "] not found", url );
+        final JsonNode expectedJson = this.mapper.readTree( url );
+
+        final JsonMapGenerator generator = new JsonMapGenerator();
+        value.serialize( generator );
+        final JsonNode actualJson = (JsonNode) generator.getRoot();
+
+        final String expectedStr = this.mapper.writeValueAsString( expectedJson );
+        final String actualStr = this.mapper.writeValueAsString( actualJson );
+
+        Assert.assertEquals( expectedStr, actualStr );
+    }
+
+    @Before
+    public void setup()
+    {
+        final PortalRequest portalRequest = new PortalRequest();
+        portalRequest.setMethod( HttpMethod.GET );
+        portalRequest.setScheme( "http" );
+        portalRequest.setHost( "localhost" );
+        portalRequest.setPort( 80 );
+        portalRequest.setPath( "/portal/live/master/a/b" );
+        portalRequest.setUrl( "http://localhost/portal/live/master/a/b?param1=value1" );
+        portalRequest.getParams().put( "param1", "value1" );
+        portalRequest.getParams().put( "param2", "value2" );
+        portalRequest.getParams().put( "param3", "value3-A" );
+        portalRequest.getParams().put( "param3", "value3-B" );
+
+        portalRequest.getHeaders().put( "header1", "value1" );
+        portalRequest.getHeaders().put( "header2", "value2" );
+        portalRequest.getHeaders().put( "header3", "value3" );
+
+        portalRequest.setApplicationKey( ApplicationKey.from( "myapplication" ) );
+        portalRequest.setContent( ContentFixtures.newContent() );
+        portalRequest.setSite( ContentFixtures.newSite() );
+        portalRequest.setPageDescriptor( ContentFixtures.newPageDescriptor() );
+
+        this.error = new PortalError();
+        this.error.setStatus( HttpStatus.NOT_FOUND );
+        this.error.setMessage( "Message: Not found." );
+        this.error.setException( new NotFoundException( "Not found." )
+        {
+        } );
+        this.error.setRequest( portalRequest );
+    }
+
+    @Test
+    public void testSimple()
+        throws Exception
+    {
+        assertJson( "simple", new PortalErrorMapper( this.error ) );
+    }
+}
