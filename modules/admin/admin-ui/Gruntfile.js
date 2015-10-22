61bcf649b4c2b714885f127229a098c3bdde9233
Sten Roger Sandvik <srs@enonic.com>
2015-10-22 15:16:59 +0200

    Removed karma from admin-ui.

--- modules/admin/admin-ui/Gruntfile.js
+++ modules/admin/admin-ui/Gruntfile.js
@@ -6,7 +6,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks("grunt-newer-explicit");
-    grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-typedoc');
 
     // Load grunt task configuration from grunt/*
