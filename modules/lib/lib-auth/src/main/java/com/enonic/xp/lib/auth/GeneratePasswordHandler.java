df68384acc29061b185708376b67e8d3a119a84d
Sten Roger Sandvik <srs@enonic.com>
2015-10-29 08:26:06 +0100

    Fixed generatePassword lib method.

--- modules/lib/lib-auth/src/main/java/com/enonic/xp/lib/auth/GeneratePasswordHandler.java
+++ modules/lib/lib-auth/src/main/java/com/enonic/xp/lib/auth/GeneratePasswordHandler.java
@@ -6,14 +6,6 @@ import java.util.Random;
 
 public final class GeneratePasswordHandler
 {
-    public String generatePassword()
-    {
-        return PasswordGenerator.generatePassword();
-    }
-}
-
-final class PasswordGenerator
-{
     private enum CharType
     {
         SPECIAL,
@@ -30,7 +22,7 @@ final class PasswordGenerator
 
     private static final String DIGIT_CHARS = "0123456789";
 
-    public static String generatePassword()
+    public String generatePassword()
     {
         int length = getRandomNumberInRange( 14, 16 ),
             maxSpecials = getRandomNumberInRange( 1, 3 ),
@@ -113,6 +105,5 @@ final class PasswordGenerator
     {
         Random r = new Random();
         return r.ints( min, ( max + 1 ) ).limit( 1 ).findFirst().getAsInt();
-
     }
 }
