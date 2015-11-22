f6adfb7f12dd8f59397e58bb452b09a4a5110caa
Sten Roger Sandvik <srs@enonic.com>
2015-11-22 21:36:44 +0100

    XP-2215 Implement cache function in lib-cache.

--- modules/lib/lib-cache/src/main/resources/site/lib/xp/cache.js
+++ modules/lib/lib-cache/src/main/resources/site/lib/xp/cache.js
@@ -7,6 +7,14 @@
  * @module lib/xp/cache
  */
 
+/**
+ * Creates a new cache with options.
+ *
+ * @param {object} options Cache options as JSON.
+ * @param {number} options.size Max size of cache.
+ * @param {number} options.expire Expire time (in sec) for cache entries. If not set, it will never expire.
+ * @constructor
+ */
 function Cache(options) {
     var builder = __.newBean('com.enonic.xp.lib.cache.CacheBeanBuilder');
 
@@ -21,21 +29,41 @@ function Cache(options) {
     this.cache = builder.build();
 }
 
+/**
+ * Returns value for cache entry if exists, otherwise it's calculated and put into the cache.
+ *
+ * @param {string} key Cache key to use.
+ * @param {function} callback Callback to a function that can calculate the cache value.
+ * @returns {*} Cache value for key.
+ */
 Cache.prototype.get = function (key, callback) {
     var result = this.cache.get(key, callback);
     return __.toNativeObject(result);
 };
 
+/**
+ * Clears the cache.
+ */
 Cache.prototype.clear = function () {
     this.cache.clear();
 };
 
+/**
+ * Returns number of elements in cache.
+ *
+ * @returns {number} Returns number of elements in cache.
+ */
 Cache.prototype.size = function () {
     return this.cache.getSize();
 };
 
 /**
  * Creates a new cache.
+ *
+ * @param {object} options Cache options as JSON.
+ * @param {number} options.size Max size of cache.
+ * @param {number} options.expire Expire time (in sec) for cache entries. If not set, it will never expire.
+ * @returns {Cache} Returns a new cache instance.
  */
 exports.newCache = function (options) {
     return new Cache(options);
