822f1cb4554ff9561a70ef2cc43b10d10435b5f6
Sten Roger Sandvik <srs@enonic.com>
2015-11-23 08:57:32 +0100

    XP-2202 Document lib-i18n in javascript.

--- modules/lib/lib-i18n/src/main/resources/site/lib/xp/i18n.js
+++ modules/lib/lib-i18n/src/main/resources/site/lib/xp/i18n.js
@@ -2,12 +2,35 @@
  * Internationalization functions.
  *
  * @example
- * var authLib = require('/lib/xp/i18n');
+ * var i18nLib = require('/lib/xp/i18n');
  *
  * @module lib/xp/i18n
  */
 
-exports.localize = function (param) {
+/**
+ * This function localizes a phrase.
+ *
+ * @example
+ * var i18nLib = require('/lib/xp/i18n');
+ *
+ * var complex_message = i18nLib.localize({
+ *   key: 'complex_message'
+ * });
+ *
+ * var message_multi_placeholder = i18nLib.localize({
+ *   key: 'message_multi_placeholder',
+ *   locale: "no",
+ *   values: ["John", "London"]
+ * });
+ *
+ * @param {object} params JSON with the parameters.
+ * @param {string} params.key The property key.
+ * @param {string} [params.locale] A string-representation of a locale. If the locale is not set, the site language is used.
+ * @param {string[]} [params.values] Optional placeholder values.
+ *
+ * @returns {string} The localized string.
+ */
+exports.localize = function (params) {
     var bean = __.newBean('com.enonic.xp.lib.i18n.LocaleScriptBean');
-    return bean.localize(param.key, __.nullOrValue(param.locale), __.toScriptValue(param.values));
+    return bean.localize(params.key, __.nullOrValue(params.locale), __.toScriptValue(params.values));
 };
