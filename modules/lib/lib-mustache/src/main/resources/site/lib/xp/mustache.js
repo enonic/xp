6ddf13b7c06b36d077e0f83df104c443ccc77875
Sten Roger Sandvik <srs@enonic.com>
2015-11-23 09:01:51 +0100

    XP-2205 Document lib-mustache in javascript.

--- modules/lib/lib-mustache/src/main/resources/site/lib/xp/mustache.js
+++ modules/lib/lib-mustache/src/main/resources/site/lib/xp/mustache.js
@@ -2,13 +2,37 @@
  * Mustache template related functions.
  *
  * @example
- * var authLib = require('/lib/xp/mustache');
+ * var mustacheLib = require('/lib/xp/mustache');
  *
  * @module lib/xp/mustache
  */
 
 var service = __.newBean('com.enonic.xp.lib.mustache.MustacheService');
 
+/**
+ * This function renders a view using mustache.
+ *
+ * @example
+ * var view = resolve('view/fruit.html');
+ * var model = {
+ * fruits: [
+ *   {
+ *     name: 'Apple',
+ *     color: 'Red'
+ *   },
+ *   {
+ *     name: 'Pear',
+ *     color: 'Green'
+ *   }
+ * ]};
+ *
+ * var mustacheLib = require('/lib/xp/mustache');
+ * var result = mustacheLib.render(view, model);
+ *
+ * @param view Location of the view. Use resolve(..) to resolve a view.
+ * @param {object} model Model that is passed to the view.
+ * @returns {string} The rendered output.
+ */
 exports.render = function (view, model) {
     var processor = service.newProcessor();
     processor.view = view;
