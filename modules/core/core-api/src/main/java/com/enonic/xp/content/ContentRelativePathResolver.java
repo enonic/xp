02a84ee1275eb93d5d904ba491c05b58f50ca4b3
sshklianko <sts@enonic.com>
2015-11-30 14:09:45 +0300

    XP-2187 Handle complicated path expressions in allowPath entry - Adjusted ContentRelativePathResolver to expand ${site} wildcard if it present in the beginning - used getNearestSite() method - Adjusted converter to manage cases with missing parent site - made converter to skip paths that can not resolve ${site} (must be quite rare case), but maybe it needs to throw an exception in that case - Adjusted tests

--- modules/core/core-api/src/main/java/com/enonic/xp/content/ContentRelativePathResolver.java
+++ modules/core/core-api/src/main/java/com/enonic/xp/content/ContentRelativePathResolver.java
@@ -1,10 +1,37 @@
 package com.enonic.xp.content;
 
+import java.util.List;
+
+import com.enonic.xp.site.Site;
+
 public class ContentRelativePathResolver
 {
-    public static String create( final Content content, final String pathExpression )
+
+    public final static String SITE_WILDCARD = "${site}";
+
+    public static String resolve( final Content content, final String pathExpression )
     {
         return getAbsolute( resolveActualPath( content, pathExpression ) );
+    }
+
+    public static String resolveWithSite( final String pathExpression, final Site parentSite )
+    {
+        return getAbsolute( makeEndWithStar( makeStartWithSlash( resolveSitePath( pathExpression, parentSite ) ) ) );
+    }
+
+    public static boolean anyPathNeedsSiteResolving( final List<String> allowedPaths )
+    {
+        return allowedPaths.stream().anyMatch( ( s ) -> s.startsWith( SITE_WILDCARD ) );
+    }
+
+    public static boolean hasSiteToResolve( final String path )
+    {
+        return path.startsWith( ContentRelativePathResolver.SITE_WILDCARD );
+    }
+
+    private static String resolveSitePath( final String pathExpression, final Site site )
+    {
+        return site != null ? pathExpression.replace( SITE_WILDCARD, site.getPath().toString() ) : pathExpression;
     }
 
     private static String getAbsolute( final String resolvedPath )
