3102cc6da66aa0a677dfa9fa84bc222c8faedb29
Sten Roger Sandvik <srs@enonic.com>
2015-12-02 12:33:27 +0100

    XP-2299 Restructure RelationshipType caching.

--- modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/relationship/BuiltinRelationshipTypes.java
+++ modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/relationship/BuiltinRelationshipTypes.java
@@ -8,7 +8,6 @@ import com.enonic.xp.app.ApplicationKey;
 import com.enonic.xp.core.impl.schema.SchemaHelper;
 import com.enonic.xp.icon.Icon;
 import com.enonic.xp.schema.content.ContentTypeNames;
-import com.enonic.xp.schema.mixin.Mixins;
 import com.enonic.xp.schema.relationship.RelationshipType;
 import com.enonic.xp.schema.relationship.RelationshipTypeName;
 import com.enonic.xp.schema.relationship.RelationshipTypes;
