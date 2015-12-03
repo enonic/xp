016ca2e45c8ec44406f603308edbc9a9425697a2
Glenn Ricaud <gri@enonic.com>
2015-12-03 12:19:25 +0100

    XP-2331 Admin Application portal handler

---
+++ modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/portal/AdminApplicationHandler.java
@@ -0,0 +1,56 @@
+package com.enonic.xp.admin.impl.portal;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import com.enonic.xp.app.ApplicationKey;
+import com.enonic.xp.portal.PortalRequest;
+import com.enonic.xp.portal.controller.ControllerScriptFactory;
+import com.enonic.xp.portal.handler.EndpointHandler;
+import com.enonic.xp.portal.handler.PortalHandler;
+import com.enonic.xp.portal.handler.PortalHandlerWorker;
+import com.enonic.xp.resource.ResourceKey;
+
+@Component(immediate = true, service = PortalHandler.class)
+public final class AdminApplicationHandler
+    extends EndpointHandler
+{
+    private final static Pattern PATTERN = Pattern.compile( "([^/]+)/([^/]+)" );
+
+    private ControllerScriptFactory controllerScriptFactory;
+
+    public AdminApplicationHandler()
+    {
+        super( "adminapps" );
+    }
+
+    @Override
+    protected PortalHandlerWorker newWorker( final PortalRequest req )
+        throws Exception
+    {
+        final String restPath = findRestPath( req );
+        final Matcher matcher = PATTERN.matcher( restPath );
+
+        if ( !matcher.find() )
+        {
+            throw notFound( "Not a valid service url pattern" );
+        }
+
+        final ApplicationKey appKey = ApplicationKey.from( matcher.group( 1 ) );
+        final ResourceKey scriptDir = ResourceKey.from( appKey, "admin/apps/" + matcher.group( 2 ) );
+
+        final AdminApplicationHandlerWorker worker = new AdminApplicationHandlerWorker();
+        worker.scriptDir = scriptDir;
+        worker.controllerScriptFactory = this.controllerScriptFactory;
+        return worker;
+    }
+
+    @Reference
+    public void setControllerScriptFactory( final ControllerScriptFactory controllerScriptFactory )
+    {
+        this.controllerScriptFactory = controllerScriptFactory;
+    }
+}
---
+++ modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/portal/AdminApplicationHandlerWorker.java
@@ -0,0 +1,34 @@
+package com.enonic.xp.admin.impl.portal;
+
+import com.enonic.xp.portal.PortalException;
+import com.enonic.xp.portal.PortalResponse;
+import com.enonic.xp.portal.RenderMode;
+import com.enonic.xp.portal.controller.ControllerScript;
+import com.enonic.xp.portal.controller.ControllerScriptFactory;
+import com.enonic.xp.portal.handler.PortalHandlerWorker;
+import com.enonic.xp.resource.ResourceKey;
+import com.enonic.xp.web.HttpStatus;
+
+final class AdminApplicationHandlerWorker
+    extends PortalHandlerWorker
+{
+    protected ResourceKey scriptDir;
+
+    protected ControllerScriptFactory controllerScriptFactory;
+
+    @Override
+    public void execute()
+        throws Exception
+    {
+
+        if ( this.request.getMode() != RenderMode.ADMIN )
+        {
+            throw new PortalException( HttpStatus.FORBIDDEN, "Render mode must be ADMIN." );
+        }
+
+        this.request.setApplicationKey( this.scriptDir.getApplicationKey() );
+
+        final ControllerScript controllerScript = this.controllerScriptFactory.fromDir( this.scriptDir );
+        this.response = PortalResponse.create( controllerScript.execute( this.request ) );
+    }
+}
