4c336763bc790a171d2fa1ccaf52a2356ee777cf
sshklianko <sts@enonic.com>
2015-12-18 15:07:35 +0300

    XP-2404 BUG? Broken images after upload crash admin - Added image load error listening functionality - Added removal of image editor upon failing to upload an image - Added image error event that notifies editor to enable delete button only disable publish button - Made uploader to check if it tries to re-create existing item - currently method setValue() of uploader is called a  few times with the same value - this helps to avoid loading same image several times

--- modules/admin/admin-ui/src/main/resources/web/admin/common/js/content/_module.ts
+++ modules/admin/admin-ui/src/main/resources/web/admin/common/js/content/_module.ts
@@ -89,6 +89,7 @@
 
 ///<reference path='TreeGridItemClickedEvent.ts' />
 ///<reference path='ContentRequiresSaveEvent.ts' />
+///<reference path='ImageErrorEvent.ts' />
 
 ///<reference path='Widget.ts' />
 ///<reference path='WidgetDescriptorResourceRequest.ts' />
