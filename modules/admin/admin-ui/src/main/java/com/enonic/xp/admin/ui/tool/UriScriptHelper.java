9822e1fa780c00f4c4b31a20ab57895207ab4f83
Glenn Ricaud <gri@enonic.com>
2015-12-23 11:53:43 +0100

    XP-2411 Implement temporary admin application "App-Launcher" to display the other admin applications

--- modules/admin/admin-ui/src/main/java/com/enonic/xp/admin/ui/tool/UriScriptHelper.java
+++ modules/admin/admin-ui/src/main/java/com/enonic/xp/admin/ui/tool/UriScriptHelper.java
@@ -10,6 +10,11 @@ public class UriScriptHelper
 
     public static final String ADMIN_TOOLS_URI_PREFIX = "/admin/tool";
 
+    public static final String generateAdminToolUri()
+    {
+        return ServletRequestUrlHelper.createUri( ADMIN_TOOLS_URI_PREFIX );
+    }
+
     public static final String generateAdminToolUri( String application, String adminTool )
     {
         String uri = ADMIN_TOOLS_URI_PREFIX + "/" + application + "/" + adminTool;
