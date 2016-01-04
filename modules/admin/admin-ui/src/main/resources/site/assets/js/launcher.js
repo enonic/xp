c43b7468632c8fc81d1e9317258d6b2e0512aca0
Alan Semenov <ase@enonic.com>
2016-01-04 19:06:28 +0100

    XP-2499 Admin tool bar * Implemented launcher panel that will expand by clicking the launcher button and collapse by clicking outside of the panel * Fixed layout of the launcher page * Fixed login page to not show the launcher button

--- modules/admin/admin-ui/src/main/resources/site/assets/js/launcher.js
+++ modules/admin/admin-ui/src/main/resources/site/assets/js/launcher.js
@@ -1,27 +1,73 @@
 (function () {
-    function injectCss() {
-        var cssRef = document.createElement("link");
-        cssRef.setAttribute("rel", "stylesheet");
-        cssRef.setAttribute("type", "text/css");
-        cssRef.setAttribute("href", CONFIG.portalAssetsUrl + "/css/launcher.css");
+    var adminUrl = "http://localhost:8080/admin/tool";
+    var launcherPanel;
 
-        document.getElementsByTagName("head")[0].appendChild(cssRef);
-    }
-
-    function appendToolbar() {
+    function appendLauncherToolbar() {
         var div = document.createElement("div");
         div.setAttribute("class", "launcher-bar");
 
         var button = document.createElement("button");
         button.setAttribute("class", "launcher-button");
 
+        button.addEventListener("click", onLauncherClick);
+
         document.getElementsByTagName("body")[0].appendChild(div);
         div.appendChild(button);
     }
 
+    function appendLauncherPanel() {
+        var div = document.createElement("div");
+        div.setAttribute("class", "launcher-panel");
+        div.appendChild(createLauncherLink(div));
+
+        document.getElementsByTagName("body")[0].appendChild(div);
+
+        launcherPanel = div;
+    }
+
+    function createLauncherLink(container) {
+        var link = document.createElement("link");
+
+        link.setAttribute("rel", "import");
+        link.setAttribute("href", adminUrl);
+
+        link.onload = function() {
+            var clonedDiv = link.import.querySelector('.home-main-container').cloneNode(true);
+            while (clonedDiv.childNodes.length > 0) {
+                container.appendChild(clonedDiv.childNodes[0]);
+            }
+        };
+
+        return link;
+    }
+
+    function setLauncherPanelDisplay(display) {
+        launcherPanel.style.display = display;
+    }
+
+
+    function isLauncherPanelVisible() {
+        return launcherPanel.style.display == "block";
+    }
+
+    function onLauncherClick() {
+        setLauncherPanelDisplay('block');
+    }
+
+    function onBodyClicked(e) {
+        if (launcherPanel && isLauncherPanelVisible() && e.target != getLauncherButton() && !launcherPanel.contains(e.target)) {
+            setLauncherPanelDisplay('none');
+        }
+    }
+
+    function getLauncherButton() {
+        return document.querySelector('.launcher-button');
+    }
+
     function init() {
-        injectCss();
-        appendToolbar();
+        document.getElementsByTagName("body")[0].addEventListener("click", onBodyClicked);
+        appendLauncherToolbar();
+        appendLauncherPanel();
     }
 
     init();
--- modules/admin/admin-ui/src/main/resources/web/admin/common/js/app/bar/AppBar.ts
+++ modules/admin/admin-ui/src/main/resources/web/admin/common/js/app/bar/AppBar.ts
@@ -21,14 +21,9 @@ module api.app.bar {
             this.tabMenu = new AppBarTabMenu();
 
             this.showAppLauncherAction = new ShowAppLauncherAction(this.application);
-/*
-            this.launcherButton = new LauncherButton(this.showAppLauncherAction);
-            this.appendChild(this.launcherButton);
-*/
+
             this.homeButton = new HomeButton(this.application, AppBarActions.SHOW_BROWSE_PANEL);
             this.appendChild(this.homeButton);
-
-            this.appendChild(this.tabMenu);
 
             this.tabMenu.onNavigationItemAdded((event: api.ui.NavigatorEvent)=> {
                 this.updateAppOpenTabs();
