a11377b106a638fa3d56352af4d286f56b297e62
Glenn Ricaud <gri@enonic.com>
2016-01-08 12:41:18 +0100

    XP-2554 Unauthorized/Forbidden error catching mechanism

--- modules/portal/portal-auth/src/main/java/com/enonic/xp/portal/auth/impl/AuthErrorFilter.java
+++ modules/portal/portal-auth/src/main/java/com/enonic/xp/portal/auth/impl/AuthErrorFilter.java
@@ -1,4 +1,4 @@
-package com.enonic.xp.web.impl.auth;
+package com.enonic.xp.portal.auth.impl;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -8,27 +8,38 @@ import javax.servlet.http.HttpServletResponse;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+import com.enonic.xp.auth.AuthDescriptorService;
 import com.enonic.xp.security.SecurityService;
 import com.enonic.xp.web.filter.OncePerRequestFilter;
+import com.enonic.xp.web.impl.auth.AuthErrorResponseWrapper;
 
 @Component(immediate = true, service = Filter.class,
-    property = {"osgi.http.whiteboard.filter.pattern=/", "service.ranking:Integer=40", "osgi.http.whiteboard.filter.dispatcher=FORWARD",
+    property = {"osgi.http.whiteboard.filter.pattern=/", "service.ranking:Integer=30", "osgi.http.whiteboard.filter.dispatcher=FORWARD",
         "osgi.http.whiteboard.filter.dispatcher=REQUEST"})
 public final class AuthErrorFilter
     extends OncePerRequestFilter
 {
     private SecurityService securityService;
 
+    private AuthDescriptorService authDescriptorService;
+
     @Override
     protected void doHandle( final HttpServletRequest req, final HttpServletResponse res, final FilterChain chain )
         throws Exception
     {
-        chain.doFilter( req, res );
+        final AuthErrorResponseWrapper responseWrapper = new AuthErrorResponseWrapper( req, res, securityService, authDescriptorService );
+        chain.doFilter( req, responseWrapper );
     }
 
     @Reference
     public void setSecurityService( final SecurityService securityService )
     {
         this.securityService = securityService;
+    }
+
+    @Reference
+    public void setAuthDescriptorService( final AuthDescriptorService authDescriptorService )
+    {
+        this.authDescriptorService = authDescriptorService;
     }
 }
