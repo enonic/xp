a5150153ee2a4323f1cf8e2f64a8eae0bccda8a8
Alan Semenov <ase@enonic.com>
2016-01-28 22:34:44 +0100

    XP-2695 Add application icons for mobile * Converted new app icons to favicon/iOS/Android icons * Removed obsolete icons * Misc restructuring

--- modules/admin/admin-ui/src/main/resources/admin/tools/launcher/launcher.js
+++ modules/admin/admin-ui/src/main/resources/admin/tools/launcher/launcher.js
@@ -19,13 +19,16 @@ function handleGet(req) {
         }
         return 1;
     });
+    log.info(portal.componentUrl({path: ""}));
+    log.info(portal.componentUrl({path: ""}));
     for (var i = 0; i < adminTools.length; i++) {
+        /*
         if (adminTools[i].icon.indexOf("//") < 0) {
             adminTools[i].icon = portal.assetUrl({
                 path: adminTools[i].icon,
                 application: adminTools[i].key.application
             });
-        }
+        }*/
         adminTools[i].appId = adminTools[i].key.name;
         adminTools[i].uri =
             uriScriptHelper.generateAdminToolUri(adminTools[i].key.application, adminTools[i].key.name);
