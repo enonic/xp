1ffe91cea33927a80fcb90c5a99f1363754ec260
Sten Roger Sandvik <srs@enonic.com>
2016-01-28 09:44:36 +0100

    Fixed the documentation links on home page.

--- modules/admin/admin-ui/src/main/resources/admin/tools/home/home.js
+++ modules/admin/admin-ui/src/main/resources/admin/tools/home/home.js
@@ -7,11 +7,22 @@ function handleGet(req) {
     var assetsUri = uriScriptHelper.ADMIN_ASSETS_URI_PREFIX;
     var view = resolve('home.html');
 
+    var isLatestSnapshot = app.version.endsWith('.0.SNAPSHOT');
+    var docLinkPrefix = 'http://xp.readthedocs.org/en/';
+
+    if (isLatestSnapshot) {
+        docLinkPrefix += 'latest';
+    } else {
+        var versionParts = app.version.split('.');
+        docLinkPrefix += versionParts[0] + '.' + versionParts[1];
+    }
+
     var params = {
         assetsUri: assetsUri,
         baseUri: '',
         portalAssetsUrl: portal.assetUrl({path: ""}),
-        xpVersion: app.version,
+        xpVersion: app.version.replace('.SNAPSHOT', ''),
+        docLinkPrefix: docLinkPrefix,
         appId: 'home',
         installation: uriScriptHelper.getInstallation()
     };
@@ -20,4 +31,5 @@ function handleGet(req) {
         body: mustache.render(view, params)
     };
 }
+
 exports.get = handleGet;
