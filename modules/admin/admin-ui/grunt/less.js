a7dcd24d17157565c66cb2404f3b21ce52427486
sshklianko <sts@enonic.com>
2016-01-29 12:21:31 +0100

    XP-2697 Clean up Home app - Moved home screen styles to a separate less file - Moved login screen styles to a separate less file - Moved launcher screen styles to a separate less file - Removed redundant css and js references from home.html - Added generation of _home.css for home.html which contains only home.less and launcher.less styles - Made common css also include home and launcher styles - Fixed home.html background picture fading-in

--- modules/admin/admin-ui/grunt/less.js
+++ modules/admin/admin-ui/grunt/less.js
@@ -2,6 +2,7 @@ var baseDir = 'src/main/resources/web/admin';
 
 add('common', 'common');
 add('live_edit', 'live-edit');
+addHome();
 
 function add(name, path) {
 
@@ -12,5 +13,15 @@ function add(name, path) {
             sourceMap: true
         }
     };
+}
 
+function addHome() {
+
+    module.exports['home'] = {
+        src: [baseDir + '/common/styles/apps/home/home.less', baseDir + '/common/styles/apps/launcher/launcher.less'],
+        dest: baseDir + '/common/styles/_home.css',
+        options: {
+            sourceMap: false
+        }
+    };
 }
