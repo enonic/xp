a531d87e21d4f9fd26fc6031378e89c0554c58ff
Glenn Ricaud <gri@enonic.com>
2016-02-01 15:14:22 +0100

    XP-2674 Auth application combobox - Filter the applications displayed &Puzzle icon use

--- modules/admin/admin-ui/src/main/resources/web/admin/common/js/application/ListAuthApplicationsRequest.ts
+++ modules/admin/admin-ui/src/main/resources/web/admin/common/js/application/ListAuthApplicationsRequest.ts
@@ -5,7 +5,6 @@ module api.application {
         sendAndParse(): wemQ.Promise<api.application.Application[]> {
 
             return this.send().then((response: api.rest.JsonResponse<api.application.ApplicationListResult>) => {
-                console.log("test");
                 var applications = response.getResult().applications.filter((application) => !!application.authConfig);
                 return api.application.Application.fromJsonArray(applications);
             });
