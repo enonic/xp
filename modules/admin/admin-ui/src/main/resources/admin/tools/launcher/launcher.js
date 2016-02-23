a972baef457dbf71409e88f3164b0769185d62d7
Alan Semenov <ase@enonic.com>
2016-02-23 14:16:28 +0100

    Fixes to the launcher panel * Separate logic of the panel container and the panel itself * Reference launcher.css via parameter path instead of dynamic injection * Fixed different fonts of the panel title when opened from the Home app or other apps * Fixed responsive styles not being applied after window resize

--- modules/admin/admin-ui/src/main/resources/admin/tools/launcher/launcher.js
+++ modules/admin/admin-ui/src/main/resources/admin/tools/launcher/launcher.js
@@ -9,7 +9,6 @@ function getAdminTools() {
 
 function handleGet(req) {
     var uriScriptHelper = Java.type("com.enonic.xp.admin.ui.tool.UriScriptHelper");
-    var assetsUri = uriScriptHelper.ADMIN_ASSETS_URI_PREFIX;
     var view = resolve('launcher.html');
 
     //Retrieves the Admin tools
@@ -28,12 +27,11 @@ function handleGet(req) {
 
     //Retrieves the profile informations
     var userIconUrl = portal.assetUrl({path: "icons/user.svg"});
+    var launcherCss = portal.assetUrl({path: "styles/_launcher.css"});
     var logoutServiceUrl = portal.serviceUrl({service: 'logout'});
     var user = auth.getUser();
 
     var params = {
-        assetsUri: assetsUri,
-        baseUri: '',
         xpVersion: app.version,
         appId: 'launcher',
         adminTools: adminTools,
@@ -41,7 +39,8 @@ function handleGet(req) {
         user: user,
         logoutServiceUrl: logoutServiceUrl,
         homeUrl: uriScriptHelper.generateAdminToolUri(),
-        installation: uriScriptHelper.getInstallation() || "Tools"
+        installation: uriScriptHelper.getInstallation() || "Tools",
+        launcherCss: launcherCss
     };
 
     return {
--- modules/admin/admin-ui/src/main/resources/assets/js/home.js
+++ modules/admin/admin-ui/src/main/resources/assets/js/home.js
@@ -15,18 +15,25 @@
         new responsiveRange(360, 540),
         new responsiveRange(540, 720),
         new responsiveRange(720, 960),
-        new responsiveRange(960, 240),
+        new responsiveRange(960, 1200),
         new responsiveRange(1200, 1380),
         new responsiveRange(1380, 1620),
         new responsiveRange(1620, 1920),
         new responsiveRange(1920, 10000)
     ];
 
-    responsiveRanges.forEach(function(responsiveRange) {
-        if (responsiveRange.isFit(document.body.clientWidth)) {
-            document.body.classList.add(responsiveRange.cls);
-            return false;
-        }
-    });
+    function applyResponsiveCls() {
+        responsiveRanges.forEach(function(responsiveRange) {
+            if (responsiveRange.isFit(document.body.clientWidth)) {
+                document.body.classList.add(responsiveRange.cls);
+            }
+            else if (document.body.classList.contains(responsiveRange.cls)) {
+                document.body.classList.remove(responsiveRange.cls)
+            }
+        });
+    }
+
+    applyResponsiveCls();
+    window.onresize = applyResponsiveCls;
 
 }());
