a035b50712b503f2a88ddd9060b70a78045b0062
Glenn Ricaud <gri@enonic.com>
2016-03-17 15:19:12 +0100

    XP-2987 Portal JS library - Method url

--- modules/lib/lib-portal/src/main/resources/site/lib/xp/examples/portal/url.js
+++ modules/lib/lib-portal/src/main/resources/site/lib/xp/examples/portal/url.js
@@ -2,8 +2,8 @@ var portalLib = require('/lib/xp/portal');
 var assert = require('/lib/xp/assert');
 
 // BEGIN
-var url = portalLib.rewriteUrl({
-    url: '/admin',
+var url = portalLib.url({
+    path: '/portal/master/mysite',
     params: {
         a: 1,
         b: 2
@@ -11,4 +11,4 @@ var url = portalLib.rewriteUrl({
 });
 // END
 
-assert.assertEquals('RewriteUrlParams{type=server, params={a=[1], b=[2]}, url=/admin}', url);
+assert.assertEquals('UrlParams{type=server, params={a=[1], b=[2]}, url=/admin}', url);
