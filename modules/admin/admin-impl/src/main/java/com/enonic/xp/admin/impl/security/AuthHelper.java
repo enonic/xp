e2f7fb0e3b7c5db10cbabad6cc6b0a520b1ebc95
Glenn Ricaud <gri@enonic.com>
2016-03-21 13:25:20 +0100

    XP-2933 PathGuard - Logout

--- modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/security/AuthHelper.java
+++ modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/security/AuthHelper.java
@@ -1,10 +1,22 @@
 package com.enonic.xp.admin.impl.security;
 
+import java.util.concurrent.Callable;
+
 import org.apache.commons.lang.StringUtils;
 
+import com.enonic.xp.auth.AuthDescriptor;
+import com.enonic.xp.auth.AuthDescriptorService;
+import com.enonic.xp.context.Context;
 import com.enonic.xp.context.ContextAccessor;
+import com.enonic.xp.context.ContextBuilder;
+import com.enonic.xp.portal.PortalRequest;
+import com.enonic.xp.portal.auth.AuthControllerScript;
+import com.enonic.xp.portal.auth.AuthControllerScriptFactory;
+import com.enonic.xp.security.AuthConfig;
+import com.enonic.xp.security.RoleKeys;
 import com.enonic.xp.security.SecurityService;
 import com.enonic.xp.security.User;
+import com.enonic.xp.security.UserStore;
 import com.enonic.xp.security.UserStoreKey;
 import com.enonic.xp.security.auth.AuthenticationInfo;
 import com.enonic.xp.security.auth.EmailPasswordAuthToken;
@@ -15,9 +27,16 @@ public final class AuthHelper
 {
     private final SecurityService securityService;
 
-    public AuthHelper( final SecurityService securityService )
+    private final AuthDescriptorService authDescriptorService;
+
+    private final AuthControllerScriptFactory authControllerScriptFactory;
+
+    public AuthHelper( final SecurityService securityService, final AuthDescriptorService authDescriptorService,
+                       final AuthControllerScriptFactory authControllerScriptFactory )
     {
         this.securityService = securityService;
+        this.authDescriptorService = authDescriptorService;
+        this.authControllerScriptFactory = authControllerScriptFactory;
     }
 
     public AuthenticationInfo login( final String user, final String password, final UserStoreKey userStoreKey, final boolean rememberMe )
@@ -36,7 +55,7 @@ public final class AuthHelper
         return info;
     }
 
-    public static void logout()
+    public void logout()
     {
         final Session session = ContextAccessor.current().getLocalScope().getSession();
         if ( session != null )
@@ -44,13 +63,16 @@ public final class AuthHelper
             final AuthenticationInfo authenticationInfo = session.getAttribute( AuthenticationInfo.class );
             if ( authenticationInfo != null )
             {
-                final User user = authenticationInfo.getUser();
-                if ( user != null )
-                {
-                    final UserStoreKey userStoreKey = user.getKey().
-                        getUserStore();
+                final UserStore userStore = retrieveUserStore( authenticationInfo );
+                final AuthDescriptor authDescriptor = retrieveAuthDescriptor( userStore );
 
-                }
+                final PortalRequest portalRequest = new PortalRequest();
+                portalRequest.setBaseUri( "/portal" );
+                portalRequest.setApplicationKey( authDescriptor.getKey() );
+                portalRequest.setUserStore( userStore );
+
+                final AuthControllerScript authControllerScript = authControllerScriptFactory.fromScript( authDescriptor.getResourceKey() );
+                authControllerScript.execute( "handleLogout", portalRequest );
             }
             session.invalidate();
         }
@@ -84,6 +106,47 @@ public final class AuthHelper
     private boolean isValidEmail( final String value )
     {
         return StringUtils.countMatches( value, "@" ) == 1;
+    }
+
+    private UserStore retrieveUserStore( final AuthenticationInfo authenticationInfo )
+    {
+        final User user = authenticationInfo.getUser();
+        if ( user != null )
+        {
+            final UserStoreKey userStoreKey = user.getKey().
+                getUserStore();
+            if ( userStoreKey != null )
+            {
+                return runWithAdminRole( () -> securityService.getUserStore( userStoreKey ) );
+            }
+        }
+        return null;
+    }
+
+    private AuthDescriptor retrieveAuthDescriptor( final UserStore userStore )
+    {
+        if ( userStore != null )
+        {
+            final AuthConfig authConfig = userStore.getAuthConfig();
+            if ( authConfig != null )
+            {
+                return authDescriptorService.getDescriptor( authConfig.getApplicationKey() );
+            }
+        }
+        return null;
+    }
+
+
+    private <T> T runWithAdminRole( final Callable<T> callable )
+    {
+        final Context context = ContextAccessor.current();
+        final AuthenticationInfo authenticationInfo = AuthenticationInfo.copyOf( context.getAuthInfo() ).
+            principals( RoleKeys.ADMIN ).
+            build();
+        return ContextBuilder.from( context ).
+            authInfo( authenticationInfo ).
+            build().
+            callWith( callable );
     }
 
 }
