7aaf1357fab29100629a3ed989e4f1d2b9fb47b7
sshklianko <sts@enonic.com>
2016-03-22 10:42:16 +0300

    XP-3002 Implement class MacroDescriptors

--- modules/core/core-api/src/test/java/com/enonic/xp/macro/MacroDescriptorTest.java
+++ modules/core/core-api/src/test/java/com/enonic/xp/macro/MacroDescriptorTest.java
@@ -12,4 +12,14 @@ public class MacroDescriptorTest
         final MacroDescriptor macroDescriptor = MacroDescriptor.create().key( MacroKey.from( "my-app:macro1" ) ).build();
         assertEquals( "my-app:/site/macros/macro1/macro1.xml", macroDescriptor.toResourceKey().toString() );
     }
+
+    @Test
+    public void testFrom()
+    {
+        final MacroDescriptor macroDescriptor1 = MacroDescriptor.from( "my-app:macro1" );
+        assertEquals( "my-app:/site/macros/macro1/macro1.xml", macroDescriptor1.toResourceKey().toString() );
+
+        final MacroDescriptor macroDescriptor2 = MacroDescriptor.from( MacroKey.from( "my-app:macro2" ) );
+        assertEquals( "my-app:/site/macros/macro2/macro2.xml", macroDescriptor2.toResourceKey().toString() );
+    }
 }
