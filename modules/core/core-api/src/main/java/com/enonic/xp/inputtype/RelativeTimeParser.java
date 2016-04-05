90ce49f3de8f7f8cc0dd4f5e2ccb63e640c97482
vbradnitski <vbradnitski@scnsoft.com>
2016-04-05 12:31:29 +0300

    XP-3097 Implement parsing of relative time expressions

---
+++ modules/core/core-api/src/main/java/com/enonic/xp/inputtype/RelativeTimeParser.java
@@ -0,0 +1,109 @@
+package com.enonic.xp.inputtype;
+
+import java.time.Duration;
+import java.time.Period;
+import java.time.temporal.TemporalAmount;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
+
+public final class RelativeTimeParser
+{
+    private static final String VALUE_GROUP = "(\\d*)";
+
+    private static final String OPERATOR_GROUP = "([\\+\\-])";
+
+    private static final String UNIT_ENDING = "(?![\\w])";
+
+    public List<TemporalAmount> parse( final String timeExpression )
+    {
+        return getTemporalAmounts( timeExpression, DateTimeUnits.DATE_TIME_UNITS );
+    }
+
+    public List<TemporalAmount> getTemporalAmounts( final String timeExpression, final Set<String> availableUnits )
+    {
+        Duration duration = Duration.ZERO;
+        Period period = Period.ZERO;
+        Boolean isEmpty = true;
+
+        if ( DateTimeUnits.CURRENT_UNITS.contains( timeExpression.trim() ) )
+        { //return zero period and duration
+            return Lists.newArrayList( duration, period );
+        }
+
+        StringBuilder builder = new StringBuilder( OPERATOR_GROUP ).
+            append( VALUE_GROUP ).
+            append( "\\s*" ).
+            append( getPatternGroup( availableUnits ) ).
+            append( UNIT_ENDING );
+
+        Matcher m = Pattern.compile( builder.toString(), Pattern.CASE_INSENSITIVE ).matcher( timeExpression );
+
+        while ( m.find() )
+        {
+            final String operatorString = m.group( 1 );
+            final String valueString = m.group( 2 );
+            final String unitTypeString = m.group( 3 );
+
+            if ( DateTimeUnits.PERIOD_UNITS.contains( unitTypeString ) )
+            {
+                period = period.plus( Period.parse( "P" + operatorString + valueString + unitTypeString.substring( 0, 1 ) ) );
+            }
+            else if ( DateTimeUnits.DURATION_UNITS.contains( unitTypeString ) )
+            {
+                duration = duration.plus( Duration.parse( "PT" + operatorString + valueString + unitTypeString.substring( 0, 1 ) ) );
+            }
+            isEmpty = false;
+        }
+        return isEmpty ? null : Lists.newArrayList( duration, period );
+    }
+
+    private String getPatternGroup( Set<String> set )
+    {
+        return new StringBuilder( "(" ).append( StringUtils.join( set, "|" ) ).append( ")" ).
+            toString();
+    }
+
+    public interface DateTimeUnits
+    {
+        public static final Set<String> YEAR_UNITS = ImmutableSet.of( "year", "years", "y" );
+
+        public static final Set<String> MONTH_UNITS = ImmutableSet.of( "month", "months", "M" );
+
+        public static final Set<String> WEEK_UNITS = ImmutableSet.of( "week", "weeks", "w" );
+
+        public static final Set<String> DAY_UNITS = ImmutableSet.of( "day", "days", "d" );
+
+        public static final Set<String> HOUR_UNITS = ImmutableSet.of( "hour", "hours", "h" );
+
+        public static final Set<String> MINUTE_UNITS = ImmutableSet.of( "minute", "minutes", "m" );
+
+        public static final Set<String> SECOND_UNITS = ImmutableSet.of( "second", "seconds", "s" );
+
+        public static final Set<String> CURRENT_UNITS = ImmutableSet.of( "now", "0" );
+
+        public static final Set<String> PERIOD_UNITS = ImmutableSet.<String>builder().
+            addAll( YEAR_UNITS ).
+            addAll( MONTH_UNITS ).
+            addAll( WEEK_UNITS ).
+            addAll( DAY_UNITS ).
+            build();
+
+        public static final Set<String> DURATION_UNITS = ImmutableSet.<String>builder().
+            addAll( HOUR_UNITS ).
+            addAll( MINUTE_UNITS ).
+            addAll( SECOND_UNITS ).
+            build();
+
+        public static final Set<String> DATE_TIME_UNITS = ImmutableSet.<String>builder().
+            addAll( PERIOD_UNITS ).
+            addAll( DURATION_UNITS ).
+            build();
+    }
+}
