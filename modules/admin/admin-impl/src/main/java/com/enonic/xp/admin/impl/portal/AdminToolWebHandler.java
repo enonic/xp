ed31b46df65ffc3b5cf77bbc1fe3e701cf368b24
Glenn Ricaud <gri@enonic.com>
2016-05-04 10:34:07 +0200

    XP-3315 Admin/Portal Refactoring - Admin Portal/Tool/Widget handlers - Admin tool

---
+++ modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/portal/AdminToolWebHandler.java
@@ -0,0 +1,82 @@
+package com.enonic.xp.admin.impl.portal;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import com.enonic.xp.admin.tool.AdminToolDescriptorService;
+import com.enonic.xp.app.ApplicationKey;
+import com.enonic.xp.page.DescriptorKey;
+import com.enonic.xp.portal.PortalWebRequest;
+import com.enonic.xp.portal.PortalWebResponse;
+import com.enonic.xp.portal.controller.ControllerScriptFactory;
+import com.enonic.xp.web.handler.BaseWebHandler;
+import com.enonic.xp.web.handler.WebHandler;
+import com.enonic.xp.web.handler.WebHandlerChain;
+import com.enonic.xp.web.handler.WebRequest;
+import com.enonic.xp.web.handler.WebResponse;
+
+@Component(immediate = true, service = WebHandler.class)
+public final class AdminToolWebHandler
+    extends BaseWebHandler
+{
+    private final static String ADMIN_TOOL_PREFIX = "/test/admin/tool/"; //TODO rewrite
+
+    private final static Pattern PATTERN = Pattern.compile( "([^/]+)/([^/]+)" );
+
+    private final static DescriptorKey DEFAULT_DESCRIPTOR_KEY = DescriptorKey.from( "com.enonic.xp.admin.ui:home" );
+
+    private AdminToolDescriptorService adminToolDescriptorService;
+
+    private ControllerScriptFactory controllerScriptFactory;
+
+    @Override
+    protected boolean canHandle( final WebRequest webRequest )
+    {
+        return webRequest.getPath().startsWith( ADMIN_TOOL_PREFIX );
+    }
+
+    @Override
+    protected WebResponse doHandle( final WebRequest webRequest, final WebResponse webResponse, final WebHandlerChain webHandlerChain )
+    {
+
+        final String subPath = webRequest.getPath().substring( ADMIN_TOOL_PREFIX.length() );
+        final Matcher matcher = PATTERN.matcher( subPath );
+
+        final DescriptorKey descriptorKey;
+        if ( matcher.find() )
+        {
+            final ApplicationKey applicationKey = ApplicationKey.from( matcher.group( 1 ) );
+            final String adminToolName = matcher.group( 2 );
+            descriptorKey = DescriptorKey.from( applicationKey, adminToolName );
+        }
+        else
+        {
+            descriptorKey = DEFAULT_DESCRIPTOR_KEY;
+        }
+
+        final PortalWebRequest portalWebRequest = PortalWebRequest.create( webRequest ).build();
+        return AdminToolWebHandlerWorker.create().
+            portalWebRequest( portalWebRequest ).
+            portalWebResponse( new PortalWebResponse() ).
+            controllerScriptFactory( controllerScriptFactory ).
+            adminToolDescriptorService( adminToolDescriptorService ).
+            descriptorKey( descriptorKey ).
+            build().
+            execute();
+    }
+
+    @Reference
+    public void setAdminToolDescriptorService( final AdminToolDescriptorService adminToolDescriptorService )
+    {
+        this.adminToolDescriptorService = adminToolDescriptorService;
+    }
+
+    @Reference
+    public void setControllerScriptFactory( final ControllerScriptFactory controllerScriptFactory )
+    {
+        this.controllerScriptFactory = controllerScriptFactory;
+    }
+}
---
+++ modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/portal/AdminToolWebHandlerWorker.java
@@ -0,0 +1,108 @@
+package com.enonic.xp.admin.impl.portal;
+
+import com.enonic.xp.admin.tool.AdminToolDescriptor;
+import com.enonic.xp.admin.tool.AdminToolDescriptorService;
+import com.enonic.xp.context.ContextAccessor;
+import com.enonic.xp.page.DescriptorKey;
+import com.enonic.xp.portal.PortalRequest;
+import com.enonic.xp.portal.PortalResponse;
+import com.enonic.xp.portal.PortalWebRequest;
+import com.enonic.xp.portal.PortalWebResponse;
+import com.enonic.xp.portal.controller.ControllerScript;
+import com.enonic.xp.portal.controller.ControllerScriptFactory;
+import com.enonic.xp.portal.handler.ControllerWebHandlerWorker;
+import com.enonic.xp.resource.ResourceKey;
+import com.enonic.xp.security.PrincipalKeys;
+
+final class AdminToolWebHandlerWorker
+    extends ControllerWebHandlerWorker
+{
+
+    private final ControllerScriptFactory controllerScriptFactory;
+
+    private final AdminToolDescriptorService adminToolDescriptorService;
+
+    private final DescriptorKey descriptorKey;
+
+    private AdminToolWebHandlerWorker( final Builder builder )
+    {
+        super( builder );
+        controllerScriptFactory = builder.controllerScriptFactory;
+        adminToolDescriptorService = builder.adminToolDescriptorService;
+        descriptorKey = builder.descriptorKey;
+    }
+
+    public static Builder create()
+    {
+        return new Builder();
+    }
+
+    @Override
+    public PortalWebResponse execute()
+    {
+        //Retrieves the AdminToolDescriptor
+        final AdminToolDescriptor adminToolDescriptor = adminToolDescriptorService.getByKey( descriptorKey );
+        if ( adminToolDescriptor == null )
+        {
+            throw notFound( "Admin application [%s] not found", descriptorKey.toString() );
+        }
+
+        //Checks if the access to AdminToolDescriptor is allowed
+        final PrincipalKeys principals = ContextAccessor.current().
+            getAuthInfo().
+            getPrincipals();
+        if ( !adminToolDescriptor.isAccessAllowed( principals ) )
+        {
+            throw forbidden( "You don't have permission to access [%s]", descriptorKey.toString() );
+        }
+
+        final ResourceKey scriptDir = ResourceKey.from( descriptorKey.getApplicationKey(), "admin/tools/" + descriptorKey.getName() );
+        final ControllerScript controllerScript = this.controllerScriptFactory.fromDir( scriptDir );
+        final PortalWebRequest portalWebRequest = PortalWebRequest.create( this.portalWebRequest ).
+            applicationKey( descriptorKey.getApplicationKey() ).
+            build();
+
+        final PortalRequest portalRequest = convertToPortalRequest( portalWebRequest );
+
+        //Render the Admin application
+        final PortalResponse portalResponse = PortalResponse.create( controllerScript.execute( portalRequest ) ).build();
+        return convertToPortalWebResponse( portalResponse );
+    }
+
+    public static final class Builder
+        extends ControllerWebHandlerWorker.Builder<Builder>
+    {
+        private ControllerScriptFactory controllerScriptFactory;
+
+        private AdminToolDescriptorService adminToolDescriptorService;
+
+        private DescriptorKey descriptorKey;
+
+        private Builder()
+        {
+        }
+
+        public Builder controllerScriptFactory( final ControllerScriptFactory controllerScriptFactory )
+        {
+            this.controllerScriptFactory = controllerScriptFactory;
+            return this;
+        }
+
+        public Builder adminToolDescriptorService( final AdminToolDescriptorService adminToolDescriptorService )
+        {
+            this.adminToolDescriptorService = adminToolDescriptorService;
+            return this;
+        }
+
+        public Builder descriptorKey( final DescriptorKey descriptorKey )
+        {
+            this.descriptorKey = descriptorKey;
+            return this;
+        }
+
+        public AdminToolWebHandlerWorker build()
+        {
+            return new AdminToolWebHandlerWorker( this );
+        }
+    }
+}
