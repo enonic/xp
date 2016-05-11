06f2643dd0ee67486b7679d01a8a10f0f4549713
vbradnitski <vbradnitski@scnsoft.com>
2016-05-11 13:53:17 +0300

    XP-2877 Default Value - DateTime
    final date, time, datetime commit

--- modules/core/core-api/src/main/java/com/enonic/xp/inputtype/DateTimeType.java
+++ modules/core/core-api/src/main/java/com/enonic/xp/inputtype/DateTimeType.java
@@ -1,10 +1,16 @@
 package com.enonic.xp.inputtype;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.ZoneId;
+
 import com.google.common.annotations.Beta;
 
 import com.enonic.xp.data.Property;
 import com.enonic.xp.data.Value;
 import com.enonic.xp.data.ValueFactory;
+import com.enonic.xp.data.ValueTypeException;
 import com.enonic.xp.data.ValueTypes;
 
 @Beta
@@ -34,6 +40,49 @@ final class DateTimeType
         {
             return ValueFactory.newLocalDateTime( value.asLocalDateTime() );
         }
+    }
+
+    @Override
+    public Value createDefaultValue( final InputTypeDefault defaultConfig )
+    {
+        final String defaultValue = defaultConfig.getRootValue();
+        if ( defaultValue != null )
+        {
+            try
+            {
+                return ValueFactory.newDateTime( ValueTypes.DATE_TIME.convert( defaultValue ) );
+            }
+            catch ( ValueTypeException e )
+            {
+                try
+                {
+                    return ValueFactory.newLocalDateTime( ValueTypes.LOCAL_DATE_TIME.convert( defaultValue ) );
+                }
+                catch ( ValueTypeException e1 )
+                {
+                    final RelativeTime result = RelativeTimeParser.parse( defaultValue );
+
+                    if ( result != null )
+                    {
+                        final Instant instant = Instant.now().plus( result.getTime() );
+                        final LocalDateTime localDateTime = instant.atZone( ZoneId.systemDefault() ).toLocalDateTime();
+
+                        final Period period = result.getDate();
+
+                        return ValueFactory.newLocalDateTime(
+                            localDateTime.plusYears( period.getYears() ).
+                                plusMonths( period.getMonths() ).
+                                plusDays( period.getDays() )
+                        );
+                    }
+                    else
+                    {
+                        throw new IllegalArgumentException( "Invalid Date format: " + defaultValue );
+                    }
+                }
+            }
+        }
+        return super.createDefaultValue( defaultConfig );
     }
 
     @Override
