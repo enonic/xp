db25a8b93984f18d435970be6e867e9cdc28bb7a
edloidas <edloidas@gmail.com>
2016-05-13 14:09:38 +0300

    XP-3376 Replace Publish action button with the new menu button in ContentWizard toolbar
    Implemented `UnpublishAction` of Content Wizard.

--- modules/admin/admin-ui/src/main/resources/web/admin/apps/content-studio/js/app/wizard/action/UnpublishAction.ts
+++ modules/admin/admin-ui/src/main/resources/web/admin/apps/content-studio/js/app/wizard/action/UnpublishAction.ts
@@ -1,16 +1,36 @@
 import "../../../api.ts";
-
 import {ContentWizardPanel} from "../ContentWizardPanel";
-import {ContentPublishPromptEvent} from "../../browse/ContentPublishPromptEvent";
+import {ContentUnpublishPromptEvent} from "../../browse/ContentUnpublishPromptEvent";
+import ContentSummaryAndCompareStatus = api.content.ContentSummaryAndCompareStatus;
 
 export class UnpublishAction extends api.ui.Action {
+
+    private wizard: ContentWizardPanel;
 
     constructor(wizard: ContentWizardPanel) {
         super("Unpublish");
 
-        this.onExecuted(() => {
-            console.log('Unpublish action')
-        });
+        this.wizard = wizard;
 
+        this.onExecuted(() => {
+            if (this.wizard.hasUnsavedChanges()) {
+                this.setEnabled(false);
+                this.wizard.saveChanges().then((content) => {
+                    if (content) {
+                        this.fireContentUnpublishPromptEvent();
+                    }
+                }).catch((reason: any) => {
+                    api.DefaultErrorHandler.handle(reason)
+                }).finally(() => this.setEnabled(true)).done();
+            } else {
+                this.fireContentUnpublishPromptEvent();
+            }
+        });
+    }
+
+    private fireContentUnpublishPromptEvent(): void {
+        let contentSummary = ContentSummaryAndCompareStatus.fromContentSummary(this.wizard.getPersistedItem());
+        contentSummary.setCompareStatus(this.wizard.getContentCompareStatus());
+        new ContentUnpublishPromptEvent([contentSummary]).fire();
     }
 }
