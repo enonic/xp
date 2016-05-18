18ec86c91f7ff380ed15c1336e25a43777b3423e
Pavel Milkevich <pmi@enonic.com>
2016-05-18 17:43:37 +0300

    XP-3428 Published-site not removed from the grid, when "Instantly delete published items " was checked in the 'Delete item' dialog

--- modules/admin/admin-ui/src/main/resources/web/admin/common/js/content/event/ContentServerEventsHandler.ts
+++ modules/admin/admin-ui/src/main/resources/web/admin/common/js/content/event/ContentServerEventsHandler.ts
@@ -68,7 +68,7 @@ module api.content.event {
             let useNewPaths = NodeServerChangeType.RENAME === event.getType() ||
                               NodeServerChangeType.MOVE == event.getType();
 
-            if (event.getType() == NodeServerChangeType.DELETE && this.hasNoMasterChanges(changes)) {
+            if (event.getType() == NodeServerChangeType.DELETE && this.hasDraftBranchChanges(changes)) {
                 // content has already been deleted so no need to fetch summaries
                 var changeItems: ContentServerChangeItem[] = changes.reduce((total, change: ContentServerChange) => {
                     return total.concat(change.getChangeItems());
@@ -122,10 +122,10 @@ module api.content.event {
             }
         }
 
-        private hasNoMasterChanges(changes: ContentServerChange[]): boolean {
-            return changes.every((change: ContentServerChange) => {
-                return change.getChangeItems().every(changeItem => {
-                    return changeItem.getBranch() != 'master';
+        private hasDraftBranchChanges(changes: ContentServerChange[]): boolean {
+            return changes.some((change: ContentServerChange) => {
+                return change.getChangeItems().some(changeItem => {
+                    return changeItem.getBranch() == 'draft';
                 })
             })
         }
