78163a23423c8c3d6979028d4db705cdf5f59380
Glenn Ricaud <gri@enonic.com>
2016-05-23 13:57:22 +0200

    XP-3115 Pluggable auth -Tests

---
+++ modules/portal/portal-impl/src/test/java/com/enonic/xp/portal/impl/auth/AuthControllerServiceImplTest.java
@@ -0,0 +1,162 @@
+package com.enonic.xp.portal.impl.auth;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+
+import com.enonic.xp.app.Application;
+import com.enonic.xp.app.ApplicationKey;
+import com.enonic.xp.app.ApplicationService;
+import com.enonic.xp.auth.AuthDescriptor;
+import com.enonic.xp.auth.AuthDescriptorService;
+import com.enonic.xp.portal.PortalRequest;
+import com.enonic.xp.portal.PortalResponse;
+import com.enonic.xp.portal.auth.AuthControllerExecutionParams;
+import com.enonic.xp.portal.impl.controller.AbstractControllerTest;
+import com.enonic.xp.portal.impl.script.PortalScriptServiceImpl;
+import com.enonic.xp.portal.script.PortalScriptService;
+import com.enonic.xp.resource.ResourceKey;
+import com.enonic.xp.resource.ResourceService;
+import com.enonic.xp.resource.UrlResource;
+import com.enonic.xp.script.impl.ScriptRuntimeFactoryImpl;
+import com.enonic.xp.security.AuthConfig;
+import com.enonic.xp.security.SecurityService;
+import com.enonic.xp.security.UserStore;
+import com.enonic.xp.security.UserStoreKey;
+import com.enonic.xp.web.HttpStatus;
+
+public class AuthControllerServiceImplTest
+{
+    private AuthControllerServiceImpl authControllerService;
+
+    private ResourceService resourceService;
+
+    @Before
+    public void setup()
+        throws Exception
+    {
+        //Mocks the AuthDescriptorService
+        final AuthDescriptor defaultAuthDescriptor = AuthDescriptor.create().key( ApplicationKey.from( "defaultapplication" ) ).build();
+        final AuthDescriptorService authDescriptorService = Mockito.mock( AuthDescriptorService.class );
+        Mockito.when( authDescriptorService.getDefaultDescriptor() ).thenReturn( defaultAuthDescriptor );
+        Mockito.when( authDescriptorService.getDescriptor( ApplicationKey.from( "myapplication" ) ) ).thenReturn(
+            AuthDescriptor.create().key( ApplicationKey.from( "myapplication" ) ).build() );
+
+        //Mocks the SecurityService
+        final SecurityService securityService = Mockito.mock( SecurityService.class );
+        final UserStore emptyUserStore = UserStore.create().build();
+        final AuthConfig authConfig = AuthConfig.create().applicationKey( ApplicationKey.from( "myapplication" ) ).build();
+        final UserStore userStore = UserStore.create().authConfig( authConfig ).build();
+        Mockito.when( securityService.getUserStore( UserStoreKey.from( "myemptyuserstore" ) ) ).thenReturn( emptyUserStore );
+        Mockito.when( securityService.getUserStore( UserStoreKey.from( "myuserstore" ) ) ).thenReturn( userStore );
+
+        //Mocks the PortalScriptService
+        final PortalScriptService portalScriptService = setupPortalScriptService();
+
+        //Creates AuthControllerScriptFactoryImpl
+        final AuthControllerScriptFactoryImpl authControllerScriptFactory = new AuthControllerScriptFactoryImpl();
+        authControllerScriptFactory.setScriptService( portalScriptService );
+
+        //Creates AuthControllerServiceImpl
+        authControllerService = new AuthControllerServiceImpl();
+        authControllerService.setAuthControllerScriptFactory( authControllerScriptFactory );
+        authControllerService.setAuthDescriptorService( authDescriptorService );
+        authControllerService.setSecurityService( securityService );
+    }
+
+    private PortalScriptService setupPortalScriptService()
+    {
+        final BundleContext bundleContext = Mockito.mock( BundleContext.class );
+
+        final Bundle bundle = Mockito.mock( Bundle.class );
+        Mockito.when( bundle.getBundleContext() ).thenReturn( bundleContext );
+
+        final Application application = Mockito.mock( Application.class );
+        Mockito.when( application.getBundle() ).thenReturn( bundle );
+        Mockito.when( application.getClassLoader() ).thenReturn( getClass().getClassLoader() );
+
+        final ApplicationService applicationService = Mockito.mock( ApplicationService.class );
+        Mockito.when( applicationService.getInstalledApplication( ApplicationKey.from( "defaultapplication" ) ) ).thenReturn( application );
+        Mockito.when( applicationService.getInstalledApplication( ApplicationKey.from( "myapplication" ) ) ).thenReturn( application );
+
+        this.resourceService = Mockito.mock( ResourceService.class );
+        Mockito.when( resourceService.getResource( Mockito.any() ) ).thenAnswer( invocation -> {
+            final ResourceKey resourceKey = (ResourceKey) invocation.getArguments()[0];
+            final URL resourceUrl =
+                AbstractControllerTest.class.getResource( "/" + resourceKey.getApplicationKey() + resourceKey.getPath() );
+            return new UrlResource( resourceKey, resourceUrl );
+        } );
+
+        final ScriptRuntimeFactoryImpl runtimeFactory = new ScriptRuntimeFactoryImpl();
+        runtimeFactory.setApplicationService( applicationService );
+        runtimeFactory.setResourceService( this.resourceService );
+
+        final PortalScriptServiceImpl scriptService = new PortalScriptServiceImpl();
+        scriptService.setScriptRuntimeFactory( runtimeFactory );
+        scriptService.initialize();
+
+        return scriptService;
+    }
+
+    @Test
+    public void executeMissingUserStore()
+        throws IOException
+    {
+        final AuthControllerExecutionParams executionParams = AuthControllerExecutionParams.create().
+            portalRequest( new PortalRequest() ).
+            userStoreKey( UserStoreKey.from( "missinguserstore" ) ).
+            functionName( "missingfunction" ).
+            build();
+        final PortalResponse portalResponse = authControllerService.execute( executionParams );
+        Assert.assertNull( portalResponse );
+    }
+
+    @Test
+    public void executeMissingFunction()
+        throws IOException
+    {
+        final AuthControllerExecutionParams executionParams = AuthControllerExecutionParams.create().
+            portalRequest( new PortalRequest() ).
+            userStoreKey( UserStoreKey.from( "myemptyuserstore" ) ).
+            functionName( "missingfunction" ).
+            build();
+        final PortalResponse portalResponse = authControllerService.execute( executionParams );
+        Assert.assertNull( portalResponse );
+    }
+
+    @Test
+    public void executeUserStoreWithoutIdProvider()
+        throws IOException
+    {
+        final AuthControllerExecutionParams executionParams = AuthControllerExecutionParams.create().
+            portalRequest( new PortalRequest() ).
+            userStoreKey( UserStoreKey.from( "myemptyuserstore" ) ).
+            functionName( "myfunction" ).
+            build();
+        final PortalResponse portalResponse = authControllerService.execute( executionParams );
+        Assert.assertNotNull( portalResponse );
+        Assert.assertEquals( HttpStatus.OK, portalResponse.getStatus() );
+        Assert.assertEquals( "defaultapplication/myfunction", portalResponse.getBody() );
+    }
+
+    @Test
+    public void execute()
+        throws IOException
+    {
+        final AuthControllerExecutionParams executionParams = AuthControllerExecutionParams.create().
+            portalRequest( new PortalRequest() ).
+            userStoreKey( UserStoreKey.from( "myuserstore" ) ).
+            functionName( "myfunction" ).
+            build();
+        final PortalResponse portalResponse = authControllerService.execute( executionParams );
+        Assert.assertNotNull( portalResponse );
+        Assert.assertEquals( HttpStatus.OK, portalResponse.getStatus() );
+        Assert.assertEquals( "myapplication/myfunction", portalResponse.getBody() );
+    }
+}
