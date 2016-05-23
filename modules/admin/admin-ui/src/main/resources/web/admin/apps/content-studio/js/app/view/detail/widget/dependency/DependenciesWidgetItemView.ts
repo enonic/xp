0688b3e63ea0f24f0d60ee5f5a1650d1aaca5d0c
Alan Semenov <ase@enonic.com>
2016-05-23 17:00:28 +0200

    XP-3399 Dependencies widget - Add buttons to filter by dependencies, fix layout * Fixed layout to center widget content dynamically * Added search buttons

--- modules/admin/admin-ui/src/main/resources/web/admin/apps/content-studio/js/app/view/detail/widget/dependency/DependenciesWidgetItemView.ts
+++ modules/admin/admin-ui/src/main/resources/web/admin/apps/content-studio/js/app/view/detail/widget/dependency/DependenciesWidgetItemView.ts
@@ -5,6 +5,8 @@ import ContentDependencyJson = api.content.json.ContentDependencyJson;
 import ContentDependencyGroupJson = api.content.json.ContentDependencyGroupJson;
 import {WidgetItemView} from "../../WidgetItemView";
 import {DependencyGroup, DependencyType} from "./DependencyGroup";
+import ActionButton = api.ui.button.ActionButton;
+import Action = api.ui.Action;
 
 export class DependenciesWidgetItemView extends WidgetItemView {
 
@@ -18,10 +20,35 @@ export class DependenciesWidgetItemView extends WidgetItemView {
     private inboundDependencies: DependencyGroup[];
     private outboundDependencies: DependencyGroup[];
 
+    private inboundButton: ActionButton;
+    private outboundButton: ActionButton;
+
     constructor() {
         super("dependency-widget-item-view");
 
+        this.inboundButton = this.appendButton("Show Inbound", "btn-inbound");
         this.appendMainContainer();
+        this.outboundButton = this.appendButton("Show Outbound", "btn-outbound");
+    }
+
+    private setButtonDecoration(button: ActionButton, dependencies: DependencyGroup[]) {
+        if (dependencies.length == 0) {
+            button.hide();
+        }
+        else {
+            button.setLabel(button.getAction().getLabel() + " (" + this.getTotalItemCount(dependencies) + ")");
+            button.show();
+        }
+    }
+
+    private appendButton(label: string, cls: string): ActionButton {
+        var action = new Action(label)
+        var button = new ActionButton(action);
+
+        button.addClass(cls);
+        this.appendChild(button);
+
+        return button;
     }
 
     public setItem(item: ContentSummaryAndCompareStatus): wemQ.Promise<any> {
@@ -30,6 +57,13 @@ export class DependenciesWidgetItemView extends WidgetItemView {
         }
 
         return this.resolveDependencies(item);
+    }
+
+    private resetContainers() {
+        this.mainContainer.removeChildren();
+
+        this.removeClass("no-inbound");
+        this.removeClass("no-outbound");
     }
 
     private appendMainContainer() {
@@ -53,6 +87,7 @@ export class DependenciesWidgetItemView extends WidgetItemView {
         var typeAsString = DependencyType[type].toLowerCase();
         var div = new api.dom.DivEl("dependencies-container " + typeAsString);
         if (dependencies.length == 0) {
+            this.addClass("no-"  + typeAsString);
             div.addClass("no-dependencies");
             div.setHtml("No " + typeAsString + " dependencies");
         }
@@ -66,19 +101,30 @@ export class DependenciesWidgetItemView extends WidgetItemView {
     }
 
     private renderContent(item: ContentSummaryAndCompareStatus) {
-
-        this.mainContainer.removeChildren();
+        this.resetContainers();
 
         this.noInboundDependencies = this.createDependenciesContainer(DependencyType.INBOUND, this.inboundDependencies);
         this.appendContentNamesAndIcon(item);
         this.noOutboundDependencies = this.createDependenciesContainer(DependencyType.OUTBOUND, this.outboundDependencies);
+
+        this.setButtonDecoration(this.inboundButton, this.inboundDependencies);
+        this.setButtonDecoration(this.outboundButton, this.outboundDependencies);
+    }
+
+    private getTotalItemCount(dependencies: DependencyGroup[]): number {
+        var sum = 0;
+        dependencies.forEach((dependencyGroup: DependencyGroup) => {
+            sum += dependencyGroup.getItemCount();
+        });
+
+        return sum;
     }
 
     private appendDependencies(container: api.dom.DivEl, dependencies: DependencyGroup[]) {
         dependencies.forEach((dependencyGroup: DependencyGroup) => {
             var dependencyGroupView = new api.app.NamesAndIconView(new api.app.NamesAndIconViewBuilder().setSize(api.app.NamesAndIconViewSize.small))
                 .setIconUrl(dependencyGroup.getIconUrl())
-                .setMainName(dependencyGroup.getItemCount().toString());
+                .setMainName("(" + dependencyGroup.getItemCount().toString() + ")");
 
             /* Tooltip is buggy
             dependencyGroupView.getEl().setTitle(dependencyGroup.getName());
