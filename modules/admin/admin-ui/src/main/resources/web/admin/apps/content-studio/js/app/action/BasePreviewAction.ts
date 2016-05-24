7df67138fca16d151bbaddbb4024ac649685e817
edloidas <edloidas@gmail.com>
2016-05-24 15:22:49 +0300

    XP-3365 Incorrect behavior for disable/enable 'Preview' button
    Added notification on the popup block.
    Added isBlocked check to prevent errors.

--- modules/admin/admin-ui/src/main/resources/web/admin/apps/content-studio/js/app/action/BasePreviewAction.ts
+++ modules/admin/admin-ui/src/main/resources/web/admin/apps/content-studio/js/app/action/BasePreviewAction.ts
@@ -3,21 +3,51 @@ import "../../api.ts";
 import Action = api.ui.Action;
 import RenderingMode = api.rendering.RenderingMode;
 
-export class BasePreviewAction extends Action {
+interface OpenedWindow {
+    openedWindow: Window,
+    isBlocked: boolean
+}
 
-    protected openWindows(contents: api.content.ContentSummary[]) { // should be called only in async block
-        var targetWindows = this.openBlankWindows(contents);
-        for (var key in targetWindows) {
-            this.updateLocation(targetWindows[key], contents[key], false);
-        }
+export class BasePreviewAction extends Action {
+    
+    private notifyBlocked: () => void;
+    
+    constructor(label: string, shortcut?: string, global?: boolean) {
+        super(label, shortcut, global);
+        // Notification is shown not less than once in a minute, if triggered
+        this.notifyBlocked = api.util.AppHelper.debounce(() => {
+            const message = "Pop-up Blocker is enabled in browser settings! Please add selected sites to the exception list.";
+            api.notify.showWarning(message, false);
+        }, 60000, true);
     }
 
-    protected openBlankWindows(contents: api.content.ContentSummary[]): any[] { // should be called only in async block
+    private popupCheck(win: Window) {
+        const isBlocked = !win || win.closed || typeof win.closed == "undefined";
+
+        if (isBlocked) {
+            this.notifyBlocked();
+        }
+        
+        return isBlocked;
+    }
+
+    protected openWindows(contents: api.content.ContentSummary[]) { // should be called only in async block
+        const targetWindows = this.openBlankWindows(contents);
+        targetWindows.forEach((value, index) => {
+            if (!value.isBlocked) {
+                this.updateLocation(value.openedWindow, contents[index], false);
+            }
+        });
+    }
+
+    protected openBlankWindows(contents: api.content.ContentSummary[]): OpenedWindow[] { // should be called only in async block
         return contents.map(content => this.openBlankWindow(content));
     }
 
-    protected openBlankWindow(content: api.content.ContentSummary): any { // should be called only in async block
-        return window.open('', content.getId());
+    protected openBlankWindow(content: api.content.ContentSummary): OpenedWindow { // should be called only in async block
+        const openedWindow = window.open('', content.getId());
+        const isBlocked = this.popupCheck(openedWindow);
+        return { openedWindow, isBlocked };
     }
 
     protected updateLocation(targetWindow, content: api.content.ContentSummary, focus: boolean = true) {
