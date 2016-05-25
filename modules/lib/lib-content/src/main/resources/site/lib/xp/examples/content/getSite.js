40abf6f74dd3a3879aac25a4ed09d623a45457db
Glenn Ricaud <gri@enonic.com>
2016-05-25 13:53:27 +0200

    XP-3475 Content library - getSite and getSiteConfig function

---
+++ modules/lib/lib-content/src/main/resources/site/lib/xp/examples/content/getSite.js
@@ -0,0 +1,34 @@
+var contentLib = require('/lib/xp/content');
+var assert = require('/lib/xp/assert');
+
+// BEGIN
+var result = contentLib.getSite({
+    key: '/path/to/mycontent'
+});
+log.info('Site name = %s', result._name);
+// END
+
+// BEGIN
+// Site data returned.
+var expected = {
+    "_id": "100123",
+    "_name": "my-content",
+    "_path": "/my-content",
+    "type": "base:unstructured",
+    "hasChildren": false,
+    "valid": false,
+    "data": {
+        "siteConfig": {
+            "applicationKey": "myapplication",
+            "config": {
+                "Field": 42
+            }
+        }
+    },
+    "x": {},
+    "page": {},
+    "attachments": {}
+};
+// END
+
+assert.assertJsonEquals(expected, result);
