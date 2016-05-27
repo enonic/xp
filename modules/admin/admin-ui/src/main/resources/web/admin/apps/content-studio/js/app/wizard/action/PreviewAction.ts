9b3515789fd26d77254d86bdf9e840f2f2a0c89f
edloidas <edloidas@gmail.com>
2016-05-27 18:03:52 +0300

    XP-3483 Tab with blank page appears after a pressing on the 'Preview' button in the content wizard
    Fixed the issue, when the target window wasn't created correctly.

--- modules/admin/admin-ui/src/main/resources/web/admin/apps/content-studio/js/app/wizard/action/PreviewAction.ts
+++ modules/admin/admin-ui/src/main/resources/web/admin/apps/content-studio/js/app/wizard/action/PreviewAction.ts
@@ -1,21 +1,20 @@
 import "../../../api.ts";
-
-import RenderingMode = api.rendering.RenderingMode;
 import {BasePreviewAction} from "../../action/BasePreviewAction";
 import {ContentWizardPanel} from "../ContentWizardPanel";
+
+import RenderingMode = api.rendering.RenderingMode;
 
 export class PreviewAction extends BasePreviewAction {
 
     constructor(wizard: ContentWizardPanel) {
         super("Preview");
         this.onExecuted(() => {
-                var previewWindow = this.openBlankWindow(wizard.getPersistedItem());
                 if (wizard.hasUnsavedChanges()) {
                     wizard.setRequireValid(true);
-                    wizard.saveChanges().then(content => this.updateLocation(previewWindow, content)).catch(
+                    wizard.saveChanges().then(content => this.openWindow(content)).catch(
                         (reason: any) => api.DefaultErrorHandler.handle(reason)).done();
                 } else {
-                    this.updateLocation(previewWindow, wizard.getPersistedItem());
+                    this.openWindow(wizard.getPersistedItem());
                 }
             }
         );
