f173ca90d1008ae7e6239aef2d4a8261ff6640d9
Glenn Ricaud <gri@enonic.com>
2016-06-13 12:23:54 +0200

    XP-3611 Id Provider Improvements - handle401

--- modules/portal/portal-api/src/main/java/com/enonic/xp/portal/PortalException.java
+++ modules/portal/portal-api/src/main/java/com/enonic/xp/portal/PortalException.java
@@ -1,5 +1,7 @@
 package com.enonic.xp.portal;
 
+import com.enonic.xp.context.ContextAccessor;
+import com.enonic.xp.security.auth.AuthenticationInfo;
 import com.enonic.xp.web.HttpStatus;
 
 public final class PortalException
@@ -27,6 +29,19 @@ public final class PortalException
     public static PortalException notFound( final String message )
     {
         return new PortalException( HttpStatus.NOT_FOUND, message );
+    }
+
+    public static PortalException forbidden( final String message )
+    {
+        final AuthenticationInfo authInfo = ContextAccessor.current().getAuthInfo();
+        if ( authInfo.isAuthenticated() )
+        {
+            return new PortalException( HttpStatus.FORBIDDEN, message );
+        }
+        else
+        {
+            return new PortalException( HttpStatus.UNAUTHORIZED, message );
+        }
     }
 
     public static PortalException internalServerError( final String message )
