b0737eb958e9b829568282e2221858edb2d6253e
Runar Myklebust <rmy@enonic.com>
2016-06-16 12:47:55 +0200

    XP-3643 Some page text component are not indexed
    Adding glob-path matching to index config

---
+++ modules/core/core-api/src/main/java/com/enonic/xp/util/GlobPatternMatcher.java
@@ -0,0 +1,144 @@
+package com.enonic.xp.util;
+
+public class GlobPatternMatcher
+{
+
+    public static boolean match( final String pattern, final String value, final String separator )
+    {
+        final String pathExpr = convertGlobToRegex( pattern, separator );
+
+        return value.matches( pathExpr );
+    }
+
+    private static final String convertGlobToRegex( String pattern, final String separator )
+    {
+        pattern = pattern.replace( separator, "\\" + separator );
+
+        StringBuilder sb = new StringBuilder( pattern.length() );
+        int inGroup = 0;
+        int inClass = 0;
+        int firstIndexInClass = -1;
+        char[] arr = pattern.toCharArray();
+        for ( int i = 0; i < arr.length; i++ )
+        {
+            char ch = arr[i];
+
+            switch ( ch )
+            {
+                case '\\':
+                    if ( ++i >= arr.length )
+                    {
+                        sb.append( '\\' );
+                    }
+                    else
+                    {
+                        char next = arr[i];
+                        switch ( next )
+                        {
+                            case ',':
+                                // escape not needed
+                                break;
+                            case 'Q':
+                            case 'E':
+                                // extra escape needed
+                                sb.append( '\\' );
+                            default:
+                                sb.append( '\\' );
+                        }
+                        sb.append( next );
+                    }
+                    break;
+                case '*':
+
+                    if ( inClass == 0 )
+                    {
+                        if ( isDoubleAsterix( arr, i ) )
+                        {
+                            sb.append( ".*" );
+                        }
+                        else
+                        {
+                            sb.append( "[^" );
+                            sb.append( separator );
+                            sb.append( "]*" );
+                        }
+                    }
+                    else
+                    {
+                        sb.append( '*' );
+                    }
+                    break;
+                case '?':
+                    if ( inClass == 0 )
+                    {
+                        sb.append( '.' );
+                    }
+                    else
+                    {
+                        sb.append( '?' );
+                    }
+                    break;
+                case '[':
+                    inClass++;
+                    firstIndexInClass = i + 1;
+                    sb.append( '[' );
+                    break;
+                case ']':
+                    inClass--;
+                    sb.append( ']' );
+                    break;
+                case '.':
+                case '(':
+                case ')':
+                case '+':
+                case '|':
+                case '^':
+                case '$':
+                case '@':
+                case '%':
+                    if ( inClass == 0 || ( firstIndexInClass == i && ch == '^' ) )
+                    {
+                        sb.append( '\\' );
+                    }
+                    sb.append( ch );
+                    break;
+                case '!':
+                    if ( firstIndexInClass == i )
+                    {
+                        sb.append( '^' );
+                    }
+                    else
+                    {
+                        sb.append( '!' );
+                    }
+                    break;
+                case '{':
+                    inGroup++;
+                    sb.append( '(' );
+                    break;
+                case '}':
+                    inGroup--;
+                    sb.append( ')' );
+                    break;
+                case ',':
+                    if ( inGroup > 0 )
+                    {
+                        sb.append( '|' );
+                    }
+                    else
+                    {
+                        sb.append( ',' );
+                    }
+                    break;
+                default:
+                    sb.append( ch );
+            }
+        }
+        return sb.toString();
+    }
+
+    private static boolean isDoubleAsterix( final char[] arr, final int i )
+    {
+        return arr.length > i + 1 && arr[i + 1] == '*';
+    }
+}
