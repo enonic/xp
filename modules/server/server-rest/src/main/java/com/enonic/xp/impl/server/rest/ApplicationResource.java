2b86e744e62196f5815214cdbc823de3affae099
Sten Roger Sandvik <srs@enonic.com>
2016-06-22 12:24:55 +0200

    XP-3683 Create new module for toolbox rest services.

---
+++ modules/server/server-rest/src/main/java/com/enonic/xp/impl/server/rest/ApplicationResource.java
@@ -0,0 +1,29 @@
+package com.enonic.xp.impl.server.rest;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.osgi.service.component.annotations.Component;
+
+import com.enonic.xp.jaxrs.JaxRsComponent;
+import com.enonic.xp.security.RoleKeys;
+
+@Path("/api/repo")
+@Produces(MediaType.APPLICATION_JSON)
+@RolesAllowed(RoleKeys.ADMIN_ID)
+@Component(immediate = true)
+public final class ApplicationResource
+    implements JaxRsComponent
+{
+    // @POST
+    // @Path("install")
+    // @Consumes(MediaType.MULTIPART_FORM_DATA)
+    // public ApplicationInstallResultJson install( final MultipartForm form )
+
+    // @POST
+    // @Path("installUrl")
+    // @Consumes(MediaType.APPLICATION_JSON)
+    // public ApplicationInstallResultJson installUrl( final ApplicationInstallParams params )
+}
