398c94a58aef950c3d89c22f679a4e63de597611
Glenn Ricaud <gri@enonic.com>
2016-06-23 16:47:36 +0200

    XP-3703 WebDispatcherServlet

---
+++ modules/web/web-api/src/main/java/com/enonic/xp/web/WebException.java
@@ -0,0 +1,56 @@
+package com.enonic.xp.web;
+
+import com.enonic.xp.context.ContextAccessor;
+import com.enonic.xp.security.auth.AuthenticationInfo;
+
+public final class WebException
+    extends RuntimeException
+{
+    private final HttpStatus status;
+
+    public WebException( final HttpStatus status, final String message )
+    {
+        super( message );
+        this.status = status;
+    }
+
+    public WebException( final HttpStatus status, final Throwable cause )
+    {
+        super( cause.getMessage(), cause );
+        this.status = status;
+    }
+
+    public HttpStatus getStatus()
+    {
+        return this.status;
+    }
+
+    public static WebException badRequest( final String message )
+    {
+        return new WebException( HttpStatus.BAD_REQUEST, message );
+    }
+
+    public static WebException forbidden( final String message )
+    {
+        final AuthenticationInfo authInfo = ContextAccessor.current().getAuthInfo();
+        if ( authInfo.isAuthenticated() )
+        {
+            return new WebException( HttpStatus.FORBIDDEN, message );
+        }
+        else
+        {
+            return new WebException( HttpStatus.UNAUTHORIZED, message );
+        }
+    }
+
+    public static WebException notFound( final String message )
+    {
+        return new WebException( HttpStatus.NOT_FOUND, message );
+    }
+
+
+    public static WebException internalServerError( final String message )
+    {
+        return new WebException( HttpStatus.INTERNAL_SERVER_ERROR, message );
+    }
+}
