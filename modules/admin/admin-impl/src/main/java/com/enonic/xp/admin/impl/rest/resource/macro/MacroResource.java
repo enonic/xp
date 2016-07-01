87be6aa7391b7df385280147270317a7fafabbee
Alex Rodriguez <aro@enonic.com>
2016-07-01 10:13:10 +0200

    XP-3732 Invalid asset URL created from macro preview

--- modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/rest/resource/macro/MacroResource.java
+++ modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/rest/resource/macro/MacroResource.java
@@ -173,7 +173,7 @@ public final class MacroResource
         portalRequest.setRawRequest( req );
         portalRequest.setMethod( HttpMethod.GET );
         portalRequest.setContentType( req.getContentType() );
-        portalRequest.setBaseUri( "/admin" );
+        portalRequest.setBaseUri( "/admin/portal/edit" );
         portalRequest.setMode( RenderMode.EDIT );
         portalRequest.setBranch( ContentConstants.BRANCH_DRAFT );
         portalRequest.setScheme( ServletRequestUrlHelper.getScheme( req ) );
