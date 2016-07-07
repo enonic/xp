0fd79bf21149e36111d15919bb4517b0d85631c7
ashklianko <asl@enonic.com>
2016-07-07 18:31:01 +0300

    XP-3704 XP Welcome Tour - Implement modal dialog and navigation XP-3705 XP Welcome Tour - Steps 1-4 XP-3706 XP Welcome Tour - Install demo apps (Step 5)

--- modules/admin/admin-ui/src/main/resources/assets/js/home/main.js
+++ modules/admin/admin-ui/src/main/resources/assets/js/home/main.js
@@ -1,53 +1,44 @@
-require('webcomponents.js');
 var $ = require('jquery');
-var aboutDialogContainer, aboutDialog;
 
 $(function () {
-
-    var responsive = require('./responsive');
-    responsive.applyResponsiveCls();
-    window.onresize = responsive.applyResponsiveCls;
-
     var launcher = require('./launcher');
     launcher.init();
 
     setupAboutDialog();
+
+    var xptour = require('./xptour');
+    xptour.init();
 });
 
 function setupAboutDialog() {
-    aboutDialogContainer = document.querySelector(".xp-about-dialog-container");
-    aboutDialog = document.querySelector(".xp-about-dialog");
-
-    document.querySelector(".xp-about").addEventListener("click", toggleAboutDialog);
-    document.querySelector(".xp-about-dialog-button-close").addEventListener("click", hideAboutDialog);
-    document.querySelector(".xp-about-dialog-corner-close").addEventListener("click", hideAboutDialog);
-    aboutDialogContainer.addEventListener("click", handleClickOutside);
+    var aboutDialog = new api.ui.dialog.ModalDialog({title: new api.ui.dialog.ModalDialogHeader("")});
+    aboutDialog.addClass("xp-about-dialog");
+    aboutDialog.appendChildToContentPanel(getAboutDialogContent());
+    aboutDialog.addCancelButtonToBottom("Close");
+    document.querySelector(".xp-about").addEventListener("click", function () {
+        aboutDialog.open();
+        aboutDialog.centerHorisontally();
+    });
+    api.dom.Body.get().appendChild(aboutDialog);
 }
 
-function toggleAboutDialog() {
+function getAboutDialogContent() {
+    var html = '<div class="xp-about-dialog-content">' +
+               '    <div class="xp-about-dialog-app-icon">' +
+               '        <img src="/admin/common/images/app-icon.svg">' +
+               '    </div>' +
+               '    <h1>Enonic XP</h1>' +
+               '    <div class="xp-about-dialog-version-block">' +
+               '        <span class="xp-about-dialog-version">' + CONFIG.xpVersion + '</span>&nbsp;' +
+               '        <a href="' + CONFIG.docLinkPrefix + '/appendix/release-notes/" target="_blank">What\'s new</a>' +
+               '    </div>' +
+               '    <div class="xp-about-dialog-text">' +
+               'Blend sites, applications and services together seamlessly.' +
+               'Our powerful Web Operating System simplifies all stages of the' +
+               'digital delivery process - focus on solution rather than technology.' +
+               '    </div>' +
+               '</div>';
 
-    if (isAboutDialogShown()) {
-        hideAboutDialog();
-    }
-    else {
-        showAboutDialog();
-    }
-}
-
-function isAboutDialogShown() {
-    return window.getComputedStyle(aboutDialogContainer).getPropertyValue("display") !== "none";
-}
-
-function hideAboutDialog() {
-    aboutDialogContainer.style.display = "none"
-}
-
-function showAboutDialog() {
-    aboutDialogContainer.style.display = "flex"
-}
-
-function handleClickOutside(e) {
-    if (!aboutDialog.contains(e.target)) {
-        hideAboutDialog();
-    }
+    var element = api.dom.Element.fromString(html);
+    return element;
 }
