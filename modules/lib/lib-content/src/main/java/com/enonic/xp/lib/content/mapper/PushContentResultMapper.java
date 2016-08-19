a48250401d5f1b0084a988303b0839fb7dfcc78e
Runar Myklebust <rmy@enonic.com>
2016-08-19 16:38:34 +0200

    XP-3978 publish in js-api uses old push method
    Fixed so publish in 6.7 is quick, but still beeing backwards-compatible

--- modules/lib/lib-content/src/main/java/com/enonic/xp/lib/content/mapper/PushContentResultMapper.java
+++ modules/lib/lib-content/src/main/java/com/enonic/xp/lib/content/mapper/PushContentResultMapper.java
@@ -3,20 +3,20 @@ package com.enonic.xp.lib.content.mapper;
 import java.util.List;
 
 import com.enonic.xp.content.ContentId;
+import com.enonic.xp.content.ContentIds;
 import com.enonic.xp.content.ContentPath;
-import com.enonic.xp.content.Contents;
-import com.enonic.xp.content.PushContentsResult;
+import com.enonic.xp.content.PublishContentResult;
 import com.enonic.xp.script.serializer.MapGenerator;
 import com.enonic.xp.script.serializer.MapSerializable;
 
 public class PushContentResultMapper
     implements MapSerializable
 {
-    private final PushContentsResult value;
+    private final PublishContentResult value;
 
     private final List<ContentPath> contentNotFound;
 
-    public PushContentResultMapper( final PushContentsResult value, final List<ContentPath> contentNotFound )
+    public PushContentResultMapper( final PublishContentResult value, final List<ContentPath> contentNotFound )
     {
         this.value = value;
         this.contentNotFound = contentNotFound;
@@ -28,27 +28,27 @@ public class PushContentResultMapper
         this.serialize( gen, this.value );
     }
 
-    private void serialize( final MapGenerator gen, final PushContentsResult value )
+    private void serialize( final MapGenerator gen, final PublishContentResult value )
     {
         serializeContentIds( gen, "pushedContents", value.getPushedContents() );
         serializeContentIds( gen, "deletedContents", value.getDeletedContents() );
         serializeFailedContent( gen, "failedContents", value.getFailedContents() );
     }
 
-    private void serializeContentIds( final MapGenerator gen, final String name, final Contents contents )
+    private void serializeContentIds( final MapGenerator gen, final String name, final ContentIds contents )
     {
         gen.array( name );
-        for ( ContentId id : contents.getIds() )
+        for ( ContentId id : contents )
         {
             gen.value( id.toString() );
         }
         gen.end();
     }
 
-    private void serializeFailedContent( final MapGenerator gen, final String name, final Contents contents )
+    private void serializeFailedContent( final MapGenerator gen, final String name, final ContentIds contents )
     {
         gen.array( name );
-        for ( ContentId id : contents.getIds() )
+        for ( ContentId id : contents )
         {
             gen.value( id.toString() );
         }
