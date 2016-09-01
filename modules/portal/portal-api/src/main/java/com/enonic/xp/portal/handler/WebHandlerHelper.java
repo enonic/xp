26df3eb5c6182f5c70d635a373f30d65054b71f9
Glenn Ricaud <gri@enonic.com>
2016-09-01 15:08:04 +0200

    XP-4055 Admin portal requests should require Admin login rights

---
+++ modules/portal/portal-api/src/main/java/com/enonic/xp/portal/handler/WebHandlerHelper.java
@@ -0,0 +1,29 @@
+package com.enonic.xp.portal.handler;
+
+import com.enonic.xp.portal.PortalRequest;
+import com.enonic.xp.portal.RenderMode;
+import com.enonic.xp.security.RoleKeys;
+import com.enonic.xp.web.WebException;
+import com.enonic.xp.web.WebRequest;
+
+public class WebHandlerHelper
+{
+    public static void checkAdminAccess( final WebRequest webRequest )
+    {
+        if ( !isLiveMode( webRequest ) && !hasAdminRight( webRequest ) )
+        {
+            throw WebException.forbidden( "You don't have permission to access" );
+        }
+    }
+
+    private static boolean isLiveMode( final WebRequest webRequest )
+    {
+        return webRequest instanceof PortalRequest && RenderMode.LIVE == ( (PortalRequest) webRequest ).getMode();
+    }
+
+    private static boolean hasAdminRight( final WebRequest webRequest )
+    {
+        return webRequest.getRawRequest().isUserInRole( RoleKeys.ADMIN_LOGIN_ID );
+    }
+
+}
