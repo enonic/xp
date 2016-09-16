ec4fc49096c7911ba47ef9d9085cc5a1767bc7de
sshklianko <stefan.shkl@gmail.com>
2016-09-16 15:19:16 +0300

    XP-3998 Replace three details panels with one - Separated slidable panels implementation from details view that contains widget - Refactored details panel to separate sliding panels functionality from widgets view functionality - Renamed DetailsPanel into DetailsView- now details view is responsible only for rendering widgets, not hiding/showing itself

---
+++ modules/admin/admin-ui/src/main/resources/web/admin/apps/content-studio/js/app/view/detail/DockedDetailsPanel.ts
@@ -0,0 +1,33 @@
+import "../../../api.ts";
+import {DetailsPanel, DETAILS_PANEL_TYPE} from "./DetailsPanel";
+import {DetailsView} from "./DetailsView";
+
+import ResponsiveManager = api.ui.responsive.ResponsiveManager;
+
+export class DockedDetailsPanel extends DetailsPanel {
+
+    constructor(detailsView: DetailsView) {
+        super(detailsView);
+        this.setDoOffset(false);
+        this.addClass("docked-details-panel");
+    }
+
+    protected subscribeOnEvents() {
+        this.onPanelSizeChanged(() => this.detailsView.setDetailsContainerHeight());
+
+        this.onShown(() => {
+            if (!!this.getItem()) {
+                setTimeout(() => this.detailsView.updateActiveWidget(), 250); // small delay so that isVisibleOrAboutToBeVisible() check detects width change
+            }
+        });
+    }
+
+    public isVisibleOrAboutToBeVisible(): boolean {
+        return this.getHTMLElement().clientWidth > 0;
+    }
+
+    public getType(): DETAILS_PANEL_TYPE {
+        return DETAILS_PANEL_TYPE.DOCKED;
+    }
+}
+
