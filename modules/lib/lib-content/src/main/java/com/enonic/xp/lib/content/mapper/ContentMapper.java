e4fe22954cec064e986c88cafff11109ae3c6069
Runar Myklebust <rmy@enonic.com>
2016-09-21 13:50:15 +0200

    XP-4071 Storing published info on content
    Added to json, fixed tests

--- modules/lib/lib-content/src/main/java/com/enonic/xp/lib/content/mapper/ContentMapper.java
+++ modules/lib/lib-content/src/main/java/com/enonic/xp/lib/content/mapper/ContentMapper.java
@@ -33,6 +33,7 @@ public final class ContentMapper
         gen.value( "modifier", value.getModifier() );
         gen.value( "createdTime", value.getCreatedTime() );
         gen.value( "modifiedTime", value.getModifiedTime() );
+        gen.value( "publishedTime", value.getPublishedTime() );
         gen.value( "type", value.getType() );
         gen.value( "displayName", value.getDisplayName() );
         gen.value( "hasChildren", value.hasChildren() );
