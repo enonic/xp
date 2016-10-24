9b575cfae5344ed8edbd8379056fc32b13e09703
Sten Roger Sandvik <srs@enonic.com>
2016-10-24 21:14:29 +0200

    XP-4320 Implement thread-dump status reporter.

---
+++ modules/server/server-status/src/main/java/com/enonic/xp/server/impl/status/ThreadDumpReporter.java
@@ -0,0 +1,43 @@
+package com.enonic.xp.server.impl.status;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.management.ManagementFactory;
+
+import org.osgi.service.component.annotations.Component;
+
+import com.codahale.metrics.jvm.ThreadDump;
+import com.google.common.net.MediaType;
+
+import com.enonic.xp.status.StatusReporter;
+
+@Component(immediate = true, service = StatusReporter.class)
+public final class ThreadDumpReporter
+    implements StatusReporter
+{
+    private final ThreadDump threadDump;
+
+    public ThreadDumpReporter()
+    {
+        this.threadDump = new ThreadDump( ManagementFactory.getThreadMXBean() );
+    }
+
+    @Override
+    public String getName()
+    {
+        return "threads";
+    }
+
+    @Override
+    public MediaType getMediaType()
+    {
+        return MediaType.PLAIN_TEXT_UTF_8;
+    }
+
+    @Override
+    public void write( final OutputStream out )
+        throws IOException
+    {
+        this.threadDump.dump( out );
+    }
+}
