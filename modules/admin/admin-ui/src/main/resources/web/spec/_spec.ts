7e390c35ca59bbc805111f80f406f90c06525f22
Alan Semenov <ase@enonic.com>
2016-10-31 10:50:28 +0100

    XP-4338 FormOptionSet - Rewrite existing test class in TypeScript * Moved FormOptionSet.spec.ts to correct folder * Added _spec.ts files to all folders for cleaner build process

--- modules/admin/admin-ui/src/main/resources/web/spec/_spec.ts
+++ modules/admin/admin-ui/src/main/resources/web/spec/_spec.ts
@@ -1,5 +1,3 @@
 /// <reference path="jasmine.d.ts" />
 ///<reference path='../admin/common/js/_all.d.ts' />
 ///<reference path='common/_spec.ts' />
-///<reference path='common/schema/optionset/_spec.ts' />
-///<reference path='common/form/set/optionset/_spec.ts' />
--- modules/admin/admin-ui/src/main/resources/web/spec/common/schema/optionset/_spec.ts
+++
@@ -1,1 +0,0 @@
-///<reference path='FormOptionSet.spec.ts' />
