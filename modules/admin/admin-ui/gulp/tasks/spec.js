c028986958b1195cdb304e51f3855968d65566cc
Alan Semenov <ase@enonic.com>
2016-10-31 10:07:54 +0100

    XP-4347 Set up Karma test runs and coverage * Run karma process in a function instead of a separate task * Open a browser with coverage stats upon completion

--- modules/admin/admin-ui/gulp/tasks/spec.js
+++ modules/admin/admin-ui/gulp/tasks/spec.js
@@ -1,12 +1,27 @@
 var gulp = require("gulp");
-var gulpSequence = require("gulp-sequence");
+var open = require('gulp-open');
 var CONFIG = require("../config");
 var Server = require('karma').Server;
+var os = require('os');
+var path = require("path");
 
-gulp.task('karma', function (done) {
+gulp.task('spec', ['ts:spec'], function(done) {
+    // Run Karma after a little timeout to make sure the spec files are built
     setTimeout(function(){
-        new Server(CONFIG.spec, done).start();
+        new Server(CONFIG.spec, function() {
+            var browser =
+                    os.platform() === 'linux' ? 'google-chrome' : (
+                    os.platform() === 'darwin' ? 'google chrome' : (
+                    os.platform() === 'win32' ? 'chrome' : 'firefox'));
+
+            var uri = path.join(CONFIG.spec.remapIstanbulReporter.reports.html, "index.html");
+
+            gulp.src('./')
+                .pipe(open({
+                    uri: uri,
+                    app: browser
+                }));
+
+        }).start();
     }, 500);
 });
-
-gulp.task('spec', gulpSequence('ts:spec', ['karma']));
