885c7a177316c6b20036104f8aec29f076160f14
Viachaslau Bradnitski <vbradnitski@scnsoft.com>
2016-11-08 15:20:40 +0300

    XP-4305 Add unit tests for SiteConfigurator class fixed according to pr comments.

--- modules/admin/admin-ui/src/main/resources/web/spec/common/_spec.ts
+++ modules/admin/admin-ui/src/main/resources/web/spec/common/_spec.ts
@@ -1,3 +1,4 @@
 ///<reference path='ClassHelper.spec.ts' />
+
 ///<reference path='form/_spec.ts' />
-///<reference path='content/site/inputtype/siteconfigurator/_spec.ts' />
+///<reference path='content/_spec.ts' />
