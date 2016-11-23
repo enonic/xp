01c1c8e91d744399f343de69f4529c2d942af0d3
Runar Myklebust <rmy@enonic.com>
2016-11-23 11:41:41 +0100

    fix optional in js-doc

--- modules/lib/lib-repo/src/main/resources/site/lib/xp/repo.js
+++ modules/lib/lib-repo/src/main/resources/site/lib/xp/repo.js
@@ -21,7 +21,7 @@ function required(params, name) {
  *
  * @example-ref examples/repo/refresh.js
  *
- * @param {object} params JSON with the parameters.
+ * @param {object?} params JSON with the parameters.
  * @param {string} [params.mode='all'] Index type to be refreshed. Possible values: 'all' | 'search' | 'storage'.
  * @param {string} [params.repo='cms-repo'] Repository id: 'cms-repo' | 'system-repo'. Default is the current repository set in portal.
  *
