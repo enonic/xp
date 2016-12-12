8eed4cc5a532527d09a1e59c90090985dad80380
Alan Semenov <ase@enonic.com>
2016-12-12 11:04:02 +0100

    XP-4667 Redesign of Content preview on mobile resolution * More merge with master.....

--- modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/rest/resource/content/page/part/PartDescriptorResource.java
+++ modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/rest/resource/content/page/part/PartDescriptorResource.java
@@ -16,12 +16,12 @@ import com.enonic.xp.admin.impl.json.content.page.region.PartDescriptorJson;
 import com.enonic.xp.admin.impl.json.content.page.region.PartDescriptorsJson;
 import com.enonic.xp.admin.impl.rest.resource.ResourceConstants;
 import com.enonic.xp.app.ApplicationKey;
+import com.enonic.xp.jaxrs.JaxRsComponent;
 import com.enonic.xp.page.DescriptorKey;
 import com.enonic.xp.region.PartDescriptor;
 import com.enonic.xp.region.PartDescriptorService;
 import com.enonic.xp.region.PartDescriptors;
 import com.enonic.xp.security.RoleKeys;
-import com.enonic.xp.jaxrs.JaxRsComponent;
 
 @Path(ResourceConstants.REST_ROOT + "content/page/part/descriptor")
 @Produces(MediaType.APPLICATION_JSON)
