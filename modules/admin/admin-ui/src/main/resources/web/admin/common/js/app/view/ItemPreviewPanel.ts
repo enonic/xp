0011372d9d7900a0e1017b6a1726df5978ef85d8
Stepan Shklianko <stefan.shkl@gmail.com>
2016-12-20 13:44:32 +0300

    XP-4673 Mobile - Preview panel transition is not entirely smooth in Firefox - Adjusted sliding process for mobile statistics panel - now it firstly slides out and then starts loading it's contents - Made loadmask of ItemPreviewPanel to be its child - Added 'blank' state for ContentItemPreviewPanel that displays no message in preview panel - Fixed styling of the context menu in mobile preview

--- modules/admin/admin-ui/src/main/resources/web/admin/common/js/app/view/ItemPreviewPanel.ts
+++ modules/admin/admin-ui/src/main/resources/web/admin/common/js/app/view/ItemPreviewPanel.ts
@@ -9,10 +9,10 @@ module api.app.view {
         constructor(className?: string) {
             super("item-preview-panel" + (className ? " " + className : ""));
             this.mask = new api.ui.mask.LoadMask(this);
+            this.appendChild(this.mask);
             this.frame = new api.dom.IFrameEl();
             this.frame.onLoaded((event: UIEvent) => this.mask.hide());
             this.appendChild(this.frame);
         }
-
     }
 }
