b0c9c6556b45d52d02ffa6b03e6ec831cad96517
Glenn Ricaud <gri@enonic.com>
2017-01-17 09:23:50 +0100

    XP-4288 Delete branch in RepositoryService / repo-lib

---
+++ modules/lib/lib-repo/src/test/java/com/enonic/xp/lib/repo/DeleteBranchHandlerTest.java
@@ -0,0 +1,38 @@
+package com.enonic.xp.lib.repo;
+
+import org.junit.Test;
+
+import com.enonic.xp.repository.RepositoryExeption;
+
+public class DeleteBranchHandlerTest
+{
+    @Test(expected = RepositoryExeption.class)
+    public void protected_system_repo()
+        throws Exception
+    {
+        final DeleteBranchHandler handler = new DeleteBranchHandler();
+        handler.setRepoId( "system-repo" );
+        handler.setBranchId( "master" );
+        handler.execute();
+    }
+
+    @Test(expected = RepositoryExeption.class)
+    public void protected_cms_repo_draft()
+        throws Exception
+    {
+        final DeleteBranchHandler handler = new DeleteBranchHandler();
+        handler.setRepoId( "cms-repo" );
+        handler.setBranchId( "draft" );
+        handler.execute();
+    }
+
+    @Test(expected = RepositoryExeption.class)
+    public void protected_cms_repo_master()
+        throws Exception
+    {
+        final DeleteBranchHandler handler = new DeleteBranchHandler();
+        handler.setRepoId( "cms-repo" );
+        handler.setBranchId( "master" );
+        handler.execute();
+    }
+}
