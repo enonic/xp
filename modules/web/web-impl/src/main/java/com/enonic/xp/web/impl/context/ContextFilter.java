4557c3839f977d1f218dd57a437317ff0a8c69ac
Runar Myklebust <rmy@enonic.com>
2017-02-21 09:41:23 +0100

    Stuff (#4399)
    Fixed import

--- modules/web/web-impl/src/main/java/com/enonic/xp/web/impl/context/ContextFilter.java
+++ modules/web/web-impl/src/main/java/com/enonic/xp/web/impl/context/ContextFilter.java
@@ -4,7 +4,6 @@ import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.osgi.service.component.annotations.Component;
 
