36cb2ed3345692a76afae3629da59969bf8fac36
Stepan Shklianko <stefan.shkl@gmail.com>
2017-02-24 15:56:08 +0300

    Move dialog - Show tree structure inside the content selector - Splitted DropdownGrid into DropdownListGrid (which holds regular grid that was previously used) and DropdownTreeGrid (which contains tree grid) - Created parametrized OptionTreeGrid that  extends tree grid in order to manage Options. Created OptionDataLoader and OptionDataHelper interfaces to assist OptionsTreeGrid to managed parametrized data. Created ContentSummaryOptionDataLoader and ContentSummaryOptionDataHelper that implement these interfaces. - Created ContentSummaryFetcher similar to ContentSummaryAndCompareStatusFetcher - Renamed RichComboboxCombobox to LoaderComboBox. - Adjusted Grid and GridBuilder classes to accept defaultWidth and defaultHeight params as strings so that such values as 'auto' and '100%' could be passed. It enabled removing some '!important;' entries from tree-grid.less - Fixed hasClass check in ElementHelper - Code refactoring

---
+++ modules/admin/admin-ui/src/main/resources/web/admin/common/js/ui/selector/combobox/LoaderComboBox.ts
@@ -0,0 +1,80 @@
+module api.ui.selector.combobox {
+
+    export class LoaderComboBox<OPTION_DISPLAY_VALUE> extends ComboBox<OPTION_DISPLAY_VALUE> {
+
+        private loader: api.util.loader.BaseLoader<any, OPTION_DISPLAY_VALUE>;
+
+        private tempValue: string;
+
+        public static debug: boolean = false;
+
+        constructor(name: string, config: ComboBoxConfig<OPTION_DISPLAY_VALUE>,
+                    loader: api.util.loader.BaseLoader<any, OPTION_DISPLAY_VALUE>) {
+            super(name, config);
+            this.loader = loader;
+        }
+
+        public setLoader(loader: api.util.loader.BaseLoader<any, OPTION_DISPLAY_VALUE>) {
+            this.loader = loader;
+        }
+
+        protected doSetValue(value: string, silent?: boolean) {
+            if (!this.loader.isLoaded()) {
+                if (RichComboBox.debug) {
+                    console.debug(this.toString() + '.doSetValue: loader is not loaded, saving temp value = ' + value);
+                }
+                this.tempValue = value;
+            }
+            this.doWhenLoaded(() => {
+                if (this.tempValue) {
+                    if (RichComboBox.debug) {
+                        console.debug(this.toString() + '.doSetValue: clearing temp value = ' + this.tempValue);
+                    }
+                    delete this.tempValue;
+                }
+                super.doSetValue(value, silent);
+            }, value);
+        }
+
+        protected doGetValue(): string {
+            if (!this.loader.isLoaded() && this.tempValue != null) {
+                if (RichComboBox.debug) {
+                    console.debug('RichComboBox: loader is not loaded, returning temp value = ' + this.tempValue);
+                }
+                return this.tempValue;
+            } else {
+                return super.doGetValue();
+            }
+        }
+
+        private doWhenLoaded(callback: Function, value: string) {
+            if (this.loader.isLoaded()) {
+                let optionsMissing = !api.util.StringHelper.isEmpty(value) && this.splitValues(value).some((val) => {
+                        return !this.getOptionByValue(val);
+                    });
+                if (optionsMissing) { // option needs loading
+                    this.loader.preLoad(value).then(() => {
+                        callback();
+                    });
+                } else { // empty option
+                    callback();
+                }
+            } else {
+                if (RichComboBox.debug) {
+                    console.debug(this.toString() + '.doWhenLoaded: waiting to be loaded');
+                }
+                let singleLoadListener = () => {
+                    if (RichComboBox.debug) {
+                        console.debug(this.toString() + '.doWhenLoaded: on loaded');
+                    }
+                    callback();
+                    this.loader.unLoadedData(singleLoadListener);
+                };
+                this.loader.onLoadedData(singleLoadListener);
+                if (!api.util.StringHelper.isEmpty(value) && this.loader.isNotStarted()) {
+                    this.loader.preLoad(value);
+                }
+            }
+        }
+    }
+}
