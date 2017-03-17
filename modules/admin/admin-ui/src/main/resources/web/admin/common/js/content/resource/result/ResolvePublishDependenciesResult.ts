553baed29a04732c2ae72c730bf418167239e434
vbradnitski <vbr@enonic.com>
2017-03-17 11:22:37 +0300

    In the Publishing Dialog replace "Publish" button with "Create issues" #4551

--- modules/admin/admin-ui/src/main/resources/web/admin/common/js/content/resource/result/ResolvePublishDependenciesResult.ts
+++ modules/admin/admin-ui/src/main/resources/web/admin/common/js/content/resource/result/ResolvePublishDependenciesResult.ts
@@ -9,12 +9,15 @@ module api.content.resource.result {
         requestedContents: ContentId[];
         requiredContents: ContentId[];
         containsInvalid: boolean;
+        allPublishable: boolean;
 
-        constructor(dependants: ContentId[], requested: ContentId[], required: ContentId[], containsInvalid: boolean) {
+        constructor(dependants: ContentId[], requested: ContentId[], required: ContentId[], containsInvalid: boolean,
+                    allPublishable: boolean) {
             this.dependentContents = dependants;
             this.requestedContents = requested;
             this.requiredContents = required;
             this.containsInvalid = containsInvalid;
+            this.allPublishable = allPublishable;
         }
 
         getDependants(): ContentId[] {
@@ -33,6 +36,10 @@ module api.content.resource.result {
             return this.containsInvalid;
         }
 
+        isAllPublishable(): boolean {
+            return this.allPublishable;
+        }
+
         static fromJson(json: ResolvePublishContentResultJson): ResolvePublishDependenciesResult {
 
             let dependants: ContentId[] = json.dependentContents
@@ -41,8 +48,9 @@ module api.content.resource.result {
             let requested: ContentId[] = json.requestedContents ? json.requestedContents.map(dependant => new ContentId(dependant.id)) : [];
             let required: ContentId[] = json.requiredContents ? json.requiredContents.map(dependant => new ContentId(dependant.id)) : [];
             let containsInvalid: boolean = json.containsInvalid;
+            let allPublishable: boolean = json.allPublishable;
 
-            return new ResolvePublishDependenciesResult(dependants, requested, required, containsInvalid);
+            return new ResolvePublishDependenciesResult(dependants, requested, required, containsInvalid, allPublishable);
         }
     }
 }
