258d367b56a32b1cf952b4496c8a2c0d82ebe88f
Sten Roger Sandvik <srs@enonic.com>
2017-04-25 23:56:01 +0200

    Removed unused files in live-edit.

--- modules/admin/admin-ui/src/main/resources/web/admin/live-edit/js/_module.ts
+++ modules/admin/admin-ui/src/main/resources/web/admin/live-edit/js/_module.ts
@@ -1,6 +1,5 @@
 // Globals
 
 ///<reference path='../../common/js/_all.d.ts' />
-///<reference path='shared/DomHelper.ts' />
 ///<reference path='LiveEditPage.ts' />
 ///<reference path='main.ts' />
