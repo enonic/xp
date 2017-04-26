47c6f7802438b70f2bb1d52208f6173b2c5e0952
Sten Roger Sandvik <srs@enonic.com>
2017-04-26 14:32:42 +0200

    Fixed some localization typescript problems.

--- modules/admin/admin-ui/src/main/resources/web/admin/common/js/util/Messages.ts
+++ modules/admin/admin-ui/src/main/resources/web/admin/common/js/util/Messages.ts
@@ -1,13 +1,14 @@
 module api.util {
 
     let messages: Object = {};
+    let defaultMessages: Object = {};
 
     export function i18nInit(bundle: Object) {
         messages = bundle;
     }
 
     export function i18n(key: string, ...args: any[]): string {
-        let message = key;
+        let message = defaultMessages[key] || key;
 
         if ((messages != null) && (messages[key] != null)) {
             message = messages[key];
