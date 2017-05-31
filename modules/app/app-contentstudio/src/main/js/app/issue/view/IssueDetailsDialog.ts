0724a131c8205b7f5b8cd7ffdbb5ec33cc821944
Alan Semenov <ase@enonic.com>
2017-05-31 10:11:30 +0200

    Renaming and small refactoring (#5020)

--- modules/app/app-contentstudio/src/main/js/app/issue/view/IssueDetailsDialog.ts
+++ modules/app/app-contentstudio/src/main/js/app/issue/view/IssueDetailsDialog.ts
@@ -12,6 +12,8 @@ import {UpdateIssueRequest} from '../resource/UpdateIssueRequest';
 import {IssueStatus, IssueStatusFormatter} from '../IssueStatus';
 import {IssueStatusSelector} from './IssueStatusSelector';
 import {IssueServerEventsHandler} from '../event/IssueServerEventsHandler';
+import {PublishRequest} from '../PublishRequest';
+import {PublishRequestItem} from '../PublishRequestItem';
 import AEl = api.dom.AEl;
 import DialogButton = api.ui.dialog.DialogButton;
 import Checkbox = api.ui.Checkbox;
@@ -30,9 +32,7 @@ import PEl = api.dom.PEl;
 import SpanEl = api.dom.SpanEl;
 import DivEl = api.dom.DivEl;
 import RequestError = api.rest.RequestError;
-import {PublishRequest} from "../PublishRequest";
 import ObjectHelper = api.ObjectHelper;
-import {PublishRequestItem} from "../PublishRequestItem";
 
 export class IssueDetailsDialog extends SchedulableDialog {
 
@@ -163,7 +163,7 @@ export class IssueDetailsDialog extends SchedulableDialog {
 
             const publishRequest = PublishRequest
                 .create(this.issue.getPublishRequest())
-                .setPublishRequestItems(this.getExistPublishItems())
+                .setPublishRequestItems(this.getExistingPublishItems())
                 .build();
 
             new UpdateIssueRequest(this.issue.getId())
@@ -181,9 +181,10 @@ export class IssueDetailsDialog extends SchedulableDialog {
         this.toggleControlsAccordingToStatus(this.issue.getIssueStatus());
     }
 
-    private getExistPublishItems(): PublishRequestItem[] {
+    private getExistingPublishItems(): PublishRequestItem[] {
+        let itemIds = this.getItemList().getItemsIds();
         return this.issue.getPublishRequest().getItems().filter(publishRequestItem =>
-            ObjectHelper.contains(this.getItemList().getItemsIds(), publishRequestItem.getId()));
+            ObjectHelper.contains(itemIds, publishRequestItem.getId()));
     }
 
     private makeStatusInfo(): DetailsDialogSubTitle {
