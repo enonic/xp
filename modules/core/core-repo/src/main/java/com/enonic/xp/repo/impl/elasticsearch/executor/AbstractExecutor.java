cd6e22f1154165d0021429672da2c5ef19f4e819
runarmyklebust <rmy@enonic.com>
2017-06-22 13:52:19 +0200

    Multi-repository search #4892
    Some more cleanup

--- modules/core/core-repo/src/main/java/com/enonic/xp/repo/impl/elasticsearch/executor/AbstractExecutor.java
+++ modules/core/core-repo/src/main/java/com/enonic/xp/repo/impl/elasticsearch/executor/AbstractExecutor.java
@@ -67,7 +67,6 @@ abstract class AbstractExecutor
         }
     }
 
-
     private String createQueryString( final SearchRequestBuilder searchRequestBuilder )
     {
         final String queryAsString = searchRequestBuilder.toString();
