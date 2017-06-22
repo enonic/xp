6ce49ea1d09e06b54ac3a007d9845a060ca288ae
edloidas <edloidas@gmail.com>
2017-06-22 18:23:31 +0300

    ContentWizard panel - publish menu on the toolbar should be enabled, when the content is 'New' or 'Offline' #5151
    Refactored Wizard publish actions.

--- modules/app/app-contentstudio/src/main/js/app/wizard/action/CreateIssueAction.ts
+++ modules/app/app-contentstudio/src/main/js/app/wizard/action/CreateIssueAction.ts
@@ -1,38 +1,16 @@
 import '../../../api.ts';
+import {BasePublishAction} from './BasePublishAction';
 import {ContentWizardPanel} from '../ContentWizardPanel';
 import {CreateIssuePromptEvent} from '../../browse/CreateIssuePromptEvent';
-import ContentSummaryAndCompareStatus = api.content.ContentSummaryAndCompareStatus;
+
 import i18n = api.util.i18n;
 
-export class CreateIssueAction extends api.ui.Action {
-
-    private wizard: ContentWizardPanel;
-
+export class CreateIssueAction extends BasePublishAction {
     constructor(wizard: ContentWizardPanel) {
-        super(i18n('action.createIssueMore'));
-
-        this.wizard = wizard;
-
-        this.onExecuted(() => {
-            if (this.wizard.hasUnsavedChanges()) {
-                this.setEnabled(false);
-                this.wizard.saveChanges().then((content) => {
-                    if (content) {
-                        this.fireContentCreateIssuePromptEvent();
-                    }
-                }).catch((reason: any) => {
-                    api.DefaultErrorHandler.handle(reason);
-                }).finally(() => this.setEnabled(true)).done();
-            } else {
-                this.fireContentCreateIssuePromptEvent();
-            }
-        });
+        super({wizard, label: i18n('action.createIssueMore')});
     }
 
-    private fireContentCreateIssuePromptEvent(): void {
-        const content = ContentSummaryAndCompareStatus.fromContentSummary(this.wizard.getPersistedItem());
-        content.setCompareStatus(this.wizard.getCompareStatus()).
-            setPublishStatus(this.wizard.getPublishStatus());
-        new CreateIssuePromptEvent([content]).fire();
+    protected createPromptEvent(summary: api.content.ContentSummaryAndCompareStatus[]): void {
+        new CreateIssuePromptEvent(summary).fire();
     }
 }
--- modules/app/app-contentstudio/src/main/js/app/wizard/action/UnpublishAction.ts
+++ modules/app/app-contentstudio/src/main/js/app/wizard/action/UnpublishAction.ts
@@ -1,38 +1,17 @@
 import '../../../api.ts';
+import {BasePublishAction} from './BasePublishAction';
 import {ContentWizardPanel} from '../ContentWizardPanel';
 import {ContentUnpublishPromptEvent} from '../../browse/ContentUnpublishPromptEvent';
-import ContentSummaryAndCompareStatus = api.content.ContentSummaryAndCompareStatus;
+
 import i18n = api.util.i18n;
 
-export class UnpublishAction extends api.ui.Action {
-
-    private wizard: ContentWizardPanel;
-
+export class UnpublishAction extends BasePublishAction {
     constructor(wizard: ContentWizardPanel) {
-        super(i18n('action.unpublish'));
-
-        this.wizard = wizard;
-
-        this.onExecuted(() => {
-            if (this.wizard.hasUnsavedChanges()) {
-                this.setEnabled(false);
-                this.wizard.saveChanges().then((content) => {
-                    if (content) {
-                        this.fireContentUnpublishPromptEvent();
-                    }
-                }).catch((reason: any) => {
-                    api.DefaultErrorHandler.handle(reason);
-                }).finally(() => this.setEnabled(true)).done();
-            } else {
-                this.fireContentUnpublishPromptEvent();
-            }
-        });
+        super({wizard, label: i18n('action.unpublish'), omitCanPublishCheck: true});
     }
 
-    private fireContentUnpublishPromptEvent(): void {
-        let content = ContentSummaryAndCompareStatus.fromContentSummary(this.wizard.getPersistedItem());
-        content.setCompareStatus(this.wizard.getCompareStatus()).
-            setPublishStatus(this.wizard.getPublishStatus());
-        new ContentUnpublishPromptEvent([content]).fire();
+    protected createPromptEvent(summary: api.content.ContentSummaryAndCompareStatus[]): void {
+        new ContentUnpublishPromptEvent(summary).fire();
     }
 }
+
