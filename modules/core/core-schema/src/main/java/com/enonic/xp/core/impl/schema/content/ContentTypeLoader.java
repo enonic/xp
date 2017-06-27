8e24ed6435ef41f60005de1fe89d725a21302f91
bradnitski <vbr@enonic.com>
2017-06-27 11:20:37 +0300

    Switch content selector to default tree-structure #4933

--- modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/content/ContentTypeLoader.java
+++ modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/content/ContentTypeLoader.java
@@ -22,9 +22,6 @@ final class ContentTypeLoader
     protected ContentType load( final ContentTypeName name, final Resource resource )
     {
         final ContentType.Builder builder = ContentType.create();
-        if(name.toString().equals( "com.enonic.app.features:selector" )) {
-            parseXml( resource, builder );
-        }
         parseXml( resource, builder );
 
         final Instant modifiedTime = Instant.ofEpochMilli( resource.getTimestamp() );
