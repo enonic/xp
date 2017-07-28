0876ee100555582fc286633813dd89d90551f8f6
vbradnitski <vbr@enonic.com>
2017-07-28 12:32:26 +0300

    Implement min and max config for Long input type #5333

---
+++ modules/admin/admin-ui/src/main/resources/web/admin/common/js/form/inputtype/number/NumberInputType.ts
@@ -0,0 +1,82 @@
+module api.content.form.inputtype.double {
+
+    import BaseInputTypeNotManagingAdd = api.form.inputtype.support.BaseInputTypeNotManagingAdd;
+    import NumberHelper = api.util.NumberHelper;
+    import i18n = api.util.i18n;
+
+    export abstract class NumberInputType extends BaseInputTypeNotManagingAdd<number> {
+
+        private min: number = null;
+
+        private max: number = null;
+
+        constructor(config: api.form.inputtype.InputTypeViewContext) {
+            super(config);
+            this.readConfig(config);
+        }
+
+        protected readConfig(config: api.form.inputtype.InputTypeViewContext): void {
+            this.min = this.getConfigProperty(config, 'min');
+            this.max = this.getConfigProperty(config, 'max');
+        }
+
+        private getConfigProperty(config: api.form.inputtype.InputTypeViewContext, propertyName: string) {
+            const configProperty = config.inputConfig[propertyName] ? config.inputConfig[propertyName][0] : {};
+            return NumberHelper.toNumber(configProperty['value']);
+        }
+
+        protected isValid(value: string, recording ?: api.form.inputtype.InputValidationRecording): boolean {
+
+            if (api.util.StringHelper.isEmpty(value)) {
+                return true;
+            }
+
+            if(!api.util.NumberHelper.isWholeNumber(+value)) {
+                return false;
+            }
+
+            if (api.util.NumberHelper.isNumber(+value)) {
+                if (!this.isValidMin(NumberHelper.toNumber(value))) {
+                    if (recording) {
+                        recording.setAdditionalValidationRecord(
+                            api.form.AdditionalValidationRecord.create().setOverwriteDefault(true).setMessage(
+                                i18n('field.value.breaks.min', this.min)).build());
+                    }
+
+                    return false;
+                }
+
+                if (!this.isValidMax(NumberHelper.toNumber(value))) {
+                    if (recording) {
+                        recording.setAdditionalValidationRecord(
+                            api.form.AdditionalValidationRecord.create().setOverwriteDefault(true).setMessage(
+                                i18n('field.value.breaks.max', this.max)).build());
+                    }
+
+                    return false;
+                }
+            } else {
+                return false;
+            }
+            return true;
+        }
+
+        private isValidMin(value: number) {
+            if (NumberHelper.isNumber(value)) {
+                if (NumberHelper.isNumber(this.min)) {
+                    return value >= this.min;
+                }
+            }
+            return true;
+        }
+
+        private isValidMax(value: number) {
+            if (NumberHelper.isNumber(value)) {
+                if (NumberHelper.isNumber(this.max)) {
+                    return value <= this.max;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ modules/admin/admin-ui/src/main/resources/web/admin/common/js/form/inputtype/number/long/Long.ts
@@ -0,0 +1,73 @@
+module api.content.form.inputtype.long {
+
+    import NumberInputType = api.content.form.inputtype.double.NumberInputType;
+    import ValueType = api.data.ValueType;
+    import ValueTypes = api.data.ValueTypes;
+    import Value = api.data.Value;
+    import Property = api.data.Property;
+
+    export class Long extends NumberInputType {
+
+        constructor(config: api.form.inputtype.InputTypeViewContext) {
+            super(config);
+        }
+
+        getValueType(): ValueType {
+            return ValueTypes.LONG;
+        }
+
+        newInitialValue(): Value {
+            return super.newInitialValue() || ValueTypes.LONG.newNullValue();
+        }
+
+        createInputOccurrenceElement(index: number, property: Property): api.dom.Element {
+            if (!ValueTypes.LONG.equals(property.getType())) {
+                property.convertValueType(ValueTypes.LONG);
+            }
+
+            let inputEl = api.ui.text.TextInput.middle(undefined, this.getPropertyValue(property));
+            inputEl.setName(this.getInput().getName() + '-' + property.getIndex());
+
+            inputEl.onValueChanged((event: api.ValueChangedEvent) => {
+
+                let isValid = this.isValid(event.getNewValue());
+                let value = isValid ? ValueTypes.LONG.newValue(event.getNewValue()) : this.newInitialValue();
+
+                this.notifyOccurrenceValueChanged(inputEl, value);
+                inputEl.updateValidationStatusOnUserInput(isValid);
+            });
+
+            property.onPropertyValueChanged((event: api.data.PropertyValueChangedEvent) => {
+                this.updateInputOccurrenceElement(inputEl, property, true);
+            });
+
+            return inputEl;
+        }
+
+        updateInputOccurrenceElement(occurrence: api.dom.Element, property: api.data.Property, unchangedOnly: boolean) {
+            let input = <api.ui.text.TextInput> occurrence;
+
+            if (!unchangedOnly || !input.isDirty()) {
+                input.setValue(this.getPropertyValue(property));
+            }
+        }
+
+        resetInputOccurrenceElement(occurrence: api.dom.Element) {
+            let input = <api.ui.text.TextInput> occurrence;
+
+            input.resetBaseValues();
+        }
+
+        valueBreaksRequiredContract(value: Value): boolean {
+            return value.isNull() || !value.getType().equals(ValueTypes.LONG);
+        }
+
+        hasInputElementValidUserInput(inputElement: api.dom.Element, recording ?: api.form.inputtype.InputValidationRecording) {
+            let value = <api.ui.text.TextInput>inputElement;
+
+            return this.isValid(value.getValue(), recording);
+        }
+    }
+
+    api.form.inputtype.InputTypeManager.register(new api.Class('Long', Long));
+}
