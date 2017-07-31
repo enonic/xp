31f6cc86ebfcdadb89c12e7ca147391caa807fb8
vbradnitski <vbr@enonic.com>
2017-07-31 15:28:07 +0300

    Implement max-length config for TextLine input type #5335

--- modules/admin/admin-ui/src/main/resources/web/admin/common/js/form/inputtype/text/TextLine.ts
+++ modules/admin/admin-ui/src/main/resources/web/admin/common/js/form/inputtype/text/TextLine.ts
@@ -6,11 +6,14 @@ module api.form.inputtype.text {
     import ValueType = api.data.ValueType;
     import ValueTypes = api.data.ValueTypes;
     import i18n = api.util.i18n;
+    import NumberHelper = api.util.NumberHelper;
+    import DivEl = api.dom.DivEl;
 
     export class TextLine extends support.BaseInputTypeNotManagingAdd<string> {
 
-        private regexpStr: string;
         private regexp: RegExp;
+
+        private maxLength: number;
 
         constructor(config: api.form.inputtype.InputTypeViewContext) {
             super(config);
@@ -18,9 +21,13 @@ module api.form.inputtype.text {
         }
 
         private readConfig(inputConfig: { [element: string]: { [name: string]: string }[]; }): void {
-            let regexpConfig = inputConfig && inputConfig['regexp'] && inputConfig['regexp'][0];
-            let regexp = regexpConfig && regexpConfig['value'];
-            this.regexpStr = regexp || null;
+            const regexpConfig = inputConfig['regexp'] ? inputConfig['regexp'][0] : {};
+            const regexp = regexpConfig ? regexpConfig['value'] : '';
+            this.regexp = new RegExp(regexp);
+
+            const maxLengthConfig = inputConfig['max-length'] ? inputConfig['max-length'][0] : {};
+            const maxLength = NumberHelper.toNumber(maxLengthConfig['value']);
+            this.maxLength = maxLength > 0 ? maxLength : null;
         }
 
         getValueType(): ValueType {
@@ -44,8 +51,30 @@ module api.form.inputtype.text {
                 let value = isValid ? ValueTypes.STRING.newValue(event.getNewValue()) : this.newInitialValue();
                 this.notifyOccurrenceValueChanged(inputEl, value);
                 inputEl.updateValidationStatusOnUserInput(isValid);
+
+                if (NumberHelper.isNumber(this.maxLength)) {
+                    const lengthCounter = inputEl.getNextElement();
+                    if (lengthCounter.hasClass('length-counter')) {
+                        this.updateLengthCounterValue(lengthCounter, inputEl.getValue());
+                    }
+                }
             });
+
+            inputEl.onRendered(() => {
+                if (NumberHelper.isNumber(this.maxLength)) {
+
+                    const lengthCounter = new DivEl('length-counter');
+                    this.updateLengthCounterValue(lengthCounter, inputEl.getValue());
+
+                    lengthCounter.insertAfterEl(inputEl);
+                }
+            });
+
             return inputEl;
+        }
+
+        private updateLengthCounterValue(lengthCounter: DivEl, newValue: string ) {
+            lengthCounter.setHtml(`${this.maxLength - newValue.length}`);
         }
 
         updateInputOccurrenceElement(occurrence: api.dom.Element, property: api.data.Property, unchangedOnly: boolean) {
@@ -71,27 +100,44 @@ module api.form.inputtype.text {
                    api.util.StringHelper.isBlank(value.getString());
         }
 
-        hasInputElementValidUserInput(inputElement: api.dom.Element) {
+        hasInputElementValidUserInput(inputElement: api.dom.Element, recording?: api.form.inputtype.InputValidationRecording) {
             let textInput = <api.ui.text.TextInput>inputElement;
-            return this.isValid(textInput.getValue(), textInput, true);
+            return this.isValid(textInput.getValue(), textInput, true, recording);
         }
 
-        private isValid(value: string, textInput: api.ui.text.TextInput, silent: boolean = false): boolean {
+        private isValid(value: string, textInput: api.ui.text.TextInput, silent: boolean = false,
+                        recording?: api.form.inputtype.InputValidationRecording): boolean {
             let parent = textInput.getParentElement();
-            if (!this.regexpStr || api.util.StringHelper.isEmpty(value)) {
+
+            if (api.util.StringHelper.isEmpty(value)) {
                 parent.removeClass('valid-regexp invalid-regexp');
                 return true;
             }
-            if (!this.regexp) {
-                this.regexp = new RegExp(this.regexpStr);
+
+            const regexpValid = this.regexp.test(value);
+
+            const lengthValid = this.isValidMaxLength(value);
+
+            if (!lengthValid) {
+                if (recording) {
+                    recording.setAdditionalValidationRecord(
+                        api.form.AdditionalValidationRecord.create().setOverwriteDefault(true).setMessage(
+                            i18n('field.value.breaks.maxlength', this.maxLength)).build());
+                }
+
             }
-            let valid = this.regexp.test(value);
+
             if (!silent) {
-                parent.toggleClass('valid-regexp', valid);
-                parent.toggleClass('invalid-regexp', !valid);
-                parent.getEl().setAttribute('data-regex-status', i18n(`field.${valid ? 'valid' : 'invalid'}`));
+                parent.toggleClass('valid-regexp', regexpValid);
+                parent.toggleClass('invalid-regexp', !regexpValid);
+                parent.getEl().setAttribute('data-regex-status', i18n(`field.${regexpValid ? 'valid' : 'invalid'}`));
             }
-            return valid;
+
+            return regexpValid && lengthValid;
+        }
+
+        private isValidMaxLength(value: string): boolean {
+            return NumberHelper.isNumber(this.maxLength) ? value.length <= this.maxLength : true;
         }
 
         static getName(): api.form.InputTypeName {
