e8faa4ca64f60782cc21d057080b9557889ed205
vbradnitski <vbr@enonic.com>
2017-08-07 10:57:45 +0300

    Implement min/max server validation for Double and Long Input Types #5399

--- modules/core/core-api/src/main/java/com/enonic/xp/inputtype/DoubleType.java
+++ modules/core/core-api/src/main/java/com/enonic/xp/inputtype/DoubleType.java
@@ -1,6 +1,8 @@
 package com.enonic.xp.inputtype;
 
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.enonic.xp.convert.ConvertException;
 import com.enonic.xp.data.Property;
@@ -10,9 +12,11 @@ import com.enonic.xp.data.ValueTypes;
 import com.enonic.xp.form.Input;
 
 final class DoubleType
-    extends NumberType
+    extends InputTypeBase
 {
     public final static DoubleType INSTANCE = new DoubleType();
+
+    private final static Logger LOG = LoggerFactory.getLogger( DoubleType.class );
 
     private DoubleType()
     {
@@ -40,6 +44,44 @@ final class DoubleType
     public void validate( final Property property, final InputTypeConfig config )
     {
         validateType( property, ValueTypes.DOUBLE );
-        super.validate( property, config );
+        validateMin( property, config );
+        validateMax( property, config );
+    }
+
+    private void validateMin( final Property property, final InputTypeConfig config )
+    {
+        try
+        {
+            final Long min = config.getValue( "min", Long.class );
+            final Long value = property.getLong();
+
+            if ( min != null && value != null )
+            {
+                validateValue( property, value >= min, "The value cannot be less than " + min.toString() );
+            }
+
+        }
+        catch ( ConvertException e )
+        {
+            LOG.warn("Cannot convert 'min' config to Double", e );
+        }
+    }
+
+    private void validateMax( final Property property, final InputTypeConfig config )
+    {
+        try
+        {
+            final Long max = config.getValue( "max", Long.class );
+            final Long value = property.getLong();
+
+            if ( max != null && value != null )
+            {
+                validateValue( property, value <= max, "The value cannot be greater than " + max.toString() );
+            }
+        }
+        catch ( ConvertException e )
+        {
+            LOG.warn("Cannot convert 'max' config to Double", e );
+        }
     }
 }
