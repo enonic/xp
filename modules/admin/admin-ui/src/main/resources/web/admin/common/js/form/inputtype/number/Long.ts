3269711d2fc8abb8ce4da5d988bc5b3aa6956b23
Alan Semenov <ase@enonic.com>
2017-08-07 11:05:54 +0200

    Implement min and max config for Long input type - small refactoring (#5333)

--- modules/admin/admin-ui/src/main/resources/web/admin/common/js/form/inputtype/number/Long.ts
+++ modules/admin/admin-ui/src/main/resources/web/admin/common/js/form/inputtype/number/Long.ts
@@ -1,44 +1,49 @@
-module api.content.form.inputtype.double {
+module api.content.form.inputtype.number.long {
 
     import ValueType = api.data.ValueType;
     import ValueTypes = api.data.ValueTypes;
     import Value = api.data.Value;
     import Property = api.data.Property;
 
-    export class Double extends NumberInputType {
+    export class Long extends NumberInputType {
 
         constructor(config: api.form.inputtype.InputTypeViewContext) {
             super(config);
         }
 
         getValueType(): ValueType {
-            return ValueTypes.DOUBLE;
+            return ValueTypes.LONG;
         }
 
         newInitialValue(): Value {
-            return super.newInitialValue() || ValueTypes.DOUBLE.newNullValue();
+            return super.newInitialValue() || ValueTypes.LONG.newNullValue();
         }
 
         createInputOccurrenceElement(index: number, property: Property): api.dom.Element {
-            if (!ValueTypes.DOUBLE.equals(property.getType())) {
-                property.convertValueType(ValueTypes.DOUBLE);
+            if (!ValueTypes.LONG.equals(property.getType())) {
+                property.convertValueType(ValueTypes.LONG);
             }
 
             let inputEl = api.ui.text.TextInput.middle(undefined, this.getPropertyValue(property));
             inputEl.setName(this.getInput().getName() + '-' + property.getIndex());
 
             inputEl.onValueChanged((event: api.ValueChangedEvent) => {
+
                 let isValid = this.isValid(event.getNewValue());
-                let value = isValid ? ValueTypes.DOUBLE.newValue(event.getNewValue()) : this.newInitialValue();
+                let value = isValid ? ValueTypes.LONG.newValue(event.getNewValue()) : this.newInitialValue();
 
                 this.notifyOccurrenceValueChanged(inputEl, value);
                 inputEl.updateValidationStatusOnUserInput(isValid);
             });
 
+            property.onPropertyValueChanged((event: api.data.PropertyValueChangedEvent) => {
+                this.updateInputOccurrenceElement(inputEl, property, true);
+            });
+
             return inputEl;
         }
 
-        updateInputOccurrenceElement(occurrence: api.dom.Element, property: api.data.Property, unchangedOnly ?: boolean) {
+        updateInputOccurrenceElement(occurrence: api.dom.Element, property: api.data.Property, unchangedOnly: boolean) {
             let input = <api.ui.text.TextInput> occurrence;
 
             if (!unchangedOnly || !input.isDirty()) {
@@ -53,7 +58,7 @@ module api.content.form.inputtype.double {
         }
 
         valueBreaksRequiredContract(value: Value): boolean {
-            return value.isNull() || !value.getType().equals(ValueTypes.DOUBLE);
+            return value.isNull() || !value.getType().equals(ValueTypes.LONG);
         }
 
         hasInputElementValidUserInput(inputElement: api.dom.Element, recording ?: api.form.inputtype.InputValidationRecording) {
@@ -63,5 +68,5 @@ module api.content.form.inputtype.double {
         }
     }
 
-    api.form.inputtype.InputTypeManager.register(new api.Class('Double', Double));
+    api.form.inputtype.InputTypeManager.register(new api.Class('Long', Long));
 }
