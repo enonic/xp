81db8285b1d156c5a9fb6e40a0340fa51098acfc
Alan Semenov <ase@enonic.com>
2017-08-22 14:04:28 +0200

    Localized dashboard icons and About XP modal dialog (#5475)

--- modules/admin/admin-ui/src/main/resources/admin/tools/home/home.js
+++ modules/admin/admin-ui/src/main/resources/admin/tools/home/home.js
@@ -2,6 +2,7 @@ var auth = require('/lib/xp/auth');
 var mustache = require('/lib/xp/mustache');
 var portal = require('/lib/xp/portal');
 var admin = require('/lib/xp/admin');
+var i18n = require('/lib/xp/i18n');
 
 function handleGet(req) {
     var uriScriptHelper = Java.type("com.enonic.xp.admin.ui.tool.UriScriptHelper");
@@ -26,20 +27,60 @@ function handleGet(req) {
         docLinkPrefix += versionParts[0] + '.' + versionParts[1];
     }
 
+    var locale = admin.getLocale();
+
+    var dashboardIcons = [{
+        src: infoIconUrl,
+        cls: 'xp-about',
+        caption: i18n.localize({
+            key: 'home.dashboard.about',
+            bundles: ['admin/i18n/common'],
+            locale: locale
+        })
+    }, {
+        src: docsIconUrl,
+        cls: '',
+        caption: 'Docs',
+        link: docLinkPrefix + '/'
+    }, {
+        src: forumIconUrl,
+        cls: '',
+        caption: 'Discuss',
+        link: 'https://discuss.enonic.com/'
+    }, {
+        src: marketIconUrl,
+        cls: '',
+        caption: i18n.localize({
+            key: 'home.dashboard.market',
+            bundles: ['admin/i18n/common'],
+            locale: locale
+        }),
+        link: 'https://market.enonic.com/'
+    }];
+
+    var tourEnabled = !(app.config.tourDisabled || false);
+    if (tourEnabled) {
+        dashboardIcons.splice(0, 0, {
+            src: busIconUrl,
+            cls: 'xp-tour',
+            caption: i18n.localize({
+                key: 'home.dashboard.tour',
+                bundles: ['admin/i18n/common'],
+                locale: locale
+            })
+        });
+    }
+
     var params = {
         adminUrl: adminUrl,
         assetsUri: assetsUri,
         backgroundUri: backgroundUri,
-        busIconUrl: busIconUrl,
-        infoIconUrl: infoIconUrl,
-        docsIconUrl: docsIconUrl,
-        forumIconUrl: forumIconUrl,
-        marketIconUrl: marketIconUrl,
+        dashboardIcons: dashboardIcons,
         baseUri: '',
         xpVersion: app.version,
         docLinkPrefix: docLinkPrefix,
         messages: admin.getPhrases(),
-        tourEnabled: !(app.config.tourDisabled || false)
+        tourEnabled: tourEnabled
     };
 
     return {
