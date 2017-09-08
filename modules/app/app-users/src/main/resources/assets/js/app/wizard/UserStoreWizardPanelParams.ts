09090e30c12623b9fd1dfb60f87639dc20d16c4f
Sten Roger Sandvik <srs@enonic.com>
2017-09-08 19:45:31 +0200

    Remove app-users in XP project (#5610).

--- modules/app/app-users/src/main/resources/assets/js/app/wizard/UserStoreWizardPanelParams.ts
+++
@@ -1,8 +0,0 @@
-import '../../api.ts';
-import {UserItemWizardPanelParams} from './UserItemWizardPanelParams';
-import UserStore = api.security.UserStore;
-import UserStoreKey = api.security.UserStoreKey;
-
-export class UserStoreWizardPanelParams extends UserItemWizardPanelParams<UserStore> {
-
-}
