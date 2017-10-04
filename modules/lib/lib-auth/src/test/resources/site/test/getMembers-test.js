9ac482667402705d1dd732821fd41937576080ae
vbradnitski <vbr@enonic.com>
2017-10-04 16:27:21 +0300

    App users - Freeze on large amount of users #5662

--- modules/lib/lib-auth/src/test/resources/site/test/getMembers-test.js
+++ modules/lib/lib-auth/src/test/resources/site/test/getMembers-test.js
@@ -3,7 +3,7 @@ var auth = require('/lib/xp/auth.js');
 
 exports.getMembers = function () {
 
-    var result = auth.getMembers('group:system:group-a');
+    var result = auth.getMembers({key:'group:system:group-a'});
 
     var expectedJson = [
         {
@@ -32,9 +32,51 @@ exports.getMembers = function () {
 
 };
 
+exports.getMembersWithFrom = function () {
+
+    var result = auth.getMembers({key:'group:system:group-a', from:1});
+
+    var expectedJson = [
+        {
+            "type": "user",
+            "key": "user:enonic:user2",
+            "displayName": "User 2",
+            "modifiedTime": "1970-01-01T00:00:00Z",
+            "disabled": false,
+            "email": "user2@enonic.com",
+            "login": "user2",
+            "userStore": "enonic"
+        }
+    ];
+
+    t.assertJsonEquals(expectedJson, result);
+
+};
+
+exports.getMembersWithSize = function () {
+
+    var result = auth.getMembers({key:'group:system:group-a', size:1});
+
+    var expectedJson = [
+        {
+            "type": "user",
+            "key": "user:enonic:user1",
+            "displayName": "User 1",
+            "modifiedTime": "1970-01-01T00:00:00Z",
+            "disabled": false,
+            "email": "user1@enonic.com",
+            "login": "user1",
+            "userStore": "enonic"
+        }
+    ];
+
+    t.assertJsonEquals(expectedJson, result);
+
+};
+
 exports.getNoMembers = function () {
 
-    var result = auth.getMembers('group:system:group-a');
+    var result = auth.getMembers({key:'group:system:group-a'});
 
     var expectedJson = [];
 
