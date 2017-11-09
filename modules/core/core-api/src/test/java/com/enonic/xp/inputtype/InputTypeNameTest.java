dff98cbc59f995e1e1755046f3dc754e4dab1521
runarmyklebust <rmy@enonic.com>
2017-11-09 15:09:08 +0100

    InputType handled case-sensitive in class, but not in usage (#5772)

--- modules/core/core-api/src/test/java/com/enonic/xp/inputtype/InputTypeNameTest.java
+++ modules/core/core-api/src/test/java/com/enonic/xp/inputtype/InputTypeNameTest.java
@@ -11,4 +11,33 @@ public class InputTypeNameTest
     {
         assertEquals( "TextArea", InputTypeName.from( "TextArea" ).toString() );
     }
+
+    @Test
+    public void null_entry()
+        throws Exception
+    {
+        assertFalse( InputTypeName.from( "fisk" ).equals( null ) );
+    }
+
+    @Test
+    public void null_value()
+        throws Exception
+    {
+        assertFalse( InputTypeName.from( "fisk" ).equals( InputTypeName.from( null ) ) );
+    }
+
+
+    @Test
+    public void null_value_2()
+        throws Exception
+    {
+        assertFalse( InputTypeName.from( null ).equals( InputTypeName.from( "fisk" ) ) );
+    }
+
+    @Test
+    public void case_insensitive()
+        throws Exception
+    {
+        assertTrue( InputTypeName.from( "HtmlArea" ).equals( InputTypeName.from( "htmlarea" ) ) );
+    }
 }
