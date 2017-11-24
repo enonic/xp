4c4ddd58c4e48942d12ca62a51bfeea0ba78b792
bradnitski <vbr@enonic.com>
2017-11-24 19:19:14 +0300

    Create a new REST API with descriptors info #5810

---
+++ modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/rest/resource/application/json/ApplicationInfoJson.java
@@ -0,0 +1,89 @@
+package com.enonic.xp.admin.impl.rest.resource.application.json;
+
+import com.enonic.xp.admin.impl.json.content.page.PageDescriptorListJson;
+import com.enonic.xp.admin.impl.json.content.page.region.LayoutDescriptorsJson;
+import com.enonic.xp.admin.impl.json.content.page.region.PartDescriptorsJson;
+import com.enonic.xp.admin.impl.json.schema.content.ContentTypeSummaryListJson;
+import com.enonic.xp.admin.impl.json.schema.relationship.RelationshipTypeListJson;
+import com.enonic.xp.admin.impl.rest.resource.macro.json.MacrosJson;
+
+public class ApplicationInfoJson
+{
+    private ContentTypeSummaryListJson contentTypesJson;
+
+    private PageDescriptorListJson pagesJson;
+
+    private PartDescriptorsJson partsJson;
+
+    private LayoutDescriptorsJson layoutsJson;
+
+    private RelationshipTypeListJson relationsJson;
+
+    private MacrosJson macrosJson;
+
+    public ContentTypeSummaryListJson getContentTypesJson()
+    {
+        return contentTypesJson;
+    }
+
+    public PageDescriptorListJson getPagesJson()
+    {
+        return pagesJson;
+    }
+
+    public PartDescriptorsJson getPartsJson()
+    {
+        return partsJson;
+    }
+
+    public LayoutDescriptorsJson getLayoutsJson()
+    {
+        return layoutsJson;
+    }
+
+    public RelationshipTypeListJson getRelationsJson()
+    {
+        return relationsJson;
+    }
+
+    public MacrosJson getMacrosJson()
+    {
+        return macrosJson;
+    }
+
+    public ApplicationInfoJson setContentTypesJson( final ContentTypeSummaryListJson contentTypesJson )
+    {
+        this.contentTypesJson = contentTypesJson;
+        return this;
+    }
+
+    public ApplicationInfoJson setPagesJson( final PageDescriptorListJson pagesJson )
+    {
+        this.pagesJson = pagesJson;
+        return this;
+    }
+
+    public ApplicationInfoJson setPartsJson( final PartDescriptorsJson partsJson )
+    {
+        this.partsJson = partsJson;
+        return this;
+    }
+
+    public ApplicationInfoJson setLayoutsJson( final LayoutDescriptorsJson layoutsJson )
+    {
+        this.layoutsJson = layoutsJson;
+        return this;
+    }
+
+    public ApplicationInfoJson setRelationsJson( final RelationshipTypeListJson relationsJson )
+    {
+        this.relationsJson = relationsJson;
+        return this;
+    }
+
+    public ApplicationInfoJson setMacrosJson( final MacrosJson macrosJson )
+    {
+        this.macrosJson = macrosJson;
+        return this;
+    }
+}
