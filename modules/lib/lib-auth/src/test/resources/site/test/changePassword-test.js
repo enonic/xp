d580e02eec0043a8f3badeb9ce03ebaf8d3ce5df
edloidas <edloidas@gmail.com>
2017-12-18 15:11:02 +0300

    UserWizard - trim spaces in a password field #5894
    Updated tests.

--- modules/lib/lib-auth/src/test/resources/site/test/changePassword-test.js
+++ modules/lib/lib-auth/src/test/resources/site/test/changePassword-test.js
@@ -3,6 +3,6 @@ var auth = require('/lib/xp/auth.js');
 exports.changePassword = function () {
     auth.changePassword({
         userKey: "user:myUserStore:userId",
-        password: "test-password"
+        password: " test-password-   without- spaces  "
     });
 };
