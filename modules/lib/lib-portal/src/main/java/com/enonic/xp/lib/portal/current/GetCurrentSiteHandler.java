6f38d4668fa92bf8032308909b7c83fc459f9deb
Glenn Ricaud <gri@enonic.com>
2017-12-18 15:42:29 +0100

    Portal lib - Get current content/site/siteConfig - Use contentPath as alternative (#5854)

--- modules/lib/lib-portal/src/main/java/com/enonic/xp/lib/portal/current/GetCurrentSiteHandler.java
+++ modules/lib/lib-portal/src/main/java/com/enonic/xp/lib/portal/current/GetCurrentSiteHandler.java
@@ -1,30 +1,19 @@
 package com.enonic.xp.lib.portal.current;
 
 import com.enonic.xp.lib.content.mapper.SiteMapper;
-import com.enonic.xp.portal.PortalRequest;
-import com.enonic.xp.script.bean.BeanContext;
-import com.enonic.xp.script.bean.ScriptBean;
 import com.enonic.xp.site.Site;
 
 public final class GetCurrentSiteHandler
-    implements ScriptBean
+    extends GetCurrentAbstractHandler
 {
-    private PortalRequest request;
-
     public SiteMapper execute()
     {
-        final Site site = this.request.getSite();
+        final Site site = getSite();
         return site != null ? convert( site ) : null;
     }
 
     private SiteMapper convert( final Site site )
     {
         return site == null ? null : new SiteMapper( site );
-    }
-
-    @Override
-    public void initialize( final BeanContext context )
-    {
-        this.request = context.getBinding( PortalRequest.class ).get();
     }
 }
