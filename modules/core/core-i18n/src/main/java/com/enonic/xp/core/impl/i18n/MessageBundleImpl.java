f7285053ccb70ae51d9df7719e0b3bc863af01c2
Alex Rodriguez <aro@enonic.com>
2017-12-18 12:43:19 +0100

    Cache properties in LocaleServiceImpl (#5896)

--- modules/core/core-i18n/src/main/java/com/enonic/xp/core/impl/i18n/MessageBundleImpl.java
+++ modules/core/core-i18n/src/main/java/com/enonic/xp/core/impl/i18n/MessageBundleImpl.java
@@ -8,8 +8,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Maps;
 
