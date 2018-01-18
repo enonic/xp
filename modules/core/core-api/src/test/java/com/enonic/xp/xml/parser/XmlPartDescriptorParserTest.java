c894b49d1bd49b5b108e7e4784ba33628366a98f
bradnitski <vbr@enonic.com>
2018-01-18 17:34:16 +0300

    Support i18n attribute for display-name in part, layout, page and macro #5971
    Part Descriptor

--- modules/core/core-api/src/test/java/com/enonic/xp/xml/parser/XmlPartDescriptorParserTest.java
+++ modules/core/core-api/src/test/java/com/enonic/xp/xml/parser/XmlPartDescriptorParserTest.java
@@ -52,6 +52,7 @@ public class XmlPartDescriptorParserTest
         assertEquals( "myapplication:mypart", result.getKey().toString() );
         assertEquals( "mypart", result.getName() );
         assertEquals( "A Part", result.getDisplayName() );
+        assertEquals( "key.display-name", result.getDisplayName() );
 
         final Form config = result.getConfig();
         assertNotNull( config );
