0be6d8658408bb1369848c75403c1bd2b732e7b9
Glenn Ricaud <gri@enonic.com>
2018-02-08 15:57:40 +0100

    Auth Lib - Delete principal function (#6029)

---
+++ modules/lib/lib-auth/src/main/java/com/enonic/xp/lib/auth/DeletePrincipalHandler.java
@@ -0,0 +1,41 @@
+package com.enonic.xp.lib.auth;
+
+import java.util.function.Supplier;
+
+import com.enonic.xp.script.bean.BeanContext;
+import com.enonic.xp.script.bean.ScriptBean;
+import com.enonic.xp.security.PrincipalKey;
+import com.enonic.xp.security.PrincipalNotFoundException;
+import com.enonic.xp.security.SecurityService;
+
+public final class DeletePrincipalHandler
+    implements ScriptBean
+{
+    private PrincipalKey principalKey;
+
+    private Supplier<SecurityService> securityService;
+
+    public void setPrincipalKey( final String principalKey )
+    {
+        this.principalKey = PrincipalKey.from( principalKey );
+    }
+
+    public boolean deletePrincipal()
+    {
+        try
+        {
+            this.securityService.get().deletePrincipal( this.principalKey );
+            return true;
+        }
+        catch ( PrincipalNotFoundException e )
+        {
+        }
+        return false;
+    }
+
+    @Override
+    public void initialize( final BeanContext context )
+    {
+        this.securityService = context.getService( SecurityService.class );
+    }
+}
---
+++ modules/lib/lib-auth/src/test/resources/site/test/deletePrincipal-test.js
@@ -0,0 +1,12 @@
+var t = require('/lib/xp/testing.js');
+var auth = require('/lib/xp/auth.js');
+
+exports.deleteUser = function () {
+    var result = auth.deletePrincipal('user:myUserStore:userId');
+    t.assertJsonEquals(true, result);
+};
+
+exports.deleteNonExistingUser = function () {
+    var result = auth.deletePrincipal('user:myUserStore:XXX');
+    t.assertEquals(false, result);
+};
