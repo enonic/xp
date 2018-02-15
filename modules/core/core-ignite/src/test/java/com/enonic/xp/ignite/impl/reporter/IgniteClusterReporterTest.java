603255cd161c2aae06b6063fc02eee06c9512725
runarmyklebust <rmy@enonic.com>
2018-02-15 15:09:31 +0100

    Add tests for Ignite-module #6051
     Added test for reporter

--- modules/core/core-ignite/src/test/java/com/enonic/xp/ignite/impl/reporter/IgniteClusterReporterTest.java
+++ modules/core/core-ignite/src/test/java/com/enonic/xp/ignite/impl/reporter/IgniteClusterReporterTest.java
@@ -2,6 +2,7 @@ package com.enonic.xp.ignite.impl.reporter;
 
 import java.util.UUID;
 
+import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCluster;
 import org.apache.ignite.cluster.ClusterNode;
 import org.junit.Before;
@@ -11,10 +12,12 @@ import org.mockito.Mockito;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.Lists;
 
+import com.enonic.xp.ignite.impl.IgniteAdminClientImpl;
 import com.enonic.xp.support.JsonTestHelper;
 
-public class IgniteClusterReportTest
+public class IgniteClusterReporterTest
 {
+    private Ignite ignite;
 
     private IgniteCluster igniteCluster;
 
@@ -22,7 +25,9 @@ public class IgniteClusterReportTest
     public void setUp()
         throws Exception
     {
+        this.ignite = Mockito.mock( Ignite.class );
         this.igniteCluster = Mockito.mock( IgniteCluster.class );
+        Mockito.when( this.ignite.cluster() ).thenReturn( this.igniteCluster );
     }
 
     @Test
@@ -41,10 +46,10 @@ public class IgniteClusterReportTest
         mockNode( node, "11111111-1111-1111-1111-11111111" );
         mockNode( node2, "22222222-2222-2222-2222-22222222" );
 
-        final JsonNode result = IgniteClusterReport.create().
-            cluster( this.igniteCluster ).
-            build().
-            toJson();
+        final IgniteClusterReporter reporter = new IgniteClusterReporter();
+        reporter.setAdminClient( new IgniteAdminClientImpl( this.ignite ) );
+
+        final JsonNode result = reporter.getReport();
 
         assertJson( "report.json", result );
     }
