74766ef18f72821dae55caa6dbf70c0c24ceaa21
Glenn Ricaud <gri@enonic.com>
2018-08-20 13:32:29 +0200

    Incorrect URL generation from endpoints under /admin/tool (#6508)

--- modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/portal/AdminToolPortalHandler.java
+++ modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/portal/AdminToolPortalHandler.java
@@ -4,6 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 import com.enonic.xp.app.ApplicationKey;
 import com.enonic.xp.page.DescriptorKey;
@@ -12,6 +13,8 @@ import com.enonic.xp.portal.RenderMode;
 import com.enonic.xp.portal.handler.BasePortalHandler;
 import com.enonic.xp.web.WebRequest;
 import com.enonic.xp.web.WebResponse;
+import com.enonic.xp.web.exception.ExceptionMapper;
+import com.enonic.xp.web.exception.ExceptionRenderer;
 import com.enonic.xp.web.handler.WebHandler;
 
 @Component(immediate = true, service = WebHandler.class)
@@ -24,7 +27,7 @@ public class AdminToolPortalHandler
 
     public final static DescriptorKey DEFAULT_DESCRIPTOR_KEY = DescriptorKey.from( "com.enonic.xp.app.main:home" );
 
-    public final static Pattern PATTERN = Pattern.compile( "([^/]+)/([^/]+)" );
+    public final static Pattern PATTERN = Pattern.compile( "^([^/^_]+)/([^/]+)" );
 
     @Override
     protected boolean canHandle( final WebRequest webRequest )
@@ -46,18 +49,29 @@ public class AdminToolPortalHandler
     public static DescriptorKey getDescriptorKey( final WebRequest webRequest )
     {
         final String path = webRequest.getRawPath();
-        final String subPath = path.length() > ADMIN_TOOL_PREFIX.length() ? path.substring( ADMIN_TOOL_PREFIX.length() ) : "";
-        final Matcher matcher = PATTERN.matcher( subPath );
+        
+        if (path.startsWith( ADMIN_TOOL_PREFIX ) ) {
+            final String subPath = path.substring( ADMIN_TOOL_PREFIX.length() );
+            final Matcher matcher = PATTERN.matcher( subPath );
+            if ( matcher.find() )
+            {
+                final ApplicationKey applicationKey = ApplicationKey.from( matcher.group( 1 ) );
+                final String adminToolName = matcher.group( 2 );
+                return DescriptorKey.from( applicationKey, adminToolName );
+            }            
+        }
+        return DEFAULT_DESCRIPTOR_KEY;
+    }
 
-        if ( matcher.find() )
-        {
-            final ApplicationKey applicationKey = ApplicationKey.from( matcher.group( 1 ) );
-            final String adminToolName = matcher.group( 2 );
-            return DescriptorKey.from( applicationKey, adminToolName );
-        }
-        else
-        {
-            return DEFAULT_DESCRIPTOR_KEY;
-        }
+    @Reference
+    public void setWebExceptionMapper( final ExceptionMapper exceptionMapper )
+    {
+        this.exceptionMapper = exceptionMapper;
+    }
+
+    @Reference
+    public void setExceptionRenderer( final ExceptionRenderer exceptionRenderer )
+    {
+        this.exceptionRenderer = exceptionRenderer;
     }
 }
