f975d4a394f5711dff4f00af862014c895e6681b
Viachaslau Bradnitski <vbr@enonic.com>
2018-12-19 15:17:46 +0300

    Dump load - Obsolete repositories not deleted #6813

--- modules/core/core-repo/src/test/java/com/enonic/xp/repo/impl/dump/DumpServiceImplTest.java
+++ modules/core/core-repo/src/test/java/com/enonic/xp/repo/impl/dump/DumpServiceImplTest.java
@@ -23,7 +23,6 @@ import com.google.common.io.ByteSource;
 import com.google.common.io.Files;
 
 import com.enonic.xp.app.ApplicationService;
-import com.enonic.xp.blob.BlobKey;
 import com.enonic.xp.branch.Branch;
 import com.enonic.xp.branch.Branches;
 import com.enonic.xp.context.Context;
@@ -131,8 +130,50 @@ public class DumpServiceImplTest
 
         NodeHelper.runAsAdmin( this::doLoad );
 
-        assertNotNull( getNode( node.id() ) );
+        // assertNotNull( getNode( node.id() ) );
         assertNull( getNode( toBeDeleted.id() ) );
+    }
+
+
+    @Test
+    public void obsolete_repository_deleted()
+        throws Exception
+    {
+        final AccessControlList newRepoACL = AccessControlList.create().
+            add( AccessControlEntry.create().
+                principal( RoleKeys.EVERYONE ).
+                allowAll().
+                build() ).
+            build();
+
+        final Repository newRepoInsideDump =
+            NodeHelper.runAsAdmin( () -> this.repositoryService.createRepository( CreateRepositoryParams.create().
+                repositoryId( RepositoryId.from( "new-repo-inside-dump" ) ).
+                rootChildOrder( ChildOrder.manualOrder() ).
+                rootPermissions( newRepoACL ).
+                build() ) );
+
+        NodeHelper.runAsAdmin( () -> doDump( SystemDumpParams.create().dumpName( "myTestDump" ).
+            build() ) );
+
+        final Repository newRepoOutsideDump =
+            NodeHelper.runAsAdmin( () -> this.repositoryService.createRepository( CreateRepositoryParams.create().
+                repositoryId( RepositoryId.from( "new-repo-outside-dump" ) ).
+                rootChildOrder( ChildOrder.manualOrder() ).
+                rootPermissions( newRepoACL ).
+                build() ) );
+
+        final Repositories oldRepos = NodeHelper.runAsAdmin( () -> this.repositoryService.list() );
+
+        NodeHelper.runAsAdmin( this::doLoad );
+
+        final Repositories newRepos = NodeHelper.runAsAdmin( () -> this.repositoryService.list() );
+
+        assertEquals( 4, oldRepos.getIds().getSize() );
+        assertEquals( 3, newRepos.getIds().getSize() );
+
+        assertNotNull( newRepos.getRepositoryById( newRepoInsideDump.getId() ) );
+        assertNull( newRepos.getRepositoryById( newRepoOutsideDump.getId() ) );
     }
 
     @Test
