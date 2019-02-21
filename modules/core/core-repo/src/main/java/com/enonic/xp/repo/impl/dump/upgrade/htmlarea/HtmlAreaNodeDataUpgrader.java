8089a5c30ebbe02ed95370a54211f07076123f33
GlennRicaud <gri@enonic.com>
2019-02-21 12:09:37 +0100

    Dump migration - HTML Areas - Processed references (#6961)

--- modules/core/core-repo/src/main/java/com/enonic/xp/repo/impl/dump/upgrade/htmlarea/HtmlAreaNodeDataUpgrader.java
+++ modules/core/core-repo/src/main/java/com/enonic/xp/repo/impl/dump/upgrade/htmlarea/HtmlAreaNodeDataUpgrader.java
@@ -1,14 +1,20 @@
 package com.enonic.xp.repo.impl.dump.upgrade.htmlarea;
 
+import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.google.common.collect.Sets;
 
 import com.enonic.xp.content.ContentConstants;
+import com.enonic.xp.data.Property;
+import com.enonic.xp.data.PropertyPath;
 import com.enonic.xp.data.PropertySet;
 import com.enonic.xp.data.PropertyTree;
+import com.enonic.xp.data.PropertyVisitor;
 import com.enonic.xp.index.IndexValueProcessor;
 import com.enonic.xp.index.PathIndexConfig;
 import com.enonic.xp.index.PatternIndexConfigDocument;
@@ -17,8 +23,22 @@ import com.enonic.xp.util.Reference;
 
 public class HtmlAreaNodeDataUpgrader
 {
+    private static final List<Pattern> BACKWARD_COMPATIBILITY_HTML_PROPERTY_PATH_PATTERNS =
+        Stream.of( "x.**", "* data.siteConfig.confg.**", "components.layout.config.*.**", "components.part.config.*.**",
+                   "components.page.config.*.**", "components.text.value" ).
+            map( HtmlAreaNodeDataUpgrader::toPattern ).
+            collect( Collectors.toList() );
+
     private static final Pattern HTML_LINK_PATTERN =
         Pattern.compile( "(?:href|src)=(\"((content|media|image)://(?:(download|inline)/)?([0-9a-z-/]+)(\\?[^\"]+)?)\")" );
+
+    private static final int HTML_LINK_PATTERN_ID_GROUP = 5;
+
+    private static final String PROCESSED_REFERENCES_PROPERTY_NAME = "processedReferences";
+
+    private static final String HTML_STRIPPER_PROCESSOR_NAME = "htmlStripper";
+
+    private static final String STRING_PROPERTY_TYPE_NAME = "String";
 
     private Set<Reference> references = Sets.newHashSet();
 
@@ -27,7 +47,6 @@ public class HtmlAreaNodeDataUpgrader
     public HtmlAreaNodeDataUpgrader( final PatternIndexConfigDocument indexConfigDocument )
     {
         this.indexConfigDocument = indexConfigDocument;
-
     }
 
     public boolean upgrade( final NodeVersion nodeVersion )
@@ -40,12 +59,9 @@ public class HtmlAreaNodeDataUpgrader
         final PropertyTree nodeData = nodeVersion.getData();
         upgradeNodeData( nodeData.getRoot() );
 
-        //TODO Handle old cases that did not have htmlStripper processors
-
-        System.out.println( "References" + references );
         if ( !references.isEmpty() )
         {
-            references.forEach( reference -> nodeData.addReference( "processedReferences", reference ) );
+            references.forEach( reference -> nodeData.addReference( PROCESSED_REFERENCES_PROPERTY_NAME, reference ) );
             return true;
         }
 
@@ -57,19 +73,51 @@ public class HtmlAreaNodeDataUpgrader
         return ContentConstants.CONTENT_NODE_COLLECTION.equals( nodeVersion.getNodeType() );
     }
 
-    private void upgradeNodeData( final PropertySet contentData )
+    private void upgradeNodeData( final PropertySet nodeData )
     {
-        indexConfigDocument.getPathIndexConfigs().
+        final List<Pattern> htmlAreaPatterns = indexConfigDocument.getPathIndexConfigs().
             stream().
             filter( this::hasHtmlStripperProcessor ).
-            forEach( pathIndexConfig -> upgradeHtmlAreaProperty( contentData, pathIndexConfig ) );
+            map( PathIndexConfig::getPath ).
+            map( PropertyPath::toString ).
+            map( HtmlAreaNodeDataUpgrader::toPattern ).
+            collect( Collectors.toList() );
+        htmlAreaPatterns.addAll( BACKWARD_COMPATIBILITY_HTML_PROPERTY_PATH_PATTERNS );
+
+        final PropertyVisitor propertyVisitor = new PropertyVisitor()
+        {
+            @Override
+            public void visit( final Property property )
+            {
+                if ( isHtmlAreaProperty( property ) )
+                {
+                    upgradeHtmlAreaProperty( property );
+                }
+            }
+
+            private boolean isHtmlAreaProperty( Property property )
+            {
+                return htmlAreaPatterns.stream().
+                    anyMatch( htmlPattern -> htmlPattern.matcher( property.getPath().toString() ).matches() );
+            }
+        };
+        propertyVisitor.traverse( nodeData );
+    }
+
+    private static Pattern toPattern( final String path )
+    {
+        final String pattern = path.
+            replace( ".", "(?:\\[\\d+\\])?." ). //Handle arrays
+            replace( "**", ".+" ).
+            replace( "*", "[^\\.]+" );
+        return Pattern.compile( pattern );
     }
 
     private boolean hasHtmlStripperProcessor( final PathIndexConfig pathIndexConfig )
     {
         for ( IndexValueProcessor indexValueProcessor : pathIndexConfig.getIndexConfig().getIndexValueProcessors() )
         {
-            if ( "htmlStripper".equals( indexValueProcessor.getName() ) )
+            if ( HTML_STRIPPER_PROCESSOR_NAME.equals( indexValueProcessor.getName() ) )
             {
                 return true;
             }
@@ -77,20 +125,22 @@ public class HtmlAreaNodeDataUpgrader
         return false;
     }
 
-    private void upgradeHtmlAreaProperty( final PropertySet nodeData, final PathIndexConfig pathIndexConfig )
+    private void upgradeHtmlAreaProperty( final Property property )
     {
-        final String source = nodeData.getString( pathIndexConfig.getPath() );
-        if ( source != null )
+        if ( STRING_PROPERTY_TYPE_NAME.equals( property.getType().getName() ) )
         {
-            System.out.println( "\nHtml area [" + pathIndexConfig.getPath().toString() + "]: " + source );
-            final Matcher contentMatcher = HTML_LINK_PATTERN.matcher( source );
-
-            while ( contentMatcher.find() )
+            final String value = property.getString();
+            if ( value != null )
             {
-                if ( contentMatcher.groupCount() >= 5 )
+                final Matcher contentMatcher = HTML_LINK_PATTERN.matcher( value );
+
+                while ( contentMatcher.find() )
                 {
-                    final String reference = contentMatcher.group( 5 );
-                    references.add( Reference.from( reference ) );
+                    if ( contentMatcher.groupCount() >= HTML_LINK_PATTERN_ID_GROUP )
+                    {
+                        final String reference = contentMatcher.group( HTML_LINK_PATTERN_ID_GROUP );
+                        references.add( Reference.from( reference ) );
+                    }
                 }
             }
         }
