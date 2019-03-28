22c0bc45f432aa2f98e421b48d2178d48d999c8e
GlennRicaud <gri@enonic.com>
2019-03-28 10:20:27 +0100

    Remove Nashorn deprecation warnings (#7036)

--- modules/runtime/src/bin/server.sh
+++ modules/runtime/src/bin/server.sh
@@ -88,7 +88,7 @@ init() {
 }
 
 run() {
-    exec "$JAVACMD" $JAVA_OPTS -Dxp.install="$XP_INSTALL" -Dfile.encoding=UTF8 $XP_OPTS -Dmapper.allow_dots_in_name=true -classpath "$XP_INSTALL/lib/*" com.enonic.xp.launcher.LauncherMain $ARGS
+    exec "$JAVACMD" $JAVA_OPTS -Dxp.install="$XP_INSTALL" -Dfile.encoding=UTF8 -Dnashorn.args="--no-deprecation-warning" $XP_OPTS -Dmapper.allow_dots_in_name=true -classpath "$XP_INSTALL/lib/*" com.enonic.xp.launcher.LauncherMain $ARGS
 }
 
 main() {
