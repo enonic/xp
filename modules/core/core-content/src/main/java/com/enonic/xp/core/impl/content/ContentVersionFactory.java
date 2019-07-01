6443d09f6cb0ac6a3ce14d9a9958c2fe3817a15f
Guðmundur Björn Birkisson <gbi@enonic.com>
2019-07-01 15:35:29 +0200

    Add publish info to content version (#7198)

--- modules/core/core-content/src/main/java/com/enonic/xp/core/impl/content/ContentVersionFactory.java
+++ modules/core/core-content/src/main/java/com/enonic/xp/core/impl/content/ContentVersionFactory.java
@@ -4,18 +4,22 @@ import com.enonic.xp.content.ContentId;
 import com.enonic.xp.content.ContentPropertyNames;
 import com.enonic.xp.content.ContentVersion;
 import com.enonic.xp.content.ContentVersionId;
+import com.enonic.xp.content.ContentVersionPublishInfo;
 import com.enonic.xp.content.ContentVersions;
 import com.enonic.xp.data.PropertyTree;
+import com.enonic.xp.node.NodeCommitEntry;
+import com.enonic.xp.node.NodeCommitId;
 import com.enonic.xp.node.NodeId;
 import com.enonic.xp.node.NodeService;
 import com.enonic.xp.node.NodeVersion;
-import com.enonic.xp.node.NodeVersionId;
 import com.enonic.xp.node.NodeVersionMetadata;
 import com.enonic.xp.node.NodeVersionsMetadata;
 import com.enonic.xp.security.PrincipalKey;
 
 class ContentVersionFactory
 {
+    private static final String NODE_CONTENT_PUBLISH_MESSAGE = "COM_ENONIC_XP_CONTENT_PUBLISH";
+
     private final NodeService nodeService;
 
     public ContentVersionFactory( final NodeService nodeService )
@@ -40,10 +44,10 @@ class ContentVersionFactory
     public ContentVersion create( final NodeVersionMetadata nodeVersionMetadata )
     {
         final NodeVersion nodeVersion = getNodeVersion( nodeVersionMetadata );
-        return doCreateContentVersion( nodeVersionMetadata.getNodeVersionId(), nodeVersion );
+        return doCreateContentVersion( nodeVersionMetadata, nodeVersion );
     }
 
-    private ContentVersion doCreateContentVersion( final NodeVersionId nodeVersionId, final NodeVersion nodeVersion )
+    private ContentVersion doCreateContentVersion( final NodeVersionMetadata nodeVersionMetadata, final NodeVersion nodeVersion )
     {
         final PropertyTree data = nodeVersion.getData();
 
@@ -52,8 +56,31 @@ class ContentVersionFactory
             comment( "No comments" ).
             modified( data.getProperty( ContentPropertyNames.MODIFIED_TIME ).getInstant() ).
             modifier( PrincipalKey.from( data.getProperty( ContentPropertyNames.MODIFIER ).getString() ) ).
-            id( ContentVersionId.from( nodeVersionId.toString() ) ).
+            id( ContentVersionId.from( nodeVersionMetadata.getNodeVersionId().toString() ) ).
+            publishInfo( doCreateContentVersionPublishInfo( nodeVersionMetadata.getNodeCommitId() ) ).
             build();
+    }
+
+    private ContentVersionPublishInfo doCreateContentVersionPublishInfo( final NodeCommitId nodeCommitId )
+    {
+        NodeCommitEntry nodeCommitEntry = nodeService.getCommit( nodeCommitId );
+        return ContentVersionPublishInfo.create().
+            message( cleanUpMessage( nodeCommitEntry.getMessage() ) ).
+            publisher( nodeCommitEntry.getCommitter() ).
+            timestamp( nodeCommitEntry.getTimestamp() ).
+            build();
+    }
+
+    private String cleanUpMessage( String message )
+    {
+        if ( message == null )
+        {
+            return null;
+        }
+
+        String res = message.replace( NODE_CONTENT_PUBLISH_MESSAGE, "" ).trim();
+
+        return res.equals( "" ) ? null : res;
     }
 
     private NodeVersion getNodeVersion( final NodeVersionMetadata nodeVersionMetadata )
