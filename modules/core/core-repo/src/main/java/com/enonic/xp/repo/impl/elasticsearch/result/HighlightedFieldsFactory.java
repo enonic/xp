57c1ac620ef8ca271713df5413277ef8218cac08
GlennRicaud <gri@enonic.com>
2019-08-16 12:56:27 +0200

    Highlight - Merge by field name #7302

--- modules/core/core-repo/src/main/java/com/enonic/xp/repo/impl/elasticsearch/result/HighlightedFieldsFactory.java
+++ modules/core/core-repo/src/main/java/com/enonic/xp/repo/impl/elasticsearch/result/HighlightedFieldsFactory.java
@@ -1,15 +1,23 @@
 package com.enonic.xp.repo.impl.elasticsearch.result;
 
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.elasticsearch.common.text.Text;
 import org.elasticsearch.search.highlight.HighlightField;
 
 import com.enonic.xp.highlight.HighlightedField;
 import com.enonic.xp.highlight.HighlightedFields;
+import com.enonic.xp.repo.impl.index.IndexValueType;
 
 public class HighlightedFieldsFactory
 {
+    private static final Pattern POSTFIX_PATTERN = Pattern.compile(
+        "\\.(?:" + IndexValueType.DATETIME.getPostfix() + "|" + IndexValueType.NUMBER.getPostfix() + "|" +
+            IndexValueType.NGRAM.getPostfix() + "|" + IndexValueType.ANALYZED.getPostfix() + "|" + IndexValueType.ORDERBY.getPostfix() +
+            "|" + IndexValueType.GEO_POINT.getPostfix() + "|" + IndexValueType.PATH.getPostfix() + ")$" );
+
     public static HighlightedFields create( final Map<String, HighlightField> highlightFieldMap )
     {
         if ( highlightFieldMap == null )
@@ -21,8 +29,9 @@ public class HighlightedFieldsFactory
 
         for ( final Map.Entry<String, HighlightField> fieldEntry : highlightFieldMap.entrySet() )
         {
+            final String fieldName = removePostfix( fieldEntry.getKey() );
             final HighlightedField.Builder builder = HighlightedField.create().
-                name( fieldEntry.getKey() );
+                name( fieldName );
             for ( Text fragment : fieldEntry.getValue().fragments() )
             {
                 builder.addFragment( fragment.string() );
@@ -31,5 +40,15 @@ public class HighlightedFieldsFactory
         }
 
         return result.build();
+    }
+
+    private static String removePostfix( final String fieldName )
+    {
+        final Matcher matcher = POSTFIX_PATTERN.matcher( fieldName );
+        if ( matcher.find() )
+        {
+            return fieldName.substring( 0, matcher.start() );
+        }
+        return fieldName;
     }
 }
