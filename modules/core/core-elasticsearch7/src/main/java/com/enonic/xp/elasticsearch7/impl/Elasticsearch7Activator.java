d88151ee1c383370829f7e91d94ace6a5f364ce1
rymsha <rymsha@yandex.ru>
2019-09-30 16:25:20 +0200

    save work

--- modules/core/core-elasticsearch7/src/main/java/com/enonic/xp/elasticsearch7/impl/Elasticsearch7Activator.java
+++ modules/core/core-elasticsearch7/src/main/java/com/enonic/xp/elasticsearch7/impl/Elasticsearch7Activator.java
@@ -1,31 +1,93 @@
 package com.enonic.xp.elasticsearch7.impl;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
+import java.nio.file.Path;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component(immediate = true)
 public class Elasticsearch7Activator
 {
+    private final static Logger LOG = LoggerFactory.getLogger( Elasticsearch7Activator.class );
+
     private Process process;
+
+    private Thread outStreamReader;
+
+    private final CountDownLatch latch = new CountDownLatch( 1 );
 
     @Activate
     public void activate( final BundleContext context, final Map<String, String> map )
-        throws IOException
+        throws IOException, InterruptedException
     {
-        process =
-            new ProcessBuilder( "C:\\es7\\elasticsearch-oss-7.3.2-windows-x86_64\\elasticsearch-7.3.2\\bin\\elasticsearch.bat" ).
-                inheritIO().
-                start();
+        startElasticProcess();
+    }
+
+    private void startElasticProcess()
+        throws IOException, InterruptedException
+    {
+        final String basePath = "\\es\\elasticsearch-oss-7.3.2-windows-x86_64\\elasticsearch-7.3.2\\bin";
+        process = new ProcessBuilder( Path.of( basePath, executableFilename() ).toString() ).
+            redirectErrorStream( true ).
+            start();
+
+        outStreamReader = new Thread( () -> {
+
+            try (final BufferedReader in = new BufferedReader( new InputStreamReader( process.getInputStream() ) ))
+            {
+                String line;
+                while ( ( line = in.readLine() ) != null )
+                {
+                    LOG.info( line );
+                    if ( line.endsWith( "] started" ) )
+                    {
+                        latch.countDown();
+                    }
+                    if ( Thread.interrupted() )
+                    {
+                        latch.countDown();
+                        return;
+                    }
+                }
+
+            }
+            catch ( IOException e )
+            {
+                throw new UncheckedIOException( e );
+            }
+        } );
+        outStreamReader.start();
+    }
+
+
+    private String executableFilename()
+    {
+        return "elasticsearch" + ( isWindows() ? ".bat" : "" );
+    }
+
+    private static boolean isWindows()
+    {
+        return System.getProperty( "os.name" ).startsWith( "Windows" );
     }
 
     @Deactivate
     public void deactivate()
+        throws IOException, InterruptedException
     {
+        latch.await();
+        outStreamReader.interrupt();
+        process.children().forEach( ProcessHandle::destroy );
         process.destroy();
+        process.waitFor();
     }
 }
