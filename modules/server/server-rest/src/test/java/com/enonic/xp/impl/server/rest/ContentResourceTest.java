13263c6a7fcb3be9d57ad1018dbedd13cb68d62c
Pavel Milkevich <pavel.milkevich@gmail.com>
2019-10-08 17:32:38 +0300

    Convert content reprocess to a runnable task with progress reporting #7517

--- modules/server/server-rest/src/test/java/com/enonic/xp/impl/server/rest/ContentResourceTest.java
+++ modules/server/server-rest/src/test/java/com/enonic/xp/impl/server/rest/ContentResourceTest.java
@@ -9,11 +9,10 @@ import com.enonic.xp.content.Content;
 import com.enonic.xp.content.ContentId;
 import com.enonic.xp.content.ContentPath;
 import com.enonic.xp.content.ContentService;
-import com.enonic.xp.content.Contents;
-import com.enonic.xp.content.FindContentByParentParams;
-import com.enonic.xp.content.FindContentByParentResult;
-import com.enonic.xp.jaxrs.impl.MockRestResponse;
 import com.enonic.xp.schema.content.ContentTypeName;
+import com.enonic.xp.task.RunnableTask;
+import com.enonic.xp.task.TaskId;
+import com.enonic.xp.task.TaskService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,104 +21,35 @@ public class ContentResourceTest
 {
     private ContentService contentService;
 
+    private TaskService taskService;
+
     @Test
     public void reprocess_skip_children()
         throws Exception
     {
         Content content = createContent( "content-id", ContentPath.from( "/path/to/content" ) );
         Content reprocessedContent = Content.create( content ).displayName( "new name" ).build();
-
+        TaskId taskId = TaskId.from( "task-id" );
         Mockito.when( this.contentService.getByPath( content.getPath() ) ).thenReturn( content );
         Mockito.when( this.contentService.reprocess( content.getId() ) ).thenReturn( reprocessedContent );
+        Mockito.when( this.taskService.submitTask( Mockito.any( RunnableTask.class ), Mockito.anyString() ) ).thenReturn( taskId );
 
         final String result = request().path( "content/reprocess" ).
             entity( readFromFile( "reprocess_params_skip_children.json" ), MediaType.APPLICATION_JSON_TYPE ).
             post().getAsString();
 
-        assertEquals( "{\"errors\":[],\"updatedContent\":[\"/path/to/content\"]}", result );
-    }
-
-    @Test
-    public void reprocess_child()
-        throws Exception
-    {
-        Content content = createContent( "content-id", ContentPath.from( "/path/to/content" ) );
-        Content reprocessedContent = Content.create( content ).displayName( "new name" ).build();
-        Content childContent = createContent( "child-id", ContentPath.from( content.getPath(), "child" ) );
-        Content reprocessedChildContent = Content.create( childContent ).displayName( "new name" ).build();
-
-        Mockito.when( this.contentService.getByPath( content.getPath() ) ).thenReturn( content );
-        Mockito.when( this.contentService.reprocess( content.getId() ) ).thenReturn( reprocessedContent );
-        Mockito.when( this.contentService.reprocess( childContent.getId() ) ).thenReturn( reprocessedChildContent );
-
-        Mockito.when( this.contentService.findByParent( FindContentByParentParams.create().parentId( content.getId() ).
-            from( 0 ).size( 5 ).build() ) ).thenReturn(
-            FindContentByParentResult.create().contents( Contents.create().add( childContent ).build() ).build() );
-
-        Mockito.when( this.contentService.findByParent( FindContentByParentParams.create().parentId( childContent.getId() ).
-            from( 0 ).size( 5 ).build() ) ).thenReturn( FindContentByParentResult.create().contents( Contents.create().build() ).build() );
-
-        Mockito.when( this.contentService.reprocess( content.getId() ) ).thenReturn( reprocessedContent );
-
-        final MockRestResponse result = request().path( "content/reprocess" ).
-            entity( readFromFile( "reprocess_params.json" ), MediaType.APPLICATION_JSON_TYPE ).
-            post();
-
-        assertEquals( "{\"errors\":[],\"updatedContent\":[\"/path/to/content\",\"/path/to/content/child\"]}", result.getAsString() );
-    }
-
-    @Test
-    public void reprocess_invalid()
-        throws Exception
-    {
-        Content content = createContent( "content-id", ContentPath.from( "/path/to/content" ) );
-
-        Mockito.when( this.contentService.getByPath( content.getPath() ) ).thenReturn( content );
-        Mockito.when( this.contentService.reprocess( content.getId() ) ).thenThrow( new RuntimeException( "exceptionMessage" ) );
-
-        final String result = request().path( "content/reprocess" ).
-            entity( readFromFile( "reprocess_params_skip_children.json" ), MediaType.APPLICATION_JSON_TYPE ).
-            post().getAsString();
-
-        assertEquals( "{\"errors\":[\"Content '/path/to/content' - java.lang.RuntimeException: exceptionMessage\"],\"updatedContent\":[]}",
-                      result );
-    }
-
-    @Test
-    public void reprocess_child_with_error()
-        throws Exception
-    {
-        Content content = createContent( "content-id", ContentPath.from( "/path/to/content" ) );
-        Content reprocessedContent = Content.create( content ).displayName( "new name" ).build();
-        Content child = createContent( "child-id", ContentPath.from( content.getPath(), "child" ) );
-
-        Mockito.when( this.contentService.getByPath( content.getPath() ) ).thenReturn( content );
-        Mockito.when( this.contentService.reprocess( content.getId() ) ).thenReturn( reprocessedContent );
-
-        Mockito.when( this.contentService.reprocess( child.getId() ) ).thenThrow( new RuntimeException( "errorMessage" ) );
-
-        final FindContentByParentParams findParams = FindContentByParentParams.create().parentId( content.getId() ).
-            from( 0 ).size( 5 ).build();
-
-        Mockito.when( this.contentService.findByParent( findParams ) ).thenReturn( FindContentByParentResult.create().contents(
-            Contents.create().add( child ).build() ).build() );
-
-        final String result = request().path( "content/reprocess" ).
-            entity( readFromFile( "reprocess_params.json" ), MediaType.APPLICATION_JSON_TYPE ).
-            post().getAsString();
-
-        assertEquals(
-            "{\"errors\":[\"Content '/path/to/content/child' - java.lang.RuntimeException: errorMessage\"],\"updatedContent\":[\"/path/to/content\"]}",
-            result );
+        assertEquals( "{\"taskId\":\"task-id\"}", result );
     }
 
     @Override
     protected Object getResourceInstance()
     {
         this.contentService = Mockito.mock( ContentService.class );
+        this.taskService = Mockito.mock( TaskService.class );
 
         final ContentResource resource = new ContentResource();
         resource.setContentService( contentService );
+        resource.setTaskService( taskService );
         return resource;
     }
 
