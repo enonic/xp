4aeaea530b8f1c53854267782e16a26682a6de65
Anatol Sialitski <asi@enonic.com>
2019-11-07 14:59:53 +0300

    Content lib - getOutboundDependencies #7577 - small corrections

--- modules/lib/lib-content/src/main/resources/lib/xp/examples/content/getOutboundDependencies.js
+++ modules/lib/lib-content/src/main/resources/lib/xp/examples/content/getOutboundDependencies.js
@@ -16,7 +16,7 @@ if (resultById) {
 
 // BEGIN
 // Outbound dependencies as they are returned.
-var expected = {"contentIds": ["d898972d-f1eb-40a8-a7f2-16abd4c105da", "9efadb7b-bb14-4c74-82ec-cec95069d0c2"]};
+var expected = {'contentIds': ['d898972d-f1eb-40a8-a7f2-16abd4c105da', '9efadb7b-bb14-4c74-82ec-cec95069d0c2']};
 // END
 
 assert.assertJsonEquals(expected, resultById);
