5d94f9866e50c69abd1cdcc22193b34009568078
Anatol Sialitski <asi@enonic.com>
2019-11-07 14:38:50 +0300

    Content lib - getOutboundDependencies #7577 - small corrections

--- modules/lib/lib-content/src/main/resources/lib/xp/examples/content/getOutboundDependencies.js
+++ modules/lib/lib-content/src/main/resources/lib/xp/examples/content/getOutboundDependencies.js
@@ -2,7 +2,7 @@ var contentLib = require('/lib/xp/content');
 var assert = require('/lib/xp/testing');
 
 // BEGIN
-// Gets a content by ID.
+// Gets outbound dependencies of content by Content ID.
 var resultById = contentLib.getOutboundDependencies({
     key: 'contentId'
 });
@@ -15,14 +15,14 @@ if (resultById) {
 // END
 
 // BEGIN
-// Content as it is returned.
+// Outbound dependencies as they are returned.
 var expected = {"contentIds": ["d898972d-f1eb-40a8-a7f2-16abd4c105da", "9efadb7b-bb14-4c74-82ec-cec95069d0c2"]};
 // END
 
 assert.assertJsonEquals(expected, resultById);
 
 // BEGIN
-// Gets a content by Path.
+// Gets outbound dependencies of a content by Content Path.
 var resultByPath = contentLib.getOutboundDependencies({
     key: '/path/to/content'
 });
