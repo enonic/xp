cb84fdcedb943d29d6998842c0e8ec7fe6a014a7
Pavel Milkevich <pavel.milkevich@gmail.com>
2019-11-14 12:32:29 +0300

    Convert content reprocess to a runnable task with progress reporting #7517
    - extracted runnable task to a separate method for backwards compatibility

--- modules/server/server-rest/src/main/java/com/enonic/xp/impl/server/rest/ContentResource.java
+++ modules/server/server-rest/src/main/java/com/enonic/xp/impl/server/rest/ContentResource.java
@@ -1,5 +1,8 @@
 package com.enonic.xp.impl.server.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -9,9 +12,17 @@ import javax.ws.rs.core.MediaType;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.enonic.xp.content.Content;
+import com.enonic.xp.content.ContentPath;
+import com.enonic.xp.content.ContentPaths;
 import com.enonic.xp.content.ContentService;
+import com.enonic.xp.content.FindContentByParentParams;
+import com.enonic.xp.content.FindContentByParentResult;
 import com.enonic.xp.impl.server.rest.model.ReprocessContentRequestJson;
+import com.enonic.xp.impl.server.rest.model.ReprocessContentResultJson;
 import com.enonic.xp.impl.server.rest.task.ReprocessRunnableTask;
 import com.enonic.xp.jaxrs.JaxRsComponent;
 import com.enonic.xp.security.RoleKeys;
@@ -30,9 +41,73 @@ public final class ContentResource
 
     private TaskService taskService;
 
+    private final static Logger LOG = LoggerFactory.getLogger( ContentResource.class );
+
     @POST
     @Path("reprocess")
-    public TaskResultJson reprocess( final ReprocessContentRequestJson request )
+    public ReprocessContentResultJson reprocess( final ReprocessContentRequestJson request )
+    {
+        final List<ContentPath> updated = new ArrayList<>();
+        final List<String> errors = new ArrayList<>();
+
+        final Content content = this.contentService.getByPath( request.getSourceBranchPath().getContentPath() );
+        try
+        {
+            reprocessContent( content, request.isSkipChildren(), updated, errors );
+        }
+        catch ( Exception e )
+        {
+            errors.add(
+                String.format( "Content '%s' - %s: %s", content.getPath().toString(), e.getClass().getCanonicalName(), e.getMessage() ) );
+            LOG.warn( "Error reprocessing content [" + content.getPath() + "]", e );
+        }
+
+        return new ReprocessContentResultJson( ContentPaths.from( updated ), errors );
+    }
+
+    private void reprocessContent( final Content content, final boolean skipChildren, final List<ContentPath> updated,
+                                   final List<String> errors )
+    {
+        final Content reprocessedContent = this.contentService.reprocess( content.getId() );
+        if ( !reprocessedContent.equals( content ) )
+        {
+            updated.add( content.getPath() );
+        }
+        if ( skipChildren )
+        {
+            return;
+        }
+
+        int from = 0;
+        int resultCount;
+        do
+        {
+            final FindContentByParentParams findParams = FindContentByParentParams.create().parentId( content.getId() ).
+                from( from ).size( 5 ).build();
+            final FindContentByParentResult results = this.contentService.findByParent( findParams );
+
+            for ( Content child : results.getContents() )
+            {
+                try
+                {
+                    reprocessContent( child, false, updated, errors );
+                }
+                catch ( Exception e )
+                {
+                    errors.add( String.format( "Content '%s' - %s: %s", child.getPath().toString(), e.getClass().getCanonicalName(),
+                                               e.getMessage() ) );
+                    LOG.warn( "Error reprocessing content [" + child.getPath() + "]", e );
+                }
+            }
+            resultCount = Math.toIntExact( results.getHits() );
+            from = from + resultCount;
+        }
+        while ( resultCount > 0 );
+    }
+
+    @POST
+    @Path("reprocessTask")
+    public TaskResultJson reprocessTask( final ReprocessContentRequestJson request )
     {
         return ReprocessRunnableTask.create().
             description( "reprocess" ).
