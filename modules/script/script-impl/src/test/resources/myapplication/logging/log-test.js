0d78e945616ee760342428615eaf6e0415cfd63a
Anatol Sialitski <asi@enonic.com>
2019-12-05 13:24:51 +0300

    JS Logger - Handle throwable #6905

--- modules/script/script-impl/src/test/resources/myapplication/logging/log-test.js
+++ modules/script/script-impl/src/test/resources/myapplication/logging/log-test.js
@@ -2,18 +2,22 @@ log.debug('debug message');
 log.debug('debug message %s', true);
 log.debug('debug message %s %s', true, {a: 3});
 log.debug('debug message %s', [1, 2]);
+log.debug('debug message', new Error('cause'));
 
 log.info('info message');
 log.info('info message %s', true);
 log.info('info message %s %s', true, {a: 3});
 log.info('info message %s', [1, 2]);
+log.info('info message', new Error('cause'));
 
 log.warning('warning message');
 log.warning('warning message %s', true);
 log.warning('warning message %s %s', true, {a: 3});
 log.warning('warning message %s', [1, 2]);
+log.warning('warning message', new Error('cause'));
 
 log.error('error message');
 log.error('error message %s', true);
 log.error('error message %s %s', true, {a: 3});
 log.error('error message %s', [1, 2]);
+log.error('error message', new Error('cause'));
