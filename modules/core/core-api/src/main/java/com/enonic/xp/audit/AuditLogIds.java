3d5dd9dcc52793b3013d381190344b26d026a29b
rymsha <rymsha@yandex.ru>
2019-12-29 19:39:16 +0100

    fix unused imports after merge

--- modules/core/core-api/src/main/java/com/enonic/xp/audit/AuditLogIds.java
+++ modules/core/core-api/src/main/java/com/enonic/xp/audit/AuditLogIds.java
@@ -6,9 +6,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import com.google.common.annotations.Beta;
-import com.google.common.base.Function;
-import com.google.common.collect.Collections2;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 
