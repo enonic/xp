20db88f9bd707741e7aafd658e8d3ec365568fca
Viachaslau Bradnitski <vbr@enonic.com>
2020-03-17 18:26:57 +0300

    Give permissions on /content node to project roles #7941

---
+++ modules/core/core-content/src/main/java/com/enonic/xp/core/impl/project/ProjectRoles.java
@@ -0,0 +1,23 @@
+package com.enonic.xp.core.impl.project;
+
+import com.enonic.xp.project.ProjectConstants;
+import com.enonic.xp.project.ProjectName;
+import com.enonic.xp.security.PrincipalKey;
+
+public enum ProjectRoles
+{
+    OWNER( "owner" ), EDITOR( "editor" ), AUTHOR( "author" ), CONTRIBUTOR( "contributor" ), VIEWER( "viewer" );
+
+    private String value;
+
+    ProjectRoles( final String value )
+    {
+        this.value = value;
+    }
+
+    public PrincipalKey getRoleKey( final ProjectName projectName )
+    {
+        final String roleName = ProjectConstants.PROJECT_REPO_ID_PREFIX + projectName + "." + value;
+        return PrincipalKey.ofRole( roleName );
+    }
+}
