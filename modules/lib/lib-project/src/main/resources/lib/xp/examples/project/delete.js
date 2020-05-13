4ed43e80a5aa53b886e63a784ae8560ff316cdb0
Alan Semenov <ase@enonic.com>
2020-05-13 17:17:21 +0200

    Added examples for `lib-project` methods (#8068)

---
+++ modules/lib/lib-project/src/main/resources/lib/xp/examples/project/delete.js
@@ -0,0 +1,28 @@
+// BEGIN
+// Deletes an existing content project
+var projectLib = require('/lib/xp/project');
+var result = projectLib.delete({
+    id: 'my-project'
+});
+// END
+
+// BEGIN
+// Deletes an existing content project inside context with `system.admin` role
+var projectLib = require('/lib/xp/project');
+var contextLib = require('/lib/xp/context');
+
+var deleteProject = function() {
+    return projectLib.delete({
+        id: 'my-project'
+    });
+}
+
+var result = contextLib.run({
+    principals: ["role:system.admin"]
+}, deleteProject);
+// END
+
+// BEGIN
+// `true` if project was deleted.
+var expected = true;
+// END
