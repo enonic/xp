8f583c21fe260686e3c10aab2b79c9fbdc796d76
Bradnitski Viachaslau <vbradnitski@users.noreply.github.com>
2020-05-13 20:23:41 +0300

    Implement lib-project #8068

---
+++ modules/lib/lib-project/src/main/java/com/enonic/xp/lib/project/ModifyProjectPermissionsHandler.java
@@ -0,0 +1,39 @@
+package com.enonic.xp.lib.project;
+
+import com.enonic.xp.lib.project.mapper.ProjectPermissionsMapper;
+import com.enonic.xp.project.ProjectName;
+import com.enonic.xp.project.ProjectPermissions;
+import com.enonic.xp.script.ScriptValue;
+
+public abstract class ModifyProjectPermissionsHandler
+    extends BaseProjectHandler
+{
+    private ProjectName id;
+
+    private ProjectPermissions permissions;
+
+    @Override
+    protected ProjectPermissionsMapper doExecute()
+    {
+        final ProjectPermissions permissionsBeforeUpdate = this.projectService.getPermissions( this.id );
+
+        final ProjectPermissions projectPermissions = this.permissions != null
+            ? this.projectService.modifyPermissions( this.id, merge( permissionsBeforeUpdate, this.permissions ) )
+            : this.projectService.getPermissions( this.id );
+
+        return new ProjectPermissionsMapper( projectPermissions );
+    }
+
+    protected abstract ProjectPermissions merge( final ProjectPermissions permissionsBeforeUpdate,
+                                                 final ProjectPermissions paramPermissions );
+
+    public void setId( final String value )
+    {
+        this.id = ProjectName.from( value );
+    }
+
+    public void setPermissions( final ScriptValue value )
+    {
+        this.permissions = buildProjectPermissions( value );
+    }
+}
---
+++ modules/lib/lib-project/src/main/java/com/enonic/xp/lib/project/command/ApplyProjectReadAccessCommand.java
@@ -0,0 +1,92 @@
+package com.enonic.xp.lib.project.command;
+
+import com.enonic.xp.content.ApplyContentPermissionsParams;
+import com.enonic.xp.content.Content;
+import com.enonic.xp.content.ContentPath;
+import com.enonic.xp.security.RoleKeys;
+import com.enonic.xp.security.acl.AccessControlEntry;
+import com.enonic.xp.security.acl.AccessControlList;
+import com.enonic.xp.security.acl.Permission;
+
+public final class ApplyProjectReadAccessCommand
+    extends AbstractProjectRootCommand
+{
+    private final boolean isPublic;
+
+    private ApplyProjectReadAccessCommand( final Builder builder )
+    {
+        super( builder );
+        this.isPublic = builder.isPublic;
+    }
+
+    public static Builder create()
+    {
+        return new Builder();
+    }
+
+    public Boolean execute()
+    {
+        return projectRepoContext.callWith( this::doExecute );
+    }
+
+    private Boolean doExecute()
+    {
+        final Content contentRoot = this.contentService.getByPath( ContentPath.ROOT );
+
+        final AccessControlList newList = isPublic
+            ? doAddEveryonePermissions( contentRoot.getPermissions() )
+            : doRemoveEveryonePermissions( contentRoot.getPermissions() );
+
+        contentService.applyPermissions( ApplyContentPermissionsParams.create().
+            permissions( newList ).
+            contentId( contentRoot.getId() ).
+            overwriteChildPermissions( true ).
+            build() );
+
+        return GetProjectReadAccessCommand.create().
+            contentService( this.contentService ).
+            projectName( this.projectName ).
+            build().
+            execute();
+    }
+
+    private AccessControlList doRemoveEveryonePermissions( final AccessControlList permissions )
+    {
+        return AccessControlList.create( permissions ).
+            remove( RoleKeys.EVERYONE ).
+            build();
+    }
+
+    private AccessControlList doAddEveryonePermissions( AccessControlList permissions )
+    {
+        return AccessControlList.create( permissions ).
+            add( AccessControlEntry.create().
+                principal( RoleKeys.EVERYONE ).
+                allow( Permission.READ ).
+                build() ).
+            build();
+    }
+
+    public static final class Builder
+        extends AbstractProjectRootCommand.Builder<Builder>
+    {
+        private boolean isPublic;
+
+        private Builder()
+        {
+        }
+
+        public Builder setPublic( final boolean value )
+        {
+            this.isPublic = value;
+            return this;
+        }
+
+        public ApplyProjectReadAccessCommand build()
+        {
+            validate();
+            return new ApplyProjectReadAccessCommand( this );
+        }
+
+    }
+}
