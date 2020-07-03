e997133558be3ea2e18f79c771c54b8dbb813391
rymsha <rymsha@yandex.ru>
2020-07-03 16:24:36 +0200

    Exclude node name form JSESSIONID entirely #8195

--- modules/web/web-jetty/src/main/java/com/enonic/xp/web/jetty/impl/session/JettySessionStoreConfiguratorImpl.java
+++ modules/web/web-jetty/src/main/java/com/enonic/xp/web/jetty/impl/session/JettySessionStoreConfiguratorImpl.java
@@ -1,7 +1,5 @@
 package com.enonic.xp.web.jetty.impl.session;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.session.DefaultSessionIdManager;
 import org.eclipse.jetty.server.session.SessionCacheFactory;
@@ -10,24 +8,18 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
-import com.enonic.xp.cluster.ClusterConfig;
-
 @Component
 public class JettySessionStoreConfiguratorImpl
     implements JettySessionStoreConfigurator
 {
-    private final String workerName;
-
     private final SessionDataStoreFactory sessionDataStoreFactory;
 
     private final SessionCacheFactory sessionCacheFactory;
 
     @Activate
-    public JettySessionStoreConfiguratorImpl( @Reference final ClusterConfig clusterConfig,
-                                              @Reference final SessionDataStoreFactory sessionDataStoreFactory,
+    public JettySessionStoreConfiguratorImpl( @Reference final SessionDataStoreFactory sessionDataStoreFactory,
                                               @Reference final SessionCacheFactory sessionCacheFactory )
     {
-        this.workerName = clusterConfig.name().toString();
         this.sessionDataStoreFactory = sessionDataStoreFactory;
         this.sessionCacheFactory = sessionCacheFactory;
     }
@@ -35,18 +27,11 @@ public class JettySessionStoreConfiguratorImpl
     @Override
     public void configure( final Server server )
     {
-        final DefaultSessionIdManager sessionManager = new DefaultSessionIdManager( server )
-        {
-            @Override
-            public String getExtendedId( final String clusterId, final HttpServletRequest request )
-            {
-                return clusterId;
-            }
-        };
+        final DefaultSessionIdManager sessionManager = new DefaultSessionIdManager( server );
 
         server.addBean( sessionDataStoreFactory );
         server.addBean( sessionCacheFactory );
-        sessionManager.setWorkerName( workerName );
+        sessionManager.setWorkerName( "" );
 
         server.setSessionIdManager( sessionManager );
     }
