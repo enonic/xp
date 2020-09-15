858e083441a54a2434b1872d9c3c10e238bb932e
Bradnitski Viachaslau <vbradnitski@users.noreply.github.com>
2020-09-15 14:01:28 +0300

    Content permissions handling does not take multiple layers into account when resetting permissions #8366

--- modules/core/core-api/src/main/java/com/enonic/xp/project/ParentProjectSynchronizer.java
+++ modules/core/core-api/src/main/java/com/enonic/xp/project/ParentProjectSynchronizer.java
@@ -473,7 +473,6 @@ public class ParentProjectSynchronizer
             !Objects.equals( sourceContent.getPage(), targetContent.getPage() ) ||
             !Objects.equals( sourceContent.getThumbnail(), targetContent.getThumbnail() ) ||
             !Objects.equals( sourceContent.getProcessedReferences(), targetContent.getProcessedReferences() ) ||
-            sourceContent.inheritsPermissions() != targetContent.inheritsPermissions() ||
             sourceContent.isValid() != targetContent.isValid();
     }
 
@@ -505,8 +504,6 @@ public class ParentProjectSynchronizer
                 edit.displayName = source.getDisplayName();
                 edit.owner = source.getOwner();
                 edit.language = source.getLanguage();
-                edit.inheritPermissions = source.inheritsPermissions();
-                edit.permissions = source.getPermissions();
                 edit.workflowInfo = source.getWorkflowInfo();
                 edit.page = source.getPage();
                 edit.thumbnail = source.getThumbnail();
