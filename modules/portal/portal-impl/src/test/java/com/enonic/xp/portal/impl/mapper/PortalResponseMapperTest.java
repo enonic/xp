f4bea86061656605471862a2960f8a7942236f9f
rymsha <rymsha@yandex.ru>
2020-10-13 13:23:48 +0200

    responseProcessors and filters break cookies #8418

---
+++ modules/portal/portal-impl/src/test/java/com/enonic/xp/portal/impl/mapper/PortalResponseMapperTest.java
@@ -0,0 +1,39 @@
+package com.enonic.xp.portal.impl.mapper;
+
+import javax.servlet.http.Cookie;
+
+import org.junit.jupiter.api.Test;
+
+import com.enonic.xp.portal.PortalResponse;
+import com.enonic.xp.portal.impl.MapSerializableAssert;
+
+class PortalResponseMapperTest
+{
+    private final MapSerializableAssert assertHelper = new MapSerializableAssert( PortalResponseMapperTest.class );
+
+    @Test
+    void cookies()
+        throws Exception
+    {
+        final Cookie simpleCookie = new Cookie( "simple", "value" );
+
+        final Cookie complexCookie = new Cookie( "complex", "some value" );
+        complexCookie.setMaxAge( 100 );
+        complexCookie.setPath( "/admin" );
+        complexCookie.setDomain( "enonic.com" );
+        complexCookie.setComment( "some comment" );
+        complexCookie.setSecure( true );
+        complexCookie.setHttpOnly( true );
+
+        final Cookie toDeleteCookie = new Cookie( "toDelete", "value" );
+        toDeleteCookie.setMaxAge( 0 );
+
+        final PortalResponse response = PortalResponse.create().
+            cookie( simpleCookie ).
+            cookie( complexCookie ).
+            cookie( toDeleteCookie ).
+            build();
+
+        assertHelper.assertJson( "response-cookies.json", new PortalResponseMapper( response ) );
+    }
+}
