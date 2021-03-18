enonic/wem-ce

srcset support #8623

modules/lib/lib-portal/src/test/java/com/enonic/xp/lib/portal/url/UrlServiceScriptTest.java
@@ -126,6 +126,12 @@ public class UrlServiceScriptTest
     }
 
     @Test
+    public void processHtmlTest_imageUrlProcessing()
+    {
+        assertTrue( execute( "processHtmlTest_imageUrlProcessing" ) );
+    }
+
+    @Test
     public void imagePlaceholderTest()
     {
         assertTrue( execute( "imagePlaceholderTest" ) );

modules/lib/lib-portal/src/test/resources/test/url-test.js
@@ -197,7 +197,7 @@ exports.processHtmlTest = function () {
 
     // NOTE: This is not the actual url. Only a mock representation.
     assert.assertEquals('ProcessHtmlParams{type=server, params={}, value=<p><a title="Link tooltip" ' +
-                        'href="content://3e266eea-9875-4cb7-b259-41ad152f8532" target="_blank">link</a></p>}', result);
+                        'href="content://3e266eea-9875-4cb7-b259-41ad152f8532" target="_blank">link</a></p>, imageWidths=[]}', result);
     return true;
 };
 
@@ -208,7 +208,20 @@ exports.processHtmlTest_unknownProperty = function () {
     });
 
     assert.assertEquals('ProcessHtmlParams{type=server, params={}, value=<p><a title="Link tooltip" ' +
-                        'href="content://3e266eea-9875-4cb7-b259-41ad152f8532" target="_blank">link</a></p>}', result);
+                        'href="content://3e266eea-9875-4cb7-b259-41ad152f8532" target="_blank">link</a></p>, imageWidths=[]}', result);
+    return true;
+};
+
+exports.processHtmlTest_imageUrlProcessing = function() {
+    var result = portal.processHtml({
+        value: '<p><figure class="editor-align-justify">' +
+                       '<img alt="Alt text" src="image://3e266eea-9875-4cb7-b259-41ad152f8532"/>' +
+                       '<figcaption>Caption text</figcaption></figure></p>',
+        imageWidths: [660, 1024]
+    });
+
+    assert.assertEquals('ProcessHtmlParams{type=server, params={}, value=<p><figure class="editor-align-justify"><img alt="Alt text" src="image://3e266eea-9875-4cb7-b259-41ad152f8532"/><figcaption>Caption text</figcaption></figure></p>, imageWidths=[660, 1024]}', result);
+
     return true;
 };
 

modules/portal/portal-api/src/main/java/com/enonic/xp/portal/url/ProcessHtmlParams.java
@@ -1,5 +1,9 @@
 package com.enonic.xp.portal.url;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Strings;
 import com.google.common.collect.Multimap;
@@ -12,6 +16,8 @@ public final class ProcessHtmlParams
 {
     private String value;
 
+    private List<Integer> imageWidths;
+
     public String getValue()
     {
         return this.value;
@@ -23,11 +29,26 @@ public final class ProcessHtmlParams
         return this;
     }
 
+    public List<Integer> getImageWidths()
+    {
+        return imageWidths;
+    }
+
+    public ProcessHtmlParams imageWidths( final List<Integer> imageWidths )
+    {
+        this.imageWidths = imageWidths;
+        return this;
+    }
+
     @Override
     public ProcessHtmlParams setAsMap( final Multimap<String, String> map )
     {
         super.setAsMap( map );
         value( singleValue( map, "_value" ) );
+        imageWidths( Objects.requireNonNullElse( map.removeAll( "_imageWidths" ), List.<String>of() ).
+            stream().
+            map( Integer::parseInt ).
+            collect( Collectors.toList() ) );
         getParams().putAll( map );
         return this;
     }
@@ -37,5 +58,6 @@ public final class ProcessHtmlParams
     {
         super.buildToString( helper );
         helper.add( "value", this.value );
+        helper.add( "imageWidths", this.imageWidths );
     }
 }

enonic/xp

modules/lib/lib-portal/src/test/java/com/enonic/xp/lib/portal/url/UrlServiceScriptTest.java
@@ -126,6 +126,12 @@ public class UrlServiceScriptTest
     }
 
     @Test
+    public void processHtmlTest_imageUrlProcessing()
+    {
+        assertTrue( execute( "processHtmlImageUrlProcessingTest" ) );
+    }
+
+    @Test
     public void imagePlaceholderTest()
     {
         assertTrue( execute( "imagePlaceholderTest" ) );

modules/lib/lib-portal/src/test/resources/test/url-test.js
@@ -197,7 +197,7 @@ exports.processHtmlTest = function () {
 
     // NOTE: This is not the actual url. Only a mock representation.
     assert.assertEquals('ProcessHtmlParams{type=server, params={}, value=<p><a title="Link tooltip" ' +
-                        'href="content://3e266eea-9875-4cb7-b259-41ad152f8532" target="_blank">link</a></p>}', result);
+                        'href="content://3e266eea-9875-4cb7-b259-41ad152f8532" target="_blank">link</a></p>, imageWidths=[]}', result);
     return true;
 };
 
@@ -208,7 +208,20 @@ exports.processHtmlTest_unknownProperty = function () {
     });
 
     assert.assertEquals('ProcessHtmlParams{type=server, params={}, value=<p><a title="Link tooltip" ' +
-                        'href="content://3e266eea-9875-4cb7-b259-41ad152f8532" target="_blank">link</a></p>}', result);
+                        'href="content://3e266eea-9875-4cb7-b259-41ad152f8532" target="_blank">link</a></p>, imageWidths=[]}', result);
+    return true;
+};
+
+exports.processHtmlImageUrlProcessingTest = function() {
+    var result = portal.processHtml({
+        value: '<p><figure class="editor-align-justify">' +
+                       '<img alt="Alt text" src="image://3e266eea-9875-4cb7-b259-41ad152f8532"/>' +
+                       '<figcaption>Caption text</figcaption></figure></p>',
+        imageWidths: [660, 1024]
+    });
+
+    assert.assertEquals('ProcessHtmlParams{type=server, params={}, value=<p><figure class="editor-align-justify"><img alt="Alt text" src="image://3e266eea-9875-4cb7-b259-41ad152f8532"/><figcaption>Caption text</figcaption></figure></p>, imageWidths=[660, 1024]}', result);
+
     return true;
 };
 

modules/portal/portal-api/src/main/java/com/enonic/xp/portal/url/ProcessHtmlParams.java
@@ -1,5 +1,9 @@
 package com.enonic.xp.portal.url;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Strings;
 import com.google.common.collect.Multimap;
@@ -12,6 +16,8 @@ public final class ProcessHtmlParams
 {
     private String value;
 
+    private List<Integer> imageWidths;
+
     public String getValue()
     {
         return this.value;
@@ -23,11 +29,26 @@ public final class ProcessHtmlParams
         return this;
     }
 
+    public List<Integer> getImageWidths()
+    {
+        return imageWidths;
+    }
+
+    public ProcessHtmlParams imageWidths( final List<Integer> imageWidths )
+    {
+        this.imageWidths = imageWidths;
+        return this;
+    }
+
     @Override
     public ProcessHtmlParams setAsMap( final Multimap<String, String> map )
     {
         super.setAsMap( map );
         value( singleValue( map, "_value" ) );
+        imageWidths( Objects.requireNonNullElse( map.removeAll( "_imageWidths" ), List.<String>of() ).
+            stream().
+            map( Integer::parseInt ).
+            collect( Collectors.toUnmodifiableList() ) );
         getParams().putAll( map );
         return this;
     }
@@ -37,5 +58,6 @@ public final class ProcessHtmlParams
     {
         super.buildToString( helper );
         helper.add( "value", this.value );
+        helper.add( "imageWidths", this.imageWidths );
     }
 }