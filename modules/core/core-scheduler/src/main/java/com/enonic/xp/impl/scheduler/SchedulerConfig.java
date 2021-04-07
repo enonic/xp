ad9a19dd0d16537be56d145309c72b305352c71e
--local <vbr@enonic.com>
2021-04-07 13:38:58 +0300

    Shedule tasks from config #8719

---
+++ modules/core/core-scheduler/src/main/java/com/enonic/xp/impl/scheduler/SchedulerConfig.java
@@ -0,0 +1,10 @@
+package com.enonic.xp.impl.scheduler;
+
+import java.util.Set;
+
+import com.enonic.xp.scheduler.CreateScheduledJobParams;
+
+public interface SchedulerConfig
+{
+    Set<CreateScheduledJobParams> jobs();
+}
