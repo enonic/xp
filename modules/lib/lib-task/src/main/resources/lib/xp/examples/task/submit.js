53e682ff81cc1b7606886cb33ea90237624a64eb
bradnitski <vbr@enonic.com>
2021-05-06 22:14:36 +0300

    lib-task submitTask method #8752

---
+++ modules/lib/lib-task/src/main/resources/lib/xp/examples/task/submit.js
@@ -0,0 +1,13 @@
+var taskLib = require('/lib/xp/task.js');
+var assert = require('/lib/xp/testing');
+
+// BEGIN
+// Execute task and keep taskId for polling status
+var taskId = taskLib.submit({
+    description: 'Background task',
+    task() {
+        longRunningTask();
+    }
+});
+// END
+assert.assertEquals('7ca603c1-3b88-4009-8f30-46ddbcc4bb19', taskId);
