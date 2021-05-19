e0873c01ca6a88d11df23e531ea591c3959a7a15
bradnitski <vbr@enonic.com>
2021-05-19 14:39:37 +0300

    Move audit log endpoint to separate resource #8832

---
+++ modules/server/server-rest/src/main/java/com/enonic/xp/impl/server/rest/AuditLogResource.java
@@ -0,0 +1,50 @@
+package com.enonic.xp.impl.server.rest;
+
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import com.enonic.xp.impl.server.rest.model.CleanUpAuditLogRequestJson;
+import com.enonic.xp.impl.server.rest.task.CleanUpAuditLogCommand;
+import com.enonic.xp.jaxrs.JaxRsComponent;
+import com.enonic.xp.security.RoleKeys;
+import com.enonic.xp.task.TaskResultJson;
+import com.enonic.xp.task.TaskService;
+
+@Path("/auditlog")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@RolesAllowed(RoleKeys.ADMIN_ID)
+@Component(immediate = true, property = "group=api")
+public final class AuditLogResource
+    implements JaxRsComponent
+{
+    private TaskService taskService;
+
+    @POST
+    @Path("cleanup")
+    public TaskResultJson cleanup( final CleanUpAuditLogRequestJson params )
+    {
+        return CleanUpAuditLogCommand.create().
+            taskService( taskService ).
+            params( params ).
+            build().
+            execute();
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    @Reference
+    public void setTaskService( final TaskService taskService )
+    {
+        this.taskService = taskService;
+    }
+
+
+}
