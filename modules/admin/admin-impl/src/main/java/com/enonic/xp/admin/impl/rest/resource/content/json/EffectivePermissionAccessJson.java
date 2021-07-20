85fd5809a31b0c478b4de0d12bea58032f11fcd7
Pavel Milkevich <pavel.milkevich@gmail.com>
2021-07-20 17:17:02 +0300

    Move ContentResource endpoint #8952

--- modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/rest/resource/content/json/EffectivePermissionAccessJson.java
+++
@@ -1,8 +0,0 @@
-package com.enonic.xp.admin.impl.rest.resource.content.json;
-
-public final class EffectivePermissionAccessJson
-{
-    public Integer count;
-
-    public EffectivePermissionMemberJson[] users;
-}
