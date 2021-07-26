eb072ce40d75c97420e5779efb700b68f180c246
Pavel Milkevich <pavel.milkevich@gmail.com>
2021-07-26 16:32:10 +0300

    Move FragmentResource endpoint #8964

--- modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/rest/resource/content/CmsResourceDynamicFeature.java
+++ modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/rest/resource/content/CmsResourceDynamicFeature.java
@@ -7,7 +7,6 @@ import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.FeatureContext;
 import javax.ws.rs.ext.Provider;
 
-import com.enonic.xp.admin.impl.rest.resource.content.page.fragment.FragmentResource;
 import com.enonic.xp.admin.impl.rest.resource.schema.content.ContentTypeResource;
 import com.enonic.xp.admin.impl.rest.resource.schema.content.FilterByContentResource;
 import com.enonic.xp.admin.impl.rest.resource.schema.xdata.XDataResource;
@@ -19,9 +18,9 @@ public final class CmsResourceDynamicFeature
     implements DynamicFeature, JaxRsComponent
 {
     private final Set<Class<? extends JaxRsComponent>> supportedResources = Set.of(
-        /*ContentResource.class, ContentImageResource.class, ContentIconResource.class, ContentMediaResource.class, IssueResource.class, PageResource.class,
-         * PageTemplateResource.class*/
-        FragmentResource.class, XDataResource.class, ContentTypeResource.class, FilterByContentResource.class );
+        /*ContentResource.class, ContentImageResource.class, ContentIconResource.class, ContentMediaResource.class, IssueResource.class,
+        PageResource.class, PageTemplateResource.class, FragmentResource.class*/
+        FilterByContentResource.class, XDataResource.class, ContentTypeResource.class );
 
 
     @Override
