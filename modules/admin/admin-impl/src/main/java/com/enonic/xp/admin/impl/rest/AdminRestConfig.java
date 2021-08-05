875f061004b8854aefd668edf5dd2b8b20656c86
rymsha <rymsha@yandex.ru>
2021-08-05 10:50:01 +0200

    Migrate Admin API Config #8997

--- modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/rest/AdminRestConfig.java
+++
@@ -1,8 +0,0 @@
-package com.enonic.xp.admin.impl.rest;
-
-public @interface AdminRestConfig
-{
-    String uploadMaxFileSize() default "100mb";
-
-    String contentTypePatternMode() default "MATCH";
-}
