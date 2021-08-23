e5533899887119f6c86e5cd50257b1a6fe61a281
Pavel Milkevich <pavel.milkevich@gmail.com>
2021-08-23 10:50:30 +0300

    Move ApplicationResource endpoint #8998

--- modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/json/ItemJson.java
+++
@@ -1,9 +0,0 @@
-package com.enonic.xp.admin.impl.json;
-
-
-public interface ItemJson
-{
-    boolean getEditable();
-
-    boolean getDeletable();
-}
--- modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/rest/resource/application/json/ApplicationSuccessJson.java
+++
@@ -1,5 +0,0 @@
-package com.enonic.xp.admin.impl.rest.resource.application.json;
-
-public final class ApplicationSuccessJson
-{
-}
