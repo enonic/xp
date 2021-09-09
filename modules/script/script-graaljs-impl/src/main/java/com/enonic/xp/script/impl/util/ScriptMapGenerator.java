f5c7ba67b65f764f52359252d305bbf210c6f7be
Anatol Sialitski <asi@enonic.com>
2021-09-09 17:16:00 +0300

    Support null for rawValue in MapGenerator #9065

--- modules/script/script-graaljs-impl/src/main/java/com/enonic/xp/script/impl/util/ScriptMapGenerator.java
+++ modules/script/script-graaljs-impl/src/main/java/com/enonic/xp/script/impl/util/ScriptMapGenerator.java
@@ -47,6 +47,12 @@ final class ScriptMapGenerator
     }
 
     @Override
+    protected void putRawValueInMap( final Object map, final String key, final Object value )
+    {
+        GraalJSHelper.addToNativeObject( map, key, value );
+    }
+
+    @Override
     protected void addToArray( final Object array, final Object value )
     {
         GraalJSHelper.addToNativeArray( array, value );
