166b700ea1075d1c51fb088f43b40edfabaca6c1
Pavel Milkevich <pavel.milkevich@gmail.com>
2021-09-10 18:14:58 +0300

    Clean AuthResource endpoint from unused methods #9072

--- modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/rest/resource/auth/AuthResource.java
+++ modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/rest/resource/auth/AuthResource.java
@@ -1,21 +1,16 @@
 package com.enonic.xp.admin.impl.rest.resource.auth;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 import com.enonic.xp.admin.impl.rest.resource.ResourceConstants;
 import com.enonic.xp.admin.impl.rest.resource.auth.json.LoginResultJson;
-import com.enonic.xp.admin.impl.security.AuthHelper;
 import com.enonic.xp.context.ContextAccessor;
 import com.enonic.xp.jaxrs.JaxRsComponent;
-import com.enonic.xp.security.RoleKeys;
-import com.enonic.xp.security.SecurityService;
 import com.enonic.xp.security.auth.AuthenticationInfo;
 import com.enonic.xp.session.Session;
 
@@ -25,37 +20,6 @@ import com.enonic.xp.session.Session;
 public final class AuthResource
     implements JaxRsComponent
 {
-
-    private SecurityService securityService;
-
-    @POST
-    @Path("login")
-    public LoginResultJson login( final LoginRequest request )
-    {
-        final AuthHelper helper = new AuthHelper( this.securityService );
-        final AuthenticationInfo authInfo =
-            helper.login( request.getUser(), request.getPassword(), request.getIdProvider(), request.isRememberMe() );
-
-        if ( authInfo.isAuthenticated() && !authInfo.hasRole( RoleKeys.ADMIN_LOGIN ) && !authInfo.hasRole( RoleKeys.ADMIN ) )
-        {
-            AuthHelper.logout();
-            return new LoginResultJson( AuthenticationInfo.unAuthenticated(), "Access Denied" );
-        }
-        if ( !authInfo.isAuthenticated() )
-        {
-            return new LoginResultJson( AuthenticationInfo.unAuthenticated() );
-        }
-
-        return new LoginResultJson( authInfo );
-    }
-
-    @POST
-    @Path("logout")
-    public void logout()
-    {
-        AuthHelper.logout();
-    }
-
     @GET
     @Path("authenticated")
     public LoginResultJson isAuthenticated()
@@ -68,11 +32,5 @@ public final class AuthResource
 
         final AuthenticationInfo authInfo = ContextAccessor.current().getAuthInfo();
         return new LoginResultJson( authInfo );
-    }
-
-    @Reference
-    public void setSecurityService( final SecurityService securityService )
-    {
-        this.securityService = securityService;
     }
 }
--- modules/admin/admin-impl/src/test/java/com/enonic/xp/admin/impl/rest/resource/auth/AuthResourceTest.java
+++ modules/admin/admin-impl/src/test/java/com/enonic/xp/admin/impl/rest/resource/auth/AuthResourceTest.java
@@ -4,23 +4,16 @@ import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
 
-import javax.ws.rs.core.MediaType;
-
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import com.enonic.xp.admin.impl.rest.resource.AdminResourceTestSupport;
 import com.enonic.xp.context.ContextAccessor;
 import com.enonic.xp.context.LocalScope;
-import com.enonic.xp.security.IdProvider;
 import com.enonic.xp.security.IdProviderKey;
-import com.enonic.xp.security.IdProviders;
 import com.enonic.xp.security.PrincipalKey;
 import com.enonic.xp.security.RoleKeys;
-import com.enonic.xp.security.SecurityService;
 import com.enonic.xp.security.User;
 import com.enonic.xp.security.auth.AuthenticationInfo;
-import com.enonic.xp.security.auth.AuthenticationToken;
 import com.enonic.xp.session.SessionMock;
 
 public class AuthResourceTest
@@ -30,126 +23,10 @@ public class AuthResourceTest
 
     private static final Clock clock = Clock.fixed( NOW, ZoneId.of( "UTC" ) );
 
-    private SecurityService securityService;
-
     @Override
     protected Object getResourceInstance()
     {
-        securityService = Mockito.mock( SecurityService.class );
-
-        final AuthResource resource = new AuthResource();
-
-        securityService = Mockito.mock( SecurityService.class );
-        resource.setSecurityService( securityService );
-        final IdProvider us1 = IdProvider.create().key( IdProviderKey.from( "remote" ) ).displayName( "Remote" ).build();
-        final IdProvider us2 = IdProvider.create().key( IdProviderKey.system() ).displayName( "System" ).build();
-        final IdProviders idProviders = IdProviders.from( us1, us2 );
-        Mockito.when( securityService.getIdProviders() ).thenReturn( idProviders );
-
-        return resource;
-    }
-
-    @Test
-    public void testLoginWithUsernameSuccess()
-        throws Exception
-    {
-        final User user = User.create().
-            key( PrincipalKey.ofUser( IdProviderKey.system(), "user1" ) ).
-            displayName( "User 1" ).
-            modifiedTime( Instant.now( clock ) ).
-            email( "user1@enonic.com" ).
-            login( "user1" ).
-            build();
-        final AuthenticationInfo authInfo = AuthenticationInfo.create().user( user ).principals( RoleKeys.ADMIN_LOGIN ).build();
-        Mockito.when( securityService.authenticate( Mockito.any( AuthenticationToken.class ) ) ).
-            thenReturn( authInfo );
-
-        String jsonString = request().path( "auth/login" ).
-            entity( "{\"user\":\"user1\",\"password\":\"password\",\"rememberMe\":false}", MediaType.APPLICATION_JSON_TYPE ).
-            post().getAsString();
-
-        assertJson( "login.json", jsonString );
-    }
-
-    @Test
-    public void testLoginWithEmailSuccess()
-        throws Exception
-    {
-        final User user = User.create().
-            key( PrincipalKey.ofUser( IdProviderKey.system(), "user1" ) ).
-            displayName( "User 1" ).
-            modifiedTime( Instant.now( clock ) ).
-            email( "user1@enonic.com" ).
-            login( "user1" ).
-            build();
-        final AuthenticationInfo authInfo = AuthenticationInfo.create().user( user ).principals( RoleKeys.ADMIN_LOGIN ).build();
-        Mockito.when( securityService.authenticate( Mockito.any( AuthenticationToken.class ) ) ).
-            thenReturn( authInfo );
-
-        String jsonString = request().path( "auth/login" ).
-            entity( "{\"user\":\"user1@enonic.com\",\"password\":\"password\",\"rememberMe\":false}", MediaType.APPLICATION_JSON_TYPE ).
-            post().getAsString();
-
-        assertJson( "login.json", jsonString );
-    }
-
-    @Test
-    public void testLoginWithUserNameIdProvider()
-        throws Exception
-    {
-        final User user = User.create().
-            key( PrincipalKey.ofUser( IdProviderKey.system(), "user1" ) ).
-            displayName( "User 1" ).
-            modifiedTime( Instant.now( clock ) ).
-            email( "user1@enonic.com" ).
-            login( "user1" ).
-            build();
-        final AuthenticationInfo authInfo = AuthenticationInfo.create().user( user ).principals( RoleKeys.ADMIN_LOGIN ).build();
-        Mockito.when( securityService.authenticate( Mockito.any( AuthenticationToken.class ) ) ).
-            thenReturn( authInfo );
-
-        String jsonString = request().path( "auth/login" ).
-            entity( "{\"user\":\"system\\\\user1\",\"password\":\"password\",\"rememberMe\":false}", MediaType.APPLICATION_JSON_TYPE ).
-            post().getAsString();
-
-        assertJson( "login.json", jsonString );
-    }
-
-    @Test
-    public void testLoginFail()
-        throws Exception
-    {
-        final AuthenticationInfo authInfo = AuthenticationInfo.unAuthenticated();
-        Mockito.when( securityService.authenticate( Mockito.any( AuthenticationToken.class ) ) ).
-            thenReturn( authInfo );
-
-        String jsonString = request().path( "auth/login" ).
-            entity( "{\"user\":\"user1\",\"password\":\"password\",\"rememberMe\":false}", MediaType.APPLICATION_JSON_TYPE ).
-            post().getAsString();
-
-        assertJson( "login_failed.json", jsonString );
-    }
-
-    @Test
-    public void testLoginUserWithoutLoginAdminRoleFail()
-        throws Exception
-    {
-        final User user = User.create().
-            key( PrincipalKey.ofUser( IdProviderKey.system(), "user1" ) ).
-            displayName( "User 1" ).
-            modifiedTime( Instant.now( clock ) ).
-            email( "user1@enonic.com" ).
-            login( "user1" ).
-            build();
-        final AuthenticationInfo authInfo = AuthenticationInfo.create().user( user ).build();
-        Mockito.when( securityService.authenticate( Mockito.any( AuthenticationToken.class ) ) ).
-            thenReturn( authInfo );
-
-        String jsonString = request().path( "auth/login" ).
-            entity( "{\"user\":\"user1\",\"password\":\"password\",\"rememberMe\":false}", MediaType.APPLICATION_JSON_TYPE ).
-            post().getAsString();
-
-        assertJson( "login_access_denied.json", jsonString );
+        return new AuthResource();
     }
 
     @Test
