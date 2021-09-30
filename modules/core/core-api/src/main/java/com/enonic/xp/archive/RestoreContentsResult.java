ba3cfe1a16193642f5f811297463c8c83df856bd
Bradnitski <vbr@enonic.com>
2021-09-30 18:05:53 +0300

    Log archive and restore commands #9110

--- modules/core/core-api/src/main/java/com/enonic/xp/archive/RestoreContentsResult.java
+++ modules/core/core-api/src/main/java/com/enonic/xp/archive/RestoreContentsResult.java
@@ -1,19 +1,24 @@
 package com.enonic.xp.archive;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 
 import com.enonic.xp.annotation.PublicApi;
 import com.enonic.xp.content.ContentId;
 import com.enonic.xp.content.ContentIds;
+import com.enonic.xp.content.ContentPath;
 
 @PublicApi
 public class RestoreContentsResult
 {
     private final ContentIds restoredContents;
 
+    private final ContentPath parentPath;
+
     private RestoreContentsResult( Builder builder )
     {
         this.restoredContents = ContentIds.from( builder.restoredContents.build() );
+        this.parentPath = builder.parentPath;
     }
 
     public static Builder create()
@@ -26,9 +31,16 @@ public class RestoreContentsResult
         return restoredContents;
     }
 
+    public ContentPath getParentPath()
+    {
+        return parentPath;
+    }
+
     public static final class Builder
     {
         private final ImmutableList.Builder<ContentId> restoredContents = ImmutableList.builder();
+
+        private ContentPath parentPath;
 
         private Builder()
         {
@@ -46,8 +58,20 @@ public class RestoreContentsResult
             return this;
         }
 
+        public Builder parentPath( final ContentPath parentPath )
+        {
+            this.parentPath = parentPath;
+            return this;
+        }
+
+        private void validate()
+        {
+            Preconditions.checkNotNull( parentPath, "parentPath must be set" );
+        }
+
         public RestoreContentsResult build()
         {
+            validate();
             return new RestoreContentsResult( this );
         }
     }
