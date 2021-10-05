34c6dfd84760b83201c68d780b254c8143e7654d
Bradnitski <vbr@enonic.com>
2021-10-05 12:27:24 +0300

    Localize content when restoring in layer's archive #9113

--- modules/core/core-content/src/main/java/com/enonic/xp/core/impl/content/ContentNodeTranslator.java
+++ modules/core/core-content/src/main/java/com/enonic/xp/core/impl/content/ContentNodeTranslator.java
@@ -46,10 +46,6 @@ public class ContentNodeTranslator
 
     public Content fromNode( final Node node, final boolean resolveHasChildren )
     {
-        return fromNode( node, resolveHasChildren, true );
-    }
-
-    public Content fromNode(final Node node, final boolean resolveHasChildren, final boolean checkContext) {
         final boolean hasChildren = resolveHasChildren && this.nodeService.hasChildren( node );
         return doTranslate( node, hasChildren );
     }
