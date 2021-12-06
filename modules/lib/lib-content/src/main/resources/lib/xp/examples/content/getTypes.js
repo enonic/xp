452a71220f3215006ae5c4d61bd200b2aa8aa074
Bradnitski <vbr@enonic.com>
2021-12-06 17:09:50 +0300

    Revert "#9224 assertJson in testing framework does not notice non-json objects"

--- modules/lib/lib-content/src/main/resources/lib/xp/examples/content/getTypes.js
+++ modules/lib/lib-content/src/main/resources/lib/xp/examples/content/getTypes.js
@@ -24,7 +24,6 @@ var expected = [
         'allowChildContent': true,
         'displayNameExpression': '${title} ${author}',
         'icon': {
-            'data': {},
             'mimeType': 'image/png',
             'modifiedTime': '2016-01-01T12:00:00Z'
         },
