243452ee171fffdf57362b57f61ea2afbdef7cc8
Anatol Sialitski <asi@enonic.com>
2021-12-15 12:12:01 +0300

    #9269 Trace is failed due to NPE if content in request is not specified

--- modules/portal/portal-impl/src/main/java/com/enonic/xp/portal/impl/handler/mapping/MappingHandlerWorker.java
+++ modules/portal/portal-impl/src/main/java/com/enonic/xp/portal/impl/handler/mapping/MappingHandlerWorker.java
@@ -45,7 +45,7 @@ final class MappingHandlerWorker
         final Trace trace = Tracer.current();
         if ( trace != null )
         {
-            trace.put( "contentPath", this.request.getContent().getPath().toString() );
+            trace.put( "contentPath", this.request.getContentPath().toString() );
             trace.put( "type", "mapping" );
         }
 
