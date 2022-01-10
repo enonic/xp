098d06fc723a02d1c2a2707b3a49c18f5c47685f
rymsha <rymsha@yandex.ru>
2022-01-10 18:03:16 +0100

    Content-Security-Policy for site preview #9293

--- modules/portal/portal-impl/src/main/java/com/enonic/xp/portal/impl/PortalConfig.java
+++ modules/portal/portal-impl/src/main/java/com/enonic/xp/portal/impl/PortalConfig.java
@@ -7,4 +7,6 @@ public @interface PortalConfig
     String media_public_cacheControl() default "public, max-age=31536000, immutable";
 
     String media_private_cacheControl() default "private, max-age=31536000, immutable";
+
+    String page_defaultContentSecurityPolicy() default "default-src 'self'; object-src 'none'; style-src 'self' 'unsafe-inline'";
 }
