ce80c17a40757c31920e6260aa0215e31529af83
Bradnitski <vbr@enonic.com>
2022-01-25 11:10:20 +0300

    'Sort' reset must update manualOrderValue of the children #9305

--- modules/core/core-content/src/main/java/com/enonic/xp/core/impl/content/SortedEventSyncCommand.java
+++ modules/core/core-content/src/main/java/com/enonic/xp/core/impl/content/SortedEventSyncCommand.java
@@ -1,11 +1,14 @@
 package com.enonic.xp.core.impl.content;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import com.google.common.base.Preconditions;
 
 import com.enonic.xp.content.Content;
 import com.enonic.xp.content.ContentInheritType;
+import com.enonic.xp.content.FindContentByParentParams;
 import com.enonic.xp.content.SetContentChildOrderParams;
 
 final class SortedEventSyncCommand
@@ -42,6 +45,36 @@ final class SortedEventSyncCommand
 
                     contentService.setChildOrder( sortParams );
                 }
+                if ( needToReorderChildren( content.getSourceContent() ) )
+                {
+                    final List<ContentToSync> childrenToSync = contentService.findByParent(
+                        FindContentByParentParams.create().parentId( content.getTargetContent().getId() ).size( -1 ).build() )
+                        .getContents()
+                        .stream()
+                        .map( currContent -> {
+                            final Content sourceContent = content.getSourceContext()
+                                .callWith( () -> contentService.contentExists( currContent.getId() ) ? contentService.getById(
+                                    currContent.getId() ) : null );
+
+                            return sourceContent != null ? ContentToSync.create()
+                                .sourceContext( content.getSourceContext() )
+                                .targetContext( content.getTargetContext() )
+                                .sourceContent( sourceContent )
+                                .targetContent( currContent )
+                                .build() : null;
+                        } )
+                        .filter( Objects::nonNull )
+                        .collect( Collectors.toList() );
+
+                    if ( !childrenToSync.isEmpty() )
+                    {
+                        ManualOrderUpdatedEventSyncCommand.create()
+                            .contentService( contentService )
+                            .params( ContentEventSyncCommandParams.create().addContents( childrenToSync ).build() )
+                            .build()
+                            .sync();
+                    }
+                }
             }
         } );
     }
@@ -54,6 +87,11 @@ final class SortedEventSyncCommand
     private boolean needToSort( final Content sourceContent, final Content targetContent )
     {
         return !Objects.equals( sourceContent.getChildOrder(), targetContent.getChildOrder() );
+    }
+
+    private boolean needToReorderChildren( final Content sourceContent )
+    {
+        return sourceContent.getChildOrder().isManualOrder();
     }
 
     public static class Builder
