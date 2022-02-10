e553325829b89c13f5fa0b3328a2edb403357cad
Alan Semenov <ase@enonic.com>
2022-02-10 14:57:19 +0100

    Content-Security-Policy for site preview (#9293)

--- modules/portal/portal-impl/src/main/java/com/enonic/xp/portal/impl/PortalConfig.java
+++ modules/portal/portal-impl/src/main/java/com/enonic/xp/portal/impl/PortalConfig.java
@@ -8,5 +8,5 @@ public @interface PortalConfig
 
     String media_private_cacheControl() default "private, max-age=31536000, immutable";
 
-    String page_defaultContentSecurityPolicy() default "default-src 'self'; object-src 'none'; style-src 'self' 'unsafe-inline'";
+    String page_defaultContentSecurityPolicy() default "default-src 'self' 'unsafe-eval'; object-src 'none'; style-src 'self' 'unsafe-inline'";
 }
