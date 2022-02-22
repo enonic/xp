71dc233f5aeec7cb1c9b93dd5dc3311827b62ad5
rymsha <rymsha@yandex.ru>
2022-02-22 13:33:09 +0100

    Missing JS for page descriptor should fail gracefully #9370

--- modules/portal/portal-impl/src/main/java/com/enonic/xp/portal/impl/rendering/ControllerMappingRenderer.java
+++ modules/portal/portal-impl/src/main/java/com/enonic/xp/portal/impl/rendering/ControllerMappingRenderer.java
@@ -1,5 +1,6 @@
 package com.enonic.xp.portal.impl.rendering;
 
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -8,13 +9,20 @@ import com.enonic.xp.portal.PortalResponse;
 import com.enonic.xp.portal.impl.processor.ProcessorChainResolver;
 import com.enonic.xp.portal.impl.processor.ResponseProcessorExecutor;
 import com.enonic.xp.portal.postprocess.PostProcessor;
-import com.enonic.xp.portal.script.PortalScriptService;
 import com.enonic.xp.site.mapping.ControllerMappingDescriptor;
 
 @Component(immediate = true, service = Renderer.class)
 public final class ControllerMappingRenderer
     extends PostProcessingRenderer<ControllerMappingDescriptor>
 {
+    @Activate
+    public ControllerMappingRenderer( @Reference final PostProcessor postProcessor,
+                                      @Reference final ResponseProcessorExecutor processorExecutor,
+                                      @Reference final ProcessorChainResolver processorChainResolver )
+    {
+        super( postProcessor, processorExecutor, processorChainResolver );
+    }
+
     @Override
     public Class<ControllerMappingDescriptor> getType()
     {
@@ -25,24 +33,5 @@ public final class ControllerMappingRenderer
     public PortalResponse doRender( final ControllerMappingDescriptor controllerMappingDescriptor, final PortalRequest portalRequest )
     {
         return portalRequest.getControllerScript().execute( portalRequest );
-    }
-
-    @Reference
-    public void setPostProcessor( final PostProcessor postProcessor )
-    {
-        this.postProcessor = postProcessor;
-    }
-
-    @Reference
-    public void setScriptService( final PortalScriptService scriptService )
-    {
-        this.processorExecutor = new ResponseProcessorExecutor( scriptService );
-    }
-
-
-    @Reference
-    public void setFilterChainResolver( final ProcessorChainResolver filterChainResolver )
-    {
-        this.processorChainResolver = filterChainResolver;
     }
 }
