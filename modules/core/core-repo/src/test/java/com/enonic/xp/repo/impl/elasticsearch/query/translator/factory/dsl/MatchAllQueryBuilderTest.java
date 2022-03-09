15bff669a9b8f078080e77eb0cd2d32106889b75
Bradnitski <vbr@enonic.com>
2022-03-09 14:46:39 +0100

    Add matchAll query expression #9387

---
+++ modules/core/core-repo/src/test/java/com/enonic/xp/repo/impl/elasticsearch/query/translator/factory/dsl/MatchAllQueryBuilderTest.java
@@ -0,0 +1,36 @@
+package com.enonic.xp.repo.impl.elasticsearch.query.translator.factory.dsl;
+
+import org.elasticsearch.index.query.QueryBuilder;
+import org.junit.jupiter.api.Test;
+
+import com.enonic.xp.data.PropertyTree;
+
+public class MatchAllQueryBuilderTest
+    extends QueryBuilderTest
+{
+
+    @Test
+    public void match_all()
+        throws Exception
+    {
+        test( "match_all" );
+    }
+
+    @Test
+    public void boosted()
+        throws Exception
+    {
+        test( "boosted" );
+    }
+
+    private void test( final String fileName )
+        throws Exception
+    {
+        final String queryString = load( "matchAll/query/" + fileName + ".json" );
+
+        final PropertyTree dslExpression = readJson( queryString );
+        final QueryBuilder builder = new MatchAllQueryBuilder( dslExpression.getSet( "matchAll" ) ).create();
+
+        assertJson( "matchAll/result/" + fileName + ".json", builder.toString() );
+    }
+}
