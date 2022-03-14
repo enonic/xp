28bc4169ad51b715d5b53f1fe3475f7c54a081a7
Bradnitski <vbr@enonic.com>
2022-03-14 16:03:23 +0100

    Dynamic Schema API #9391

---
+++ modules/lib/lib-schema/src/main/java/com/enonic/xp/lib/schema/CreateDynamicSchemaHandler.java
@@ -0,0 +1,55 @@
+package com.enonic.xp.lib.schema;
+
+import java.util.function.Supplier;
+
+import com.enonic.xp.page.DescriptorKey;
+import com.enonic.xp.resource.CreateDynamicSchemaParams;
+import com.enonic.xp.resource.DynamicSchemaService;
+import com.enonic.xp.resource.DynamicSchemaType;
+import com.enonic.xp.resource.Resource;
+import com.enonic.xp.script.bean.BeanContext;
+import com.enonic.xp.script.bean.ScriptBean;
+
+public class CreateDynamicSchemaHandler
+    implements ScriptBean
+{
+    private String key;
+
+    private String type;
+
+    private String resource;
+
+    private Supplier<DynamicSchemaService> dynamicSchemaServiceSupplier;
+
+    public void setKey( final String key )
+    {
+        this.key = key;
+    }
+
+    public void setType( final String type )
+    {
+        this.type = type;
+    }
+
+    public void setResource( final String resource )
+    {
+        this.resource = resource;
+    }
+
+    public Resource execute()
+    {
+        final CreateDynamicSchemaParams params = CreateDynamicSchemaParams.create()
+            .descriptorKey( DescriptorKey.from( key ) )
+            .type( DynamicSchemaType.valueOf( type ) )
+            .resource( resource )
+            .build();
+
+        return dynamicSchemaServiceSupplier.get().create( params );
+    }
+
+    @Override
+    public void initialize( final BeanContext context )
+    {
+        dynamicSchemaServiceSupplier = context.getService( DynamicSchemaService.class );
+    }
+}
