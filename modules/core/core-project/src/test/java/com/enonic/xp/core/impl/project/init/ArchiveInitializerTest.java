8212c590a7aed09621da3dc23ef343a683d765e2
Bradnitski <vbr@enonic.com>
2022-04-11 17:40:41 +0200

    BranchNotFoundException on cluster initialization #9426

---
+++ modules/core/core-project/src/test/java/com/enonic/xp/core/impl/project/init/ArchiveInitializerTest.java
@@ -0,0 +1,42 @@
+package com.enonic.xp.core.impl.project.init;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.enonic.xp.branch.Branch;
+import com.enonic.xp.index.IndexService;
+import com.enonic.xp.node.NodeService;
+import com.enonic.xp.project.ProjectName;
+import com.enonic.xp.repository.BranchNotFoundException;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ArchiveInitializerTest
+{
+    @Mock
+    private NodeService nodeService;
+
+    @Mock(stubOnly = true)
+    private IndexService indexService;
+
+
+    @Test
+    public void testBranchNotFound()
+    {
+        when( nodeService.getByPath( Mockito.any() ) ).thenThrow( new BranchNotFoundException( Branch.from( "draft" ) ) );
+
+        final ProjectName projectName = ProjectName.from( "my-project" );
+
+        Assertions.assertFalse( ArchiveInitializer.create()
+                                    .setIndexService( indexService )
+                                    .setNodeService( nodeService )
+                                    .repositoryId( projectName.getRepoId() )
+                                    .build()
+                                    .isInitialized() );
+    }
+}
