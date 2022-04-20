3b1947df6846566a3b2f64da3e400287f2d5f98c
Mikita Taukachou <edloidas@gmail.com>
2022-04-21 00:14:48 +0300

    Add script-impl types #9434
    Added npm package.

---
+++ modules/script/script-impl/index.d.ts
@@ -0,0 +1,26 @@
+/// <reference path="global.d.ts" />
+
+declare const app: {
+    name: string;
+    version: string;
+    config: Record<string, string>;
+};
+
+declare const log: {
+    info: (...args: unknown[]) => void;
+    warning: (...args: unknown[]) => void;
+    error: (...args: unknown[]) => void;
+};
+
+declare const __: {
+    newBean: <T = unknown>(bean: string) => T;
+    toNativeObject: <T = unknown>(beanResult: T) => T;
+    toScriptValue: <T = Object>(value: T) => T;
+    disposer: (callback: (...args: unknown[]) => unknown) => void;
+};
+
+declare const require: <Key extends keyof XpLibraries | string = string>(path: Key) =>
+    Key extends keyof XpLibraries ? XpLibraries[Key] : any;
+
+declare const __non_webpack_require__: <Key extends keyof XpLibraries | string = string>(path: Key) =>
+    Key extends keyof XpLibraries ? XpLibraries[Key] : any;
