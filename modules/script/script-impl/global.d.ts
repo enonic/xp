62aa3312005f935580b2c757d220abfc3d33c03e
Mikita Taukachou <edloidas@gmail.com>
2022-04-27 11:30:16 +0300

    Add script-impl types #9434
    Simplified global definitions of XpLibraries by redifining this
    interface directly.
    Fixed typo with using Object that includes primitive types instead of
    object.

--- modules/script/script-impl/global.d.ts
+++ modules/script/script-impl/global.d.ts
@@ -1,12 +1,14 @@
-/*
-This project don't include XP libraries definitions and need these interfaces to properly
-map imports via require or __non_webpack_require__.
-
-Warning: all of these interfaces are empty. If you want type definitions for various libraries,
-you need to add types for there libraries, where those interfaces are redefined.
+/**
+ * This project don't include XP libraries definitions and need this interface
+ * to properly map imports via require or __non_webpack_require__.
+ *
+ * Warning: this interface is empty. If you want type definitions for various
+ * libraries, you need to redefine this interface in your library and add a new
+ * property with library path.
+ *
+ * @example
+ * interface XpLibraries {
+ *     '/lib/xp/event': typeof import('./event'),
+ * }
 */
-interface XpEventLibrary {}
-
-interface XpLibraries {
-    '/lib/xp/event': XpEventLibrary;
-}
+interface XpLibraries {}
