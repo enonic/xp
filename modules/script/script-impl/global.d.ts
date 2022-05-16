2ed84daeff7819ed1dbbbf5ca200dcc60e745397
Mikita Taukachou <edloidas@gmail.com>
2022-05-16 14:25:56 +0300

    POC: Migrate Javascript API to Typescript #9450
    Changed the licence type for libs to Apache-2.0.
    Removed watch task, since types will later be copied to root folder.
    Removed `__non_webpack_require__` from globals. It must be added in
    the webpack-related apps, like starter-webpack.
    Made XpRequire global to be used in other projects.
    Added more XP global events.
    Removed `/build` folder from scanning for types.

--- modules/script/script-impl/global.d.ts
+++ modules/script/script-impl/global.d.ts
@@ -1,6 +1,6 @@
 /**
  * This project don't include XP libraries definitions and need this interface
- * to properly map imports via require or __non_webpack_require__.
+ * to properly map imports via require.
  *
  * Warning: this interface is empty. If you want type definitions for various
  * libraries, you need to redefine this interface in your library in global
