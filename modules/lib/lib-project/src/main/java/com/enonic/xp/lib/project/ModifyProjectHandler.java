043203050e57f20f643e1ac7e46f75b30a0df825
Bradnitski <vbr@enonic.com>
2022-06-02 18:58:51 +0300

    Add applications field to project #9490

--- modules/lib/lib-project/src/main/java/com/enonic/xp/lib/project/ModifyProjectHandler.java
+++ modules/lib/lib-project/src/main/java/com/enonic/xp/lib/project/ModifyProjectHandler.java
@@ -1,9 +1,13 @@
 package com.enonic.xp.lib.project;
 
+import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 import com.google.common.base.Preconditions;
 
+import com.enonic.xp.app.ApplicationKey;
+import com.enonic.xp.app.ApplicationKeys;
 import com.enonic.xp.lib.project.command.ApplyProjectLanguageCommand;
 import com.enonic.xp.lib.project.command.GetProjectLanguageCommand;
 import com.enonic.xp.lib.project.command.GetProjectReadAccessCommand;
@@ -24,6 +28,8 @@ public final class ModifyProjectHandler
 
     private Locale language;
 
+    private ApplicationKeys applications;
+
     @Override
     protected ProjectMapper doExecute()
     {
@@ -35,45 +41,46 @@ public final class ModifyProjectHandler
         Locale projectLanguage;
         if ( this.language != null )
         {
-            projectLanguage = ApplyProjectLanguageCommand.create().
-                projectName( this.id ).
-                language( this.language ).
-                contentService( this.contentService ).
-                build().
-                execute();
+            projectLanguage = ApplyProjectLanguageCommand.create()
+                .projectName( this.id )
+                .language( this.language )
+                .contentService( this.contentService )
+                .build()
+                .execute();
         }
         else
         {
-            projectLanguage = GetProjectLanguageCommand.create().
-                projectName( this.id ).
-                contentService( this.contentService ).
-                build().
-                execute();
+            projectLanguage =
+                GetProjectLanguageCommand.create().projectName( this.id ).contentService( this.contentService ).build().execute();
         }
 
-        final Boolean isPublic = GetProjectReadAccessCommand.create().
-            projectName( this.id ).
-            contentService( this.contentService ).
-            build().
-            execute();
+        final Boolean isPublic =
+            GetProjectReadAccessCommand.create().projectName( this.id ).contentService( this.contentService ).build().execute();
 
         final ProjectPermissions projectPermissions = this.projectService.getPermissions( this.id );
 
-        return ProjectMapper.create().
-            setProject( project ).
-            setLanguage( projectLanguage ).
-            setProjectPermissions( projectPermissions ).
-            setIsPublic( isPublic ).
-            build();
+        return ProjectMapper.create()
+            .setProject( project )
+            .setLanguage( projectLanguage )
+            .setProjectPermissions( projectPermissions )
+            .setIsPublic( isPublic )
+            .build();
     }
 
     private ModifyProjectParams modifyProjectParams( final Project projectBeforeUpdate )
     {
-        return ModifyProjectParams.create().
-            name( this.id ).
-            displayName( this.displayName != null ? this.displayName : projectBeforeUpdate.getDisplayName() ).
-            description( this.description != null ? this.description : projectBeforeUpdate.getDescription() ).
-            build();
+        final ModifyProjectParams.Builder params = ModifyProjectParams.create()
+            .name( this.id )
+            .displayName( this.displayName != null ? this.displayName : projectBeforeUpdate.getDisplayName() )
+            .description( this.description != null ? this.description : projectBeforeUpdate.getDescription() );
+
+        final List<ApplicationKey> applications = this.applications != null
+            ? this.applications.stream().collect( Collectors.toList() )
+            : projectBeforeUpdate.getApplications().getList();
+
+        applications.forEach( params::addApplication );
+
+        return params.build();
     }
 
     @Override
@@ -100,5 +107,10 @@ public final class ModifyProjectHandler
     public void setLanguage( final String value )
     {
         this.language = buildLanguage( value );
+    }
+
+    public void setApplications( final String[] value )
+    {
+        this.applications = value != null ? ApplicationKeys.from( value ) : null;
     }
 }
