abbca69ac166eb37493e0fd186d176ffe02d5d37
Mikita Taukachou <edloidas@gmail.com>
2022-06-29 16:19:17 +0300

    Make package.json in libs generatable #9477
    Added version insertion.

---
+++ modules/lib/utils/copyPackage.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const fs = require('fs');
+const glob = require('glob');
+
+// TODO: Replace with `xpVersion()` before the first actual release
+const XP_VERSION = '0.1.0';
+
+const PACKAGE_TEMPLATE = fs.readFileSync(path.resolve(__dirname, 'templates/package.template.json')).toString();
+
+function xpVersion() {
+    const properties = fs.readFileSync(path.resolve('../../gradle.properties')).toString();
+    const [, version] = /^version\s*=\s*(.+)$/m.exec(properties) ?? [];
+    return version;
+}
+
+void function copyPackages() {
+    glob.sync('lib-*').forEach(async (libPath) => {
+        const libName = libPath.substr(4);
+
+        const hasTs = fs.existsSync(path.join(libPath, `src/main/resources/lib/xp/${libName}.ts`));
+        if (!hasTs) {
+            return;
+        }
+
+        const packageData = PACKAGE_TEMPLATE
+            .replaceAll(/%VERSION%/g, XP_VERSION)
+            .replaceAll(/%NAME%/g, libName);
+
+        fs.writeFileSync(path.join(libPath, 'package.json'), packageData);
+    });
+}();
