9b01ff56ba03da33a434dedc235b78802ca2cef3
Bradnitski <vbr@enonic.com>
2022-07-01 10:27:43 +0200

    add project config to portalLib getSiteConfig method #9515

--- modules/lib/lib-portal/src/test/java/com/enonic/xp/lib/portal/current/GetCurrentSiteConfigScriptTest.java
+++ modules/lib/lib-portal/src/test/java/com/enonic/xp/lib/portal/current/GetCurrentSiteConfigScriptTest.java
@@ -1,9 +1,13 @@
 package com.enonic.xp.lib.portal.current;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import com.enonic.xp.content.Content;
+import com.enonic.xp.context.ContextAccessor;
 import com.enonic.xp.lib.portal.TestDataFixtures;
+import com.enonic.xp.project.Project;
+import com.enonic.xp.project.ProjectName;
 import com.enonic.xp.site.Site;
 import com.enonic.xp.testing.ScriptTestSupport;
 
@@ -50,5 +54,14 @@ public class GetCurrentSiteConfigScriptTest
         this.portalRequest.setSite( site );
 
         runScript( "/lib/xp/examples/portal/getSiteConfig.js" );
+    }
+
+    @Test
+    public void configFromProject()
+    {
+        final Project project = TestDataFixtures.newDefaultProject().build();
+        Mockito.when( projectService.get( ProjectName.from( ContextAccessor.current().getRepositoryId() ) ) ).thenReturn( project );
+
+        runFunction( "/test/getCurrentSiteConfig-test.js", "configFromProject" );
     }
 }
