f8d6327450658184b038c66b7542d2207595dbf5
Anatol Sialitski <asi@enonic.com>
2022-07-28 16:34:53 +0200

    Replace Java.type with bean call #9590

---
+++ modules/lib/lib-common/src/main/java/com/enonic/xp/lib/common/NamePrettyfierHandler.java
@@ -0,0 +1,11 @@
+package com.enonic.xp.lib.common;
+
+import com.enonic.xp.name.NamePrettyfier;
+
+public class NamePrettyfierHandler
+{
+    public String create( String text )
+    {
+        return NamePrettyfier.create( text );
+    }
+}
