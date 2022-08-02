ffac2c5bdda64aaad23f2b227ba1e70344ae37f5
Anatol Sialitski <asi@enonic.com>
2022-08-02 18:53:33 +0200

    Migrate Javascript API to Typescript lib-value #9549

---
+++ modules/lib/lib-value/src/main/java/com/enonic/xp/lib/value/ReferenceHandler.java
@@ -0,0 +1,11 @@
+package com.enonic.xp.lib.value;
+
+import com.enonic.xp.util.Reference;
+
+public class ReferenceHandler
+{
+    public Reference from( String value )
+    {
+        return Reference.from( value );
+    }
+}
