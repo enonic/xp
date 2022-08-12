42d1a55b73e2032a677bdda85feb482e162228ef
Anatol Sialitski <asi@enonic.com>
2022-08-12 12:46:38 +0200

    lib-app migrate to TypeScript #9632

---
+++ modules/lib/lib-app/README.md
@@ -0,0 +1,97 @@
+# Enonic XP lib-app TS types
+
+> TypeScript definitions for `lib-app` library of Enonic XP
+
+## Install
+
+```bash
+npm i --save-dev @enonic/lib-app
+```
+
+## Use
+
+Add the corresponding types to your `tsconfig.json` file that is used for application's server-side TypeScript code.
+
+`tsconfig.json`
+
+```json
+{
+  "compilerOptions": {
+    "types": [
+      "@enonic-types/lib-app"
+    ]
+  }
+}
+```
+
+### Require and custom imports
+
+To make `require` work out of box, you must install and add the `@enonic-types/global` types. Aside from providing definitions for XP global
+objects, e.g. `log`, `app`, `__`, etc, requiring library by the default path will return typed object.
+
+`tsconfig.json`
+
+```diff
+{
+  "compilerOptions": {
+    "types": [
++     "@enonic-types/global"
+      "@enonic-types/lib-app"
+    ]
+  }
+}
+```
+
+`example.ts`
+
+```ts
+const {
+    createVirtualApplication,
+    deleteVirtualApplication,
+    get,
+    list,
+    getDescriptor,
+    hasVirtual,
+    hasReal
+} = require('/lib/xp/app');
+```
+
+More detailed explanation on how it works and how to type custom import function can be
+found [here](https://github.com/enonic/xp/tree/master/modules/lib/typescript/README.md).
+
+### ES6-style import
+
+If you are planning to use `import` in your code and transpile it with the default `tsc` TypeScript compiler, you'll need to add proper
+types mapping to your configuration.
+
+`tsconfig.json`
+
+```diff
+{
+  "compilerOptions": {
+    "types": [
+      "@enonic-types/lib-app"
+    ]
++   "baseUrl": "./",
++   "paths": {
++     "/lib/xp/app": ["node_modules/@enonic-types/lib-app"],
++   }
+  }
+}
+```
+
+`example.ts`
+
+```ts
+import {
+    createVirtualApplication,
+    deleteVirtualApplication,
+    get,
+    list,
+    getDescriptor,
+    hasVirtual,
+    hasReal
+} from '/lib/xp/app';
+```
+
+Setting `baseUrl` and `paths` will allow the `tsc` to keep the valid paths in the resulting JavaScript files.
---
+++ modules/lib/lib-app/src/main/resources/lib/xp/app.ts
@@ -0,0 +1,215 @@
+/**
+ * Application related functions.
+ *
+ * @example
+ * var schema = require('/lib/xp/app');
+ *
+ * @module app
+ */
+
+declare global {
+    interface XpLibraries {
+        '/lib/xp/app': typeof import('./app');
+    }
+}
+
+function checkRequired<T extends object>(obj: T, name: keyof T): void {
+    if (obj == null || obj[name] === null) {
+        throw `Parameter '${String(name)}' is required`;
+    }
+}
+
+export interface CreateVirtualApplicationParams {
+    key: string;
+}
+
+export interface Application {
+    key: string;
+    displayName: string | null;
+    vendorName: string | null;
+    vendorUrl: string | null;
+    url: string | null;
+    version: string | null;
+    systemVersion: string | null;
+    minSystemVersion: string | null;
+    maxSystemVersion: string | null;
+    modifiedTime: string | null;
+    started: boolean | null;
+    system: boolean | null;
+}
+
+interface CreateVirtualApplicationHandler {
+    setKey(value: string): void;
+
+    execute(): Application;
+}
+
+/**
+ * Creates virtual application.
+ *
+ * @param {object} params JSON with the parameters.
+ * @param {string} params.key Application key.
+ *
+ * @returns {object} created application.
+ */
+export function createVirtualApplication(params: CreateVirtualApplicationParams): Application {
+    checkRequired(params, 'key');
+
+    const bean = __.newBean<CreateVirtualApplicationHandler>('com.enonic.xp.lib.app.CreateVirtualApplicationHandler');
+    bean.setKey(params.key);
+    return __.toNativeObject(bean.execute());
+}
+
+export interface DeleteVirtualApplicationParams {
+    key: string;
+}
+
+interface DeleteVirtualApplicationHandler {
+    setKey(value: string): void;
+
+    execute(): boolean;
+}
+
+/**
+ * Deletes virtual application.
+ *
+ * @param {object} params JSON with the parameters.
+ * @param {string} params.key Application key.
+ *
+ * @returns {boolean} deletion result.
+ */
+export function deleteVirtualApplication(params: DeleteVirtualApplicationParams): boolean {
+    checkRequired(params, 'key');
+
+    const bean = __.newBean<DeleteVirtualApplicationHandler>('com.enonic.xp.lib.app.DeleteVirtualApplicationHandler');
+    bean.setKey(params.key);
+    return __.toNativeObject(bean.execute());
+}
+
+export interface GetApplicationParams {
+    key: string;
+}
+
+interface GetApplicationHandler {
+    setKey(value: string): void;
+
+    execute(): Application;
+}
+
+/**
+ * Fetches application by key.
+ *
+ * @param {object} params JSON with the parameters.
+ * @param {string} params.key Application key.
+ *
+ * @returns {Application} fetched application.
+ */
+export function get(params: GetApplicationParams): Application {
+    checkRequired(params, 'key');
+
+    const bean = __.newBean<GetApplicationHandler>('com.enonic.xp.lib.app.GetApplicationHandler');
+    bean.setKey(params.key);
+    return __.toNativeObject(bean.execute());
+}
+
+interface ListApplicationsHandler {
+    execute(): Application[];
+}
+
+/**
+ * Fetches both static and virtual applications.
+ *
+ * @returns {object[]} applications list.
+ */
+export function list(): Application[] {
+    const bean = __.newBean<ListApplicationsHandler>('com.enonic.xp.lib.app.ListApplicationsHandler');
+    return __.toNativeObject(bean.execute());
+}
+
+export interface GetApplicationDescriptorParams {
+    key: string;
+}
+
+export interface Icon {
+    data: unknown;
+    mimeType: string;
+    modifiedTime: string;
+}
+
+export interface ApplicationDescriptor {
+    key: string;
+    description: string | null;
+    icon?: Icon;
+}
+
+interface GetApplicationDescriptorHandler {
+    setKey(value: string): void;
+
+    execute(): ApplicationDescriptor;
+}
+
+/**
+ * Fetches application descriptor by key.
+ *
+ * @param {object} params JSON with the parameters.
+ * @param {string} params.key Application key.
+ *
+ * @returns {object} fetched application descriptor.
+ */
+export function getDescriptor(params: GetApplicationDescriptorParams): ApplicationDescriptor {
+    const bean = __.newBean<GetApplicationDescriptorHandler>('com.enonic.xp.lib.app.GetApplicationDescriptorHandler');
+    bean.setKey(params.key);
+    return __.toNativeObject(bean.execute());
+}
+
+export interface HasVirtualApplicationParams {
+    key: string;
+}
+
+interface HasVirtualApplicationHandler {
+    setKey(value: string): void;
+
+    execute(): boolean;
+}
+
+/**
+ * Checks if there is a virtual app with the app key.
+ *
+ * @param {object} params JSON with the parameters.
+ * @param {string} params.key Application key.
+ *
+ * @returns {boolean} result.
+ */
+export function hasVirtual(params: HasVirtualApplicationParams): boolean {
+    checkRequired(params, 'key');
+
+    const bean = __.newBean<HasVirtualApplicationHandler>('com.enonic.xp.lib.app.HasVirtualApplicationHandler');
+    bean.setKey(params.key);
+    return __.toNativeObject(bean.execute());
+}
+
+export interface HasRealApplicationParams {
+    key: string;
+}
+
+interface HasRealApplicationHandler {
+    setKey(value: string): void;
+
+    execute(): boolean;
+}
+
+/**
+ * Checks if there is a real app with the app key.
+ *
+ * @param {object} params JSON with the parameters.
+ * @param {string} params.key Application key.
+ *
+ * @returns {boolean} result.
+ */
+export function hasReal(params: HasRealApplicationParams): boolean {
+    checkRequired(params, 'key');
+
+    const bean = __.newBean<HasRealApplicationHandler>('com.enonic.xp.lib.app.HasRealApplicationHandler');
+    bean.setKey(params.key);
+    return __.toNativeObject(bean.execute());
+}
