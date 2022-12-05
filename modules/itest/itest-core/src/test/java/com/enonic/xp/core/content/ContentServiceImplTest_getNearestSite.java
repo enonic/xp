c8a6e8e34874c1285c9b4743580807709cc93387
rymsha <rymsha@yandex.ru>
2022-12-05 13:25:46 +0100

    publish.from unstable precision #9894 fix future date

--- modules/itest/itest-core/src/test/java/com/enonic/xp/core/content/ContentServiceImplTest_getNearestSite.java
+++ modules/itest/itest-core/src/test/java/com/enonic/xp/core/content/ContentServiceImplTest_getNearestSite.java
@@ -127,7 +127,7 @@ public class ContentServiceImplTest_getNearestSite
                                          .contentIds( ContentIds.from( site.getId() ) )
                                          .contentPublishInfo( ContentPublishInfo.create()
                                                                   .from( Instant.parse( "2022-12-01T14:00:00.668487800Z" ) )
-                                                                  .to( Instant.parse( "2022-12-03T14:00:00.669487800Z" ) )
+                                                                  .to( Instant.parse( "2099-12-03T14:00:00.669487800Z" ) )
                                                                   .build() )
                                          .build() );
 
@@ -135,7 +135,7 @@ public class ContentServiceImplTest_getNearestSite
         assertNotNull( publishedContent );
         assertNotNull( publishedContent.getPublishInfo() );
         assertEquals( "2022-12-01T14:00:00.668Z", publishedContent.getPublishInfo().getFrom().toString() );
-        assertEquals( "2022-12-03T14:00:00.669Z", publishedContent.getPublishInfo().getTo().toString() );
+        assertEquals( "2099-12-03T14:00:00.669Z", publishedContent.getPublishInfo().getTo().toString() );
     }
 
     @Test
