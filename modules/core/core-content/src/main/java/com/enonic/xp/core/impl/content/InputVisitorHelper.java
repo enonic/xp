657990be96e5adf29900c6638ebc2c7a634323d4
Bradnitski Viachaslau <vbradnitski@users.noreply.github.com>
2022-12-08 17:42:48 +0100

    Content references are not extracted from all input occurrences #9915 (#9919)
    (cherry picked from commit aee90a615116ed150bc0905d04a00a25980f31f0)

---
+++ modules/core/core-content/src/main/java/com/enonic/xp/core/impl/content/InputVisitorHelper.java
@@ -0,0 +1,41 @@
+package com.enonic.xp.core.impl.content;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import com.enonic.xp.data.Property;
+import com.enonic.xp.data.PropertySet;
+import com.enonic.xp.data.PropertyTree;
+import com.enonic.xp.form.Input;
+
+public class InputVisitorHelper
+{
+    private InputVisitorHelper()
+    {
+
+    }
+
+    public static void visitProperties( final Input input, final PropertyTree data, final Consumer<Property> consumer )
+    {
+        final List<String> formInputPathElements = input.getPath().getElements();
+
+        final int inputNameElementIndex = formInputPathElements.size() - 1;
+        final List<String> formSetPathElements = formInputPathElements.subList( 0, inputNameElementIndex );
+
+        List<PropertySet> currentSets = List.of( data.getRoot() );
+
+        for ( String formElement : formSetPathElements )
+        {
+            currentSets = currentSets.stream()
+                .flatMap( currentSet -> StreamSupport.stream( currentSet.getSets( formElement ).spliterator(), false ) )
+                .collect( Collectors.toList() );
+        }
+
+        final String inputNameElement = formInputPathElements.get( inputNameElementIndex );
+
+        currentSets.stream().flatMap( currentSet -> currentSet.getProperties( inputNameElement ).stream() ).forEach( consumer );
+    }
+
+}
