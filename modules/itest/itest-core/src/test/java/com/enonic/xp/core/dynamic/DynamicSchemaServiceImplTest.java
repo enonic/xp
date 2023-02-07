ce289f0736655445f73dd6521a66b86a6aa4e0ff
Bradnitski Viachaslau <vbradnitski@users.noreply.github.com>
2023-02-07 10:58:41 +0100

    Disable virtual apps by default #10014 (#10016)
    (cherry picked from commit 6afc6a4fdbb2178843c1df1753706902a1224cfb)

--- modules/itest/itest-core/src/test/java/com/enonic/xp/core/dynamic/DynamicSchemaServiceImplTest.java
+++ modules/itest/itest-core/src/test/java/com/enonic/xp/core/dynamic/DynamicSchemaServiceImplTest.java
@@ -18,6 +18,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 
@@ -114,6 +116,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class DynamicSchemaServiceImplTest
     extends AbstractElasticsearchIntegrationTest
 {
@@ -236,6 +239,7 @@ public class DynamicSchemaServiceImplTest
         BundleContext bundleContext = felix.getBundleContext();
 
         AppConfig appConfig = mock( AppConfig.class, invocation -> invocation.getMethod().getDefaultValue() );
+        when( appConfig.virtual_enabled() ).thenReturn( true );
 
         ApplicationFactoryServiceImpl applicationFactoryService =
             new ApplicationFactoryServiceImpl( bundleContext, nodeService, appConfig );
