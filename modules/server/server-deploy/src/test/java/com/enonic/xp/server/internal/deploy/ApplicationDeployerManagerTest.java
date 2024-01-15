1926d161072accc272a4364e3b1a7ea230cb58c0
rymsha <rymsha@yandex.ru>
2024-01-15 10:53:18 +0100

    Services not available for system apps scripts #10385

--- modules/server/server-deploy/src/test/java/com/enonic/xp/server/internal/deploy/ApplicationDeployerManagerTest.java
+++ modules/server/server-deploy/src/test/java/com/enonic/xp/server/internal/deploy/ApplicationDeployerManagerTest.java
@@ -2,16 +2,24 @@ package com.enonic.xp.server.internal.deploy;
 
 import java.net.URL;
 import java.nio.file.Path;
+import java.util.Dictionary;
 import java.util.HashMap;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
-import org.mockito.Mockito;
+import org.mockito.ArgumentCaptor;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.condition.Condition;
 
 import com.enonic.xp.app.ApplicationService;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class ApplicationDeployerManagerTest
 {
@@ -42,7 +50,7 @@ public class ApplicationDeployerManagerTest
         applicationDeployerManager.setAutoDeployer( autoDeployer );
 
         deployDirectoryWatcher = new DeployDirectoryWatcher();
-        final DeployConfig deployConfig = Mockito.mock( DeployConfig.class );
+        final DeployConfig deployConfig = mock( DeployConfig.class );
         System.setProperty( "xp.home", temporaryFolder.toFile().getAbsolutePath() );
         deployDirectoryWatcher.activate( deployConfig );
         applicationDeployerManager.setDeployDirectoryWatcher( deployDirectoryWatcher );
@@ -52,13 +60,18 @@ public class ApplicationDeployerManagerTest
     public void test_activate()
         throws Exception
     {
-        final ApplicationService applicationService = Mockito.mock( ApplicationService.class );
+        final ApplicationService applicationService = mock( ApplicationService.class );
         storedApplicationsDeployer.setApplicationService( applicationService );
         autoDeployer.setApplicationService( applicationService );
         deployDirectoryWatcher.setApplicationService( applicationService );
 
-        applicationDeployerManager.activate();
-        Mockito.verify( applicationService ).installAllStoredApplications( any() );
-        Mockito.verify( applicationService ).installGlobalApplication( new URL( "http://localhost/url1" ) );
+        final BundleContext bundleContext = mock( BundleContext.class );
+        applicationDeployerManager.activate( bundleContext );
+        verify( applicationService ).installAllStoredApplications( any() );
+        verify( applicationService ).installGlobalApplication( new URL( "http://localhost/url1" ) );
+
+        var captor = ArgumentCaptor.forClass( Dictionary.class );
+        verify( bundleContext ).registerService( same( Condition.class ), eq(Condition.INSTANCE), captor.capture());
+        assertEquals( "com.enonic.xp.server.deploy.ready", captor.getValue().get( Condition.CONDITION_ID ) );
     }
 }
