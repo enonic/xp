a0b06c14db092885afef50d12c9de03809ed7f3c
Bradnitski <vbr@enonic.com>
2024-03-05 09:42:43 +0100

    permissions update for published nodes #10420

--- modules/core/core-api/src/test/java/com/enonic/xp/content/ApplyContentPermissionsParamsTest.java
+++ modules/core/core-api/src/test/java/com/enonic/xp/content/ApplyContentPermissionsParamsTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import com.enonic.xp.security.acl.AccessControlList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplyContentPermissionsParamsTest
@@ -14,18 +13,14 @@ public class ApplyContentPermissionsParamsTest
     public void testCreate()
     {
         final ApplyContentPermissionsParams params = ApplyContentPermissionsParams.create()
-            .contentId( ContentId.from( "id1" ) )
-            .overwriteChildPermissions( true ).immediate( true )
-            .permissions( AccessControlList.create().build() )
-            .applyContentPermissionsListener( new ApplyPermissionsListener()
-            {
-            } )
+            .contentId( ContentId.from( "id1" ) ).overwriteChildPermissions( true ).immediate( true )
+            .permissions( AccessControlList.create().build() ).applyContentPermissionsListener( ApplyPermissionsListener.EMPTY )
             .build();
 
         assertEquals( ContentId.from( "id1" ), params.getContentId() );
         assertTrue( params.isOverwriteChildPermissions() );
         assertEquals( AccessControlList.create().build(), params.getPermissions() );
         assertTrue( params.isImmediate() );
-        assertNotNull( params.getListener() );
+        assertEquals( ApplyPermissionsListener.EMPTY, params.getListener() );
     }
 }
