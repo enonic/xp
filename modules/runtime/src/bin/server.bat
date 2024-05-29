ca50cfce507445dd4873c2ca3a4cb369e9d04d19
rymsha <rymsha@yandex.ru>
2024-05-29 13:11:01 +0200

    Migrate to GraalVM 21 #10582

--- modules/runtime/src/bin/server.bat
+++ modules/runtime/src/bin/server.bat
@@ -40,10 +40,7 @@ goto fail
 set XP_INSTALL=%DIRNAME%..
 set DEFAULT_JAVA_OPTS=-XX:-OmitStackTraceInFastThrow -XX:+AlwaysPreTouch
 set DEFAULT_JAVA_DEBUG_OPTS=-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005
-set CONSTANT_XP_OPTS=-Dfile.encoding=UTF8 -Dmapper.allow_dots_in_name=true --add-modules java.se,org.graalvm.polyglot --module-path graal --add-exports java.base/jdk.internal.ref=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.management/sun.management=ALL-UNNAMED --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED
-
-"%JAVA_EXE%" --list-modules -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCIProduct >NUL 2>NUL
-if %ERRORLEVEL% EQU 0 set CONSTANT_XP_OPTS=-XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCIProduct %CONSTANT_XP_OPTS%
+set CONSTANT_XP_OPTS=-Dfile.encoding=UTF8 -Dmapper.allow_dots_in_name=true --add-modules java.se,org.graalvm.polyglot --module-path "%XP_INSTALL%\graal" --add-exports java.base/jdk.internal.ref=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.management/sun.management=ALL-UNNAMED --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED
 
 if "%JAVA_OPTS%" == "" set JAVA_OPTS=%DEFAULT_JAVA_OPTS%
 if "%JAVA_DEBUG_OPTS%" == "" set JAVA_DEBUG_OPTS=%DEFAULT_JAVA_DEBUG_OPTS%
