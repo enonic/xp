24d35313e32c834c1d2f75b467e7628630b1a432
Christian Westgaard <comlock@gmail.com>
2024-10-11 13:36:11 +0200

    tests for ErrorController and IdProviderController

---
+++ modules/lib/test/ErrorController.test-d.ts
@@ -0,0 +1,39 @@
+import type {
+    ErrorController,
+    ErrorRequest,
+} from '../core/index';
+
+import {
+    expectAssignable,
+    // expectDeprecated,
+    // expectDocCommentIncludes,
+    // expectError,
+    // expectNever,
+	// expectNotAssignable,
+    // printType,
+    // expectNotDeprecated,
+    // expectNotType,
+    // expectType,
+    // printType,
+} from 'tsd';
+
+const log = {
+    info: (message?: string, ...optionalParams: unknown[]) => { /* no-op */ },
+};
+
+const myErrorController = {
+    handle404: (errorRequest: ErrorRequest) => {
+        log.info('404 errorRequest:%s', JSON.stringify(errorRequest, null, 4));
+        return {
+            status: 404 as const,
+        };
+    },
+    handleError: (errorRequest: ErrorRequest) => {
+        log.info('generic errorRequest:%s', JSON.stringify(errorRequest, null, 4));
+        return {
+            status: 500 as const,
+        };
+    },
+};
+
+expectAssignable<ErrorController>(myErrorController);
