3361b34006137cc175604fc712d9e9a01f479e92
anatol-sialitski <53557255+anatol-sialitski@users.noreply.github.com>
2024-12-17 15:22:17 +0100

    Page still returns 200 when descriptor's application is stopped #10682 (#10814)
    (cherry picked from commit a32cf4ec7b3c575673875888abdf45a44ca524e0)

---
+++ modules/portal/portal-impl/src/test/java/com/enonic/xp/portal/impl/html/HtmlBuilderTest.java
@@ -0,0 +1,28 @@
+package com.enonic.xp.portal.impl.html;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HtmlBuilderTest
+{
+
+    @Test
+    void test()
+    {
+        final HtmlBuilder builder = new HtmlBuilder();
+        builder.open( "div" ).open( "br" ).closeEmpty().open( "br" ).closeEmpty().text( "Text" ).open( "br" ).closeEmpty().close();
+
+        assertEquals( "<div><br/><br/>Text<br/></div>", builder.toString() );
+    }
+
+    @Test
+    void testWithAttribute()
+    {
+        final HtmlBuilder builder = new HtmlBuilder();
+        builder.open( "div" ).attribute( "data-config", "value" ).text( "Text" ).close();
+
+        assertEquals( "<div data-config=\"value\">Text</div>", builder.toString() );
+    }
+
+}
