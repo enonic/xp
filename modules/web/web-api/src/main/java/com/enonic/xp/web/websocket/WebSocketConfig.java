c43c8f5f46eea0859d39754cd390cefa3cf99ed5
Sergey Rymsha <rymsha@users.noreply.github.com>
2025-02-19 13:14:21 +0100

    WebSocket: context is lost in message events #10873 (#10921)

--- modules/web/web-api/src/main/java/com/enonic/xp/web/websocket/WebSocketConfig.java
+++ modules/web/web-api/src/main/java/com/enonic/xp/web/websocket/WebSocketConfig.java
@@ -1,33 +1,32 @@
 package com.enonic.xp.web.websocket;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public final class WebSocketConfig
 {
-    private List<String> subProtocols;
+    private List<String> subProtocols = List.of();
 
-    private Map<String, String> data;
+    private Map<String, String> data = new ConcurrentHashMap<>();
 
     public List<String> getSubProtocols()
     {
-        return this.subProtocols != null ? this.subProtocols : new ArrayList<>();
+        return this.subProtocols;
     }
 
     public void setSubProtocols( final List<String> subProtocols )
     {
-        this.subProtocols = subProtocols;
+        this.subProtocols = List.copyOf( subProtocols );
     }
 
     public Map<String, String> getData()
     {
-        return this.data != null ? this.data : new HashMap<>();
+        return this.data;
     }
 
     public void setData( final Map<String, String> data )
     {
-        this.data = data;
+        this.data = new ConcurrentHashMap<>( data );
     }
 }
