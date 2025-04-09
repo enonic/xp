5a3730d69b62db7d645e6df566ab55864685975a
rymsha <sry@enonic.com>
2025-04-09 13:16:11 +0200

    Use micrometer for collecting metrics #7953

--- modules/server/server-metrics/src/test/java/com/enonic/xp/server/internal/metrics/MetricsReporterTest.java
+++ modules/server/server-metrics/src/test/java/com/enonic/xp/server/internal/metrics/MetricsReporterTest.java
@@ -2,19 +2,18 @@ package com.enonic.xp.server.internal.metrics;
 
 import org.junit.jupiter.api.Test;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.net.MediaType;
 
 import com.enonic.xp.status.BaseReporterTest;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MetricsReporterTest
     extends BaseReporterTest<MetricsReporter>
 {
     public MetricsReporterTest()
     {
-        super( "metrics", MediaType.JSON_UTF_8 );
+        super( "metrics", MediaType.parse( "application/openmetrics-text; version=1.0.0; charset=utf-8" ));
     }
 
     @Override
@@ -28,9 +27,6 @@ public class MetricsReporterTest
     public void testReport()
         throws Exception
     {
-        this.params.put( "filter", "" );
-
-        final JsonNode json = jsonReport();
-        assertNotNull( json );
+        assertThat(textReport()).endsWith( "# EOF\n" );
     }
 }
