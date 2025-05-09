d6505099187fd68ee4ba36620ff2fc784f584b4f
rymsha <sry@enonic.com>
2025-05-09 08:59:45 +0200

    don't set X-XSS-Protection be default #11066

--- modules/web/web-header/src/main/java/com/enonic/xp/web/impl/header/HeaderFilterConfig.java
+++ modules/web/web-header/src/main/java/com/enonic/xp/web/impl/header/HeaderFilterConfig.java
@@ -2,5 +2,5 @@ package com.enonic.xp.web.impl.header;
 
 public @interface HeaderFilterConfig
 {
-    String headerConfig() default "set X-Frame-Options: DENY,set X-XSS-Protection: 1; mode=block,set X-Content-Type-Options: nosniff";
+    String headerConfig() default "set X-Frame-Options: DENY,set X-Content-Type-Options: nosniff";
 }
