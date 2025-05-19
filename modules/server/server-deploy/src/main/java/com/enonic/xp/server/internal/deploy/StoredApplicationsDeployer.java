e78a933af3d3695501974af45f9960cdbb688ced
rymsha <sry@enonic.com>
2025-05-19 09:36:45 +0200

    XP8 is never ready (again) #11083

--- modules/server/server-deploy/src/main/java/com/enonic/xp/server/internal/deploy/StoredApplicationsDeployer.java
+++ modules/server/server-deploy/src/main/java/com/enonic/xp/server/internal/deploy/StoredApplicationsDeployer.java
@@ -1,5 +1,6 @@
 package com.enonic.xp.server.internal.deploy;
 
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -9,17 +10,17 @@ import com.enonic.xp.app.ApplicationService;
 @Component(service = StoredApplicationsDeployer.class)
 public final class StoredApplicationsDeployer
 {
-    private ApplicationService applicationService;
+    private final ApplicationService applicationService;
+
+    @Activate
+    public StoredApplicationsDeployer( @Reference final ApplicationService applicationService )
+    {
+        this.applicationService = applicationService;
+    }
 
     public void deploy()
     {
         DeployHelper.runAsAdmin( () -> applicationService.installAllStoredApplications( ApplicationInstallationParams.create().
             build() ) );
-    }
-
-    @Reference
-    public void setApplicationService( final ApplicationService applicationService )
-    {
-        this.applicationService = applicationService;
     }
 }
--- modules/server/server-status/src/test/java/com/enonic/xp/server/impl/status/ReadyProbeServletTest.java
+++ modules/server/server-status/src/test/java/com/enonic/xp/server/impl/status/ReadyProbeServletTest.java
@@ -25,15 +25,14 @@ public class ReadyProbeServletTest
 {
     private static final List<String> TRACKED_SERVICE_NAMES =
         List.of( "com.enonic.xp.export.ExportService", "com.enonic.xp.scheduler.SchedulerService",
-                 "com.enonic.xp.portal.websocket.WebSocketManager", "com.enonic.xp.portal.view.ViewFunctionService",
+                 "com.enonic.xp.portal.websocket.WebSocketManager",
                  "com.enonic.xp.task.TaskService", "com.enonic.xp.task.TaskDescriptorService",
                  "com.enonic.xp.script.event.ScriptEventManager", "com.enonic.xp.security.SecurityService",
                  "com.enonic.xp.portal.url.PortalUrlService", "com.enonic.xp.web.multipart.MultipartService",
                  "com.enonic.xp.mail.MailService", "com.enonic.xp.i18n.LocaleService", "com.enonic.xp.portal.owasp.HtmlSanitizer",
                  "com.enonic.xp.schema.content.ContentTypeService", "com.enonic.xp.admin.tool.AdminToolDescriptorService",
                  "com.enonic.xp.admin.widget.WidgetDescriptorService",
-                 "com.enonic.xp.server.internal.deploy.DeployDirectoryWatcher",
-                 "com.enonic.xp.server.internal.deploy.StoredApplicationsDeployer" );
+                 "com.enonic.xp.app.ApplicationService" );
 
     @Mock(stubOnly = true)
     private HttpServletRequest req;
