0e9c813b34704faa0642aed7855ce4a6ff419eb5
Anatol Sialitski <asi@enonic.com>
2025-05-20 16:52:11 +0200

    Remove lib-portal dependency from lib-admin #11042

--- modules/lib/lib-admin/src/main/java/com/enonic/xp/lib/admin/AdminLibHelper.java
+++ modules/lib/lib-admin/src/main/java/com/enonic/xp/lib/admin/AdminLibHelper.java
@@ -1,10 +1,26 @@
 package com.enonic.xp.lib.admin;
 
+import java.util.function.Supplier;
+
+import com.enonic.xp.app.ApplicationKey;
+import com.enonic.xp.portal.url.GenerateUrlParams;
+import com.enonic.xp.portal.url.PortalUrlService;
+import com.enonic.xp.script.bean.BeanContext;
+import com.enonic.xp.script.bean.ScriptBean;
 import com.enonic.xp.server.ServerInfo;
 import com.enonic.xp.server.VersionInfo;
 
 public final class AdminLibHelper
+    implements ScriptBean
 {
+    private Supplier<PortalUrlService> portalUrlServiceSupplier;
+
+    @Override
+    public void initialize( final BeanContext context )
+    {
+        this.portalUrlServiceSupplier = context.getService( PortalUrlService.class );
+    }
+
     public String getInstallation()
     {
         return ServerInfo.get().getName();
@@ -13,5 +29,22 @@ public final class AdminLibHelper
     public String getVersion()
     {
         return VersionInfo.get().getVersion();
+    }
+
+    public String getToolUrl( final String application, final String toolName )
+    {
+        final GenerateUrlParams params = new GenerateUrlParams();
+        params.url( "/admin/" + ApplicationKey.from( application ) + "/" + toolName );
+
+        return this.portalUrlServiceSupplier.get().generateUrl( params );
+    }
+
+    public String getHomeToolUrl( final String urlType )
+    {
+        final GenerateUrlParams params = new GenerateUrlParams();
+        params.url( "/admin" );
+        params.type( urlType );
+
+        return this.portalUrlServiceSupplier.get().generateUrl( params );
     }
 }
