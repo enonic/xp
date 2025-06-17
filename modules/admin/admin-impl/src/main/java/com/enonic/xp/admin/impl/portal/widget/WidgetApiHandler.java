634ee1020d980e15b79c8131cab067bfae5cff43
Anatol Sialitski <asi@enonic.com>
2025-06-17 16:20:19 +0200

    Admin Tools with no interfaces should only get generic widgets #11119

--- modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/portal/widget/WidgetApiHandler.java
+++ modules/admin/admin-impl/src/main/java/com/enonic/xp/admin/impl/portal/widget/WidgetApiHandler.java
@@ -26,9 +26,9 @@ import com.enonic.xp.web.WebResponse;
 @Component(immediate = true, service = WidgetApiHandler.class)
 public class WidgetApiHandler
 {
-    private static final Pattern WIDGET_API_PATTERN = Pattern.compile( "^/(_|api)/admin:widget/(?<appKey>[^/]+)/(?<widgetKey>[^/]+)" );
+    private static final Pattern WIDGET_API_PATTERN = Pattern.compile( "^/_/admin:widget/(?<appKey>[^/]+)/(?<widgetKey>[^/]+)" );
 
-    private static final Pattern TOOL_PREFIX_PATTERN = Pattern.compile( "^/admin/(?<appKey>[^/]+)/(?<toolName>[^/]+)" );
+    private static final Pattern TOOL_PREFIX_PATTERN = Pattern.compile( "^/admin/(?<appKey>[^/]+)/(?<toolName>[^/]+)/_/admin:widget/" );
 
     private static final String GENERIC_WIDGET_INTERFACE = "generic";
 
@@ -50,7 +50,7 @@ public class WidgetApiHandler
 
     public WebResponse handle( final WebRequest webRequest )
     {
-        final String path = Objects.requireNonNullElse( webRequest.getEndpointPath(), webRequest.getRawPath() );
+        final String path = Objects.requireNonNull( webRequest.getEndpointPath(), "Endpoint path cannot be null" );
 
         final Matcher matcher = WIDGET_API_PATTERN.matcher( path );
 
@@ -86,21 +86,26 @@ public class WidgetApiHandler
 
     private void verifyMounts( final WidgetDescriptor widgetDescriptor, final WebRequest webRequest )
     {
-        if ( !widgetDescriptor.hasInterface( GENERIC_WIDGET_INTERFACE ) && webRequest.getEndpointPath() != null )
+        if ( widgetDescriptor.hasInterface( GENERIC_WIDGET_INTERFACE ) )
         {
-            final Matcher toolMatcher = TOOL_PREFIX_PATTERN.matcher( webRequest.getRawPath() );
-            if ( toolMatcher.find() )
+            return;
+        }
+
+        final Matcher toolMatcher = TOOL_PREFIX_PATTERN.matcher( webRequest.getRawPath() );
+        if ( toolMatcher.find() )
+        {
+            final DescriptorKey toolDescriptorKey =
+                DescriptorKey.from( resolveApplicationKey( toolMatcher.group( "appKey" ) ), toolMatcher.group( "toolName" ) );
+            final AdminToolDescriptor adminToolDescriptor = adminToolDescriptorService.getByKey( toolDescriptorKey );
+            if ( adminToolDescriptor == null || widgetDescriptor.getInterfaces().stream().noneMatch( adminToolDescriptor::hasInterface ) )
             {
-                final DescriptorKey toolDescriptorKey =
-                    DescriptorKey.from( resolveApplicationKey( toolMatcher.group( "appKey" ) ), toolMatcher.group( "toolName" ) );
-                final AdminToolDescriptor adminToolDescriptor = adminToolDescriptorService.getByKey( toolDescriptorKey );
-                if ( adminToolDescriptor != null && !adminToolDescriptor.getInterfaces().isEmpty() &&
-                    widgetDescriptor.getInterfaces().stream().noneMatch( adminToolDescriptor::hasInterface ) )
-                {
-                    throw WebException.notFound(
-                        String.format( "Widget [%s] is not mounted to admin tool [%s]", widgetDescriptor.getKey(), toolDescriptorKey ) );
-                }
+                throw WebException.notFound(
+                    String.format( "Widget [%s] is not mounted to admin tool [%s]", widgetDescriptor.getKey(), toolDescriptorKey ) );
             }
+        }
+        else
+        {
+            throw WebException.notFound( String.format( "Invalid admin tool URL [%s]", webRequest.getRawPath() ) );
         }
     }
 
