d17cf4524487216ed2edba1ede023a61d537b6a7
Bradnitski <vbr@enonic.com>
2025-07-24 14:50:39 +0200

    Content Patch JS API #11199

---
+++ modules/lib/lib-content/src/main/java/com/enonic/xp/lib/content/PatchContentHandler.java
@@ -0,0 +1,204 @@
+package com.enonic.xp.lib.content;
+
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import com.enonic.xp.branch.Branch;
+import com.enonic.xp.branch.Branches;
+import com.enonic.xp.content.Content;
+import com.enonic.xp.content.ContentId;
+import com.enonic.xp.content.ContentIds;
+import com.enonic.xp.content.ContentInheritType;
+import com.enonic.xp.content.ContentName;
+import com.enonic.xp.content.ContentNotFoundException;
+import com.enonic.xp.content.ContentPatcher;
+import com.enonic.xp.content.ContentPath;
+import com.enonic.xp.content.ContentPublishInfo;
+import com.enonic.xp.content.PatchContentParams;
+import com.enonic.xp.content.PatchContentResult;
+import com.enonic.xp.content.PatchableContent;
+import com.enonic.xp.convert.Converters;
+import com.enonic.xp.index.ChildOrder;
+import com.enonic.xp.lib.content.mapper.ContentMapper;
+import com.enonic.xp.lib.content.mapper.PatchContentResultMapper;
+import com.enonic.xp.project.ProjectName;
+import com.enonic.xp.script.ScriptValue;
+import com.enonic.xp.security.PrincipalKey;
+
+public final class PatchContentHandler
+    extends BaseContentHandler
+{
+    private String key;
+
+    private ScriptValue patcher;
+
+    private String[] branches;
+
+    @Override
+    protected Object doExecute()
+    {
+        final Content existingContent = getExistingContent( this.key );
+        if ( existingContent == null )
+        {
+            return null;
+        }
+
+        final PatchContentParams.Builder params =
+            PatchContentParams.create().contentId( existingContent.getId() ).patcher( newContentPatcher( existingContent ) );
+
+        if ( branches != null )
+        {
+            params.branches( Arrays.stream( branches ).map( Branch::from ).collect( Branches.collecting() ) );
+        }
+
+        final PatchContentResult result;
+        try
+        {
+            result = this.contentService.patch( params.build() );
+        }
+        catch ( Exception e )
+        {
+            throw new RuntimeException( e );
+        }
+
+        return new PatchContentResultMapper( result );
+    }
+
+    @Override
+    protected boolean strictDataValidation()
+    {
+        return false;
+    }
+
+    private Content getExistingContent( final String key )
+    {
+        try
+        {
+            if ( !key.startsWith( "/" ) )
+            {
+                return this.contentService.getById( ContentId.from( key ) );
+            }
+            else
+            {
+                return this.contentService.getByPath( ContentPath.from( key ) );
+            }
+        }
+        catch ( final ContentNotFoundException e )
+        {
+            return null;
+        }
+    }
+
+    private ContentPatcher newContentPatcher( final Content existingContent )
+    {
+        return edit -> {
+            final ScriptValue value = this.patcher.call( new ContentMapper( edit.source ) );
+            if ( value != null )
+            {
+                patchContent( edit, value.getMap(), existingContent );
+            }
+        };
+    }
+
+    private void patchContent( final PatchableContent target, final Map<String, Object> map, final Content existingContent )
+    {
+        parse( map, "displayName", String.class, val -> target.displayName.setValue( val ) );
+        parse( map, "language", String.class, val -> target.language.setValue( Locale.forLanguageTag( val ) ) );
+        parse( map, "childOrder", String.class, val -> target.childOrder.setValue( ChildOrder.from( val ) ) );
+        parse( map, "owner", String.class, val -> target.owner.setValue( PrincipalKey.from( val ) ) );
+        parse( map, "creator", String.class, val -> target.creator.setValue( PrincipalKey.from( val ) ) );
+        parse( map, "createdTime", String.class, val -> target.createdTime.setValue( Instant.parse( val ) ) );
+        parse( map, "modifier", String.class, val -> target.modifier.setValue( PrincipalKey.from( val ) ) );
+        parse( map, "modifiedTime", String.class, val -> target.modifiedTime.setValue( Instant.parse( val ) ) );
+
+        parse( map, "data", Map.class, val -> target.data.setValue( createPropertyTree( val, existingContent.getType() ) ) );
+        parse( map, "x", Map.class, val -> target.extraDatas.setValue( createExtraDatas( val, existingContent.getType() ) ) );
+        parse( map, "publish", Map.class, val -> target.publishInfo.setValue( createContentPublishInfo( val ) ) );
+        parse( map, "workflow", Map.class, val -> target.workflowInfo.setValue( createWorkflowInfo( val ) ) );
+
+//        requires https://github.com/enonic/xp/issues/11209
+//        parse(map, "page", Map.class, val -> target.page.setValue( new ContentDataSerializer createPropertyTree( val, existingContent.getType() ) ) );
+//        parse(map, "validationErrors", Map.class, target.validationErrors::setValue);
+//        parse(map, "attachments", Map.class, val -> target.attachments::setValue);
+
+        parse( map, "valid", Boolean.class, target.valid::setValue );
+        parse( map, "processedReferences", String[].class, val -> target.processedReferences.setValue(
+            Arrays.stream( val ).map( ContentId::from ).collect( ContentIds.collector() ) ) );
+        parse( map, "manualOrderValue", Long.class, target.manualOrderValue::setValue );
+        parse( map, "inherit", String[].class,
+               val -> target.inherit.setValue( Arrays.stream( val ).map( ContentInheritType::valueOf ).collect( Collectors.toSet() ) ) );
+        parse( map, "variantOf", ContentId.class, target.variantOf::setValue );
+
+        parse( map, "originProject", String.class, val -> target.originProject.setValue( ProjectName.from( val ) ) );
+        parse( map, "originalParentPath", String.class, val -> target.originalParentPath.setValue( ContentPath.from( val ) ) );
+        parse( map, "originalName", String.class, val -> target.originalName.setValue( ContentName.from( val ) ) );
+        parse( map, "archivedTime", String.class, val -> target.archivedTime.setValue( Instant.parse( val ) ) );
+        parse( map, "archivedBy", String.class, val -> target.archivedBy.setValue( PrincipalKey.from( val ) ) );
+    }
+
+    private <T> void parse( Map<?, ?> map, String key, Class<T> type, Consumer<T> consumer )
+    {
+        T converted = Converters.convert( map.get( key ), type );
+        if ( converted != null )
+        {
+            consumer.accept( converted );
+        }
+    }
+
+    private ContentPublishInfo createContentPublishInfo( final Map<String, Object> value )
+    {
+        if ( value == null )
+        {
+            return null;
+        }
+
+        return ContentPublishInfo.create()
+            .from( getInstant( value, "from" ) )
+            .to( getInstant( value, "to" ) )
+            .first( getInstant( value, "first" ) )
+            .build();
+    }
+
+    private Instant getInstant( final Map<String, Object> valueMap, final String key )
+    {
+        final Object value = valueMap.get( key );
+        if ( value != null )
+        {
+            try
+            {
+                return Instant.parse( value.toString() );
+            }
+            catch ( DateTimeParseException e )
+            {
+                throw new IllegalArgumentException( key + " value could not be parsed to instant: [" + value + "]" );
+            }
+        }
+        return null;
+    }
+
+//    @Override
+//    protected boolean strictDataValidation()
+//    {
+//        return this.requireValid;
+//    }
+
+    public void setKey( final String key )
+    {
+        this.key = key;
+    }
+
+    public void setPatcher( final ScriptValue patcher )
+    {
+        this.patcher = patcher;
+    }
+
+    public void setBranches( final String[] branches )
+    {
+        this.branches = branches;
+    }
+}
---
+++ modules/lib/lib-content/src/test/resources/test/PatchContentHandlerTest.js
@@ -0,0 +1,324 @@
+var assert = require('/lib/xp/testing.js');
+var content = require('/lib/xp/content.js');
+
+var expectedJson = {
+    '_id': '123456',
+    '_name': 'mycontent',
+    '_path': '/a/b/mycontent',
+    'creator': 'user:system:admin',
+    'modifier': 'user:system:admin',
+    'createdTime': '1970-01-01T00:00:00Z',
+    'modifiedTime': '1970-01-01T00:00:00Z',
+    'type': 'test:myContentType',
+    'displayName': 'Modified',
+    'hasChildren': false,
+    'language': 'es',
+    'valid': false,
+    'data': {
+        'a': 2,
+        'b': '2',
+        'c': [
+            {
+                'd': true
+            },
+            {
+                'd': false,
+                'e': [
+                    '3',
+                    '42',
+                    '5'
+                ],
+                'f': 2
+            }
+        ],
+        'z': '99'
+    },
+    'x': {
+        'com-enonic-myapplication': {
+            'myschema': {
+                'a': 1
+            },
+            'other': {
+                'name': 'test'
+            }
+        }
+    },
+    'page': {
+        'type': 'page',
+        'path': '/',
+        'descriptor': 'my-app-key:mycontroller',
+        'config': {
+            'a': '1'
+        },
+        'regions': {
+            'top': {
+                'components': [
+                    {
+                        'path': '/top/0',
+                        'type': 'part',
+                        'descriptor': 'app-descriptor-x:name-x',
+                        'config': {
+                            'a': '1'
+                        }
+                    },
+                    {
+                        'path': '/top/1',
+                        'type': 'layout',
+                        'descriptor': 'layoutDescriptor:name',
+                        'config': {},
+                        'regions': {
+                            'left': {
+                                'components': [
+                                    {
+                                        'path': '/top/1/left/0',
+                                        'type': 'part',
+                                        'config': {}
+                                    },
+                                    {
+                                        'path': '/top/1/left/1',
+                                        'type': 'text',
+                                        'text': 'text text text'
+                                    },
+                                    {
+                                        'path': '/top/1/left/2',
+                                        'type': 'text',
+                                        'text': ''
+                                    }
+                                ],
+                                name: 'left'
+                            },
+                            'right': {
+                                'components': [
+                                    {
+                                        'path': '/top/1/right/0',
+                                        'type': 'image',
+                                        'image': 'image-id'
+                                    },
+                                    {
+                                        'path': '/top/1/right/1',
+                                        'type': 'fragment',
+                                        'fragment': '213sda-ss222'
+                                    }
+                                ],
+                                name: 'right'
+                            }
+                        }
+                    },
+                    {
+                        'path': '/top/2',
+                        'type': 'layout',
+                        'config': {},
+                        'regions': {}
+                    }
+                ],
+                name: 'top'
+            },
+            'bottom': {
+                'components': [
+                    {
+                        'path': '/bottom/0',
+                        'type': 'part',
+                        'descriptor': 'app-descriptor-y:name-y',
+                        'config': {
+                            'a': '1'
+                        }
+                    },
+                    {
+                        'path': '/bottom/1',
+                        'type': 'image',
+                        'image': 'img-id-x'
+                    },
+                    {
+                        'path': '/bottom/2',
+                        'type': 'image'
+                    }
+                ],
+                name: 'bottom'
+            }
+        }
+    },
+    'attachments': {},
+    'publish': {
+        'from': '2018-11-03T10:00:01Z',
+        'to': '2018-11-03T10:00:01Z',
+    },
+    'workflow': {
+        'state': 'READY',
+        'checks': {
+            'Review by marketing': 'APPROVED'
+        }
+    }
+};
+
+function globalPatcher(c) {
+    c.displayName = 'Modified';
+    c.data.a++;
+    c.data.z = '99';
+    c.data.c[1].d = false;
+    c.data.c[1].e[1] = 42;
+
+    if (!c.x['com-enonic-myapplication']) {
+        c.x['com-enonic-myapplication'] = {};
+    }
+
+    c.x['com-enonic-myapplication'].other = {
+        name: 'test'
+    };
+
+    c.language = 'es';
+
+    c.publish.from = '2018-11-03T10:00:01Z';
+    c.publish.to = '2018-11-03T10:00:01Z';
+    c.workflow.state = 'READY';
+    c.workflow.checks = {
+        'Review by marketing': 'APPROVED'
+    };
+
+    return c;
+}
+
+exports.patch_notFound = function () {
+    var result = content.patch({
+        key: '123456',
+        patcher: globalPatcher
+    });
+
+    assert.assertNull(result);
+};
+
+exports.patchById = function () {
+    var result = content.patch({
+        key: '123456',
+        patcher: globalPatcher
+    });
+
+    assert.assertJsonEquals(expectedJson, result.results[0].content);
+};
+
+exports.patchByPath = function () {
+    var result = content.patch({
+        key: '/a/b/mycontent',
+        patcher: globalPatcher
+    });
+
+    assert.assertJsonEquals(expectedJson, result.results[0].content);
+};
+
+exports.patchSiteConfig = function () {
+
+    var result = content.patch({
+        key: '/a/b/mycontent',
+        patcher(c) {
+            c.data.siteConfig = [];
+            c.data.siteConfig[0] = {};
+            c.data.siteConfig[0].applicationKey = 'appKey1';
+            c.data.siteConfig[0].config = {};
+            c.data.siteConfig[0].config.a = 'a';
+            c.data.siteConfig[0].config.b = true;
+            c.data.siteConfig[1] = {};
+            c.data.siteConfig[1].applicationKey = 'appKey2';
+            c.data.siteConfig[1].config = {};
+            c.data.siteConfig[1].config.c = '4';
+
+            return c;
+        }
+    });
+
+    var expect = [
+        {
+            'applicationKey': 'appKey1',
+            'config': {
+                'a': 'a',
+                'b': true
+            }
+        },
+        {
+            'applicationKey': 'appKey2',
+            'config': {
+                'c': 4
+            }
+        }
+    ];
+
+    assert.assertJsonEquals(expect, result.results[0].content.data.siteConfig);
+};
+
+exports.patchSiteSingleDescriptor = function () {
+
+    var result = content.patch({
+        key: '/a/b/mycontent',
+        patcher(c) {
+            c.data.siteConfig = {};
+            c.data.siteConfig.applicationKey = 'appKey1';
+            c.data.siteConfig.config = {};
+            c.data.siteConfig.config.a = 'a';
+            c.data.siteConfig.config.b = true;
+
+            return c;
+        }
+    });
+
+    var expect =
+        {
+            'applicationKey': 'appKey1',
+            'config': {
+                'a': 'a',
+                'b': true
+            }
+        }
+    ;
+
+    assert.assertJsonEquals(expect, result.results[0].content.data.siteConfig);
+};
+
+exports.patchNotMappedXDataFieldName = function () {
+
+    var result = content.patch({
+        key: '/a/b/mycontent',
+        patcher(c) {
+            globalPatcher(c);
+            c.x['com-enonic-myapplication'].other = {
+                name: 'test',
+                notMappedField: 'value'
+            };
+
+            return c;
+        }
+    });
+
+    assert.assertJsonEquals("test", result.results[0].content.x['com-enonic-myapplication'].other.name);
+    assert.assertJsonEquals("value", result.results[0].content.x['com-enonic-myapplication'].other.notMappedField);
+
+
+};
+
+
+exports.patchWorkflowInfo = function () {
+
+    var expected = {
+        "state": "IN_PROGRESS",
+        "checks": {
+            "a": "PENDING",
+            "b": "APPROVED"
+        }
+    };
+
+    var result = content.patch({
+        key: '/a/b/mycontent',
+        patcher(c) {
+            globalPatcher(c);
+            c.workflow = {
+                state: 'IN_PROGRESS',
+                checks: {
+                    a: 'PENDING',
+                    b: 'APPROVED'
+                }
+            };
+
+            return c;
+        }
+    });
+
+    assert.assertJsonEquals(expected, result.results[0].content.workflow);
+};
+
