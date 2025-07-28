df7589d289940392e8b83356a3463309ade0a669
Bradnitski Viachaslau <vbradnitski@users.noreply.github.com>
2025-07-28 11:01:11 +0200

    Content Patch JS API #11199 (#11210)

--- modules/core/core-api/src/main/java/com/enonic/xp/content/ContentIds.java
+++ modules/core/core-api/src/main/java/com/enonic/xp/content/ContentIds.java
@@ -2,7 +2,7 @@ package com.enonic.xp.content;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Set;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import com.google.common.collect.ImmutableSet;
@@ -49,6 +49,11 @@ public final class ContentIds
         }
 
         return fromInternal( ImmutableSet.copyOf( ids ) );
+    }
+
+    public static Collector<ContentId, ?, ContentIds> collector()
+    {
+        return Collectors.collectingAndThen( ImmutableSet.toImmutableSet(), ContentIds::fromInternal );
     }
 
     private static ContentIds fromInternal( final ImmutableSet<ContentId> set )
