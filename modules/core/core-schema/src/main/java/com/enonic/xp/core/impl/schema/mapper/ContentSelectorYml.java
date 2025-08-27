b06de8a27190a1026e9a4116f184a6e4ffe2eec3
Anatol Sialitski <asi@enonic.com>
2025-08-27 09:42:15 +0200

    Improving InputType creation from YML #11302

--- modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/mapper/ContentSelectorYml.java
+++ modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/mapper/ContentSelectorYml.java
@@ -2,6 +2,11 @@ package com.enonic.xp.core.impl.schema.mapper;
 
 import java.util.List;
 
+import com.enonic.xp.form.Input;
+import com.enonic.xp.inputtype.InputTypeConfig;
+import com.enonic.xp.inputtype.InputTypeName;
+import com.enonic.xp.inputtype.InputTypeProperty;
+
 public class ContentSelectorYml
     extends InputYml
 {
@@ -12,4 +17,36 @@ public class ContentSelectorYml
     public Boolean treeMode;
 
     public Boolean hideToggleIcon;
+
+    @Override
+    public InputTypeName getInputTypeName()
+    {
+        return InputTypeName.CONTENT_SELECTOR;
+    }
+
+    @Override
+    public void customizeInputType( final Input.Builder builder )
+    {
+        final InputTypeConfig.Builder configBuilder = InputTypeConfig.create();
+
+        if ( hideToggleIcon != null )
+        {
+            configBuilder.property( InputTypeProperty.create( "hideToggleIcon", hideToggleIcon.toString() ).build() );
+        }
+        if ( treeMode != null )
+        {
+            configBuilder.property( InputTypeProperty.create( "treeMode", treeMode.toString() ).build() );
+        }
+        if ( allowContentType != null )
+        {
+            allowContentType.forEach(
+                allowType -> configBuilder.property( InputTypeProperty.create( "allowContentType", allowType ).build() ) );
+        }
+        if ( allowPath != null )
+        {
+            allowPath.forEach( allowPath -> configBuilder.property( InputTypeProperty.create( "allowPath", allowPath ).build() ) );
+        }
+
+        builder.inputTypeConfig( configBuilder.build() );
+    }
 }
--- modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/mapper/InputRegistry.java
+++ modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/mapper/InputRegistry.java
@@ -1,156 +1,30 @@
 package com.enonic.xp.core.impl.schema.mapper;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-import com.enonic.xp.form.Input;
-import com.enonic.xp.inputtype.InputTypeConfig;
-import com.enonic.xp.inputtype.InputTypeDefault;
 import com.enonic.xp.inputtype.InputTypeName;
-import com.enonic.xp.inputtype.InputTypeProperty;
 
 public final class InputRegistry
 {
-    private static final Map<Class<?>, InputFactory<?>> FACTORIES = new HashMap<>();
+    private static final Map<Class<? extends InputYml>, InputTypeName> FACTORIES = new ConcurrentHashMap<>();
 
     static
     {
-        register( RadioButtonYml.class, yml -> {
-            final Input.Builder builder = defaultConverter( yml ).inputType( InputTypeName.from( "RadioButton" ) );
-
-            if ( yml.defaultValue != null )
-            {
-                builder.defaultValue(
-                    InputTypeDefault.create().property( InputTypeProperty.create( "default", yml.defaultValue ).build() ).build() );
-            }
-
-            if ( yml.options != null )
-            {
-                final InputTypeConfig.Builder configBuilder = InputTypeConfig.create();
-                yml.options.forEach( option -> {
-                    final InputTypeProperty.Builder propertyBuilder = InputTypeProperty.create( "option", option.value );
-                    propertyBuilder.attribute( "value", option.name );
-                    option.getAttributes().forEach( propertyBuilder::attribute );
-                    configBuilder.property( propertyBuilder.build() );
-                } );
-                builder.inputTypeConfig( configBuilder.build() );
-            }
-            return builder.build();
-        } );
-
-        register( TextLineYml.class, yml -> {
-            final Input.Builder builder = defaultConverter( yml ).inputType( InputTypeName.from( "TextLine" ) );
-
-            if ( yml.defaultValue != null )
-            {
-                builder.defaultValue(
-                    InputTypeDefault.create().property( InputTypeProperty.create( "default", yml.defaultValue ).build() ).build() );
-            }
-
-            if ( yml.maxLength != null )
-            {
-                builder.inputTypeConfig( InputTypeConfig.create()
-                                             .property( InputTypeProperty.create( "maxLength", yml.maxLength.toString() ).build() )
-                                             .build() );
-            }
-
-            if ( yml.regexp != null )
-            {
-                builder.inputTypeConfig(
-                    InputTypeConfig.create().property( InputTypeProperty.create( "regexp", yml.regexp ).build() ).build() );
-            }
-
-            return builder.build();
-        } );
-
-        register( DoubleYml.class, yml -> {
-            final Input.Builder builder = defaultConverter( yml ).inputType( InputTypeName.from( "Double" ) );
-
-            if ( yml.defaultValue != null )
-            {
-                builder.defaultValue( InputTypeDefault.create()
-                                          .property( InputTypeProperty.create( "default", yml.defaultValue.toString() ).build() )
-                                          .build() );
-            }
-
-            final InputTypeConfig.Builder configBuilder = InputTypeConfig.create();
-            if ( yml.min != null )
-            {
-                configBuilder.property( InputTypeProperty.create( "min", yml.min.toString() ).build() );
-            }
-            if ( yml.max != null )
-            {
-                configBuilder.property( InputTypeProperty.create( "max", yml.max.toString() ).build() );
-            }
-
-            builder.inputTypeConfig( configBuilder.build() );
-
-            return builder.build();
-        } );
-
-        register( ContentSelectorYml.class, yml -> {
-            final Input.Builder builder = defaultConverter( yml ).inputType( InputTypeName.from( "ContentSelector" ) );
-
-            final InputTypeConfig.Builder configBuilder = InputTypeConfig.create();
-            if ( yml.hideToggleIcon != null )
-            {
-                configBuilder.property( InputTypeProperty.create( "hideToggleIcon", yml.hideToggleIcon.toString() ).build() );
-            }
-            if ( yml.treeMode != null )
-            {
-                configBuilder.property( InputTypeProperty.create( "treeMode", yml.treeMode.toString() ).build() );
-            }
-            if ( yml.allowContentType != null )
-            {
-                yml.allowContentType.forEach(
-                    allowType -> configBuilder.property( InputTypeProperty.create( "allowContentType", allowType ).build() ) );
-            }
-            if ( yml.allowPath != null )
-            {
-                yml.allowPath.forEach( allowPath -> configBuilder.property( InputTypeProperty.create( "allowPath", allowPath ).build() ) );
-            }
-
-            builder.inputTypeConfig( configBuilder.build() );
-
-            return builder.build();
-        } );
+        register( RadioButtonYml.class, InputTypeName.RADIO_BUTTON );
+        register( TextLineYml.class, InputTypeName.TEXT_LINE );
+        register( DoubleYml.class, InputTypeName.DOUBLE );
+        register( ContentSelectorYml.class, InputTypeName.CONTENT_SELECTOR );
     }
 
-    public static <T extends InputYml> void register( final Class<T> type, final InputFactory<T> factory )
+    public static Map<Class<? extends InputYml>, InputTypeName> getFactories()
     {
-        FACTORIES.put( type, factory );
+        return Collections.unmodifiableMap( FACTORIES );
     }
 
-    @SuppressWarnings("unchecked")
-    public static Input toInput( final InputYml yaml )
+    public static <T extends InputYml> void register( final Class<T> type, final InputTypeName typeName )
     {
-        InputFactory<InputYml> factory = (InputFactory<InputYml>) FACTORIES.get( yaml.getClass() );
-        if ( factory == null )
-        {
-            throw new IllegalArgumentException( "No factory for " + yaml.getClass() );
-        }
-        return factory.build( yaml );
-    }
-
-    private static Input.Builder defaultConverter( final InputYml inputYml )
-    {
-        final Input.Builder builder = Input.create().name( inputYml.name );
-
-        if ( inputYml.label != null )
-        {
-            builder.label( inputYml.label.text() ).labelI18nKey( inputYml.label.i18n() );
-        }
-
-        if ( inputYml.helpText != null )
-        {
-            builder.helpText( inputYml.helpText.text() ).helpTextI18nKey( inputYml.helpText.i18n() );
-        }
-
-        if ( inputYml.occurrences != null )
-        {
-            builder.occurrences( inputYml.occurrences );
-        }
-
-        return builder;
+        FACTORIES.put( type, typeName );
     }
 }
