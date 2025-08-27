722a1c1840eca6b2498e42dd5ec0c0f016af808a
Anatol Sialitski <asi@enonic.com>
2025-08-27 09:42:15 +0200

    Improving InputType creation from YML #11302

--- modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/mapper/TextLineYml.java
+++ modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/mapper/TextLineYml.java
@@ -2,6 +2,12 @@ package com.enonic.xp.core.impl.schema.mapper;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import com.enonic.xp.form.Input;
+import com.enonic.xp.inputtype.InputTypeConfig;
+import com.enonic.xp.inputtype.InputTypeDefault;
+import com.enonic.xp.inputtype.InputTypeName;
+import com.enonic.xp.inputtype.InputTypeProperty;
+
 public class TextLineYml
     extends InputYml
 {
@@ -11,4 +17,34 @@ public class TextLineYml
 
     @JsonProperty("default")
     public String defaultValue;
+
+    @Override
+    public InputTypeName getInputTypeName()
+    {
+        return InputTypeName.TEXT_LINE;
+    }
+
+    @Override
+    public void customizeInputType( final Input.Builder builder )
+    {
+        if ( defaultValue != null )
+        {
+            builder.defaultValue(
+                InputTypeDefault.create().property( InputTypeProperty.create( "default", defaultValue ).build() ).build() );
+        }
+
+        final InputTypeConfig.Builder configBuilder = InputTypeConfig.create();
+
+        if ( maxLength != null )
+        {
+            configBuilder.property( InputTypeProperty.create( "maxLength", maxLength.toString() ).build() );
+        }
+
+        if ( regexp != null )
+        {
+            configBuilder.property( InputTypeProperty.create( "regexp", regexp ).build() ).build();
+        }
+
+        builder.inputTypeConfig( configBuilder.build() );
+    }
 }
