163300a9c41f93b410ab9783d72b12118af3a333
Anatol Sialitski <asi@enonic.com>
2025-08-27 09:42:15 +0200

    Improving InputType creation from YML #11302

--- modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/mapper/InputYml.java
+++ modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/mapper/InputYml.java
@@ -2,10 +2,12 @@ package com.enonic.xp.core.impl.schema.mapper;
 
 import com.enonic.xp.form.FormItem;
 import com.enonic.xp.form.FormItemType;
+import com.enonic.xp.form.Input;
 import com.enonic.xp.form.Occurrences;
+import com.enonic.xp.inputtype.InputTypeName;
 import com.enonic.xp.schema.LocalizedText;
 
-public class InputYml
+public abstract class InputYml
     extends FormItem
 {
     public String type;
@@ -33,6 +35,29 @@ public class InputYml
     @Override
     public FormItem copy()
     {
-        return InputRegistry.toInput( this );
+        final Input.Builder builder = Input.create().name( name ).inputType( getInputTypeName() );
+
+        if ( label != null )
+        {
+            builder.label( label.text() ).labelI18nKey( label.i18n() );
+        }
+
+        if ( helpText != null )
+        {
+            builder.helpText( helpText.text() ).helpTextI18nKey( helpText.i18n() );
+        }
+
+        if ( occurrences != null )
+        {
+            builder.occurrences( occurrences );
+        }
+
+        customizeInputType( builder );
+
+        return builder.build();
     }
+
+    public abstract InputTypeName getInputTypeName();
+
+    public abstract void customizeInputType( Input.Builder builder );
 }
--- modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/mapper/RadioButtonYml.java
+++ modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/mapper/RadioButtonYml.java
@@ -10,6 +10,12 @@ import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import com.enonic.xp.form.Input;
+import com.enonic.xp.inputtype.InputTypeConfig;
+import com.enonic.xp.inputtype.InputTypeDefault;
+import com.enonic.xp.inputtype.InputTypeName;
+import com.enonic.xp.inputtype.InputTypeProperty;
+
 public class RadioButtonYml
     extends InputYml
 {
@@ -17,6 +23,34 @@ public class RadioButtonYml
 
     @JsonProperty("default")
     public String defaultValue;
+
+    @Override
+    public InputTypeName getInputTypeName()
+    {
+        return InputTypeName.RADIO_BUTTON;
+    }
+
+    @Override
+    public void customizeInputType( final Input.Builder builder )
+    {
+        if ( defaultValue != null )
+        {
+            builder.defaultValue(
+                InputTypeDefault.create().property( InputTypeProperty.create( "default", defaultValue ).build() ).build() );
+        }
+
+        if ( options != null )
+        {
+            final InputTypeConfig.Builder configBuilder = InputTypeConfig.create();
+            options.forEach( option -> {
+                final InputTypeProperty.Builder propertyBuilder = InputTypeProperty.create( "option", option.value );
+                propertyBuilder.attribute( "value", option.name );
+                option.getAttributes().forEach( propertyBuilder::attribute );
+                configBuilder.property( propertyBuilder.build() );
+            } );
+            builder.inputTypeConfig( configBuilder.build() );
+        }
+    }
 
     public static class Option
     {
