e93abd8da78263e65f05c3013c7aee02064ba602
anatol-sialitski <53557255+anatol-sialitski@users.noreply.github.com>
2025-08-28 13:00:45 +0200

    Support CustomSelector in YAML #11310 (#11311)

--- modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/YmlTypeParser.java
+++ modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/YmlTypeParser.java
@@ -3,8 +3,8 @@ package com.enonic.xp.core.impl.schema;
 import java.io.UncheckedIOException;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.InjectableValues;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.NamedType;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
@@ -14,12 +14,11 @@ import com.enonic.xp.core.impl.schema.mapper.ContentTypeMapper;
 import com.enonic.xp.core.impl.schema.mapper.ContentTypeNameMapper;
 import com.enonic.xp.core.impl.schema.mapper.FieldSetMapper;
 import com.enonic.xp.core.impl.schema.mapper.FormDeserializer;
-import com.enonic.xp.core.impl.schema.mapper.FormItemMapper;
+import com.enonic.xp.core.impl.schema.mapper.FormItemDeserializer;
 import com.enonic.xp.core.impl.schema.mapper.FormItemSetMapper;
 import com.enonic.xp.core.impl.schema.mapper.FormOptionSetMapper;
 import com.enonic.xp.core.impl.schema.mapper.FormOptionSetOptionMapper;
 import com.enonic.xp.core.impl.schema.mapper.InlineMixinMapper;
-import com.enonic.xp.core.impl.schema.mapper.InputRegistry;
 import com.enonic.xp.core.impl.schema.mapper.MixinNameMapper;
 import com.enonic.xp.core.impl.schema.mapper.OccurrencesMapper;
 import com.enonic.xp.form.FieldSet;
@@ -29,7 +28,6 @@ import com.enonic.xp.form.FormItemSet;
 import com.enonic.xp.form.FormOptionSet;
 import com.enonic.xp.form.FormOptionSetOption;
 import com.enonic.xp.form.InlineMixin;
-import com.enonic.xp.form.Input;
 import com.enonic.xp.form.Occurrences;
 import com.enonic.xp.schema.content.ContentType;
 import com.enonic.xp.schema.content.ContentTypeName;
@@ -43,14 +41,13 @@ public final class YmlTypeParser
     {
         final SimpleModule module = new SimpleModule();
         module.addDeserializer( Form.class, new FormDeserializer() );
+        module.addDeserializer( FormItem.class, new FormItemDeserializer() );
         MAPPER.registerModule( module );
 
         MAPPER.addMixIn( ContentTypeName.class, ContentTypeNameMapper.class );
         MAPPER.addMixIn( ContentType.Builder.class, ContentTypeMapper.Builder.class );
 
         MAPPER.addMixIn( Occurrences.class, OccurrencesMapper.class );
-
-        MAPPER.addMixIn( FormItem.class, FormItemMapper.class );
 
         MAPPER.addMixIn( FieldSet.class, FieldSetMapper.class );
         MAPPER.addMixIn( FieldSet.Builder.class, FieldSetMapper.Builder.class );
@@ -67,30 +64,20 @@ public final class YmlTypeParser
 
         MAPPER.addMixIn( FormOptionSetOption.class, FormOptionSetOptionMapper.class );
         MAPPER.addMixIn( FormOptionSetOption.Builder.class, FormOptionSetOptionMapper.Builder.class );
-
-
-        InputRegistry.getFactories()
-            .forEach( ( clazz, typeName ) -> MAPPER.registerSubtypes( new NamedType( clazz, typeName.toString() ) ) );
-
-        MAPPER.registerSubtypes( new NamedType( Input.class, "TextArea" ), new NamedType( Input.class, "CheckBox" ),
-                                 new NamedType( Input.class, "ComboBox" ), new NamedType( Input.class, "CustomSelector" ),
-                                 new NamedType( Input.class, "ContentTypeFilter" ), new NamedType( Input.class, "Date" ),
-                                 new NamedType( Input.class, "DateTime" ), new NamedType( Input.class, "MediaUploader" ),
-                                 new NamedType( Input.class, "AttachmentUploader" ), new NamedType( Input.class, "GeoPoint" ),
-                                 new NamedType( Input.class, "HtmlArea" ), new NamedType( Input.class, "ImageSelector" ),
-                                 new NamedType( Input.class, "MediaSelector" ), new NamedType( Input.class, "ImageUploader" ),
-                                 new NamedType( Input.class, "Long" ), new NamedType( Input.class, "Tag" ),
-                                 new NamedType( Input.class, "Time" ), new NamedType( FieldSet.class, "FieldSet" ),
-                                 new NamedType( InlineMixin.class, "InlineMixin" ), new NamedType( FormItemSet.class, "ItemSet" ),
-                                 new NamedType( FormOptionSet.class, "OptionSet" ),
-                                 new NamedType( FormOptionSetOption.class, "OptionSetOption" ) );
     }
 
     public <T> T parse( final String yml, final Class<T> clazz )
     {
+        return parse( yml, clazz, null );
+    }
+
+    public <T> T parse( final String yml, final Class<T> clazz, final InjectableValues injectableValues )
+    {
         try
         {
-            return MAPPER.readValue( yml, clazz );
+            final ObjectMapper localMapper = MAPPER.copy();
+            localMapper.setInjectableValues( injectableValues );
+            return localMapper.readValue( yml, clazz );
         }
         catch ( final JsonProcessingException e )
         {
@@ -100,13 +87,16 @@ public final class YmlTypeParser
 
     public ContentType.Builder parseContentType( final String yml, final ApplicationKey currentApplication )
     {
-        final ContentType.Builder builder = parse( yml, ContentType.Builder.class );
+        final ApplicationRelativeResolver applicationRelativeResolver = new ApplicationRelativeResolver( currentApplication );
+
+        final ContentType.Builder builder = parse( yml, ContentType.Builder.class,
+                                                   new InjectableValues.Std().addValue( "applicationRelativeResolver",
+                                                                                        applicationRelativeResolver ) );
         builder.name( "_TEMP_NAME_" );
 
         final ContentType contentType = builder.build();
 
         return ContentType.create( contentType )
-            .superType( new ApplicationRelativeResolver( currentApplication ).toContentTypeName( contentType.getSuperType().toString() ) );
+            .superType( applicationRelativeResolver.toContentTypeName( contentType.getSuperType().toString() ) );
     }
-
 }
