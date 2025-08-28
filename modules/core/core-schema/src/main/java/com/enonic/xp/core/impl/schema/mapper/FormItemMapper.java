877159031a3d16ec2eb99193a29d1f139e2f03d7
anatol-sialitski <53557255+anatol-sialitski@users.noreply.github.com>
2025-08-28 13:00:45 +0200

    Support CustomSelector in YAML #11310 (#11311)

--- modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/mapper/FormItemMapper.java
+++
@@ -1,8 +0,0 @@
-package com.enonic.xp.core.impl.schema.mapper;
-
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", visible = true)
-public interface FormItemMapper
-{
-}
