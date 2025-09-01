d168771b1ad25363c40e2a6539412ae43cff9a42
Anatol Sialitski <asi@enonic.com>
2025-09-01 16:16:02 +0200

    Support `Long` in YAML #11323

---
+++ modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/mapper/LongYml.java
@@ -0,0 +1,39 @@
+package com.enonic.xp.core.impl.schema.mapper;
+
+import com.enonic.xp.form.Input;
+import com.enonic.xp.inputtype.InputTypeConfig;
+import com.enonic.xp.inputtype.InputTypeName;
+import com.enonic.xp.inputtype.InputTypeProperty;
+
+public class LongYml
+    extends InputYml
+{
+
+    public static final InputTypeName INPUT_TYPE_NAME = InputTypeName.LONG;
+
+    public Long min;
+
+    public Long max;
+
+    public LongYml()
+    {
+        super( INPUT_TYPE_NAME );
+    }
+
+    @Override
+    public void customizeInputType( final Input.Builder builder )
+    {
+        final InputTypeConfig.Builder configBuilder = InputTypeConfig.create();
+
+        if ( min != null )
+        {
+            configBuilder.property( InputTypeProperty.create( "min", min.toString() ).build() );
+        }
+        if ( max != null )
+        {
+            configBuilder.property( InputTypeProperty.create( "max", max.toString() ).build() );
+        }
+
+        builder.inputTypeConfig( configBuilder.build() );
+    }
+}
