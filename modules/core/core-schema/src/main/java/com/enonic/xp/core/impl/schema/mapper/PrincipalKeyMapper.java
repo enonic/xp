27a295627e46d1c098e5ab4e95237de54050f5b1
Anatol Sialitski <asi@enonic.com>
2025-09-04 14:31:05 +0200

    Support `WebappDescriptor` using YAML #11349

--- modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/mapper/PrincipalKeyMapper.java
+++ modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/mapper/PrincipalKeyMapper.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 
 import com.enonic.xp.security.PrincipalKey;
 
+import static com.google.common.base.Strings.nullToEmpty;
+
 public class PrincipalKeyMapper
 {
     @JsonCreator
     public static PrincipalKey from( String value )
     {
-        return PrincipalKey.from( value );
+        return PrincipalKey.from( nullToEmpty( value ).trim() );
     }
 }
