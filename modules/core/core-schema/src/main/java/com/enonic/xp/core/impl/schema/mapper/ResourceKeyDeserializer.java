933a42fbad36daec757c7f3ca89f25253cfcf9a0
anatol-sialitski <53557255+anatol-sialitski@users.noreply.github.com>
2025-09-08 10:47:40 +0200

    Support `SiteDescriptor` using YAML #11358 (#11363)

---
+++ modules/core/core-schema/src/main/java/com/enonic/xp/core/impl/schema/mapper/ResourceKeyDeserializer.java
@@ -0,0 +1,35 @@
+package com.enonic.xp.core.impl.schema.mapper;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.enonic.xp.app.ApplicationKey;
+import com.enonic.xp.resource.ResourceKey;
+
+public final class ResourceKeyDeserializer
+    extends JsonDeserializer<ResourceKey>
+{
+    @Override
+    public ResourceKey deserialize( final JsonParser jsonParser, final DeserializationContext ctxt )
+        throws IOException
+    {
+        final ObjectMapper mapper = (ObjectMapper) jsonParser.getCodec();
+        final JsonNode node = mapper.readTree( jsonParser );
+
+        final String rawValue = node.asText();
+        if ( rawValue.contains( ":" ) )
+        {
+            return ResourceKey.from( rawValue );
+        }
+        else
+        {
+            final ApplicationKey currentApplication = (ApplicationKey) ctxt.findInjectableValue( "currentApplication", null, null );
+            return ResourceKey.from( currentApplication, rawValue );
+        }
+    }
+}
