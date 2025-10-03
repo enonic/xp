ae2b0be2ab9d56bf59f0dd7e8d86a07b8494b354
Anatol Sialitski <asi@enonic.com>
2025-10-03 15:53:45 +0200

    Remove XmlDataDescriptorParser #11432

--- modules/core/core-content/src/main/java/com/enonic/xp/core/impl/content/schema/XDataLoader.java
+++ modules/core/core-content/src/main/java/com/enonic/xp/core/impl/content/schema/XDataLoader.java
@@ -2,13 +2,13 @@ package com.enonic.xp.core.impl.content.schema;
 
 import java.time.Instant;
 
+import com.enonic.xp.core.impl.content.parser.YmlXDataParser;
 import com.enonic.xp.core.impl.schema.SchemaLoader;
 import com.enonic.xp.descriptor.DescriptorKey;
 import com.enonic.xp.resource.Resource;
 import com.enonic.xp.resource.ResourceService;
 import com.enonic.xp.schema.xdata.XData;
 import com.enonic.xp.schema.xdata.XDataName;
-import com.enonic.xp.xml.parser.XmlXDataParser;
 
 final class XDataLoader
     extends SchemaLoader<XDataName, XData>
@@ -21,24 +21,13 @@ final class XDataLoader
     @Override
     protected XData load( final XDataName name, final Resource resource )
     {
-        final XData.Builder builder = XData.create();
-        parseXml( resource, builder );
+        final XData.Builder builder = YmlXDataParser.parse( resource.readString(), name.getApplicationKey() );
 
         final Instant modifiedTime = Instant.ofEpochMilli( resource.getTimestamp() );
         builder.modifiedTime( modifiedTime );
         builder.createdTime( modifiedTime );
-
         builder.icon( loadIcon( name ) );
         return builder.name( name ).build();
-    }
-
-    private void parseXml( final Resource resource, final XData.Builder builder )
-    {
-        final XmlXDataParser parser = new XmlXDataParser();
-        parser.currentApplication( resource.getKey().getApplicationKey() );
-        parser.source( resource.readString() );
-        parser.builder( builder );
-        parser.parse();
     }
 
     @Override
--- modules/lib/lib-schema/src/main/resources/lib/xp/examples/schema/createXData.js
+++ modules/lib/lib-schema/src/main/resources/lib/xp/examples/schema/createXData.js
@@ -3,21 +3,15 @@ var assert = require('/lib/xp/testing');
 
 /* global log*/
 
-let resource = `<?xml version='1.0' encoding='UTF-8'?>
-        <x-data xmlns='urn:enonic:xp:model:1.0'>
-          <display-name>Virtual X-data</display-name>
-          <description>X-data description</description>
-          <form>
-            <input type='TextLine' name='label'>
-              <label>Label</label>
-              <immutable>false</immutable>
-              <indexed>true</indexed>
-              <custom-text/>
-              <help-text/>
-              <occurrences minimum='0' maximum='2'/>
-            </input>
-          </form>
-        </x-data>`;
+let resource = `displayName: "Virtual X-data"
+description: "X-data description"
+form:
+- type: "TextLine"
+  name: "label"
+  label: "Label"
+  occurrences:
+    minimum: 0
+    maximum: 2`;
 
 // BEGIN
 // Create virtual mixin.
@@ -36,26 +30,18 @@ log.info('Created x-data: ' + result.name);
 assert.assertJsonEquals({
     name: 'myapp:mydata',
     displayName: 'Virtual X-data',
-    displayNameI18nKey: '',
     description: 'X-data description',
-    descriptionI18nKey: '',
     createdTime: '2021-09-25T10:00:00Z',
     modifiedTime: '2021-09-25T10:00:00Z',
-    resource: '<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n' +
-              '        <x-data xmlns=\'urn:enonic:xp:model:1.0\'>\n' +
-              '          <display-name>Virtual X-data</display-name>\n' +
-              '          <description>X-data description</description>\n' +
-              '          <form>\n' +
-              '            <input type=\'TextLine\' name=\'label\'>\n' +
-              '              <label>Label</label>\n' +
-              '              <immutable>false</immutable>\n' +
-              '              <indexed>true</indexed>\n' +
-              '              <custom-text/>\n' +
-              '              <help-text/>\n' +
-              '              <occurrences minimum=\'0\' maximum=\'2\'/>\n' +
-              '            </input>\n' +
-              '          </form>\n' +
-              '        </x-data>',
+    resource: 'displayName: "Virtual X-data"\n' +
+              'description: "X-data description"\n' +
+              'form:\n' +
+              '- type: "TextLine"\n' +
+              '  name: "label"\n' +
+              '  label: "Label"\n' +
+              '  occurrences:\n' +
+              '    minimum: 0\n' +
+              '    maximum: 2',
     type: 'XDATA',
     form: [
         {
