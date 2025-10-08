b386605a050b6d2ef5b86f57f50bd8825dc84667
Sergey Rymsha <rymsha@users.noreply.github.com>
2025-10-08 12:25:00 +0200

    Gradle 9 #11456 (#11457)

--- buildSrc/src/main/java/com/enonic/gradle/BuildInfoHelper.java
+++ buildSrc/src/main/java/com/enonic/gradle/BuildInfoHelper.java
@@ -1,49 +1,35 @@
 package com.enonic.gradle;
 
 import java.io.File;
-import java.time.Instant;
+import java.util.Map;
 
+import org.codehaus.groovy.util.ListHashMap;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.RepositoryBuilder;
-import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 
 public final class BuildInfoHelper
 {
-    public static BuildInfo extract( final File baseDir )
+    private static final String NA = "N/A";
+
+    public static Map<String, String> extract( final File baseDir )
         throws Exception
     {
+        final Map<String, String> info = new ListHashMap<>();
 
-        final BuildInfo info = new BuildInfo();
+        final Repository repository = new RepositoryBuilder().findGitDir( baseDir ).readEnvironment().build();
 
-        final RepositoryBuilder builder = new RepositoryBuilder();
-        final Repository repository = builder.findGitDir( baseDir ).readEnvironment().build();
-        info.setBranch( repository.getBranch() );
+        info.put( "branch", repository.getBranch() );
 
         final ObjectId revision = repository.resolve( Constants.HEAD );
-        if ( revision == null )
-        {
-            return info;
-        }
-
-        info.setHash( revision.getName() );
-        info.setShortHash( revision.abbreviate( 7 ).name() );
-        info.setTimestamp( findLastCommitTimestamp( repository, revision ) );
+        info.put( "hash", revision != null ? revision.getName() : NA );
+        info.put( "shortHash", revision != null ? revision.abbreviate( 7 ).name() : NA );
+        info.put( "timestamp", revision != null
+            ? new RevWalk( repository ).parseCommit( revision ).getCommitterIdent().getWhenAsInstant().toString()
+            : NA );
         return info;
-    }
-
-    private static String findLastCommitTimestamp( final Repository repository, final ObjectId revision )
-        throws Exception
-    {
-        final RevWalk rw = new RevWalk( repository );
-        final RevCommit commit = rw.parseCommit( revision );
-        final PersonIdent committer = commit.getCommitterIdent();
-        final Instant commitDate = committer.getWhen().toInstant();
-
-        return commitDate.toString();
     }
 }
 
