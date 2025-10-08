beae801c19026b901efe6283c11d31c858839cf2
Anatol Sialitski <asi@enonic.com>
2025-10-08 15:33:49 +0200

    Remove XmlApiDescriptorParser #11450

--- modules/portal/portal-impl/src/main/java/com/enonic/xp/portal/impl/api/YmlApiDescriptorParser.java
+++ modules/portal/portal-impl/src/main/java/com/enonic/xp/portal/impl/api/YmlApiDescriptorParser.java
@@ -1,8 +1,11 @@
 package com.enonic.xp.portal.impl.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import com.enonic.xp.api.ApiDescriptor;
 import com.enonic.xp.app.ApplicationKey;
 import com.enonic.xp.core.impl.schema.YmlParserBase;
+import com.enonic.xp.security.PrincipalKeys;
 
 public final class YmlApiDescriptorParser
 {
@@ -16,5 +19,23 @@ public final class YmlApiDescriptorParser
     public static ApiDescriptor.Builder parse( final String resource, final ApplicationKey currentApplication )
     {
         return PARSER.parse( resource, ApiDescriptor.Builder.class, currentApplication );
+    }
+
+    private abstract static class ApiDescriptorBuilderMapper
+    {
+        @JsonProperty("allow")
+        public abstract ApiDescriptor.Builder allowedPrincipals( PrincipalKeys allowedPrincipals );
+
+        @JsonProperty("displayName")
+        public abstract ApiDescriptor.Builder displayName( String displayName );
+
+        @JsonProperty("documentationUrl")
+        public abstract ApiDescriptor.Builder documentationUrl( String documentationUrl );
+
+        @JsonProperty("description")
+        public abstract ApiDescriptor.Builder description( String description );
+
+        @JsonProperty("mount")
+        abstract ApiDescriptor.Builder mount( Boolean value );
     }
 }
