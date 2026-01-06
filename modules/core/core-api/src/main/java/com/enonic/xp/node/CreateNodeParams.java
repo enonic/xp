033da6e2d9461b491886058b48309c8741a5cf23
rymsha <sry@enonic.com>
2026-01-06 09:23:19 +0100

    content.duplicate attr in not set #11630

--- modules/core/core-api/src/main/java/com/enonic/xp/node/CreateNodeParams.java
+++ modules/core/core-api/src/main/java/com/enonic/xp/node/CreateNodeParams.java
@@ -201,6 +201,7 @@ public final class CreateNodeParams
             this.manualOrderValue = createNodeParams.manualOrderValue;
             this.nodeType = createNodeParams.nodeType;
             createNodeParams.binaryAttachments.stream().forEach( binaryAttachments::add );
+            this.versionAttributes = createNodeParams.versionAttributes;
             this.refresh = createNodeParams.refresh;
         }
 
