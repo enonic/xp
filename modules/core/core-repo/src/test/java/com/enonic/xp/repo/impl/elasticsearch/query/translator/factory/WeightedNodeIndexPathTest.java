1068ece14a7b1681b2c9d16572b2890a0dc1c3b5
rymsha <sry@enonic.com>
2026-01-14 15:36:53 +0100

    More fine-grained boost factor #7195

--- modules/core/core-repo/src/test/java/com/enonic/xp/repo/impl/elasticsearch/query/translator/factory/WeightedNodeIndexPathTest.java
+++ modules/core/core-repo/src/test/java/com/enonic/xp/repo/impl/elasticsearch/query/translator/factory/WeightedNodeIndexPathTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import com.enonic.xp.repo.impl.elasticsearch.query.translator.factory.function.WeightedQueryFieldName;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class WeightedNodeIndexPathTest
 {
@@ -12,16 +13,24 @@ class WeightedNodeIndexPathTest
     void createWeightedQueryFieldName()
     {
         createWeightedQueryFieldName( "myField", "myField", null );
-        createWeightedQueryFieldName( "myField^5", "myField", 5 );
+        createWeightedQueryFieldName( "myField^0.5", "myField", 0.5f );
+        createWeightedQueryFieldName( "myField^5", "myField", 5f );
         createWeightedQueryFieldName( "myField^", "myField", null );
-        createWeightedQueryFieldName( "^5", "", 5 );
+        createWeightedQueryFieldName( "^5", "", 5f );
     }
 
-    private void createWeightedQueryFieldName( final String value, final String baseFieldName, final Integer weight )
+    private void createWeightedQueryFieldName( final String value, final String baseFieldName, final Float weight )
     {
         final WeightedQueryFieldName weightedQueryFieldName = WeightedQueryFieldName.from( value );
 
         assertEquals( baseFieldName, weightedQueryFieldName.getBaseFieldName() );
         assertEquals( weight, weightedQueryFieldName.getWeight() );
+    }
+
+    @Test
+    void invalidWeight()
+    {
+        assertThrows( IllegalArgumentException.class, () -> WeightedQueryFieldName.from( "myField^abc" ) );
+        assertThrows( IllegalArgumentException.class, () -> WeightedQueryFieldName.from( "myField^-1" ) );
     }
 }
