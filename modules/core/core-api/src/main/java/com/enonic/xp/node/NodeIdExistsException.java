fc10aa0ab854a58fc258009fb2f69d97001fe33d
Bradnitski <vbr@enonic.com>
2026-01-27 10:56:25 +0100

    Different error message when importing content that is in archive #10473

--- modules/core/core-api/src/main/java/com/enonic/xp/node/NodeIdExistsException.java
+++ modules/core/core-api/src/main/java/com/enonic/xp/node/NodeIdExistsException.java
@@ -1,6 +1,8 @@
 package com.enonic.xp.node;
 
 import com.enonic.xp.annotation.PublicApi;
+import com.enonic.xp.branch.Branch;
+import com.enonic.xp.repository.RepositoryId;
 
 @PublicApi
 public class NodeIdExistsException
@@ -10,11 +12,34 @@ public class NodeIdExistsException
 
     private final NodePath nodePath;
 
-    public NodeIdExistsException( final NodeId nodeId, final NodePath nodePath )
+    private final RepositoryId repositoryId;
+
+    private final Branch branch;
+
+    public NodeIdExistsException( final NodeId nodeId, final NodePath nodePath, final RepositoryId repositoryId, final Branch branch )
     {
-        super( "Node " + nodeId + " at path " + nodePath + " already exists" );
+        super( buildMessage( nodeId, nodePath, repositoryId, branch ) );
         this.nodeId = nodeId;
         this.nodePath = nodePath;
+        this.repositoryId = repositoryId;
+        this.branch = branch;
+    }
+
+    private static String buildMessage( NodeId nodeId, NodePath nodePath, RepositoryId repositoryId, Branch branch )
+    {
+        final StringBuilder message =
+            new StringBuilder().append( "Node " ).append( nodeId ).append( " already exists at " ).append( nodePath );
+
+        if ( repositoryId != null )
+        {
+            message.append( " repository: " ).append( repositoryId );
+        }
+        if ( branch != null )
+        {
+            message.append( " branch: " ).append( branch );
+        }
+
+        return message.toString();
     }
 
     public NodeId getNodeId()
@@ -26,4 +51,16 @@ public class NodeIdExistsException
     {
         return nodePath;
     }
+
+    public RepositoryId getRepositoryId()
+    {
+        return repositoryId;
+    }
+
+    public Branch getBranch()
+    {
+        return branch;
+    }
+
+
 }
