93c9dd877b4c90ddf2a2c56461f4a212f8bba48a
rymsha <sry@enonic.com>
2026-04-05 15:51:20 +0200

    fix merge

--- modules/lib/lib-portal/src/main/resources/lib/xp/examples/portal/apiUrl.js
+++ modules/lib/lib-portal/src/main/resources/lib/xp/examples/portal/apiUrl.js
@@ -5,10 +5,15 @@ const assert = require('/lib/xp/testing');
 const url = portalLib.apiUrl({
     api: 'com.enonic.app.myapp:myapi',
     params: {
+        'å': 'a',
+        'ø': 'o',
+        'æ': ['a', 'e'],
+        'empty': '',
         a: 1,
         b: 2
     },
-    path: 'mypath/subpath'
+    path: ['segment1', 'segment2'],
+    baseUrl: 'https://example.com',
 });
 
 const apiUrlWithPathSegments = portalLib.apiUrl({
@@ -27,7 +32,7 @@ const apiUrlWithBaseUrl = portalLib.apiUrl({
 
 // END
 
-assert.assertEquals('/site/mocksite/_/api/com.enonic.app.myapp:myapi?a=1&b=2', url);
+assert.assertEquals('/site/mocksite/_/api/com.enonic.app.myapp:myapi?%C3%A5=a&%C3%B8=o&%C3%A6=a&%C3%A6=e&empty=&a=1&b=2', url);
 assert.assertEquals('/site/mocksite/_/api/com.enonic.app.myapp:myapi', apiUrlWithPathSegments);
 assert.assertEquals('/site/mocksite/_/api/com.enonic.app.myapp:myapi', apiUrl);
 assert.assertEquals('/site/mocksite/_/api/myapplication:myapi', apiUrlWithBaseUrl);
--- modules/lib/lib-portal/src/main/resources/lib/xp/examples/portal/serviceUrl.js
+++ modules/lib/lib-portal/src/main/resources/lib/xp/examples/portal/serviceUrl.js
@@ -5,10 +5,14 @@ var assert = require('/lib/xp/testing');
 var url = portalLib.serviceUrl({
     service: 'myservice',
     params: {
+        'å': 'a',
+        'ø': 'o',
+        'æ': ['a', 'e'],
+        'empty': '',
         a: 1,
         b: 2
     }
 });
 // END
 
-assert.assertEquals('/site/mocksite/_/service/myservice?a=1&b=2', url);
+assert.assertEquals('/site/mocksite/_/service/myservice?%C3%A5=a&%C3%B8=o&%C3%A6=a&%C3%A6=e&empty=&a=1&b=2', url);
