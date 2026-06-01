4b563d5c4dd3534e2fb667efcc4e1d170d711cc9
Sergey Rymsha <rymsha@users.noreply.github.com>
2026-06-01 10:56:46 +0200

    No way to restrict urls in installUrl #12086 (#12100)
    (cherry picked from commit 1c2bb9cc0630bf4f82fd96eb0166ee6ea4200869)

--- modules/server/server-rest/src/main/java/com/enonic/xp/impl/server/rest/AppManagementConfig.java
+++ modules/server/server-rest/src/main/java/com/enonic/xp/impl/server/rest/AppManagementConfig.java
@@ -2,7 +2,7 @@ package com.enonic.xp.impl.server.rest;
 
 public @interface AppManagementConfig
 {
-    String installUrl_allowedUrls() default "https://repo.enonic.com/public/*";
+    String installUrl_allowedUrls() default "https://*";
 
     boolean installUrl_checksumRequired() default true;
 }
