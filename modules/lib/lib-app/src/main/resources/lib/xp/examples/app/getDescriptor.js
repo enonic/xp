6dda3183b2b547f29dbd9c2d7dd1fc160f8108f2
Anatol Sialitski <asi@enonic.com>
2026-07-31 16:33:16 +0200

    Implement server:schema Universal API #12218

--- modules/lib/lib-app/src/main/resources/lib/xp/examples/app/getDescriptor.js
+++ modules/lib/lib-app/src/main/resources/lib/xp/examples/app/getDescriptor.js
@@ -4,7 +4,7 @@ var assert = require('/lib/xp/testing');
 /* global log*/
 
 // BEGIN
-// Create namespace.
+// Get application descriptor.
 var result = appLib.getDescriptor({
     key: 'my_app',
 });
