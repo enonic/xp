enonic/xp

commit 15020fde44dbdaacd683407b286614bf70f37029

read and write exports to/from exports folder only #8863

modules/lib/lib-export/src/main/java/com/enonic/xp/lib/export/ImportHandler.java
@@ -1,6 +1,5 @@
 package com.enonic.xp.lib.export;
 
-import java.nio.file.Path;
 import java.util.function.Function;
 
 import org.osgi.framework.Bundle;
@@ -9,7 +8,6 @@ import com.enonic.xp.app.ApplicationService;
 import com.enonic.xp.export.ExportService;
 import com.enonic.xp.export.ImportNodesParams;
 import com.enonic.xp.export.NodeImportResult;
-import com.enonic.xp.home.HomeDir;
 import com.enonic.xp.node.NodePath;
 import com.enonic.xp.resource.ResourceKey;
 import com.enonic.xp.script.ScriptValue;
@@ -21,8 +19,6 @@ import com.enonic.xp.vfs.VirtualFiles;
 public class ImportHandler
     implements ScriptBean
 {
-    private final Path exportsFolder = HomeDir.get().toPath().resolve( "data" ).resolve( "export" );
-
     private BeanContext context;
 
     private Object source;
@@ -44,10 +40,26 @@ public class ImportHandler
     public NodeImportResultMapper execute()
     {
         final ImportNodesParams.Builder paramsBuilder = ImportNodesParams.create()
-            .source( toVirtualFile( source ) )
             .targetNodePath( NodePath.create( targetNodePath ).build() )
-            .xslt( toVirtualFile( xslt ) )
             .nodeImportListener( new FunctionBasedNodeImportListener( nodeImported, nodeResolved ) );
+
+        if ( source instanceof ResourceKey )
+        {
+            paramsBuilder.source( toVirtualFile( (ResourceKey) source ) );
+        }
+        else
+        {
+            paramsBuilder.exportName( source.toString() );
+        }
+
+        if ( xslt instanceof ResourceKey )
+        {
+            paramsBuilder.xslt( toVirtualFile( (ResourceKey) xslt ) );
+        }
+        else
+        {
+            paramsBuilder.xsltFileName( xslt.toString() );
+        }
 
         if ( xsltParams != null )
         {
@@ -105,24 +117,11 @@ public class ImportHandler
         this.nodeResolved = nodeResolved;
     }
 
-    private VirtualFile toVirtualFile( final Object value )
+    private VirtualFile toVirtualFile( final ResourceKey resourceKey )
     {
-        if ( value == null )
-        {
-            return null;
-        }
-
-        if ( value instanceof ResourceKey )
-        {
-            final ResourceKey resourceKey = (ResourceKey) value;
-            final Bundle bundle =
-                context.getService( ApplicationService.class ).get().getInstalledApplication( resourceKey.getApplicationKey() ).getBundle();
-            return VirtualFiles.from( bundle, resourceKey.getPath() );
-        }
-        else
-        {
-            return VirtualFiles.from( exportsFolder.resolve( value.toString() ) );
-        }
+        final Bundle bundle =
+            context.getService( ApplicationService.class ).get().getInstalledApplication( resourceKey.getApplicationKey() ).getBundle();
+        return VirtualFiles.from( bundle, resourceKey.getPath() );
     }
 
     @Override

modules/core/core-api/src/main/java/com/enonic/xp/export/ExportNodesParams.java
@@ -3,6 +3,7 @@ package com.enonic.xp.export;
 import com.google.common.base.Preconditions;
 
 import com.enonic.xp.annotation.PublicApi;
+import com.enonic.xp.core.internal.FileNames;
 import com.enonic.xp.node.NodePath;
 
 @PublicApi
@@ -11,6 +12,8 @@ public class ExportNodesParams
     private final String rootDirectory;
 
     private final String targetDirectory;
+
+    private final String exportName;
 
     private final NodePath sourceNodePath;
 
@@ -33,6 +36,7 @@ public class ExportNodesParams
             this.rootDirectory = builder.rootDirectory;
         }
         this.targetDirectory = builder.targetDirectory;
+        this.exportName = builder.exportName;
         this.sourceNodePath = builder.sourceNodePath;
         this.dryRun = builder.dryRun;
         this.includeNodeIds = builder.includeNodeIds;
@@ -53,6 +57,11 @@ public class ExportNodesParams
     public String getTargetDirectory()
     {
         return targetDirectory;
+    }
+
+    public String getExportName()
+    {
+        return exportName;
     }
 
     public NodePath getSourceNodePath()
@@ -84,6 +93,8 @@ public class ExportNodesParams
     {
         private String rootDirectory;
 
+        private String exportName;
+
         private String targetDirectory;
 
         private NodePath sourceNodePath;
@@ -110,6 +121,12 @@ public class ExportNodesParams
         public Builder targetDirectory( final String targetDirectory )
         {
             this.targetDirectory = targetDirectory;
+            return this;
+        }
+
+        public Builder exportName( final String exportName )
+        {
+            this.exportName = exportName;
             return this;
         }
 
@@ -145,7 +162,14 @@ public class ExportNodesParams
 
         private void validate()
         {
-            Preconditions.checkNotNull( targetDirectory );
+            Preconditions.checkArgument( exportName != null || targetDirectory != null,
+                                         "Either exportName or targetDirectory must be set" );
+            Preconditions.checkArgument( !( exportName != null && targetDirectory != null ),
+                                         "exportName and targetDirectory are mutually exclusive" );
+            if ( exportName != null )
+            {
+                Preconditions.checkArgument( FileNames.isSafeFileName( exportName ), "Invalid export name" );
+            }
             Preconditions.checkNotNull( sourceNodePath );
         }
 

modules/core/core-api/src/main/java/com/enonic/xp/export/ImportNodesParams.java
@@ -3,7 +3,10 @@ package com.enonic.xp.export;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.common.base.Preconditions;
+
 import com.enonic.xp.annotation.PublicApi;
+import com.enonic.xp.core.internal.FileNames;
 import com.enonic.xp.node.NodePath;
 import com.enonic.xp.vfs.VirtualFile;
 
@@ -16,6 +19,8 @@ public class ImportNodesParams
 
     private final VirtualFile source;
 
+    private final String exportName;
+
     private final boolean importNodeids;
 
     private final boolean importPermissions;
@@ -23,7 +28,7 @@ public class ImportNodesParams
     private final VirtualFile xslt;
 
     private final Map<String, Object> xsltParams;
-    
+
     private final NodeImportListener nodeImportListener;
 
     private ImportNodesParams( final Builder builder )
@@ -36,6 +41,7 @@ public class ImportNodesParams
         this.xslt = builder.xslt;
         this.xsltParams = builder.xsltParams;
         this.nodeImportListener = builder.nodeImportListener;
+        this.exportName = builder.exportName;
     }
 
     public static Builder create()
@@ -56,6 +62,11 @@ public class ImportNodesParams
     public VirtualFile getSource()
     {
         return source;
+    }
+
+    public String getExportName()
+    {
+        return exportName;
     }
 
     public boolean isImportNodeids()
@@ -89,6 +100,8 @@ public class ImportNodesParams
 
         private VirtualFile source;
 
+        private String exportName;
+
         private boolean dryRun;
 
         private boolean importNodeIds;
@@ -97,8 +110,10 @@ public class ImportNodesParams
 
         private VirtualFile xslt;
 
+        private String xsltFileName;
+
         private Map<String, Object> xsltParams;
-        
+
         private NodeImportListener nodeImportListener;
 
         private Builder()
@@ -123,6 +138,12 @@ public class ImportNodesParams
             return this;
         }
 
+        public Builder exportName( final String exportName )
+        {
+            this.exportName = exportName;
+            return this;
+        }
+
         public Builder includeNodeIds( final boolean importNodeIds )
         {
             this.importNodeIds = importNodeIds;
@@ -138,6 +159,12 @@ public class ImportNodesParams
         public Builder xslt( final VirtualFile xslt )
         {
             this.xslt = xslt;
+            return this;
+        }
+
+        public Builder xsltFileName( final String xsltFileName )
+        {
+            this.xsltFileName = xsltFileName;
             return this;
         }
 
@@ -163,8 +190,25 @@ public class ImportNodesParams
             return this;
         }
 
+        private void validate()
+        {
+            Preconditions.checkArgument( exportName != null || source != null, "Either exportName or source must be set" );
+            Preconditions.checkArgument( !( exportName != null && source != null ), "exportName and source are mutually exclusive" );
+            if ( exportName != null )
+            {
+                Preconditions.checkArgument( FileNames.isSafeFileName( exportName ), "Invalid export name" );
+            }
+
+            Preconditions.checkArgument( !( xsltFileName != null && xslt != null ), "xsltFileName and xslt are mutually exclusive" );
+            if ( xsltFileName != null )
+            {
+                Preconditions.checkArgument( FileNames.isSafeFileName( xsltFileName ), "Invalid xslt file name" );
+            }
+        }
+
         public ImportNodesParams build()
         {
+            this.validate();
             return new ImportNodesParams( this );
         }
     }

modules/core/core-export/src/main/java/com/enonic/xp/core/impl/export/ExportConfiguration.java
@@ -1,0 +1,30 @@
+package com.enonic.xp.core.impl.export;
+
+import java.nio.file.Path;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+import com.enonic.xp.config.ConfigBuilder;
+import com.enonic.xp.config.ConfigInterpolator;
+import com.enonic.xp.config.Configuration;
+
+@Component(service = {ExportConfiguration.class}, configurationPid = "com.enonic.xp.export")
+public class ExportConfiguration
+{
+    private Configuration config;
+
+    public Path getExportsDir()
+    {
+        return Path.of( this.config.get( "exports.dir" ) );
+    }
+
+    @Activate
+    public void activate( final Map<String, String> map )
+    {
+        this.config = ConfigBuilder.create().load( getClass(), "default.properties" ).addAll( map ).build();
+
+        this.config = new ConfigInterpolator().interpolate( this.config );
+    }
+}

modules/core/core-export/src/main/java/com/enonic/xp/core/impl/export/ExportServiceImpl.java
@@ -1,6 +1,7 @@
 package com.enonic.xp.core.impl.export;
 
 import java.nio.file.Path;
+import java.util.Optional;
 
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
@@ -14,63 +15,69 @@ import com.enonic.xp.export.ImportNodesParams;
 import com.enonic.xp.export.NodeExportResult;
 import com.enonic.xp.export.NodeImportResult;
 import com.enonic.xp.node.NodeService;
+import com.enonic.xp.vfs.VirtualFile;
+import com.enonic.xp.vfs.VirtualFiles;
 
 @Component(immediate = true)
 @SuppressWarnings("UnusedDeclaration")
 public class ExportServiceImpl
     implements ExportService
 {
-    private String xpVersion;
+    private final String xpVersion;
 
-    private NodeService nodeService;
+    private final NodeService nodeService;
+
+    private final Path exportsDir;
 
     @Activate
-    public void activate( final ComponentContext context )
+    public ExportServiceImpl( final ComponentContext context, @Reference final ExportConfiguration exportConfiguration,
+                              @Reference final NodeService nodeService )
     {
-        xpVersion = context.getBundleContext().
-            getBundle().
-            getVersion().
-            toString();
+        this.xpVersion = context.getBundleContext().getBundle().getVersion().toString();
+        this.exportsDir = exportConfiguration.getExportsDir();
+        this.nodeService = nodeService;
     }
 
     @Override
     public NodeExportResult exportNodes( final ExportNodesParams params )
     {
-        return NodeExporter.create().
-            sourceNodePath( params.getSourceNodePath() ).
-            nodeService( this.nodeService ).
-            nodeExportWriter( new FileExportWriter() ).
-            rootDirectory( Path.of( params.getRootDirectory() ) ).
-            targetDirectory( Path.of( params.getTargetDirectory() ) ).
-            xpVersion( xpVersion ).
-            dryRun( params.isDryRun() ).
-            exportNodeIds( params.isIncludeNodeIds() ).
-            exportVersions( params.isIncludeVersions() ).
-            nodeExportListener( params.getNodeExportListener() ).
-            build().
-            execute();
+        final Path targetDirectory =
+            Optional.ofNullable( params.getTargetDirectory() ).map( Path::of ).orElse( exportsDir.resolve( params.getExportName() ) );
+
+        final Path rootDirectory = Optional.ofNullable( params.getRootDirectory() ).map( Path::of ).orElse( targetDirectory );
+
+        return NodeExporter.create()
+            .sourceNodePath( params.getSourceNodePath() )
+            .nodeService( this.nodeService )
+            .nodeExportWriter( new FileExportWriter() )
+            .rootDirectory( rootDirectory )
+            .targetDirectory( targetDirectory )
+            .xpVersion( xpVersion )
+            .dryRun( params.isDryRun() )
+            .exportNodeIds( params.isIncludeNodeIds() )
+            .exportVersions( params.isIncludeVersions() )
+            .nodeExportListener( params.getNodeExportListener() )
+            .build()
+            .execute();
     }
 
     @Override
     public NodeImportResult importNodes( final ImportNodesParams params )
     {
-        return NodeImporter.create().
-            nodeService( this.nodeService ).
-            sourceDirectory( params.getSource() ).
-            targetNodePath( params.getTargetNodePath() ).
-            dryRun( params.isDryRun() ).
-            importNodeIds( params.isImportNodeids() ).
-            importPermissions( params.isImportPermissions() ).
-            xslt( params.getXslt() ).
-            xsltParams( params.getXsltParams() ).
-            nodeImportListener( params.getNodeImportListener() ).
-            build().
-            execute();
-    }
+        VirtualFile source =
+            Optional.ofNullable( params.getSource() ).orElse( VirtualFiles.from( exportsDir.resolve( params.getExportName() ) ) );
 
-    @Reference
-    public void setNodeService( final NodeService nodeService )
-    {
-        this.nodeService = nodeService;
+        return NodeImporter.create()
+            .nodeService( this.nodeService )
+            .sourceDirectory( source )
+            .targetNodePath( params.getTargetNodePath() )
+            .dryRun( params.isDryRun() )
+            .importNodeIds( params.isImportNodeids() )
+            .importPermissions( params.isImportPermissions() )
+            .xslt( params.getXslt() )
+            .xsltParams( params.getXsltParams() )
+            .nodeImportListener( params.getNodeImportListener() )
+            .build()
+            .execute();
     }
 }

modules/core/core-export/src/main/resources/com/enonic/xp/core/impl/export/default.properties
@@ -1,0 +1,1 @@
+exports.dir=${xp.home}/data/export

modules/lib/lib-export/src/main/java/com/enonic/xp/lib/export/ExportHandler.java
@@ -1,12 +1,10 @@
 package com.enonic.xp.lib.export;
 
-import java.nio.file.Path;
 import java.util.function.Function;
 
 import com.enonic.xp.export.ExportNodesParams;
 import com.enonic.xp.export.ExportService;
 import com.enonic.xp.export.NodeExportResult;
-import com.enonic.xp.home.HomeDir;
 import com.enonic.xp.node.NodePath;
 import com.enonic.xp.script.bean.BeanContext;
 import com.enonic.xp.script.bean.ScriptBean;
@@ -14,13 +12,11 @@ import com.enonic.xp.script.bean.ScriptBean;
 public class ExportHandler
     implements ScriptBean
 {
-    private final Path exportsFolder = HomeDir.get().toPath().resolve( "data" ).resolve( "export" );
-
     private BeanContext context;
 
     private String sourceNodePath;
 
-    private String targetDirectory;
+    private String exportName;
 
     private Boolean includeNodeIds;
 
@@ -34,7 +30,7 @@ public class ExportHandler
     {
         final ExportNodesParams.Builder paramsBuilder = ExportNodesParams.create()
             .sourceNodePath( NodePath.create( sourceNodePath ).build() )
-            .targetDirectory( exportsFolder.resolve( targetDirectory ).toString() )
+            .exportName( exportName )
             .nodeExportListener( new FunctionBasedNodeExportListener( nodeExported, nodeResolved ) );
 
         if ( includeNodeIds != null )
@@ -55,9 +51,9 @@ public class ExportHandler
         this.sourceNodePath = sourceNodePath;
     }
 
-    public void setTargetDirectory( final String targetDirectory )
+    public void setExportName( final String exportName )
     {
-        this.targetDirectory = targetDirectory;
+        this.exportName = exportName;
     }
 
     public void setIncludeNodeIds( final Boolean includeNodeIds )

modules/lib/lib-export/src/main/resources/lib/xp/examples/export/exportNodes.js
@@ -6,7 +6,7 @@ var t = require('/lib/xp/testing');
 // Export content nodes.
 let exportNodes = exportLib.exportNodes({
     sourceNodePath: '/content',
-    targetDirectory: 'export-1',
+    exportName: 'export-1',
     includeNodeIds: true,
     includeVersions: true,
     nodeImported: (i) => {

commit 34da3f93fc60e46848472a639ddba40a9d2dc0ce

importNodes throws NullPointerException when optional xslt is omitted #12315
Guarded the xslt filename branch on a non-null value. The JS wrapper calls
bean.setXslt(__.nullOrValue(xslt)), so omitting the documented-optional xslt sets
the Java field to null; null instanceof ResourceKey is false, so the null fell
through to the branch written for the filename-string case and threw on
xslt.toString() before the import started.

Only importNodes reaches this. The source parameter directly above uses the
identical if/else shape, but checkRequired in export.ts rejects a missing source
before the bean is built, so its else branch can never see null.

Nothing downstream needed a matching change: ImportNodesParams.Builder.validate
checks xsltFileName only when it is non-null, and ExportServiceImpl.importNodes
reads only getXslt(), so leaving both unset builds and imports as it did before
the regression.

Broke in 15020fde44, which narrowed toVirtualFile(Object) to
toVirtualFile(ResourceKey) and split the two call sites into if/else. The Object
form returned null for a null argument, so the call site had needed no guard.

Added importNodesWithoutXslt.js and a captor-based test asserting the params that
reach ExportService. The shipped example importNodes.js has always passed
xslt: 'transform.xslt', which is why the omitted-xslt path had never been
executed by a test.

Left alone: ImportNodesParams never copies builder.xsltFileName into the built
instance and exposes no getter for it, so a string xslt is validated and then
silently dropped. That predates this change and affects ImportRunnableTask too.

modules/lib/lib-export/src/main/java/com/enonic/xp/lib/export/ImportHandler.java
@@ -61,7 +61,7 @@ public class ImportHandler
         {
             paramsBuilder.xslt( toVirtualFile( (ResourceKey) xslt ) );
         }
-        else
+        else if ( xslt != null )
         {
             paramsBuilder.xsltFileName( xslt.toString() );
         }

modules/lib/lib-export/src/main/resources/lib/xp/examples/export/importNodesWithoutXslt.js
@@ -1,0 +1,24 @@
+var exportLib = require('/lib/xp/export');
+var t = require('/lib/xp/testing');
+
+// BEGIN
+// Import from exports directory without XSLT transformation.
+let importNodes = exportLib.importNodes({
+    source: 'myexport',
+    targetNodePath: '/content'
+});
+// END
+// BEGIN
+// Information about imported nodes.
+var expected = {
+    'addedNodes': [
+        '/added'
+    ],
+    'updatedNodes': [],
+    'skippedNodes': [],
+    'importedBinaries': [],
+    'importErrors': []
+};
+// END
+
+t.assertJsonEquals(expected, importNodes);

modules/lib/lib-export/src/test/java/com/enonic/xp/lib/export/ImportHandlerTest.java
@@ -6,14 +6,18 @@ import java.nio.file.Path;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import org.mockito.ArgumentCaptor;
 
 import com.enonic.xp.export.ExportService;
+import com.enonic.xp.export.ImportNodesParams;
 import com.enonic.xp.export.NodeImportResult;
 import com.enonic.xp.home.HomeDirSupport;
 import com.enonic.xp.node.NodePath;
 import com.enonic.xp.testing.ScriptTestSupport;
 import com.enonic.xp.util.BinaryReference;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -53,6 +57,23 @@ class ImportHandlerTest
 
         when( exportService.importNodes( any() ) ).thenReturn( result );
         runScript( "/lib/xp/examples/export/importNodes.js" );
+    }
+
+    @Test
+    void testImportWithoutXslt()
+    {
+        final NodeImportResult result = NodeImportResult.create().added( new NodePath( "/added" ) ).build();
+
+        final ArgumentCaptor<ImportNodesParams> paramsCaptor = ArgumentCaptor.forClass( ImportNodesParams.class );
+        when( exportService.importNodes( paramsCaptor.capture() ) ).thenReturn( result );
+
+        runScript( "/lib/xp/examples/export/importNodesWithoutXslt.js" );
+
+        final ImportNodesParams capturedParams = paramsCaptor.getValue();
+        assertEquals( "myexport", capturedParams.getExportName() );
+        assertEquals( new NodePath( "/content" ), capturedParams.getTargetNodePath() );
+        assertFalse( capturedParams.isImportNodeIds() );
+        assertFalse( capturedParams.isImportPermissions() );
     }
 
     private static class NoStacktraceException